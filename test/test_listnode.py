fce3edbd5d44b23e7b400696335f325ca8920c6a
Lukas Rosenthaler <lukas.rosenthaler@unibas.ch>
2020-02-21 16:03:21 +0100

    Tests and documentation for listnode.py

--- test/test_listnode.py
+++ test/test_listnode.py
@@ -80,6 +80,19 @@ class TestListNode(unittest.TestCase):
         self.assertEqual(node.label['en'], 'Tree list root')
         self.assertTrue(node.isRootNode)
 
+    def test_ListNode_read2(self):
+        con = Connection('http://0.0.0.0:3333')
+        con.login('root@example.com', 'test')
+        node = ListNode(
+            con=con,
+            id='http://rdfh.ch/lists/0001/otherTreeList03'
+        ).read()
+        self.assertEqual(node.id, 'http://rdfh.ch/lists/0001/otherTreeList03')
+        self.assertEqual(node.label['en'], 'Other Tree list node 03')
+        self.assertFalse(node.isRootNode)
+        self.assertEqual(node.rootNodeIri, 'http://rdfh.ch/lists/0001/otherTreeList')
+        self.assertIsNone(node.children)
+
     def test_ListNode_create(self):
         node = self.createTestNode()
         self.assertEqual(node.project, self.project)
@@ -113,9 +126,55 @@ class TestListNode(unittest.TestCase):
         node.rmLabel('de')
         node.addComment('fr', 'un commentaire en français')
         node.rmComment('de')
+        node.name = 'GAGAGA'
         node.update()
         self.assertEqual(node.label['fr'], 'Une racine d\' une liste')
         self.assertEqual(node.comment['fr'], 'un commentaire en français')
+        self.assertEqual(node.name, 'GAGAGA')
+
+    def test_ListNode_getAllLists(self):
+        con = Connection('http://0.0.0.0:3333')
+        con.login('root@example.com', 'test')
+        lists = ListNode.getAllLists(con, 'http://rdfh.ch/projects/0001')
+        tmp = list(map(lambda a: a.id, lists))
+        self.assertIn('http://rdfh.ch/lists/0001/otherTreeList', tmp)
+        self.assertIn('http://rdfh.ch/lists/0001/treeList', tmp)
+
+    def test_ListNode_getAllNodes(self):
+        con = Connection('http://0.0.0.0:3333')
+        con.login('root@example.com', 'test')
+        root = ListNode(
+            con=con,
+            id='http://rdfh.ch/lists/0001/otherTreeList'
+        ).getAllNodes()
+        self.assertTrue(root.isRootNode)
+        self.assertEqual(root.project, 'http://rdfh.ch/projects/0001')
+        self.assertEqual(root.label['en'], 'Tree list root')
+        self.assertIsNotNone(root.children)
+
+        self.assertEqual(root.children[0].id, 'http://rdfh.ch/lists/0001/otherTreeList01')
+        self.assertEqual(root.children[0].name, 'Other Tree list node 01')
+        self.assertEqual(root.children[0].label['en'], 'Other Tree list node 01')
+
+        self.assertEqual(root.children[1].id, 'http://rdfh.ch/lists/0001/otherTreeList02')
+        self.assertEqual(root.children[1].name, 'Other Tree list node 02')
+        self.assertEqual(root.children[1].label['en'], 'Other Tree list node 02')
+
+        self.assertEqual(root.children[2].id, 'http://rdfh.ch/lists/0001/otherTreeList03')
+        self.assertEqual(root.children[2].name, 'Other Tree list node 03')
+        self.assertEqual(root.children[2].label['en'], 'Other Tree list node 03')
+
+        self.assertIsNotNone(root.children[2].children)
+        self.assertEqual(root.children[2].children[0].id, 'http://rdfh.ch/lists/0001/otherTreeList10')
+        self.assertEqual(root.children[2].children[0].name, 'Other Tree list node 10')
+        self.assertEqual(root.children[2].children[0].label['en'], 'Other Tree list node 10')
+
+        self.assertEqual(root.children[2].children[1].id, 'http://rdfh.ch/lists/0001/otherTreeList11')
+        self.assertEqual(root.children[2].children[1].name, 'Other Tree list node 11')
+        self.assertEqual(root.children[2].children[1].label['en'], 'Other Tree list node 11')
+
+        #self.assertTrue(False)
+
 
 if __name__ == '__main__':
     unittest.main()

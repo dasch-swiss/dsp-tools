4daf9a693dd6ff47e187872bb5b692e7e17ec9c3
Loïc Jaouen <loic.jaouen@unil.ch>
2020-06-16 18:48:08 +0200

    test embrio (stolen from `create_onto.py`)

--- test/test_create_ontology.py
+++ test/test_create_ontology.py
@@ -1,12 +1,32 @@
 import unittest
-
+from knora import Knora, Sipi
+from jsonschema import validate
+import json
 
 class TestCreateOntology(unittest.TestCase):
 
-    @unittest.skip("not implemented")
+    # @unittest.skip("not implemented")
     def test_create_ontology(self):
-        pass
+        server = "http://0.0.0.0:3333"
+        sipi = "http://0.0.0.0:1024"
+        email = "root@example.com"
+        password = "test"
+        projectcode = "00FE"
+        ontoname = "KPT"
 
+        con = Knora(server)
+        con.login(email, password)
+
+        with open("../knora/knora-schema.json") as s:
+            schema = json.load(s)
+
+        with open("test-onto.json") as f:
+            ontology = json.load(f)
+
+        validate(ontology, schema)
+        print("Ontology is syntactically correct and passed validation!")
+
+        pass
 
 if __name__ == "__main__":
     unittest.main()

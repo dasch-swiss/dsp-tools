c14128868a88942fbb1539a5c4fd9c298d0f2e6d
Tobias Schweizer <t.schweizer@unibas.ch>
2021-03-18 12:14:57 +0100

    test(value): add test for date value

---
+++ test/test_value.py
@@ -0,0 +1,24 @@
+import unittest
+from dsplib.models.value import DateValue
+
+class TestValue(unittest.TestCase):
+
+    def test_date_value(self):
+        date: DateValue = DateValue("JULIAN:BCE:0700:BCE:0600")
+        self.assertEqual(date._calendar, 'JULIAN')
+
+        self.assertEqual(date._e1, 'BCE')
+        self.assertEqual(date._y1, 700)
+        self.assertEqual(date._m1, None)
+        self.assertEqual(date._d1, None)
+
+        self.assertEqual(date._e2, 'BCE')
+        self.assertEqual(date._y2, 600)
+        self.assertEqual(date._m2, None)
+        self.assertEqual(date._d2, None)
+
+
+
+
+if __name__ == '__main__':
+    unittest.main()

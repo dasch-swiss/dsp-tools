adc71fe1c0983ca00f6de1715045cf422904c02a
irinaschubert <irina.schubert@dasch.swiss>
2021-10-28 12:11:17 +0200

    remove remnants of id_to_iri feature

--- test/e2e/test_tools.py
+++ test/e2e/test_tools.py
@@ -6,7 +6,6 @@ from knora.dsplib.utils import excel_to_json_lists
 from knora.dsplib.utils.excel_to_json_lists import list_excel2json
 from knora.dsplib.utils.excel_to_json_properties import properties_excel2json
 from knora.dsplib.utils.excel_to_json_resources import resources_excel2json
-from knora.dsplib.utils.id_to_iri import id_to_iri
 from knora.dsplib.utils.onto_create_ontology import create_ontology
 from knora.dsplib.utils.onto_get import get_ontology
 from knora.dsplib.utils.onto_validate import validate_ontology
@@ -113,14 +112,7 @@ class TestTools(unittest.TestCase):
                    imgdir='testdata/bitstreams',
                    sipi='http://0.0.0.0:1024',
                    verbose=True,
-                   validate_only=False,
-                   incremental=False)
-
-    def test_id_to_iri(self):
-        id_to_iri(xml_file='testdata/test-id2iri-data.xml',
-                  json_file='testdata/test-id2iri-mapping.json',
-                  out_file='_test-id2iri-replaced.xml',
-                  verbose=True)
+                   validate_only=False)
 
 
 if __name__ == '__main__':

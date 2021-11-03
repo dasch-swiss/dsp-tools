89ce041f524193f6e9a904e6d9e39c90acba2a33
irinaschubert <irina.schubert@dasch.swiss>
2021-11-03 18:09:52 +0100

    improve test_resource

--- test/e2e/test_resource.py
+++ test/e2e/test_resource.py
@@ -10,66 +10,67 @@ from knora.dsplib.models.value import KnoraStandoffXml, make_value
 
 class TestResource(unittest.TestCase):
 
-    def test_Resource_create(self):
-        #
-        # Connect to Knora
-        #
-        con = Connection('http://0.0.0.0:3333')
-        con.login('root@example.com', 'test')
+    def setUp(self) -> None:
+        """
+        is executed before all tests; sets up a connection and logs in as user root
+        """
+        self.con = Connection('http://0.0.0.0:3333')
+        self.con.login('root@example.com', 'test')
 
-        #
-        # Make class factory for project 'anything. The factory creates classes that implement the CRUD methods
-        # for the given resource classes, that is to create, read, update and delete instances (=resources) of the given classesd
-        #
-        factory = ResourceInstanceFactory(con, 'anything')
-        resclassnames = factory.get_resclass_names()
+    def test_Resource_create(self) -> None:
+        # make class factory for project anything. The factory creates classes that implement the CRUD methods
+        # for the given resource classes
 
-        #
-        # Get an python class of a BlueThing resource class
-        #
-        BlueThing = factory.get_resclass('anything:BlueThing')
+        factory = ResourceInstanceFactory(self.con, 'anything')
 
-        an_old_thing = BlueThing(con=con, iri="http://rdfh.ch/0001/H6gBWUuJSuuO-CilHV8kQw").read()
+        # get a blue_thing resource class
+        blue_thing = factory.get_resclass('anything:BlueThing')
 
-        a_blue_thing = BlueThing(con=con,
-                                 label='BlueThing',
-                                 values={
-                                     'anything:hasBoolean': True,
-                                     'anything:hasColor': ['#ff0033', '#0077FF'],
-                                     'anything:hasDate': make_value(value='1833', comment='not verified!'),
-                                     'anything:hasDecimal': 3.14159,
-                                     'anything:hasInteger': "42",
-                                     'anything:hasTimeStamp': "2004-04-12T13:20:00Z",
-                                     'anything:hasInterval': [make_value(value="0.0:3.0", comment="first interval"),
-                                                              make_value(value="3.5:3.7", comment="second interval")],
-                                     'anything:hasRichtext': KnoraStandoffXml("This is <em>bold</em> text."),
-                                     'anything:hasText': "Dies ist ein einfacher Text",
-                                     'anything:hasUri': 'http://gaga.com:65500/gugus'
-                                 }).create()
+        a_blue_thing = blue_thing(con=self.con,
+                                  label='BlueThing',
+                                  values={
+                                      'anything:hasBoolean': True,
+                                      'anything:hasColor': ['#ff0033', '#0077FF'],
+                                      'anything:hasDate': make_value(value='1833', comment='not verified!'),
+                                      'anything:hasDecimal': 3.14159,
+                                      'anything:hasInteger': "42",
+                                      'anything:hasTimeStamp': "2004-04-12T13:20:00Z",
+                                      'anything:hasInterval': [make_value(value="0.0:3.0", comment="first interval"),
+                                                               make_value(value="3.5:3.7", comment="second interval")],
+                                      'anything:hasRichtext': KnoraStandoffXml("This is <em>bold</em> text."),
+                                      'anything:hasText': "Dies ist ein einfacher Text",
+                                      'anything:hasUri': 'http://test.com:65500/res'
+                                  }).create()
 
         new_blue_thing = a_blue_thing.read()
-        self.assertEqual(a_blue_thing.label, "BlueThing")
-        self.assertEqual(a_blue_thing.value("anything:hasColor"), ['#ff0033', '#0077FF'])
-        self.assertEqual(a_blue_thing.value("anything:hasInteger"), 42)
-        self.assertEqual(a_blue_thing.value("anything:hasBoolean"), True)
-        self.assertEqual(a_blue_thing.value("anything:hasDecimal"), 3.14159)
-        self.assertEqual(a_blue_thing.value("anything:hasText"), "Dies ist ein einfacher Text")
+        self.assertEqual(new_blue_thing.label, "BlueThing")
+        self.assertEqual(new_blue_thing.value("anything:hasColor"), ['#ff0033', '#0077FF'])
+        self.assertEqual(new_blue_thing.value("anything:hasInteger"), 42)
+        self.assertEqual(new_blue_thing.value("anything:hasBoolean"), True)
+        self.assertEqual(new_blue_thing.value("anything:hasDecimal"), 3.14159)
+        self.assertEqual(new_blue_thing.value("anything:hasText"), "Dies ist ein einfacher Text")
 
-        ThingPicture = factory.get_resclass('anything:ThingPicture')
-        sipi = Sipi('http://0.0.0.0:1024', con.get_token())
+        thing_picture = factory.get_resclass('anything:ThingPicture')
+        sipi = Sipi('http://0.0.0.0:1024', self.con.get_token())
         img = sipi.upload_bitstream('testdata/bitstreams/TEMP11.TIF')
-        fileref = img['uploadedFiles'][0]['internalFilename']
-        resperm = Permissions({PermissionValue.M: ["knora-admin:UnknownUser", "knora-admin:KnownUser"],
-                               PermissionValue.CR: ["knora-admin:Creator", "knora-admin:ProjectAdmin"]})
-        a_thing_picture = ThingPicture(
-            con=con,
+        file_ref = img['uploadedFiles'][0]['internalFilename']
+        res_perm = Permissions({PermissionValue.M: ["knora-admin:UnknownUser", "knora-admin:KnownUser"],
+                                PermissionValue.CR: ["knora-admin:Creator", "knora-admin:ProjectAdmin"]})
+        thing_picture(
+            con=self.con,
             label='ThingPicture',
-            bitstream=fileref,
-            permissions=resperm,
+            bitstream=file_ref,
+            permissions=res_perm,
             values={
-                'anything:hasPictureTitle': make_value(value="A Thing Picture named Lena", permissions=resperm)
+                'anything:hasPictureTitle': make_value(value="A Thing Picture named Lena", permissions=res_perm)
             }
         ).create()
+
+    def tearDown(self) -> None:
+        """
+        is executed after all tests are run through; performs a log out
+        """
+        self.con.logout()
 
 
 if __name__ == '__main__':

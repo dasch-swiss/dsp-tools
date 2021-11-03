bb82135b861d7e6067ae89cde26883509d210046
irinaschubert <irina.schubert@dasch.swiss>
2021-11-03 18:25:15 +0100

    improve test_resourceclass

--- test/e2e/test_resourceclass.py
+++ test/e2e/test_resourceclass.py
@@ -8,102 +8,82 @@ from knora.dsplib.models.resourceclass import ResourceClass
 
 
 class TestResourceClass(unittest.TestCase):
-    project = "http://rdfh.ch/projects/0001"
+    test_project = "http://rdfh.ch/projects/0001"
 
-    label = LangString({Languages.DE: 'MyResClassLabel'})
-    comment = LangString({Languages.DE: 'This is a resource class for testing'})
-    name = 'MyResClassName'
+    res_name = 'res_class_name'
+    res_label = LangString({Languages.EN: 'Resource Class Label'})
+    res_comment = LangString({Languages.EN: 'This is a resource class for testing'})
 
-    def test_ResourceClass_create(self):
-        #
-        # Connect to Knora
-        #
-        con = Connection('http://0.0.0.0:3333')
-        con.login('root@example.com', 'test')
+    def setUp(self) -> None:
+        """
+        is executed before all tests; sets up a connection and logs in as user root
+        """
+        self.con = Connection('http://0.0.0.0:3333')
+        self.con.login('root@example.com', 'test')
 
-        #
-        # Create a test ontology
-        #
+    def test_ResourceClass_create(self) -> None:
         onto = Ontology(
-            con=con,
-            project=self.project,
-            name='resclass-test-onto-1',
-            label='resclass test ontology 1',
+            con=self.con,
+            project=self.test_project,
+            name='test_onto',
+            label='Test Ontology',
         ).create()
-        last_modification_date = onto.lastModificationDate
-        self.assertIsNotNone(onto.id)
-        #
-        # Create new resource class
-        #
-        last_modification_date, resclass = ResourceClass(
-            con=con,
-            context=onto.context,
-            name=self.name,
-            ontology_id=onto.id,
-            label=self.label,
-            comment=self.comment
-        ).create(last_modification_date)
-        onto.lastModificationDate = last_modification_date
-        self.assertIsNotNone(resclass.id)
 
-        self.assertEqual(resclass.name, self.name)
-        self.assertEqual(resclass.label['de'], self.label['de'])
-        self.assertEqual(resclass.comment['de'], self.comment['de'])
+        last_modification_date_onto = onto.lastModificationDate
 
-        #
-        # Delete the new resource class
-        #
-        last_modification_date = resclass.delete(last_modification_date)
-        onto.lastModificationDate = last_modification_date
-
-    def test_ResourceClass_update(self):
-        #
-        # Connect to Knora
-        #
-        con = Connection('http://0.0.0.0:3333')
-        con.login('root@example.com', 'test')
-
-        #
-        # Create a test ontology
-        #
-        onto = Ontology(
-            con=con,
-            project=self.project,
-            name='resclass-test-onto-2',
-            label='resclass test ontology 2',
-        ).create()
-        last_modification_date = onto.lastModificationDate
-        self.assertIsNotNone(onto.id)
-        #
         # create test resource class
-        #
-        last_modification_date, resclass = ResourceClass(
-            con=con,
+        last_modification_date_res, res_class = ResourceClass(
+            con=self.con,
             context=onto.context,
-            name=self.name,
+            name=self.res_name,
             ontology_id=onto.id,
-            label=self.label,
-            comment=self.comment
+            label=self.res_label,
+            comment=self.res_comment
+        ).create(last_modification_date_onto)
+
+        self.assertIsNotNone(res_class.id)
+        self.assertEqual(res_class.name, self.res_name)
+        self.assertEqual(res_class.label['en'], self.res_label['en'])
+        self.assertEqual(res_class.comment['en'], self.res_comment['en'])
+
+    def test_ResourceClass_update(self) -> None:
+        onto = Ontology(
+            con=self.con,
+            project=self.test_project,
+            name='test_onto_2',
+            label='Test Ontology 2',
+        ).create()
+
+        last_modification_date = onto.lastModificationDate
+
+        # create test resource class
+        last_modification_date, res_class = ResourceClass(
+            con=self.con,
+            context=onto.context,
+            name=self.res_name,
+            ontology_id=onto.id,
+            label=self.res_label,
+            comment=self.res_comment
         ).create(last_modification_date)
-        onto.lastModificationDate = last_modification_date
-        self.assertIsNotNone(resclass.id)
 
-        #
-        # Modify the resource class
-        #
-        resclass.addLabel('en', "This is english gaga")
-        resclass.rmLabel('de')
-        resclass.addComment('it', "Commentario italiano")
-        last_modification_date, resclass = resclass.update(last_modification_date)
         onto.lastModificationDate = last_modification_date
-        self.assertEqual(resclass.label['en'], "This is english gaga")
-        self.assertEqual(resclass.comment['it'], "Commentario italiano")
 
-        #
-        # Now delete the resource class to clean up
-        #
-        last_modification_date = resclass.delete(last_modification_date)
-        onto.lastModificationDate = last_modification_date
+        self.assertIsNotNone(res_class.id)
+
+        # modify the resource class
+        res_class.addLabel('de', "Dies ist ein Kommentar")
+        res_class.rmLabel('en')
+        res_class.addComment('it', "Commentario italiano")
+
+        last_modification_date, res_class = res_class.update(last_modification_date)
+        self.assertEqual(res_class.label['de'], "Dies ist ein Kommentar")
+        self.assertEqual(res_class.comment['it'], "Commentario italiano")
+
+    def tearDown(self) -> None:
+        """
+        is executed after all tests are run through; performs a log out
+        """
+        self.con.logout()
 
 
 if __name__ == '__main__':

7f13719f64035996a1474a2355fb7e5e27bd865d
Balduin Landolt <33053745+BalduinLandolt@users.noreply.github.com>
2022-01-04 12:32:02 +0100

    refactor: reduce code smell

--- knora/dsplib/utils/excel_to_json_resources.py
+++ knora/dsplib/utils/excel_to_json_resources.py
@@ -4,6 +4,7 @@ from typing import Any
 
 import jsonschema
 from openpyxl import load_workbook
+from openpyxl.workbook.workbook import Workbook
 
 
 def validate_resources_with_schema(json_file: str) -> bool:
@@ -41,56 +42,6 @@ def resources_excel2json(excelfile: str, outfile: str) -> None:
     Returns:
         None
     """
-    def __extract_row(row: tuple[str, str, str, str, str, str, str, str, str, str]) -> dict[str, Any]:
-        # get name
-        name = row[0]
-        # get labels
-        labels = {}
-        if row[1]:
-            labels['en'] = row[1]
-        if row[2]:
-            labels['de'] = row[2]
-        if row[3]:
-            labels['fr'] = row[3]
-        if row[4]:
-            labels['it'] = row[4]
-        # get comments
-        comments = {}
-        if row[5]:
-            comments['en'] = row[5]
-        if row[6]:
-            comments['de'] = row[6]
-        if row[7]:
-            comments['fr'] = row[7]
-        if row[8]:
-            comments['it'] = row[8]
-        # get super
-        sup = row[9]
-
-        # load details for this resource
-        sh = wb[name]
-        property_list = [c for c in sh.iter_rows(min_row=2, values_only=True)]
-
-        cards = []
-        # for each of the detail sheets
-        for i, prop in enumerate(property_list):
-            # get name and cardinality.
-            # GUI-order is equal to order in the sheet.
-            property_ = {
-                "propname": ":" + prop[0],
-                "cardinality": str(prop[1]),
-                "gui_order": i + 1
-            }
-            cards.append(property_)
-
-        # return resource dict
-        return {
-            "name": name,
-            "labels": labels,
-            "comments": comments,
-            "super": sup,
-            "cardinalities": cards
-        }
 
     # load file
     wb = load_workbook(excelfile, read_only=True)
@@ -100,7 +51,7 @@ def resources_excel2json(excelfile: str, outfile: str) -> None:
     resource_list = [c for c in sheet.iter_rows(min_row=2, values_only=True)]
 
     prefix = '"resources":'
-    resources = [__extract_row(res) for res in resource_list]
+    resources = [__extract_row(res, wb) for res in resource_list]
 
     if validate_resources_with_schema(json.loads(json.dumps(resources, indent=4))):
         # write final list to JSON file if list passed validation
@@ -110,3 +61,56 @@ def resources_excel2json(excelfile: str, outfile: str) -> None:
             print('Resource file was created successfully and written to file:', outfile)
     else:
         print('Resource data is not valid according to schema.')
+
+
+def __extract_row(row: tuple[str, str, str, str, str, str, str, str, str, str], wb: Workbook) -> dict[str, Any]:
+    """build a property dict from a row of the excel file"""
+    # get name
+    name = row[0]
+    # get labels
+    labels = {}
+    if row[1]:
+        labels['en'] = row[1]
+    if row[2]:
+        labels['de'] = row[2]
+    if row[3]:
+        labels['fr'] = row[3]
+    if row[4]:
+        labels['it'] = row[4]
+    # get comments
+    comments = {}
+    if row[5]:
+        comments['en'] = row[5]
+    if row[6]:
+        comments['de'] = row[6]
+    if row[7]:
+        comments['fr'] = row[7]
+    if row[8]:
+        comments['it'] = row[8]
+    # get super
+    sup = row[9]
+
+    # load details for this resource
+    sh = wb[name]
+    property_list = [c for c in sh.iter_rows(min_row=2, values_only=True)]
+
+    cards = []
+    # for each of the detail sheets
+    for i, prop in enumerate(property_list):
+        # get name and cardinality.
+        # GUI-order is equal to order in the sheet.
+        property_ = {
+            "propname": ":" + prop[0],
+            "cardinality": str(prop[1]),
+            "gui_order": i + 1
+        }
+        cards.append(property_)
+
+    # return resource dict
+    return {
+        "name": name,
+        "labels": labels,
+        "comments": comments,
+        "super": sup,
+        "cardinalities": cards
+    }

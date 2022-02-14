b4afc09cf7ec3e85344c454f5882d8bd989a8c8a
Johannes Nussbaum <johannes.nussbaum@zoho.com>
2022-02-14 15:52:04 +0100

    elminate little bug

--- knora/dsplib/utils/expand_all_lists.py
+++ knora/dsplib/utils/expand_all_lists.py
@@ -3,7 +3,9 @@ from typing import Any, Union
 from knora.dsplib.utils.excel_to_json_lists import make_json_list_from_excel, prepare_list_creation
 
 
-def expand_lists_from_excel(data_model: dict[str, dict[str, dict[str, dict[str, Any]]]]) -> list[dict[str, Any]]:
+def expand_lists_from_excel(
+    data_model: dict[str, dict[str, list[dict[str, Union[str, dict[str, Any]]]]]]
+) -> list[dict[str, Any]]:
     """
     Get all list definitions from a data model and expand them to JSON if they are only referenced via an Excel file
 
@@ -20,13 +22,13 @@ def expand_lists_from_excel(data_model: dict[str, dict[str, dict[str, dict[str, 
     lists = data_model['project']['lists']
     new_lists = []
 
-    for rootnode in lists.values():
-        nodes = rootnode.get("nodes")
+    for rootnode in lists:
+        nodes = rootnode['nodes']
         # check if the folder parameter is used
         if nodes and isinstance(nodes, dict) and nodes.get("folder"):
             # get the Excel files from the folder and create the rootnode of the list
             prepared_rootnode, excel_files = prepare_list_creation(
-                nodes['folder'], rootnode['name'], rootnode['comments']
+                nodes['folder'], str(rootnode['name']), rootnode['comments']
             )
             # create the list from the Excel files
             finished_list = make_json_list_from_excel(prepared_rootnode, excel_files)

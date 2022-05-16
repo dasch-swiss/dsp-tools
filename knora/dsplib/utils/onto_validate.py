8be6a0f2de991db7154e93b01634ba0cdab1d65d
Johannes Nussbaum <johannes.nussbaum@zoho.com>
2022-05-16 17:43:42 +0200

    refactor code to improve readability

--- knora/dsplib/utils/onto_validate.py
+++ knora/dsplib/utils/onto_validate.py
@@ -60,18 +60,14 @@ def check_cardinalities_of_circular_references(data_model: dict[Any, Any]) -> bo
     properties must have the cardinality 0-1 or 0-n, because during the xmlupload process, these values
     are temporarily removed.
     """
-    # 1. prepare
-    properties = get_properties(data_model=data_model)
-    resources = get_resources(data_model=data_model)
-    shortname = get_shortname(data_model=data_model)
-    links = get_HasLinkTo_dict(properties=properties, shortname=shortname)
+    properties: list[dict[str, Any]] = data_model['project']['ontologies'][0]['properties']
+    resources: list[dict[str, Any]] = data_model['project']['ontologies'][0]['resources']
+    shortname: str = data_model['project']['shortname']
 
-    # get resources and their links
+    links = get_HasLinkTo_dict(properties=properties, shortname=shortname)
     resources_and_links = get_resources_and_links(resources, links)
-    # get circles
     circles = get_circles(resources_and_links, shortname)
 
-    # check circles
     ok_cardinalities = ['0-1', '0-n']
     errors = get_circle_errors(circles, resources_and_links, ok_cardinalities)
     if len(errors) == 0:
@@ -80,15 +76,20 @@ def check_cardinalities_of_circular_references(data_model: dict[Any, Any]) -> bo
         print('ERROR: Your ontology contains properties derived from "hasLinkTo" that allow circular references '
               'between resources. This is not a problem in itself, but if you try to upload data that actually '
               'contains circular references, these "hasLinkTo" cardinalities will be temporarily removed from the '
-              'affected resources. Therefore, it is necessary that ALL involved "hasLinkTo" cardinalities have a '
-              'cardinality of 0-1 or 0-n. \n''Some of them might already be okay, but some need to be adapted. '
+              'affected resources. Therefore, it is necessary that all involved "hasLinkTo" cardinalities have a '
+              'cardinality of 0-1 or 0-n. \n'
+              'Some of them might already be okay, but some need to be adapted. '
               'Please make sure that the following cardinalities have a cardinality of 0-1 or 0-n:')
         for error in errors:
-            print(error)
+            print(f'{error}\n')
         return False
 
 
-def get_circle_errors(circles, resources_and_links, ok_cardinalities) -> List:
+def get_circle_errors(
+    circles: list[Any],
+    resources_and_links: dict[Any, list[list[str]]],
+    ok_cardinalities: list[str]
+) -> list[str]:
     """
     checks circles and returns errors
 
@@ -101,23 +102,20 @@ def get_circle_errors(circles, resources_and_links, ok_cardinalities) -> List:
         errors: List of errors
 
     """
-    errors: List = list()
+    errors: set[str] = set()
     for circle in circles:
         for element in circle:
-            for key_name in resources_and_links:
-                resource = resources_and_links[key_name]
+            for key_name, resource in resources_and_links.items():
                 for hasLinkTo_props in resource:
                     # hasLinkTo: Resource could always cause a circle
                     if hasLinkTo_props[1] == element or hasLinkTo_props[1] == 'Resource':
                         cardinality = hasLinkTo_props[2]
                         if cardinality not in ok_cardinalities:
-                            error_message = "Resource " + key_name + " with hasLinkTo-Property " + str(hasLinkTo_props)
-                            if not errors.__contains__(error_message):
-                                errors.append(error_message)
-    return errors
+                            errors.add(f"Resource {key_name} with hasLinkTo-Property {hasLinkTo_props}")
+    return list(errors)
 
 
-def get_resources_and_links(resources, links) -> dict:
+def get_resources_and_links(resources: list[dict[str, Any]], links: dict[str, str]) -> dict[Any, list[list[str]]]:
     """
     get the resources with their respective hasLinkToProperties
 
@@ -133,7 +131,7 @@ def get_resources_and_links(resources, links) -> dict:
     resources_and_links = dict()
     for resource in resources:
         resource_propnames = get_properties_resource(resource)
-        list_: List = list()
+        list_ = list()
         # append a list of lists for every resource name
         for name in resource_propnames:
             if name in links:
@@ -162,7 +160,7 @@ def get_edges_of_resource(resource):
     return list_edges
 
 
-def get_circles(resources_and_links, shortname) -> List:
+def get_circles(resources_and_links: dict[Any, list[list[str]]], shortname: str) -> list[Any]:
     """
     detects circles and returns them
 
@@ -174,59 +172,27 @@ def get_circles(resources_and_links, shortname) -> List:
         circles: list of circles
     """
     graph = nx.DiGraph()
-    graph = populate_graph(graph=graph, resources_and_links=resources_and_links, shortname=shortname)
-    circles = nx.simple_cycles(graph)
-    return circles
-
-
-def populate_graph(graph, resources_and_links, shortname):
-    """Populates graph with resources and the resources they are pointing to and returns it
-
-    Args:
-        graph: a void graph
-        resources_and_links: resources with their respective hasLinkToProperties
-        shortname: shortname of project
-
-    Returns:
-        graph: a populated graph
-
-    """
     for name_key in resources_and_links:
         entry = resources_and_links[name_key]
         list_of_edges = get_edges_of_resource(entry)
         name_key = shortname + ":" + name_key
         for edge_part in list_of_edges:
             graph.add_edge(u_of_edge=name_key, v_of_edge=edge_part)
-    return graph
+    circles = list(nx.simple_cycles(graph))
+    return circles
 
 
-def get_properties(data_model) -> List:
-    """returns all properties of an ontology"""
-    ontology = data_model['project']['ontologies']
-    ontology_dict = ontology[0]
-    return ontology_dict["properties"]
-
-
-def get_resources(data_model) -> List:
-    """returns all resources of an ontology"""
-    ontology = data_model['project']['ontologies']
-    ontology_dict = ontology[0]
-    return ontology_dict["resources"]
-
-
-def get_properties_resource(resource) -> dict:
+def get_properties_resource(resource: dict[str, Any]) -> dict[str, str]:
     """returns all the properties of a single resource with their respective cardinalities"""
     entries = dict()
-    cardinalities = resource["cardinalities"]
-    for entry in cardinalities:
-        name = entry["propname"]
-        name = name[1:]
-        cardinality = entry["cardinality"]
+    for entry in resource["cardinalities"]:
+        name: str = entry["propname"][1:]
+        cardinality: str = entry["cardinality"]
         entries[name] = cardinality
     return entries
 
 
-def get_HasLinkTo_dict(properties, shortname) -> dict:
+def get_HasLinkTo_dict(properties: list[dict[str, Any]], shortname: str) -> dict[str, str]:
     """returns a dict, with every hasLinkTo-Property-name and the respective resource name they are pointing to
         Args:
             properties: all properties of the ontology
@@ -236,18 +202,11 @@ def get_HasLinkTo_dict(properties, shortname) -> dict:
     """
     links: dict[str, str] = dict()
     for prop in properties:
-        super_prop = prop["super"]
-        if super_prop.__contains__("hasLinkTo"):
-            object_prop = prop["object"]
+        if "hasLinkTo" in prop["super"]:
+            object_prop: str = prop["object"]
             # add shortname, if shortname is not already part of object_prop
-            if object_prop.find(":") == 0:
+            if object_prop.startswith(":"):
                 object_prop = shortname + object_prop
-            name = prop["name"]
+            name: str = prop["name"]
             links[name] = object_prop
     return links
-
-
-def get_shortname(data_model):
-    """returns the shortname of the data model"""
-    project = data_model["project"]
-    return project["shortname"]

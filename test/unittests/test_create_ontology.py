e02dc46df6055108234479d5ffd8b3e85fa69d80
Johannes Nussbaum <johannes.nussbaum@zoho.com>
2022-05-25 12:04:40 +0200

    extend test-onto for circularity-test

--- test/unittests/test_create_ontology.py
+++ test/unittests/test_create_ontology.py
@@ -2,8 +2,10 @@
 import unittest
 import json
 from typing import Any
+import jsonpath_ng.ext
 
-from knora.dsplib.utils.onto_create_ontology import *
+from knora.dsplib.utils.onto_create_ontology import sort_resources, sort_prop_classes
+from knora.dsplib.utils.onto_validate import collect_link_properties, identify_problematic_cardinalities
 
 
 class TestOntoCreation(unittest.TestCase):
@@ -41,6 +43,48 @@ class TestOntoCreation(unittest.TestCase):
         sorted_props = sorted(sorted_props, key=lambda a: a['name'])
 
         self.assertListEqual(unsorted_props, sorted_props)
+
+
+    def test_circular_references_in_onto(self) -> None:
+        self.ontology['properties'].extend([
+            {
+                "name": "hasLinkToResource",
+                "super": ["hasLinkTo"],
+                "object": "Resource",
+                "labels": {"en": "has region"},
+                "gui_element": "Searchbox"
+            },
+            {
+                "name": "hasLinkToAudioThing",
+                "super": ["hasLinkTo"],
+                "object": ":AudioThing",
+                "labels": {"en": "hasLinkToAudioThing"},
+                "gui_element": "Searchbox"
+            }
+        ])
+
+        TestThing_hasTestThing2 = jsonpath_ng.ext.parse(
+            '$.resources[?name="TestThing"].cardinalities[?propname=":hasTestThing2"]').find(self.ontology)[0].value
+        TestThing_hasTestThing2['cardinality'] = '1-n'
+        TestThing2_hasTestThing = jsonpath_ng.ext.parse(
+            '$.resources[?name="TestThing2"].cardinalities[?propname=":hasTestThing"]').find(self.ontology)[0].value
+        TestThing2_hasTestThing['cardinality'] = '1-n'
+        TestThing2_cards = jsonpath_ng.ext.parse(
+            '$.resources[?name="TestThing2"].cardinalities').find(self.ontology)[0].value
+        TestThing2_cards.append({
+            "propname": ":hasLinkToAudioThing",
+            "cardinality": "1"
+        })
+        AudioThing_cards = jsonpath_ng.ext.parse('$.resources[?name="AudioThing"].cardinalities').find(self.ontology)[0].value
+        AudioThing_cards.append({
+            "propname": ":hasTestThing",
+            "cardinality": "1"
+        })
+
+        link_properties = collect_link_properties(self.project)
+        errors = identify_problematic_cardinalities(self.project, link_properties)
+        expected_errors = {''}
+        self.assertSetEqual(errors, expected_errors)
 
 
 if __name__ == '__main__':

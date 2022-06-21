6bb6917913921e19820154c437e738910a51878a
Johannes Nussbaum <johannes.nussbaum@zoho.com>
2022-06-21 11:20:23 +0200

    refactor tests

--- test/unittests/test_excel_to_properties.py
+++ test/unittests/test_excel_to_properties.py
@@ -6,8 +6,6 @@ import re
 import json
 import jsonpath_ng
 
-from openpyxl import Workbook
-
 from knora.dsplib.utils import excel_to_json_properties as e2j
 
 
@@ -20,31 +18,28 @@ class TestExcelToProperties(unittest.TestCase):
     def test_excel2json(self) -> None:
         excelfile = "testdata/Properties.xlsx"
         outfile = "testdata/tmp/_out_properties.json"
+        languages = ["en", "de", "fr", "it", "rm"]
         e2j.properties_excel2json(excelfile, outfile)
 
         # read excel: skip all rows that lack one of the required values
-        excel_df = pd.read_excel(excelfile)
-        excel_df = excel_df[pd.notna(excel_df['name'])]
-        excel_df = excel_df[[bool(re.search(r'[\wäöü]', x)) for x in excel_df['name']]]
-        excel_df = excel_df[pd.notna(excel_df['super'])]
-        excel_df = excel_df[[bool(re.search(r'[\wäöü]', x)) for x in excel_df['super']]]
-        excel_df = excel_df[pd.notna(excel_df['object'])]
-        excel_df = excel_df[[bool(re.search(r'[\wäöü]', x)) for x in excel_df['object']]]
-        excel_df = excel_df[pd.notna(excel_df['gui_element'])]
-        excel_df = excel_df[[bool(re.search(r'[\wäöü]', x)) for x in excel_df['gui_element']]]
+        excel_df = pd.read_excel(excelfile, dtype=str)
+        for required_column in ["name", "super", "object", "gui_element"]:
+            excel_df = excel_df[pd.notna(excel_df[required_column])]
+            excel_df = excel_df[[bool(re.search(r'[\wäöü]', x)) for x in excel_df[required_column]]]
 
         # extract infos from excel file
         excel_names = [s.strip() for s in excel_df['name']]
         excel_supers = [[x.strip() for x in s.split(',')] for s in excel_df['super']]
         excel_objects = [s.strip() for s in excel_df['object']]
-        excel_labels_en = [s.strip() if isinstance(s, str) and re.search(r'[\wäöü]', s) else '' for s in list(excel_df['en'])]
-        excel_labels_de = [s.strip() if isinstance(s, str) and re.search(r'[\wäöü]', s) else '' for s in list(excel_df['de'])]
-        excel_labels_fr = [s.strip() if isinstance(s, str) and re.search(r'[\wäöü]', s) else '' for s in list(excel_df['fr'])]
-        excel_labels_it = [s.strip() if isinstance(s, str) and re.search(r'[\wäöü]', s) else '' for s in list(excel_df['it'])]
-        excel_comments_en = [s.strip() if isinstance(s, str) and re.search(r'[\wäöü]', s) else '' for s in list(excel_df['comment_en'])]
-        excel_comments_de = [s.strip() if isinstance(s, str) and re.search(r'[\wäöü]', s) else '' for s in list(excel_df['comment_de'])]
-        excel_comments_fr = [s.strip() if isinstance(s, str) and re.search(r'[\wäöü]', s) else '' for s in list(excel_df['comment_fr'])]
-        excel_comments_it = [s.strip() if isinstance(s, str) and re.search(r'[\wäöü]', s) else '' for s in list(excel_df['comment_it'])]
+
+        excel_labels: dict[str, list[str]] = dict()
+        for _id in languages:
+            excel_labels[_id] = [s.strip() if isinstance(s, str) and re.search(r'[\wäöü]', s) else '' for s in list(excel_df[_id])]
+
+        excel_comments: dict[str, list[str]] = dict()
+        for _id in [f"comment_{lang}" for lang in languages]:
+            excel_comments[_id] = [s.strip() if isinstance(s, str) and re.search(r'[\wäöü]', s) else '' for s in list(excel_df[_id])]
+
         excel_gui_elements = [s.strip() for s in list(excel_df['gui_element'])]
 
         # read json file
@@ -57,70 +52,25 @@ class TestExcelToProperties(unittest.TestCase):
         json_names = [match.value for match in jsonpath_ng.parse('$.properties[*].name').find(json_file)]
         json_supers = [match.value for match in jsonpath_ng.parse('$.properties[*].super').find(json_file)]
         json_objects = [match.value for match in jsonpath_ng.parse('$.properties[*].object').find(json_file)]
-        json_labels = [match.value for match in jsonpath_ng.parse('$.properties[*].labels').find(json_file)]
-        json_labels_en = [label.get('en', '').strip() for label in json_labels]
-        json_labels_de = [label.get('de', '').strip() for label in json_labels]
-        json_labels_fr = [label.get('fr', '').strip() for label in json_labels]
-        json_labels_it = [label.get('it', '').strip() for label in json_labels]
-        json_comments = [match.value for match in jsonpath_ng.parse('$.properties[*].comments').find(json_file)]
-        json_comments_en = [comment.get('en', '').strip() for comment in json_comments]
-        json_comments_de = [comment.get('de', '').strip() for comment in json_comments]
-        json_comments_fr = [comment.get('fr', '').strip() for comment in json_comments]
-        json_comments_it = [comment.get('it', '').strip() for comment in json_comments]
+
+        json_labels_all = [match.value for match in jsonpath_ng.parse('$.properties[*].labels').find(json_file)]
+        json_labels: dict[str, list[str]] = dict()
+        for _id in languages:
+            json_labels[_id] = [label.get(_id, '').strip() for label in json_labels_all]
+
+        json_comments: dict[str, list[str]] = dict()
+        for _id in languages:
+            json_comments[f"comment_{_id}"] = [resource.get("comments", {}).get(_id, '').strip() for resource in json_file["properties"]]
+
         json_gui_elements = [match.value for match in jsonpath_ng.parse('$.properties[*].gui_element').find(json_file)]
 
         # make checks
         self.assertListEqual(excel_names, json_names)
         self.assertListEqual(excel_supers, json_supers)
         self.assertListEqual(excel_objects, json_objects)
-        self.assertListEqual(excel_labels_en, json_labels_en)
-        self.assertListEqual(excel_labels_de, json_labels_de)
-        self.assertListEqual(excel_labels_fr, json_labels_fr)
-        self.assertListEqual(excel_labels_it, json_labels_it)
-        self.assertListEqual(excel_comments_en, json_comments_en)
-        self.assertListEqual(excel_comments_de, json_comments_de)
-        self.assertListEqual(excel_comments_fr, json_comments_fr)
-        self.assertListEqual(excel_comments_it, json_comments_it)
+        self.assertDictEqual(excel_labels, json_labels)
+        self.assertDictEqual(excel_comments, json_comments)
         self.assertListEqual(excel_gui_elements, json_gui_elements)
-
-    def test_row_to_prop(self) -> None:
-        wb = Workbook()
-        ws = wb.create_sheet("Tabelle1")
-        row = (
-            "hasAnthroponym  ",
-            " hasValue, dcterms:creator   ",
-            " TextValue ",
-            "anthroponym",
-            "",
-            "Anthroponyme",
-            "",
-            " A strange chance put me in possession of this journal.  ",
-            "",
-            "",
-            "",
-            " Richtext ",
-            ""
-        )
-        for i, c in enumerate(row):
-            ws.cell(row=2, column=i+1, value=c)
-        properties_dict = e2j.row_to_prop(row)
-        expected_dict = {
-            "name": "hasAnthroponym",
-            "super": [
-                "hasValue",
-                "dcterms:creator"
-            ],
-            "object": "TextValue",
-            "labels": {
-                "en": "anthroponym",
-                "fr": "Anthroponyme",
-            },
-            "comments": {
-                "en": "A strange chance put me in possession of this journal."
-            },
-            "gui_element": "Richtext"
-        }
-        self.assertDictEqual(properties_dict, expected_dict)
 
 
 if __name__ == '__main__':
--- test/unittests/test_excel_to_resource.py
+++ test/unittests/test_excel_to_resource.py
@@ -6,8 +6,6 @@ import json
 import jsonpath_ng
 import re
 
-from openpyxl import Workbook
-
 from knora.dsplib.utils import excel_to_json_resources as e2j
 
 
@@ -20,31 +18,32 @@ class TestExcelToResource(unittest.TestCase):
     def test_excel2json(self) -> None:
         excelfile = "testdata/Resources.xlsx"
         outfile = "testdata/tmp/_out_res.json"
+        languages = ["en", "de", "fr", "it", "rm"]
         e2j.resources_excel2json(excelfile, outfile)
 
         # read excel: skip all rows that lack one of the required values
-        excel_df = pd.read_excel(excelfile)
-        excel_df = excel_df[pd.notna(excel_df['name'])]
-        excel_df = excel_df[[bool(re.search(r'[\wäöü]', x)) for x in excel_df['name']]]
-        excel_df = excel_df[pd.notna(excel_df['super'])]
-        excel_df = excel_df[[bool(re.search(r'[\wäöü]', x)) for x in excel_df['super']]]
-        excel_first_class_df = pd.read_excel(excelfile, sheet_name=1)
-        excel_first_class_df = excel_first_class_df[pd.notna(excel_first_class_df['Property'])]
-        excel_first_class_df = excel_first_class_df[[bool(re.search(r'[\wäöü]', x)) for x in excel_first_class_df['Property'].astype(str)]]
-        excel_first_class_df = excel_first_class_df[pd.notna(excel_first_class_df['Cardinality'])]
-        excel_first_class_df = excel_first_class_df[[bool(re.search(r'[\wäöü]', x)) for x in excel_first_class_df['Cardinality'].astype(str)]]
+        excel_df = pd.read_excel(excelfile, dtype=str)
+        for required_column in ["name", "super"]:
+            excel_df = excel_df[pd.notna(excel_df[required_column])]
+            excel_df = excel_df[[bool(re.search(r'[\wäöü]', x)) for x in excel_df[required_column]]]
+
+        excel_first_class_df = pd.read_excel(excelfile, sheet_name=1, dtype=str)
+        for required_column in ["Property", "Cardinality"]:
+            excel_first_class_df = excel_first_class_df[pd.notna(excel_first_class_df[required_column])]
+            excel_first_class_df = excel_first_class_df[[bool(re.search(r'[\wäöü]', x)) for x in excel_first_class_df[required_column]]]
 
         # extract infos from excel file
         excel_names = [s.strip() for s in excel_df['name']]
         excel_supers = [[x.strip() for x in s.split(',')] for s in excel_df['super']]
-        excel_labels_en = [s.strip() if isinstance(s, str) and re.search(r'[\wäöü]', s) else '' for s in list(excel_df['en'])]
-        excel_labels_de = [s.strip() if isinstance(s, str) and re.search(r'[\wäöü]', s) else '' for s in list(excel_df['de'])]
-        excel_labels_fr = [s.strip() if isinstance(s, str) and re.search(r'[\wäöü]', s) else '' for s in list(excel_df['fr'])]
-        excel_labels_it = [s.strip() if isinstance(s, str) and re.search(r'[\wäöü]', s) else '' for s in list(excel_df['it'])]
-        excel_comments_en = [s.strip() if isinstance(s, str) and re.search(r'[\wäöü]', s) else '' for s in list(excel_df['comment_en'])]
-        excel_comments_de = [s.strip() if isinstance(s, str) and re.search(r'[\wäöü]', s) else '' for s in list(excel_df['comment_de'])]
-        excel_comments_fr = [s.strip() if isinstance(s, str) and re.search(r'[\wäöü]', s) else '' for s in list(excel_df['comment_fr'])]
-        excel_comments_it = [s.strip() if isinstance(s, str) and re.search(r'[\wäöü]', s) else '' for s in list(excel_df['comment_it'])]
+
+        excel_labels: dict[str, list[str]] = dict()
+        for _id in languages:
+            excel_labels[_id] = [s.strip() if isinstance(s, str) and re.search(r'[\wäöü]', s) else '' for s in list(excel_df[_id])]
+
+        excel_comments: dict[str, list[str]] = dict()
+        for _id in [f"comment_{lang}" for lang in languages]:
+            excel_comments[_id] = [s.strip() if isinstance(s, str) and re.search(r'[\wäöü]', s) else '' for s in list(excel_df[_id])]
+
         excel_first_class_properties = [f':{s.strip()}' for s in excel_first_class_df['Property']]
         excel_first_class_cardinalities = [str(s).strip().lower() for s in excel_first_class_df['Cardinality']]
 
@@ -57,70 +56,27 @@ class TestExcelToResource(unittest.TestCase):
         # extract infos from json file
         json_names = [match.value for match in jsonpath_ng.parse('$.resources[*].name').find(json_file)]
         json_supers = [match.value for match in jsonpath_ng.parse('$.resources[*].super').find(json_file)]
-        json_labels = [match.value for match in jsonpath_ng.parse('$.resources[*].labels').find(json_file)]
-        json_labels_en = [label.get('en', '').strip() for label in json_labels]
-        json_labels_de = [label.get('de', '').strip() for label in json_labels]
-        json_labels_fr = [label.get('fr', '').strip() for label in json_labels]
-        json_labels_it = [label.get('it', '').strip() for label in json_labels]
-        json_comments = [match.value for match in jsonpath_ng.parse('$.resources[*].comments').find(json_file)]
-        json_comments_en = [comment.get('en', '').strip() for comment in json_comments]
-        json_comments_de = [comment.get('de', '').strip() for comment in json_comments]
-        json_comments_fr = [comment.get('fr', '').strip() for comment in json_comments]
-        json_comments_it = [comment.get('it', '').strip() for comment in json_comments]
+
+        json_labels_all = [match.value for match in jsonpath_ng.parse('$.resources[*].labels').find(json_file)]
+        json_labels: dict[str, list[str]] = dict()
+        for _id in languages:
+            json_labels[_id] = [label.get(_id, '').strip() for label in json_labels_all]
+
+        json_comments: dict[str, list[str]] = dict()
+        for _id in languages:
+            # make sure the lists of the json comments contain a blank string even if there is no "comments" section at all in this resource
+            json_comments[f"comment_{_id}"] = [resource.get("comments", {}).get(_id, '').strip() for resource in json_file["resources"]]
+
         json_first_class_properties = [match.value for match in jsonpath_ng.parse('$.resources[0].cardinalities[*].propname').find(json_file)]
         json_first_class_cardinalities = [match.value for match in jsonpath_ng.parse('$.resources[0].cardinalities[*].cardinality').find(json_file)]
 
         # make checks
         self.assertListEqual(excel_names, json_names)
         self.assertListEqual(excel_supers, json_supers)
-        self.assertListEqual(excel_labels_en, json_labels_en)
-        self.assertListEqual(excel_labels_de, json_labels_de)
-        self.assertListEqual(excel_labels_fr, json_labels_fr)
-        self.assertListEqual(excel_labels_it, json_labels_it)
-        self.assertListEqual(excel_comments_en, json_comments_en)
-        self.assertListEqual(excel_comments_de, json_comments_de)
-        self.assertListEqual(excel_comments_fr, json_comments_fr)
-        self.assertListEqual(excel_comments_it, json_comments_it)
+        self.assertDictEqual(excel_labels, json_labels)
+        self.assertDictEqual(excel_comments, json_comments)
         self.assertListEqual(excel_first_class_properties, json_first_class_properties)
         self.assertListEqual(excel_first_class_cardinalities, json_first_class_cardinalities)
-
-    def test_extract_row(self) -> None:
-        wb = Workbook()
-        ws_classes = wb.create_sheet("classes")
-        res_name = "ClassA"
-        row = (
-            res_name,
-            "Class A",
-            "",
-            "",
-            "",
-            "A comment on Class A",
-            "",
-            "",
-            "",
-            "Resource",
-        )
-        for i, c in enumerate(row):
-            ws_classes.cell(row=2, column=i+1, value=c)
-        ws_class_a = wb.create_sheet(res_name)
-        ws_class_a["A2"] = "property1"
-        ws_class_a["B2"] = "1"
-        resource_dict = e2j._extract_row(row, wb)
-        expected_dict = {
-            'name': 'ClassA',
-            'labels': {
-                'en': 'Class A'
-            },
-            'comments': {
-                'en': 'A comment on Class A'
-            },
-            'super': ['Resource'],
-            'cardinalities': [{
-                'propname': ':property1',
-                'cardinality': '1',
-                'gui_order': 1
-            }]}
-        self.assertDictEqual(resource_dict, expected_dict)
 
 
 if __name__ == '__main__':

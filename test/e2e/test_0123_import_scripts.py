33d77a4561ddfab571fdac172973e31ae8f9198e
Johannes Nussbaum <johannes.nussbaum@zoho.com>
2022-10-03 16:15:34 +0200

    edit

--- test/e2e/test_0123_import_scripts.py
+++ test/e2e/test_0123_import_scripts.py
@@ -1,8 +1,10 @@
+import filecmp
+import glob
 import os
 import re
 import unittest
+from zipfile import ZipFile
 
-import git
 import pytest
 
 from knora.dsplib.utils.onto_create_ontology import create_project
@@ -11,18 +13,31 @@ from knora.dsplib.utils.xml_upload import xml_upload
 
 class TestImportScripts(unittest.TestCase):
 
+    def test_zip_folder(self) -> None:
+        """
+        A ZIP of the directory docs/assets/0123-import-scripts is available for download on docs.dasch.swiss. Make
+        sure that the ZIP's contents are identical to the directory
+        """
+        with ZipFile("docs/assets/0123-import-scripts.zip", "r") as zipfolder:
+            zipfolder.extractall("docs/assets/0123-import-scripts-extracted")
+        original_files = [x for x in glob.glob("docs/assets/0123-import-scripts/**", recursive=True) if os.path.isfile(x)]
+        extracted_files = [x for x in glob.glob("docs/assets/0123-import-scripts-extracted/0123-import-scripts/**", recursive=True)
+                           if os.path.isfile(x)]
+
+        # check that for every original file, there is exactly one extracted counterpart
+        original_files_expected = [re.sub(r"0123-import-scripts-extracted/", "", x) for x in extracted_files]
+        self.assertListEqual(original_files, original_files_expected, "docs/assets/0123-import-scripts.zip is outdated")
+
+        # check that the files have the same size and modification time
+        for orig, extr in zip(original_files, extracted_files):
+            self.assertTrue(filecmp.cmp(orig, extr), "docs/assets/0123-import-scripts.zip is outdated")
+
     @pytest.mark.filterwarnings("ignore")
     def test_import_scripts(self) -> None:
-        # a ZIP of the directory docs/assets/0123-import-scripts is available for download on docs.dasch.swiss. To
-        # make sure that the ZIP is not older than any file in the directory, it must be checked that the UNIX time
-        # stamp of the last commit of the ZIP is >= the UNIX time stamp of the last commit of any file in the directory
-        # See https://git-scm.com/docs/pretty-formats#Documentation/pretty-formats.txt-emadem
-        # and https://git-scm.com/docs/git-log#Documentation/git-log.txt---ltpathgt82308203
-        repo = git.Git(".")
-        zip_last_commit_date = repo.log("--pretty=format:%at", "docs/assets/0123-import-scripts.zip").split("\n")[0]
-        dir_last_commit_date = repo.log("--pretty=format:%at", "docs/assets/0123-import-scripts").split("\n")[0]
-        self.assertGreaterEqual(zip_last_commit_date, dir_last_commit_date)
-
+        """
+        Execute the import script in its directory, create the project on the DSP server, and upload the created XML to
+        the DSP server.
+        """
         # execute the import script in its directory, and check if the output XML is as expected
         old_working_directory = os.getcwd()
         os.chdir("docs/assets/0123-import-scripts")

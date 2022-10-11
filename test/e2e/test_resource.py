20f03fefc67c05402aeefddd06297e9fd46360c4
Johannes Nussbaum <39048939+jnussbaum@users.noreply.github.com>
2022-10-11 16:17:06 +0200

    add negative tests for creation date

--- test/e2e/test_resource.py
+++ test/e2e/test_resource.py
@@ -9,15 +9,17 @@ from knora.dsplib.models.value import KnoraStandoffXml, make_value
 
 
 class TestResource(unittest.TestCase):
+    con = Connection('http://0.0.0.0:3333')
 
-    def setUp(self) -> None:
-        """
-        is executed before all tests; sets up a connection and logs in as user root
-        """
-        self.con = Connection('http://0.0.0.0:3333')
-        self.con.login('root@example.com', 'test')
+    @classmethod
+    def setUpClass(cls) -> None:
+        cls.con.login('root@example.com', 'test')
 
-    def test_Resource_create(self) -> None:
+    @classmethod
+    def tearDownClass(cls) -> None:
+        cls.con.logout()
+
+    def test_resource_create(self) -> None:
         # make class factory for project anything. The factory creates classes that implement the CRUD methods
         # for the given resource classes
 
@@ -28,6 +30,7 @@ class TestResource(unittest.TestCase):
 
         a_blue_thing = blue_thing(con=self.con,
                                   label='BlueThing',
+                                  creation_date='1999-12-31T23:59:59.9999999+01:00',
                                   values={
                                       'anything:hasBoolean': True,
                                       'anything:hasColor': ['#ff0033', '#0077FF'],
@@ -49,6 +52,7 @@ class TestResource(unittest.TestCase):
         self.assertEqual(new_blue_thing.value("anything:hasBoolean"), True)
         self.assertEqual(new_blue_thing.value("anything:hasDecimal"), 3.14159)
         self.assertEqual(new_blue_thing.value("anything:hasText"), "Dies ist ein einfacher Text")
+        self.assertEqual(new_blue_thing._creation_date, "1999-12-31T23:59:59.9999999+01:00")
 
         thing_picture = factory.get_resclass_type('anything:ThingPicture')
         sipi = Sipi('http://0.0.0.0:1024', self.con.get_token())
@@ -73,11 +77,30 @@ class TestResource(unittest.TestCase):
             }
         ).create()
 
-    def tearDown(self) -> None:
-        """
-        is executed after all tests are run through; performs a log out
-        """
-        self.con.logout()
+
+    def test_creation_date(self) -> None:
+        factory = ResourceInstanceFactory(self.con, 'anything')
+        blue_thing = factory.get_resclass_type('anything:BlueThing')
+        invalid_timestamps = ["1999-12-32T23:59:59+01:00", "1999-12-31T25:59:59+01:00", "1999-12-31T25:59:59"]
+        for invalid_timestamp in invalid_timestamps:
+            with self.assertRaisesRegex(BaseError, "KNORA-ERROR"):
+                a_blue_thing = blue_thing(con=self.con,
+                                          label='BlueThing',
+                                          creation_date=invalid_timestamp,
+                                          values={
+                                              'anything:hasBoolean': True,
+                                              'anything:hasColor': ['#ff0033', '#0077FF'],
+                                              'anything:hasDate': make_value(value='1833', comment='not verified!'),
+                                              'anything:hasDecimal': 3.14159,
+                                              'anything:hasInteger': "42",
+                                              'anything:hasTimeStamp': "2004-04-12T13:20:00Z",
+                                              'anything:hasInterval': [make_value(value="0.0:3.0", comment="first interval"),
+                                                                       make_value(value="3.5:3.7", comment="second interval")],
+                                              'anything:hasRichtext': KnoraStandoffXml("This is <em>bold</em> text."),
+                                              'anything:hasText': "Dies ist ein einfacher Text",
+                                              'anything:hasUri': 'http://test.com:65500/res'
+                                          })
+                a_blue_thing.create()
 
 
 if __name__ == '__main__':

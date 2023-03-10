e5cddf5ef73425ebf1f408d58930f76d759c1c7b
Johannes Nussbaum <39048939+jnussbaum@users.noreply.github.com>
2023-03-10 10:14:13 +0100

    chore: merge dublette class into one class (DEV-1831) #322

--- src/dsp_tools/models/set_encoder.py
+++ src/dsp_tools/models/set_encoder.py
@@ -1,8 +1,14 @@
 import json
 
+from dsp_tools.models.helpers import Context, OntoIri
+
 
 class SetEncoder(json.JSONEncoder):
     def default(self, obj):
         if isinstance(obj, set):
             return list(obj)
+        elif isinstance(obj, Context):
+            return obj.toJsonObj()
+        elif isinstance(obj, OntoIri):
+            return {"iri": obj.iri, "hashtag": obj.hashtag}
         return json.JSONEncoder.default(self, obj)

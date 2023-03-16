10f989bd44a9bb0b6f33ca9ccf940af654a24088
Johannes Nussbaum <johannes.nussbaum@dasch.swiss>
2023-03-16 12:01:35 +0100

    add e2e test

---
+++ test/e2e/test_rosetta.py
@@ -0,0 +1,25 @@
+import unittest
+from pathlib import Path
+
+from dsp_tools.utils import rosetta
+
+
+class TestRosetta(unittest.TestCase):
+
+    def test_rosetta(self) -> None:
+        enclosing_folder = Path("tmp/.dsp-tools/rosetta")
+        enclosing_folder.mkdir(parents=True, exist_ok=True)
+        rosetta_folder = enclosing_folder / "082E-rosetta-scripts"
+
+        is_rosetta_up_to_date = rosetta._update_possibly_existing_repo(rosetta_folder=rosetta_folder)
+        self.assertFalse(is_rosetta_up_to_date)
+
+        rosetta._clone_repo(rosetta_folder=rosetta_folder, enclosing_folder=enclosing_folder)
+        is_rosetta_up_to_date = rosetta._update_possibly_existing_repo(rosetta_folder=rosetta_folder)
+        self.assertTrue(is_rosetta_up_to_date)
+
+        success1 = rosetta._create_json(rosetta_folder=rosetta_folder)
+        self.assertTrue(success1)
+        success2 = rosetta._upload_xml(rosetta_folder=rosetta_folder)
+        self.assertTrue(success2)
+

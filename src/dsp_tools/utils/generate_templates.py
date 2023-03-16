bcc93a49d5e28a7ee098f227bf3fde051eef0e03
Johannes Nussbaum <johannes.nussbaum@dasch.swiss>
2023-03-16 16:09:51 +0100

    fix shortcode

--- src/dsp_tools/utils/generate_templates.py
+++ src/dsp_tools/utils/generate_templates.py
@@ -7,7 +7,7 @@ from dsp_tools.models.exceptions import UserError
 
 def generate_template_repo() -> bool:
     """
-    Copies the folder src/dsp_tools/resources/4124template-repo to the working directory.
+    Copies the folder src/dsp_tools/resources/0100-template-repo to the working directory.
 
     Raises:
         UserError: If a folder with the same name already exists in the working directory
@@ -15,13 +15,13 @@ def generate_template_repo() -> bool:
     Returns:
         True if the folder could be copied without problems
     """
-    # copy contents of src/dsp_tools/resources/4124-template-repo to cwd/template-repo
-    template_path_of_user = Path("4124-template-repo")
+    # copy contents of src/dsp_tools/resources/0100-template-repo to cwd/template-repo
+    template_path_of_user = Path("0100-template-repo")
     try:
         template_path_of_user.mkdir()
     except OSError:
-        raise UserError("A folder '4124-template-repo' already exists in your current working directory.") from None
-    template_path_of_distribution = importlib.resources.files("dsp_tools").joinpath("resources/4124-template-repo")
+        raise UserError("A folder '0100-template-repo' already exists in your current working directory.") from None
+    template_path_of_distribution = importlib.resources.files("dsp_tools").joinpath("resources/0100-template-repo")
     for file in template_path_of_distribution.iterdir():
         with importlib.resources.as_file(file) as f:
             file_path = Path(f)

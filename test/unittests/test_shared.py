67a38999fab56a649abdc9e65ac1b4da429b725c
Johannes Nussbaum <johannes.nussbaum@dasch.swiss>
2023-05-12 10:37:21 +0200

    prevent label from being empty

--- test/unittests/test_shared.py
+++ test/unittests/test_shared.py
@@ -48,6 +48,12 @@ class TestShared(unittest.TestCase):
         ):
             shared.validate_xml_against_schema(input_file="testdata/invalid-testdata/xml-data/duplicate-ark.xml")
 
+        with self.assertRaisesRegex(
+            UserError,
+            "Line 11: Element 'resource', attribute 'label': .+ The value '' has a length of '0'; this underruns the allowed minimum length of '1'"
+        ):
+            shared.validate_xml_against_schema(input_file="testdata/invalid-testdata/xml-data/empty-label.xml")
+
 
     def test_validate_xml_tags_in_text_properties(self) -> None:
         utf8_texts_with_allowed_html_escapes = [

f84a24e66e3224ffc4a0debac5b8ff43ba6c1a68
Johannes Nussbaum <johannes.nussbaum@dasch.swiss>
2023-06-09 09:40:38 +0200

    edit

--- src/dsp_tools/models/exceptions.py
+++ src/dsp_tools/models/exceptions.py
@@ -8,24 +8,20 @@ class BaseError(Exception):
     A basic error class for DSP-TOOLS.
 
     Attributes:
-        message:                        A message that describes the error
-        status_code:                    HTTP status code of the response from DSP-API 
-                                        (only applicable if the error comes from DSP-API)
-        json_content_of_api_response:   The message that DSP-API returns 
-                                        (only applicable if the error comes from DSP-API)
-        original_error_msg_from_api:    The original error message that DSP-API returns 
-                                        (only applicable if the error comes from DSP-API)
-        reason_from_api_response:       The reason for the failure that DSP-API returns 
-                                        (only applicable if the error comes from DSP-API)
-        api_route:                      The route that was called 
-                                        (only applicable if the error comes from DSP-API)
+
+        message: A message that describes the error
+        status_code: HTTP status code of the response from DSP-API (only applicable if error comes from DSP-API)
+        json_content_of_api_response: The message that DSP-API returns (only applicable if error comes from DSP-API)
+        orig_err_msg_from_api: Original error message that DSP-API returns (only applicable if error comes from DSP-API)
+        reason_from_api: Reason for the failure that DSP-API returns (only applicable if error comes from DSP-API)
+        api_route: The route that was called (only applicable if the error comes from DSP-API)
     """
 
     message: str
     status_code: Optional[int]
     json_content_of_api_response: Optional[str]
-    original_error_msg_from_api: Optional[str] = None
-    reason_from_api_response: Optional[str]
+    orig_err_msg_from_api: Optional[str] = None
+    reason_from_api: Optional[str]
     api_route: Optional[str]
 
     def __init__(
@@ -33,22 +29,18 @@ class BaseError(Exception):
         message: str,
         status_code: Optional[int] = None,
         json_content_of_api_response: Optional[str] = None,
-        reason_from_api_response: Optional[str] = None,
+        reason_from_api: Optional[str] = None,
         api_route: Optional[str] = None,
     ) -> None:
         """
         A basic error class for DSP-TOOLS.
 
         Args:
-            message:                        A message that describes the error
-            status_code:                    HTTP status code of the response from DSP-API 
-                                            (only applicable if the error comes from DSP-API)
-            json_content_of_api_response:   The message that DSP-API returns 
-                                            (only applicable if the error comes from DSP-API)
-            reason_from_api_response:       The reason for the failure that DSP-API returns 
-                                            (only applicable if the error comes from DSP-API)
-            api_route:                      The route that was called 
-                                            (only applicable if the error comes from DSP-API)
+            message: A message that describes the error
+            status_code: HTTP status code of the response from DSP-API (only applicable if error comes from DSP-API)
+            json_content_of_api_response: The message that DSP-API returns (only applicable if error comes from DSP-API)
+            reason_from_api: Reason for the failure that DSP-API returns (only applicable if error comes from DSP-API)
+            api_route: The route that was called (only applicable if the error comes from DSP-API)
         """
         super().__init__()
         self.message = message
@@ -60,10 +52,10 @@ class BaseError(Exception):
                 if "knora-api:error" in parsed_json:
                     knora_api_error = parsed_json["knora-api:error"]
                     knora_api_error = re.sub(r"^dsp\.errors\.[A-Za-z]+?: ?", "", knora_api_error)
-                    self.original_error_msg_from_api = knora_api_error
+                    self.orig_err_msg_from_api = knora_api_error
             except json.JSONDecodeError:
                 pass
-        self.reason_from_api_response = reason_from_api_response
+        self.reason_from_api = reason_from_api
         self.api_route = api_route
 
     def __str__(self) -> str:

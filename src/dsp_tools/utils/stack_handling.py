6ff2c487348afbc1fc09f7e71fa4d591b15e9a54
Johannes Nussbaum <39048939+jnussbaum@users.noreply.github.com>
2023-06-30 16:56:38 +0200

    feat(start-stack): add flag to start stack from main branch of DSP-API repository (DEV-2396) (#424)

--- src/dsp_tools/utils/stack_handling.py
+++ src/dsp_tools/utils/stack_handling.py
@@ -1,3 +1,4 @@
+from dataclasses import dataclass
 import importlib.resources
 import re
 import shutil
@@ -12,38 +13,48 @@ import yaml
 from dsp_tools.models.exceptions import UserError
 from dsp_tools.utils.logging import get_logger
 
+logger = get_logger(__name__)
 
-class StackHandling:
+
+@dataclass
+class StackConfiguration:
     """
-    This class contains functions to start and stop the Docker containers of DSP-API and DSP-APP.
+    Groups together configuration information for the StackHandler.
+
+    Args:
+        max_file_size: max. multimedia file size allowed by SIPI, in MB (max: 100'000)
+        enforce_docker_system_prune: if True, prune Docker without asking the user
+        suppress_docker_system_prune: if True, don't prune Docker (and don't ask)
+        latest_dev_version: if True, start DSP-API from repo's main branch, instead of the latest deployed version
     """
 
-    docker_path_of_user: Path
-    url_prefix: str
-    enforce_docker_system_prune: bool
-    suppress_docker_system_prune: bool
+    max_file_size: Optional[int] = None
+    enforce_docker_system_prune: bool = False
+    suppress_docker_system_prune: bool = False
+    latest_dev_version: bool = False
 
-    def __init__(self) -> None:
-        self.logger = get_logger(__name__)
-        self.docker_path_of_user = Path.home() / Path(".dsp-tools/start-stack")
-        self.docker_path_of_user.mkdir(parents=True, exist_ok=True)
-        self.url_prefix = self._get_url_prefix()
-
-    def _validate_input(
-        self,
-        max_file_size: Optional[int],
-        enforce_docker_system_prune: bool,
-        suppress_docker_system_prune: bool,
-    ) -> None:
-        """
-        Validate the input parameters passed by the user.
-        Raises a UserError if one of the parameters is invalid.
-        """
-        if max_file_size is not None:
-            if not 1 <= max_file_size <= 100_000:
+    def __post_init__(self) -> None:
+        """Validate the input parameters passed by the user. Raises a UserError if one of the parameters is invalid."""
+        if self.max_file_size is not None:
+            if not 1 <= self.max_file_size <= 100_000:
                 raise UserError("max_file_size must be between 1 and 100000")
-        if enforce_docker_system_prune and suppress_docker_system_prune:
+        if self.enforce_docker_system_prune and self.suppress_docker_system_prune:
             raise UserError('The arguments "--prune" and "--no-prune" are mutually exclusive')
+
+
+class StackHandler:
+    """This class contains functions to start and stop the Docker containers of DSP-API and DSP-APP."""
+
+    __stack_configuration: StackConfiguration
+    __url_prefix: str
+    __docker_path_of_user: Path
+
+    def __init__(self, stack_configuration: StackConfiguration) -> None:
+        """Initialize a StackHandler with a StackConfiguration"""
+        self.__stack_configuration = stack_configuration
+        self.__url_prefix = self._get_url_prefix()
+        self.__docker_path_of_user = Path.home() / Path(".dsp-tools/start-stack")
+        self.__docker_path_of_user.mkdir(parents=True, exist_ok=True)
 
     def _get_url_prefix(self) -> str:
         """
@@ -60,13 +71,20 @@ class StackHandling:
         If something goes wrong,
         the URL prefix falls back to pointing to the main branch of the DSP-API repository.
 
+        If the latest development version of DSP-API is started,
+        the URL prefix points to the main branch of the DSP-API repository.
+
         Returns:
             URL prefix used to retrieve files from the DSP-API repository
         """
-        url_prefix_base = "https://github.com/dasch-swiss/dsp-api/raw/"
+        url_prefix_base = "https://github.com/dasch-swiss/dsp-api/raw"
+
+        if self.__stack_configuration.latest_dev_version:
+            return f"{url_prefix_base}/main/"
+
         config_file = Path("src/dsp_tools/resources/start-stack/start-stack-config.yml")
         if not config_file.is_file():
-            return url_prefix_base + "main/"
+            return f"{url_prefix_base}/main/"
 
         with open("src/dsp_tools/resources/start-stack/start-stack-config.yml", "r", encoding="utf-8") as f:
             try:
@@ -74,45 +92,45 @@ class StackHandling:
             except yaml.YAMLError:
                 start_stack_config = {}
         commit_of_used_api_version = start_stack_config.get("DSP-API commit", "main")
-        url_prefix = f"https://github.com/dasch-swiss/dsp-api/raw/{commit_of_used_api_version}/"
-        return url_prefix
+        return f"{url_prefix_base}/{commit_of_used_api_version}/"
 
     def _copy_resources_to_home_dir(self) -> None:
         """
         On most systems, Docker is not allowed to access files outside of the user's home directory.
-        For this reason, copy the contents of src/dsp_tools/resources/start-stack to ~/.dsp-tools/start-stack.
+        For this reason, copy the contents of the distribution (src/dsp_tools/resources/start-stack)
+        to the user's home directory (~/.dsp-tools/start-stack).
+
+        Important: The files of the home directory might have been modified
+        by an earlier run of _inject_latest_tag_into_docker_compose_file().
+        So, this method must always be called, at every run of start-stack.
         """
         docker_path_of_distribution = importlib.resources.files("dsp_tools").joinpath("resources/start-stack")
         for file in docker_path_of_distribution.iterdir():
             with importlib.resources.as_file(file) as f:
                 file_path = Path(f)
-            shutil.copy(file_path, self.docker_path_of_user / file.name)
+            shutil.copy(file_path, self.__docker_path_of_user / file.name)
+        if not self.__stack_configuration.latest_dev_version:
+            Path(self.__docker_path_of_user / "docker-compose.override.yml").unlink()
 
-    def _get_sipi_docker_config_lua(
-        self,
-        max_file_size: Optional[int],
-    ) -> None:
+    def _get_sipi_docker_config_lua(self) -> None:
         """
         Retrieve the config file sipi.docker-config.lua from the DSP-API repository,
         and set the max_file_size parameter if necessary.
 
-        Args:
-            max_file_size: new value for max_file_size to inject into sipi.docker-config.lua
-
         Raises:
             UserError: if max_file_size is set but cannot be injected into sipi.docker-config.lua
         """
-        docker_config_lua_text = requests.get(f"{self.url_prefix}sipi/config/sipi.docker-config.lua", timeout=5).text
-        if max_file_size:
+        docker_config_lua_text = requests.get(f"{self.__url_prefix}sipi/config/sipi.docker-config.lua", timeout=5).text
+        if self.__stack_configuration.max_file_size:
             max_post_size_regex = r"max_post_size ?= ?[\'\"]\d+M[\'\"]"
             if not re.search(max_post_size_regex, docker_config_lua_text):
                 raise UserError("Unable to set max_file_size. Please try again without this flag.")
             docker_config_lua_text = re.sub(
                 max_post_size_regex,
-                f"max_post_size = '{max_file_size}M'",
+                f"max_post_size = '{self.__stack_configuration.max_file_size}M'",
                 docker_config_lua_text,
             )
-        with open(self.docker_path_of_user / "sipi.docker-config.lua", "w", encoding="utf-8") as f:
+        with open(self.__docker_path_of_user / "sipi.docker-config.lua", "w", encoding="utf-8") as f:
             f.write(docker_config_lua_text)
 
     def _start_up_fuseki(self) -> None:
@@ -125,12 +143,12 @@ class StackHandling:
         completed_process = subprocess.run(
             "docker compose up db -d",
             shell=True,
-            cwd=self.docker_path_of_user,
+            cwd=self.__docker_path_of_user,
             check=False,
         )
         if not completed_process or completed_process.returncode != 0:
             msg = "Cannot start the API: Error while executing 'docker compose up db -d'"
-            self.logger.error(f"{msg}. completed_process = '{completed_process}'")
+            logger.error(f"{msg}. completed_process = '{completed_process}'")
             raise UserError(msg)
 
     def _wait_for_fuseki(self) -> None:
@@ -156,7 +174,7 @@ class StackHandling:
             UserError: in case of failure
         """
         repo_template = requests.get(
-            f"{self.url_prefix}webapi/scripts/fuseki-repository-config.ttl.template",
+            f"{self.__url_prefix}webapi/scripts/fuseki-repository-config.ttl.template",
             timeout=5,
         ).text
         repo_template = repo_template.replace("@REPOSITORY@", "knora-test")
@@ -171,7 +189,7 @@ class StackHandling:
                 "Cannot start DSP-API: Error when creating the 'knora-test' repository. "
                 "Is DSP-API perhaps running already?"
             )
-            self.logger.error(f"{msg}. response = {response}")
+            logger.error(f"{msg}. response = {response}")
             raise UserError(msg)
 
     def _load_data_into_repo(self) -> None:
@@ -196,7 +214,7 @@ class StackHandling:
             ("test_data/all_data/anything-data.ttl", "http://www.knora.org/data/0001/anything"),
         ]
         for ttl_file, graph in ttl_files:
-            ttl_text = requests.get(self.url_prefix + ttl_file, timeout=5).text
+            ttl_text = requests.get(self.__url_prefix + ttl_file, timeout=5).text
             response = requests.post(
                 url=graph_prefix + graph,
                 files={"file": ("file.ttl", ttl_text, "text/turtle; charset: utf-8")},
@@ -204,7 +222,7 @@ class StackHandling:
                 timeout=5,
             )
             if not response.ok:
-                self.logger.error(f"Cannot start DSP-API: Error when creating graph '{graph}'. response = {response}")
+                logger.error(f"Cannot start DSP-API: Error when creating graph '{graph}'. response = {response}")
                 raise UserError(f"Cannot start DSP-API: Error when creating graph '{graph}'")
 
     def _initialize_fuseki(self) -> None:
@@ -219,7 +237,14 @@ class StackHandling:
         Start the other Docker containers that are not running yet.
         (Fuseki is already running at this point.)
         """
-        subprocess.run("docker compose up -d", shell=True, cwd=self.docker_path_of_user, check=True)
+        if self.__stack_configuration.latest_dev_version:
+            subprocess.run(
+                "docker pull daschswiss/knora-api:latest",
+                shell=True,
+                cwd=self.__docker_path_of_user,
+                check=True,
+            )
+        subprocess.run("docker compose up -d", shell=True, cwd=self.__docker_path_of_user, check=True)
         print("DSP-API is now running on http://0.0.0.0:3333/ and DSP-APP on http://0.0.0.0:4200/")
 
     def _execute_docker_system_prune(self) -> None:
@@ -227,9 +252,9 @@ class StackHandling:
         Depending on the CLI parameters or the user's input,
         execute "docker system prune" or not.
         """
-        if self.enforce_docker_system_prune:
+        if self.__stack_configuration.enforce_docker_system_prune:
             prune_docker = "y"
-        elif self.suppress_docker_system_prune:
+        elif self.__stack_configuration.suppress_docker_system_prune:
             prune_docker = "n"
         else:
             prune_docker = None
@@ -239,7 +264,7 @@ class StackHandling:
                     "If you are unsure what that means, just type y and press Enter. [y/n]"
                 )
         if prune_docker == "y":
-            subprocess.run("docker system prune -f", shell=True, cwd=self.docker_path_of_user, check=False)
+            subprocess.run("docker system prune -f", shell=True, cwd=self.__docker_path_of_user, check=False)
 
     def _start_docker_containers(self) -> None:
         """
@@ -255,20 +280,10 @@ class StackHandling:
         self._start_remaining_docker_containers()
         self._execute_docker_system_prune()
 
-    def start_stack(
-        self,
-        max_file_size: Optional[int] = None,
-        enforce_docker_system_prune: bool = False,
-        suppress_docker_system_prune: bool = False,
-    ) -> bool:
+    def start_stack(self) -> bool:
         """
-        Start the Docker containers of DSP-API and DSP-APP, and load some basic data models and data. After startup, ask
-        user if Docker should be pruned or not.
-
-        Args:
-            max_file_size: max. multimedia file size allowed by SIPI, in MB (max: 100'000)
-            enforce_docker_system_prune: if True, prune Docker without asking the user
-            suppress_docker_system_prune: if True, don't prune Docker (and don't ask)
+        Start the Docker containers of DSP-API and DSP-APP, and load some basic data models and data.
+        After startup, ask user if Docker should be pruned or not.
 
         Raises:
             UserError if the stack cannot be started with the parameters passed by the user
@@ -276,18 +291,9 @@ class StackHandling:
         Returns:
             True if everything went well, False otherwise
         """
-        self._validate_input(
-            max_file_size=max_file_size,
-            enforce_docker_system_prune=enforce_docker_system_prune,
-            suppress_docker_system_prune=suppress_docker_system_prune,
-        )
-        self.enforce_docker_system_prune = enforce_docker_system_prune
-        self.suppress_docker_system_prune = suppress_docker_system_prune
-
         self._copy_resources_to_home_dir()
-        self._get_sipi_docker_config_lua(max_file_size=max_file_size)
+        self._get_sipi_docker_config_lua()
         self._start_docker_containers()
-
         return True
 
     def stop_stack(self) -> bool:
@@ -297,5 +303,5 @@ class StackHandling:
         Returns:
             True if everything went well, False otherwise
         """
-        subprocess.run("docker compose down --volumes", shell=True, cwd=self.docker_path_of_user, check=True)
+        subprocess.run("docker compose down --volumes", shell=True, cwd=self.__docker_path_of_user, check=True)
         return True

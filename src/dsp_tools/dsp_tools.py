06195221e991559ad398991ea918a9d79c49710a
Johannes Nussbaum <johannes.nussbaum@dasch.swiss>
2023-07-26 13:17:43 +0200

    refactor

--- src/dsp_tools/dsp_tools.py
+++ src/dsp_tools/dsp_tools.py
@@ -348,25 +348,25 @@ def _derive_sipi_url(
     if not hasattr(parsed_arguments, "server"):
         return parsed_arguments
 
-    if regex.search(r"(0\.0\.0\.0|localhost):3333", parsed_arguments.server):
+    localhost_match = regex.search(r"(0\.0\.0\.0|localhost):3333", parsed_arguments.server)
+    remote_url_match = regex.search(
+        r"^(?:https?:\/\/)?(?:admin\.|api\.|iiif\.|app\.)?((?:.+\.)?dasch)\.swiss",
+        parsed_arguments.server,
+    )
+
+    if localhost_match:
         parsed_arguments.server = default_dsp_api_url
         parsed_arguments.sipi_url = default_sipi_url
-        logger.info(f"Using DSP server '{parsed_arguments.server}' and SIPI server '{parsed_arguments.sipi_url}'")
-        print(f"Using DSP server '{parsed_arguments.server}' and SIPI server '{parsed_arguments.sipi_url}'")
-        return parsed_arguments
-
-    remote_url_regex = r"^(?:https?:\/\/)?(?:admin\.|api\.|iiif\.|app\.)?((?:.+\.)?dasch)\.swiss"
-
-    remote_url_match = regex.search(remote_url_regex, parsed_arguments.server)
-    if remote_url_match:
+    elif remote_url_match:
         parsed_arguments.server = f"https://api.{remote_url_match.group(1)}.swiss"
         parsed_arguments.sipi_url = f"https://iiif.{remote_url_match.group(1)}.swiss"
-        logger.info(f"Using DSP server '{parsed_arguments.server}' and SIPI server '{parsed_arguments.sipi_url}'")
-        print(f"Using DSP server '{parsed_arguments.server}' and SIPI server '{parsed_arguments.sipi_url}'")
-        return parsed_arguments
+    else:
+        logger.error(f"Invalid DSP server URL '{parsed_arguments.server}'")
+        raise UserError(f"ERROR: Invalid DSP server URL '{parsed_arguments.server}'")
 
-    logger.error(f"Invalid DSP server URL '{parsed_arguments.server}'")
-    raise UserError(f"ERROR: Invalid DSP server URL '{parsed_arguments.server}'")
+    logger.info(f"Using DSP server '{parsed_arguments.server}' and SIPI server '{parsed_arguments.sipi_url}'")
+    print(f"Using DSP server '{parsed_arguments.server}' and SIPI server '{parsed_arguments.sipi_url}'")
+    return parsed_arguments
 
 
 def _call_requested_action(args: argparse.Namespace) -> bool:

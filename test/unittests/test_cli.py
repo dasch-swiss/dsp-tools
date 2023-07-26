90f9e1849c450360856474bac335e4f2004f2ecb
Johannes Nussbaum <johannes.nussbaum@dasch.swiss>
2023-07-26 10:57:24 +0200

    refactor test

--- test/unittests/test_cli.py
+++ test/unittests/test_cli.py
@@ -1,5 +1,6 @@
 """unit tests for the command line interface"""
 
+import argparse
 import unittest
 
 import pytest
@@ -18,97 +19,102 @@ class TestCLI(unittest.TestCase):
     default_sipi_url = "http://0.0.0.0:1024"
     root_user_email = "root@example.com"
     root_user_pw = "test"
-    parser = _make_parser(
-        default_dsp_api_url=default_dsp_api_url,
-        root_user_email=root_user_email,
-        root_user_pw=root_user_pw,
-    )
-    positive_testcases = {
-        "https://0.0.0.0:3333/": [
-            default_dsp_api_url,
-            default_sipi_url,
-        ],
-        "0.0.0.0:3333": [
-            default_dsp_api_url,
-            default_sipi_url,
-        ],
-        "localhost:3333": [
-            default_dsp_api_url,
-            default_sipi_url,
-        ],
-        "https://admin.dasch.swiss": [
-            "https://api.dasch.swiss",
-            "https://iiif.dasch.swiss",
-        ],
-        "https://api.dasch.swiss": [
-            "https://api.dasch.swiss",
-            "https://iiif.dasch.swiss",
-        ],
-        "https://app.dasch.swiss": [
-            "https://api.dasch.swiss",
-            "https://iiif.dasch.swiss",
-        ],
-        "https://iiif.dasch.swiss": [
-            "https://api.dasch.swiss",
-            "https://iiif.dasch.swiss",
-        ],
-        "https://dasch.swiss": [
-            "https://api.dasch.swiss",
-            "https://iiif.dasch.swiss",
-        ],
-        "dasch.swiss": [
-            "https://api.dasch.swiss",
-            "https://iiif.dasch.swiss",
-        ],
-        "http://admin.test.dasch.swiss/": [
-            "https://api.test.dasch.swiss",
-            "https://iiif.test.dasch.swiss",
-        ],
-        "http://app.staging.dasch.swiss/": [
-            "https://api.staging.dasch.swiss",
-            "https://iiif.staging.dasch.swiss",
-        ],
-        "https://demo.dasch.swiss/": [
-            "https://api.demo.dasch.swiss",
-            "https://iiif.demo.dasch.swiss",
-        ],
-        "http://api.dev.dasch.swiss/": [
-            "https://api.dev.dasch.swiss",
-            "https://iiif.dev.dasch.swiss",
-        ],
-        "dev-02.dasch.swiss": [
-            "https://api.dev-02.dasch.swiss",
-            "https://iiif.dev-02.dasch.swiss",
-        ],
-        "082a-test-server.dasch.swiss": [
-            "https://api.082a-test-server.dasch.swiss",
-            "https://iiif.082a-test-server.dasch.swiss",
-        ],
-        "admin.08F4-test-server.dasch.swiss": [
-            "https://api.08F4-test-server.dasch.swiss",
-            "https://iiif.08F4-test-server.dasch.swiss",
-        ],
-        "app.08F4-test-server.dasch.swiss": [
-            "https://api.08F4-test-server.dasch.swiss",
-            "https://iiif.08F4-test-server.dasch.swiss",
-        ],
-        "iiif.E5bC-test-server.dasch.swiss": [
-            "https://api.E5bC-test-server.dasch.swiss",
-            "https://iiif.E5bC-test-server.dasch.swiss",
-        ],
-        "not-yet-0826-test-server.dasch.swiss": [
-            "https://api.not-yet-0826-test-server.dasch.swiss",
-            "https://iiif.not-yet-0826-test-server.dasch.swiss",
-        ],
-        "https://admin.ls-prod-server.dasch.swiss": [
-            "https://api.ls-prod-server.dasch.swiss",
-            "https://iiif.ls-prod-server.dasch.swiss",
-        ],
-        "https://ls-test-server.dasch.swiss": [
-            "https://api.ls-test-server.dasch.swiss",
-            "https://iiif.ls-test-server.dasch.swiss",
-        ],
-    }
+    parser: argparse.ArgumentParser
+    positive_testcases: dict[str, list[str]]
+
+    def __init__(self) -> None:
+        super().__init__()
+        self.parser = _make_parser(
+            default_dsp_api_url=self.default_dsp_api_url,
+            root_user_email=self.root_user_email,
+            root_user_pw=self.root_user_pw,
+        )
+        self.positive_testcases = {
+            "https://0.0.0.0:3333/": [
+                self.default_dsp_api_url,
+                self.default_sipi_url,
+            ],
+            "0.0.0.0:3333": [
+                self.default_dsp_api_url,
+                self.default_sipi_url,
+            ],
+            "localhost:3333": [
+                self.default_dsp_api_url,
+                self.default_sipi_url,
+            ],
+            "https://admin.dasch.swiss": [
+                "https://api.dasch.swiss",
+                "https://iiif.dasch.swiss",
+            ],
+            "https://api.dasch.swiss": [
+                "https://api.dasch.swiss",
+                "https://iiif.dasch.swiss",
+            ],
+            "https://app.dasch.swiss": [
+                "https://api.dasch.swiss",
+                "https://iiif.dasch.swiss",
+            ],
+            "https://iiif.dasch.swiss": [
+                "https://api.dasch.swiss",
+                "https://iiif.dasch.swiss",
+            ],
+            "https://dasch.swiss": [
+                "https://api.dasch.swiss",
+                "https://iiif.dasch.swiss",
+            ],
+            "dasch.swiss": [
+                "https://api.dasch.swiss",
+                "https://iiif.dasch.swiss",
+            ],
+            "http://admin.test.dasch.swiss/": [
+                "https://api.test.dasch.swiss",
+                "https://iiif.test.dasch.swiss",
+            ],
+            "http://app.staging.dasch.swiss/": [
+                "https://api.staging.dasch.swiss",
+                "https://iiif.staging.dasch.swiss",
+            ],
+            "https://demo.dasch.swiss/": [
+                "https://api.demo.dasch.swiss",
+                "https://iiif.demo.dasch.swiss",
+            ],
+            "http://api.dev.dasch.swiss/": [
+                "https://api.dev.dasch.swiss",
+                "https://iiif.dev.dasch.swiss",
+            ],
+            "dev-02.dasch.swiss": [
+                "https://api.dev-02.dasch.swiss",
+                "https://iiif.dev-02.dasch.swiss",
+            ],
+            "082a-test-server.dasch.swiss": [
+                "https://api.082a-test-server.dasch.swiss",
+                "https://iiif.082a-test-server.dasch.swiss",
+            ],
+            "admin.08F4-test-server.dasch.swiss": [
+                "https://api.08F4-test-server.dasch.swiss",
+                "https://iiif.08F4-test-server.dasch.swiss",
+            ],
+            "app.08F4-test-server.dasch.swiss": [
+                "https://api.08F4-test-server.dasch.swiss",
+                "https://iiif.08F4-test-server.dasch.swiss",
+            ],
+            "iiif.E5bC-test-server.dasch.swiss": [
+                "https://api.E5bC-test-server.dasch.swiss",
+                "https://iiif.E5bC-test-server.dasch.swiss",
+            ],
+            "not-yet-0826-test-server.dasch.swiss": [
+                "https://api.not-yet-0826-test-server.dasch.swiss",
+                "https://iiif.not-yet-0826-test-server.dasch.swiss",
+            ],
+            "https://admin.ls-prod-server.dasch.swiss": [
+                "https://api.ls-prod-server.dasch.swiss",
+                "https://iiif.ls-prod-server.dasch.swiss",
+            ],
+            "https://ls-test-server.dasch.swiss": [
+                "https://api.ls-test-server.dasch.swiss",
+                "https://iiif.ls-test-server.dasch.swiss",
+            ],
+        }
 
     def test_derive_sipi_url_positive_cases(self) -> None:
         """

c02c7f9f83a7a40f55d375cc08257250a60c961a
Johannes Nussbaum <johannes.nussbaum@dasch.swiss>
2023-08-16 17:43:07 +0200

    add debug statement

--- src/dsp_tools/models/connection.py
+++ src/dsp_tools/models/connection.py
@@ -63,6 +63,7 @@ class Connection:
             self.dump_directory.mkdir(exist_ok=True)
 
         if self.user_email and self.password:
+            print(f"__post_init__ of Connection: login with self.server={self.server}")
             response = requests.post(
                 url=f"{self.server}/v2/authentication",
                 headers={"Content-Type": "application/json; charset=UTF-8"},

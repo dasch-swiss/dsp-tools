e7b3fd43297a9556848431d2d2e2600f67d80218
Johannes Nussbaum <johannes.nussbaum@dasch.swiss>
2023-08-22 14:45:23 +0200

    beautify xmlbitstream

--- src/dsp_tools/models/xmlbitstream.py
+++ src/dsp_tools/models/xmlbitstream.py
@@ -3,22 +3,18 @@ from typing import cast, Optional
 from lxml import etree
 
 
-class XMLBitstream:
-    """Represents a bitstream object (file) of a resource in the XML used for data import"""
+class XMLBitstream:  # pylint: disable=too-few-public-methods
+    """
+    Represents a bitstream object (file) of a resource in the XML used for data import
 
-    _value: str
-    _permissions: Optional[str]
+    Attributes:
+        value: The file path of the bitstream object
+        permissions: Reference to the set of permissions for the bitstream object
+    """
+
+    value: str
+    permissions: Optional[str]
 
     def __init__(self, node: etree._Element) -> None:
-        self._value = cast(str, node.text)
-        self._permissions = node.get("permissions")
-
-    @property
-    def value(self) -> str:
-        """The file path of the bitstream object"""
-        return self._value
-
-    @property
-    def permissions(self) -> Optional[str]:
-        """Reference to the set of permissions for the bitstream object"""
-        return self._permissions
+        self.value = cast(str, node.text)
+        self.permissions = node.get("permissions")

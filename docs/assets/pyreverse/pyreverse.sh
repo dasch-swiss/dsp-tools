96e08bf3017b13aa635b6f53064626fae0f535ba
Johannes Nussbaum <johannes.nussbaum@dasch.swiss>
2023-09-13 09:18:27 +0200

    improve formatting

--- docs/assets/pyreverse/pyreverse.sh
+++ docs/assets/pyreverse/pyreverse.sh
@@ -1,1 +1,9 @@
-pyreverse -o png --max-color-depth 3 --ignore logging.py,exceptions.py,connection.py,helpers.py,propertyelement.py,utils.py,import_script.py,shared.py,model.py,set_encoder.py,xmlallow.py,xmlbitstream.py,bitstream.py,permission.py,xmlvalue.py --colorized ../src/dsp_tools
+# https://pylint.pycqa.org/en/latest/pyreverse.html
+
+pyreverse \
+-o png \
+--max-color-depth 3 \
+--colorized  \
+--ignore logging.py,exceptions.py,connection.py,helpers.py,propertyelement.py,utils.py,import_script.py,shared.py,set_encoder.py \
+--output-directory docs/assets/pyreverse \
+src/dsp_tools

e1b2d649c0a6c69d19a04329b5b5f25b2b9f3aa2
Johannes Nussbaum <johannes.nussbaum@dasch.swiss>
2023-09-13 09:22:57 +0200

    ignore entire "import_scripts" directory

--- docs/assets/pyreverse/pyreverse.sh
+++ docs/assets/pyreverse/pyreverse.sh
@@ -1,9 +1,9 @@
 # https://pylint.pycqa.org/en/latest/pyreverse.html
 
 pyreverse \
--o png \
+--output png \
 --max-color-depth 3 \
 --colorized  \
---ignore logging.py,exceptions.py,connection.py,helpers.py,propertyelement.py,utils.py,import_script.py,shared.py,set_encoder.py \
+--ignore __init__.py,logging.py,exceptions.py,connection.py,helpers.py,propertyelement.py,utils.py,import_scripts,shared.py,set_encoder.py \
 --output-directory docs/assets/pyreverse \
 src/dsp_tools

f5039c23cb309416b343bff0d6209c7ef23473c8
Johannes Nussbaum <johannes.nussbaum@dasch.swiss>
2023-09-13 08:55:57 +0200

    edit

---
+++ docs/assets/pyreverse/pyreverse.sh
@@ -0,0 +1,1 @@
+pyreverse -o png --max-color-depth 3 --ignore logging.py,exceptions.py,connection.py,helpers.py,propertyelement.py,utils.py,import_script.py,shared.py,model.py,set_encoder.py,xmlallow.py,xmlbitstream.py,bitstream.py,permission.py,xmlvalue.py --colorized ../src/dsp_tools

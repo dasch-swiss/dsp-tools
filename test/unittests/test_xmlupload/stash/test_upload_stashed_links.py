34b21a6cdeb40b12c5bae95931fe371440c5758c
Balduin Landolt <33053745+BalduinLandolt@users.noreply.github.com>
2023-10-05 17:49:03 +0200

    go back in the abstractions

--- test/unittests/test_xmlupload/stash/test_upload_stashed_links.py
+++ test/unittests/test_xmlupload/stash/test_upload_stashed_links.py
@@ -1,31 +1,60 @@
-from typing import TypeAlias
+from typing import Any, Optional, TypeAlias
 
 from dsp_tools.models.value import KnoraStandoffXml
 from dsp_tools.models.xmlproperty import XMLProperty
 from dsp_tools.models.xmlresource import XMLResource
+from dsp_tools.utils.xmlupload.stash.stash_upload_service_live import StashUploadServiceLive
 from dsp_tools.utils.xmlupload.stash.upload_stashed_links import upload_stashed_links
 
 ResptrStash: TypeAlias = dict[XMLResource, dict[XMLProperty, list[str]]]
 StandoffStash: TypeAlias = dict[XMLResource, dict[XMLProperty, dict[str, KnoraStandoffXml]]]
 
 
-class StashUploadServiceMock:
-    """Service that handles uploading of stashed links and standoff links to DSP."""
+# class StashUploadServiceMock:
+#     """Service that handles uploading of stashed links and standoff links to DSP."""
 
-    def upload_links(  # pylint: disable=missing-function-docstring
+#     def upload_links(  # pylint: disable=missing-function-docstring
+#         self,
+#         verbose: bool,  # pylint: disable=unused-argument
+#         id2iri_mapping: dict[str, str],  # pylint: disable=unused-argument
+#         stashed_resptr_props: ResptrStash,  # pylint: disable=unused-argument
+#     ) -> dict[XMLResource, dict[XMLProperty, list[str]]]:  # pylint: disable=unused-argument
+#         return {}
+
+#     def upload_standoff_links(  # pylint: disable=missing-function-docstring
+#         self,
+#         verbose: bool,  # pylint: disable=unused-argument
+#         id2iri_mapping: dict[str, str],  # pylint: disable=unused-argument
+#         stashed_xml_texts: StandoffStash,  # pylint: disable=unused-argument
+#     ) -> dict[XMLResource, dict[XMLProperty, dict[str, KnoraStandoffXml]]]:
+#         return {}
+
+
+class ConnectionMock:
+    def get(
         self,
-        verbose: bool,  # pylint: disable=unused-argument
-        id2iri_mapping: dict[str, str],  # pylint: disable=unused-argument
-        stashed_resptr_props: ResptrStash,  # pylint: disable=unused-argument
-    ) -> dict[XMLResource, dict[XMLProperty, list[str]]]:  # pylint: disable=unused-argument
+        route: str,
+        headers: Optional[dict[str, str]] = None,
+    ) -> dict[str, Any]:
+        """Mock GET request."""
         return {}
 
-    def upload_standoff_links(  # pylint: disable=missing-function-docstring
+    def post(
         self,
-        verbose: bool,  # pylint: disable=unused-argument
-        id2iri_mapping: dict[str, str],  # pylint: disable=unused-argument
-        stashed_xml_texts: StandoffStash,  # pylint: disable=unused-argument
-    ) -> dict[XMLResource, dict[XMLProperty, dict[str, KnoraStandoffXml]]]:
+        route: str,
+        jsondata: Optional[str] = None,
+        content_type: str = "application/json",
+    ) -> dict[str, Any]:
+        """Mock POST request."""
+        return {}
+
+    def put(
+        self,
+        route: str,
+        jsondata: Optional[str] = None,
+        content_type: str = "application/json",
+    ) -> dict[str, Any]:
+        """Mock PUT request."""
         return {}
 
 
@@ -34,7 +63,8 @@ class TestUploadStashedLinks:  # pylint: disable=too-few-public-methods
 
     def test_upload_stashed_links_no_stashed_links(self) -> None:
         """Test that upload_stashed_links returns empty dicts if no stashed links are given."""
-        serviceMock = StashUploadServiceMock()
+        con = ConnectionMock()
+        serviceMock = StashUploadServiceLive(con)
         result = upload_stashed_links(
             stashUploadService=serviceMock,
             verbose=False,

5ca92e82f6519fea8c8573183563c2547d50f22b
Nora-Olivia-Ammann <103038637+Nora-Olivia-Ammann@users.noreply.github.com>
2023-10-06 11:04:50 +0200

    Update create_complex_circular_references.py

--- testdata/generate_test_data/create_complex_circular_references.py
+++ testdata/generate_test_data/create_complex_circular_references.py
@@ -6,13 +6,17 @@ from lxml import etree
 from dsp_tools import excel2xml
 
 
-def create_and_save_circular_references_test_graph(number_of_graphs: int = 1, save_location: str = "") -> None:
+def create_and_save_circular_references_test_graph(replication_counter: int = 1, save_location: str = "") -> None:
     """
     This function creates a graph with circular references.
     By default, it saves it in the current directory; this can be changed with the parameter save_location.
+
+    Args:
+        replication_counter: number of times the sub-graphs should be created in one root-graph.
+        save_location: path to the folder, where the file should be saved
     """
-    root = create_circular_references_test_graph(replication_counter=number_of_graphs)
-    excel2xml.write_xml(root, os.path.join(save_location, f"test_circular_references_{number_of_graphs}.xml"))
+    root = create_circular_references_test_graph(replication_counter=replication_counter)
+    excel2xml.write_xml(root, os.path.join(save_location, f"test_circular_references_{replication_counter}.xml"))
 
 
 def create_circular_references_test_graph(replication_counter: int) -> etree._Element:
@@ -21,6 +25,12 @@ def create_circular_references_test_graph(replication_counter: int) -> etree._El
     It is capable of reproducing one graph with all the references a specified number of times.
     Each circle type has its own sub-graph number in order to differentiate them by their ID.
     The resulting etree is suitable for upload to the DSP-API.
+
+    Args:
+        replication_counter: number of times the sub-graphs should be created in one root-graph.
+
+    Returns:
+        A etree which is compatible for an upload into the DSP-API
     """
     root = excel2xml.make_root("4123", "testonto")
     for i in range(1, replication_counter + 1):
@@ -64,9 +74,8 @@ def _make_simple_text(resource: etree._Element) -> etree._Element:
 
 
 def _make_chain(replication_counter: str) -> list[etree._Element]:
-    """
-    A -> B -> C -> D -> E (resptr-prop)
-    """
+    # A -> B -> C -> D -> E (resptr-prop)
+
     resources = _make_list_of_resources(5, replication_counter)
     resptr = [_make_resptr_prop(x) for x in resources[1:]]
     for i, link in enumerate(resptr):
@@ -75,11 +84,10 @@ def _make_chain(replication_counter: str) -> list[etree._Element]:
 
 
 def _make_one_circle_with_three_resources(replication_counter: str) -> list[etree._Element]:
-    """
-    A -> B (xml-text) & (resptr-prop)
-    B -> C (xml-text) & (resptr-prop)
-    C -> A (xml-text) & (resptr-prop)
-    """
+    # A -> B (xml-text) & (resptr-prop)
+    # B -> C (xml-text) & (resptr-prop)
+    # C -> A (xml-text) & (resptr-prop)
+
     res_li = _make_list_of_resources(number_of_resources=3, replication_counter=replication_counter)
     salsah_list = [_make_salsah_link_prop(x) for x in res_li]
     resptr_list = [_make_resptr_prop(x) for x in res_li]
@@ -93,10 +101,9 @@ def _make_one_circle_with_three_resources(replication_counter: str) -> list[etre
 
 
 def _make_two_references(replication_counter: str) -> list[etree._Element]:
-    """
-    A -> B (resptr-prop)
-    B -> A (xml-text)
-    """
+    # A -> B (resptr-prop)
+    # B -> A (xml-text)
+
     res_li = _make_list_of_resources(2, replication_counter)
     res_li[0].append(_make_resptr_prop(res_li[1]))
     res_li[1].append(_make_salsah_link_prop(res_li[0]))
@@ -104,12 +111,11 @@ def _make_two_references(replication_counter: str) -> list[etree._Element]:
 
 
 def _make_complex_dependencies_add_on(replication_counter: str) -> list[etree._Element]:
-    """
-    Same as _make_complex_dependencies
-    plus
-    D -> F (xml-text)
-    F -> E (resptr-prop)
-    """
+    # Same as _make_complex_dependencies
+    # plus
+    # D -> F (xml-text)
+    # F -> E (resptr-prop)
+
     complex_dep_li = _make_complex_dependencies(replication_counter)
     f_id = f"res_F_{replication_counter}"
     f_res: etree._Element = excel2xml.make_resource(restype=":TestThing", label=f_id, id=f_id)
@@ -121,22 +127,20 @@ def _make_complex_dependencies_add_on(replication_counter: str) -> list[etree._E
 
 
 def _make_complex_dependencies_with_simpletext(replication_counter: str) -> list[etree._Element]:
-    """
-    Same as _make_complex_dependencies
-    plus each value has a simple text property
-    """
+    # Same as _make_complex_dependencies
+    # plus each value has a simple text property
+
     complex_dep_li = _make_complex_dependencies(replication_counter)
     return [_make_simple_text(x) for x in complex_dep_li]
 
 
 def _make_complex_dependencies(replication_counter: str) -> list[etree._Element]:
-    """
-    A -> D (xml-text)
-    B -> D (xml-text)
-    C -> D (xml-text)
-    D -> E (resptr-prop)
-    E -> A / B / C (resptr-prop)
-    """
+    # A -> D (xml-text)
+    # B -> D (xml-text)
+    # C -> D (xml-text)
+    # D -> E (resptr-prop)
+    # E -> A / B / C (resptr-prop)
+
     all_resources = _make_list_of_resources(number_of_resources=5, replication_counter=replication_counter)
     all_resources = _make_complex_dependencies_resource_ABC(all_resources)
     all_resources = _make_complex_dependencies_resource_D(all_resources)

db175eb22ded7562d0b9d79f0729ce55f2d40bf6
Nora-Olivia-Ammann <103038637+Nora-Olivia-Ammann@users.noreply.github.com>
2023-10-09 16:40:32 +0200

    Create test_stash_circular_references.py

---
+++ test/unittests/test_xmlupload/test_stash_circular_references.py
@@ -0,0 +1,19 @@
+# pylint: disable=missing-class-docstring,missing-function-docstring,protected-access
+
+import pytest
+
+from dsp_tools.utils.xml_utils import parse_and_clean_xml_file
+from dsp_tools.utils.xmlupload.stash_circular_references import remove_circular_references
+from dsp_tools.utils.xmlupload.xmlupload import _extract_resources_from_xml
+
+test_root = parse_and_clean_xml_file("testdata/generate_test_data/test_circular_references_1.xml")
+resources = _extract_resources_from_xml(test_root, "simcir")
+
+
+def test_remove_circular_references() -> None:
+    ok_resources, _, _ = remove_circular_references(resources, False)
+    assert len(ok_resources) <= 26
+
+
+if __name__ == "__main__":
+    pytest.main([__file__])

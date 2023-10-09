fa66deba659be65555fc020daa11311e59538bd4
Balduin Landolt <33053745+BalduinLandolt@users.noreply.github.com>
2023-10-09 16:03:18 +0200

    check

--- src/dsp_tools/cli.py
+++ src/dsp_tools/cli.py
@@ -589,4 +589,6 @@ def main(args: list[str]) -> None:
 
 
 if __name__ == "__main__":
-    main(sys.argv[1:])
+    args = sys.argv[1:]
+    print(f"Called DSP-TOOLS with these arguments: {args}")
+    main(args)

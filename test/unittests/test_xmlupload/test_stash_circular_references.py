8f3d8ac2cd1e277218ba4d52e5e908b55edd8229
Nora-Olivia-Ammann <103038637+Nora-Olivia-Ammann@users.noreply.github.com>
2023-10-10 07:59:01 +0200

    Update test_stash_circular_references.py

--- test/unittests/test_xmlupload/test_stash_circular_references.py
+++ test/unittests/test_xmlupload/test_stash_circular_references.py
@@ -6,11 +6,10 @@ from dsp_tools.utils.xml_utils import parse_and_clean_xml_file
 from dsp_tools.utils.xmlupload.stash_circular_references import remove_circular_references
 from dsp_tools.utils.xmlupload.xmlupload import _extract_resources_from_xml
 
-test_root = parse_and_clean_xml_file("testdata/generate_test_data/test_circular_references_1.xml")
-resources = _extract_resources_from_xml(test_root, "simcir")
 
-
-def test_remove_circular_references() -> None:
+def test_check_if_number_ok_links_increases_stashes_decrease() -> None:
+    test_root = parse_and_clean_xml_file("testdata/generate_test_data/test_circular_references_1.xml")
+    resources = _extract_resources_from_xml(test_root, "simcir")
     ok_resources, _, _ = remove_circular_references(resources, False)
     assert len(ok_resources) >= 26
 

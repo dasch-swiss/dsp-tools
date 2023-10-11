1476364173c65a5dced62fee6f542e9994af7b88
Nora-Olivia-Ammann <103038637+Nora-Olivia-Ammann@users.noreply.github.com>
2023-10-11 07:54:48 +0200

    Regex search link

--- src/dsp_tools/analyse_xml_data/extract_links_from_XML.py
+++ src/dsp_tools/analyse_xml_data/extract_links_from_XML.py
@@ -2,12 +2,14 @@ import regex
 from lxml import etree
 
 
-def _extract_links_one_text_prop(text_prop: etree._Element) -> list[str]:
+def _extract_id_one_text_prop(text_prop: etree._Element) -> list[str]:
     all_links = set()
     for child in text_prop.iterdescendants():
         if href := child.attrib.get("href"):
-            searched = regex.findall(r"IRI:(.*):IRI", href)[0]
-            match type(searched):
-                case str:
-                    all_links.add(searched)
+            searched = regex.search(r"IRI:(.*):IRI", href)
+            match searched:
+                case regex.Match():
+                    all_links.add(searched.group(1))
+                case None:
+                    continue
     return list(all_links)
--- test/unittests/test_analyse_xml_data/test_extract_links_from_XML.py
+++ test/unittests/test_analyse_xml_data/test_extract_links_from_XML.py
@@ -2,31 +2,43 @@
 
 import pytest
 from lxml import etree
+from pytest_unordered import unordered
 
-from dsp_tools.analyse_xml_data.extract_links_from_XML import _extract_links_one_text_prop
+from dsp_tools.analyse_xml_data.extract_links_from_XML import _extract_id_one_text_prop
 
 
-def test_extract_links_one_text_prop_with_one_iri() -> None:
+def test_extract_extract_id_one_text_prop_with_one_id() -> None:
     test_ele = etree.fromstring(
         '<text-prop xmlns="https://dasch.swiss/schema" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" '
         'name=":hasRichtext"><text permissions="prop-default" encoding="xml"><a class="salsah-link" '
         'href="IRI:res_A_11:IRI">res_A_11</a></text></text-prop>'
     )
-    res = _extract_links_one_text_prop(test_ele)
+    res = _extract_id_one_text_prop(test_ele)
     expected = ["res_A_11"]
-    assert res == expected
+    assert expected == res
 
 
-def test_extract_links_one_text_prop_with_several_iri() -> None:
+def test_extract_id_one_text_prop_with_iri() -> None:
+    test_ele = etree.fromstring(
+        '<text-prop xmlns="https://dasch.swiss/schema" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" '
+        'name=":hasRichtext"><text permissions="prop-default" encoding="xml"><a class="salsah-link" '
+        'href="http://rdfh.ch/0801/RDE7_KU1STuDhHnGr5uu0g">res_A_11</a></text></text-prop>'
+    )
+    res = _extract_id_one_text_prop(test_ele)
+    expected = []
+    assert expected == res
+
+
+def test_extract_id_one_text_prop_with_several_id() -> None:
     test_ele = etree.fromstring(
         '<text-prop xmlns="https://dasch.swiss/schema" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" '
         'name=":hasRichtext"><text permissions="prop-default" encoding="xml"><a class="salsah-link" '
         'href="IRI:res_A_11:IRI">res_A_11</a><a class="salsah-link" href="IRI:res_B_11:IRI">res_A_11</a><a '
         'class="salsah-link" href="IRI:res_B_11:IRI">res_A_11</a></text></text-prop>'
     )
-    res = _extract_links_one_text_prop(test_ele)
+    res = _extract_id_one_text_prop(test_ele)
     expected = ["res_A_11", "res_B_11"]
-    assert res == expected
+    assert expected == unordered(res)
 
 
 if __name__ == "__main__":

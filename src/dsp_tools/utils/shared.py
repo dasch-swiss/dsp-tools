3e47c72e1a1dc1d48eaa21df4a4ef34ba6caf1fb
Balduin Landolt <33053745+BalduinLandolt@users.noreply.github.com>
2023-10-12 00:17:05 +0200

    Update shared.py

--- src/dsp_tools/utils/shared.py
+++ src/dsp_tools/utils/shared.py
@@ -158,6 +158,9 @@ def try_network_action(
         the return value of action
     """
     action_as_str = f"action='{action}', args='{args}', kwargs='{kwargs}'"
+    print("-" * 80)
+    print(action_as_str)
+    print("-" * 80)
     for i in range(7):
         try:
             if args and not kwargs:

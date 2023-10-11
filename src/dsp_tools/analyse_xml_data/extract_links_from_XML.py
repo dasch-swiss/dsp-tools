2319d14fc4d361dea7b4051aa86fa3df9b0d2263
Nora-Olivia-Ammann <103038637+Nora-Olivia-Ammann@users.noreply.github.com>
2023-10-11 09:24:59 +0200

    all text prop one resource

--- src/dsp_tools/analyse_xml_data/extract_links_from_XML.py
+++ src/dsp_tools/analyse_xml_data/extract_links_from_XML.py
@@ -1,8 +1,17 @@
+from typing import Any
+
 import regex
 from lxml import etree
 
 
-def _extract_id_one_text_prop(text_prop: etree._Element) -> list[str]:
+def _get_all_text_prop_one_resource(resource: etree._Element) -> list[Any]:
+    id_list = []
+    for text_prop in resource.iter("{https://dasch.swiss/schema}text-prop"):
+        id_list.extend(_extract_id_one_text_prop(text_prop))
+    return id_list
+
+
+def _extract_id_one_text_prop(text_prop: etree._Element) -> list[Any]:
     # the same ID is in several separate <text-props> are considered separate links
     all_links = []
     for text in text_prop.getchildren():
@@ -10,7 +19,7 @@ def _extract_id_one_text_prop(text_prop: etree._Element) -> list[str]:
     return all_links
 
 
-def _extract_id_one_text(text: etree._Element) -> list[str]:
+def _extract_id_one_text(text: etree._Element) -> list[Any]:
     # the same id in one <text> only means one link to the resource
     all_links = set()
     for ele in text.iterdescendants():
--- test/unittests/test_analyse_xml_data/test_extract_links_from_XML.py
+++ test/unittests/test_analyse_xml_data/test_extract_links_from_XML.py
@@ -4,7 +4,11 @@ import pytest
 from lxml import etree
 from pytest_unordered import unordered
 
-from dsp_tools.analyse_xml_data.extract_links_from_XML import _extract_id_one_text, _extract_id_one_text_prop
+from dsp_tools.analyse_xml_data.extract_links_from_XML import (
+    _extract_id_one_text,
+    _extract_id_one_text_prop,
+    _get_all_text_prop_one_resource,
+)
 
 
 def test__extract_id_one_text_with_one_id() -> None:
@@ -23,7 +27,7 @@ def test_extract_id_one_text_with_iri() -> None:
         'href="http://rdfh.ch/0801/RDE7_KU1STuDhHnGr5uu0g">res_A_11</a></text>'
     )
     res = _extract_id_one_text(test_ele)
-    expected = []
+    expected = []  # type: ignore[var-annotated]
     assert expected == res
 
 
@@ -46,6 +50,19 @@ def test_extract_id_one_text_prop_with_several_text_links() -> None:
         'class="salsah-link" href="IRI:res_B_18:IRI">res_B_18</a></text></text-prop>'
     )
     res = _extract_id_one_text_prop(test_ele)
+    expected = ["res_A_18", "res_B_18"]
+    assert expected == unordered(res)
+
+
+def test_get_all_text_prop_one_resource() -> None:
+    test_ele = etree.fromstring(
+        '<resource xmlns="https://dasch.swiss/schema" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" '
+        'label="res_C_18" restype=":TestThing" id="res_C_18" permissions="res-default"><text-prop '
+        'name=":hasRichtext"><text permissions="prop-default" encoding="xml"><a class="salsah-link" '
+        'href="IRI:res_A_18:IRI">res_A_18</a></text><text permissions="prop-default" encoding="xml"><a '
+        'class="salsah-link" href="IRI:res_B_18:IRI">res_B_18</a></text></text-prop></resource>'
+    )
+    res = _get_all_text_prop_one_resource(test_ele)
     expected = ["res_A_18", "res_B_18"]
     assert expected == unordered(res)
 

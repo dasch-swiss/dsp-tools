e21966d8724ecd94486b211b4055077693d77422
Balduin Landolt <33053745+BalduinLandolt@users.noreply.github.com>
2023-10-13 23:04:12 +0200

    Update extract_links_from_XML.py

--- src/dsp_tools/analyse_xml_data/extract_links_from_XML.py
+++ src/dsp_tools/analyse_xml_data/extract_links_from_XML.py
@@ -1,12 +1,13 @@
 from itertools import chain
 
 import regex
+import rustworkx as rx
 from lxml import etree
 
-from dsp_tools.analyse_xml_data.models_xml_to_graph import ResptrLink, TripleGraph, XMLLink
+from dsp_tools.analyse_xml_data.models_xml_to_graph import ResptrLink, XMLLink
 
 
-def create_classes_from_root(root: etree._Element) -> list[ResptrLink] and list[XMLLink] and set[str]:
+def create_classes_from_root(root: etree._Element) -> tuple[list[ResptrLink], list[XMLLink], set[str]]:
     resptr_instances = []
     xml_instances = []
     all_link_ids = []
@@ -23,7 +24,7 @@ def create_classes_from_root(root: etree._Element) -> list[ResptrLink] and list[
 
 def _create_classes_single_resource(
     resource: etree._Element,
-) -> list[ResptrLink] | None and list[XMLLink] | None and list[str] | None:
+) -> tuple[list[ResptrLink] | None, list[XMLLink] | None, list[str] | None]:
     subject_id = resource.attrib.get("id")
     all_used_ids = [subject_id]
     resptr_links, xml_links = _get_all_links_one_resource(resource)
@@ -105,3 +106,60 @@ def _extract_id_one_text(text: etree._Element) -> set[str] | None:
         return None
     else:
         return all_links
+
+
+def make_graph(
+    resptr_instances: list[ResptrLink], xml_instances: list[XMLLink], all_link_ids: set[str]
+) -> rx.PyDiGraph:
+    g = rx.PyDiGraph()
+    nodes = [(id_, None, None) for id_ in all_link_ids]
+    node_ids = [x[0] for x in nodes]
+    node_inidices = g.add_nodes_from(nodes)
+    lookup = dict(zip(node_ids, node_inidices))
+    print(f"number of nodes: {len(nodes)}")
+    resptr_edges = [(lookup[x.subject_id], lookup[x.object_id], 1) for x in resptr_instances]
+    g.add_edges_from(resptr_edges)
+    print(f"number of resptr edges: {len(resptr_edges)}")
+    xml_edges = []
+    for xml in xml_instances:
+        xml_edges.extend([(lookup[xml.subject_id], lookup[x], xml.cost_links) for x in xml.object_link_ids])
+    g.add_edges_from(xml_edges)
+    print(f"number of xml edges: {len(xml_edges)}")
+    return g
+
+
+def find_cheapest_link(g: rx.PyDiGraph, cycle: rx.EdgeList) -> tuple[int, int, float]:
+    costs = []
+    for source, target in cycle:
+        edges_in = g.in_edges(source)
+        node_gain = len(edges_in)
+        edges_out = g.out_edges(source)
+        node_cost = sum([x[2] for x in edges_out])
+        node_value = node_cost / node_gain
+        costs.append((source, target, node_value))
+    out_going_link_to_remove = sorted(costs, key=lambda x: x[2])[0]
+    return out_going_link_to_remove
+
+
+def main() -> None:
+    tree = etree.parse("testdata/xml-data/circular-references/test_circular_references_1.xml")
+    root = tree.getroot()
+    resptr_instances, xml_instances, all_link_ids = create_classes_from_root(root)
+    print(f"number of node ids: {len(all_link_ids)}")
+    print(f"number of resptr instances: {len(resptr_instances)}")
+    print(f"number of xml instances: {len(xml_instances)}")
+    print("-" * 20)
+    g = make_graph(resptr_instances, xml_instances, all_link_ids)
+    print("-" * 20)
+    removed_edges = []
+    while cycle := rx.digraph_find_cycle(g):
+        print(f"cycle: {cycle}")
+        link = find_cheapest_link(g, cycle)
+        source, target, _ = link
+        g.remove_edge(source, target)
+        removed_edges.append(link)
+    print(removed_edges)
+
+
+if __name__ == "__main__":
+    main()

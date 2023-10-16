2272237734dc604c989b984d08c979e881e91525
Balduin Landolt <33053745+BalduinLandolt@users.noreply.github.com>
2023-10-16 08:32:33 +0200

    continue network examination (#569)
    Co-authored-by: Nora-Olivia-Ammann <103038637+Nora-Olivia-Ammann@users.noreply.github.com>

--- src/dsp_tools/analyse_xml_data/extract_links_from_XML.py
+++ src/dsp_tools/analyse_xml_data/extract_links_from_XML.py
@@ -1,12 +1,14 @@
 from itertools import chain
 
 import regex
+import rustworkx as rx
 from lxml import etree
 
-from dsp_tools.analyse_xml_data.models_xml_to_graph import ResptrLink, TripleGraph, XMLLink
+from dsp_tools.analyse_xml_data.models_xml_to_graph import ResptrLink, UploadResource, XMLLink
 
 
-def create_classes_from_root(root: etree._Element) -> list[ResptrLink] and list[XMLLink] and set[str]:
+def create_classes_from_root(root: etree._Element) -> tuple[list[ResptrLink], list[XMLLink], set[str]]:
+    """Create instances of the classes ResptrLink and XMLLink from the root of the XML file."""
     resptr_instances = []
     xml_instances = []
     all_link_ids = []
@@ -23,85 +25,145 @@ def create_classes_from_root(root: etree._Element) -> list[ResptrLink] and list[
 
 def _create_classes_single_resource(
     resource: etree._Element,
-) -> list[ResptrLink] | None and list[XMLLink] | None and list[str] | None:
+) -> tuple[list[ResptrLink] | None, list[XMLLink] | None, list[str] | None]:
     subject_id = resource.attrib.get("id")
+    if not subject_id:
+        return None, None, None
     all_used_ids = [subject_id]
     resptr_links, xml_links = _get_all_links_one_resource(resource)
+    resptr_link_objects = []
+    xml_link_objects = []
     if resptr_links:
         all_used_ids.extend(resptr_links)
-        weight_dict = _make_weighted_resptr_links(resptr_links)
-        resptr_links = [ResptrLink(subject_id=subject_id, object_id=k, edge_weight=v) for k, v in weight_dict.items()]
+        resptr_link_objects = [ResptrLink(subject_id, object_id) for object_id in resptr_links]
     if xml_links:
         all_used_ids.extend(chain.from_iterable(xml_links))
-        xml_links = [XMLLink(subject_id=subject_id, object_link_ids=x) for x in xml_links]
-    return resptr_links, xml_links, all_used_ids
+        xml_link_objects = [XMLLink(subject_id, x) for x in xml_links]
+    return resptr_link_objects, xml_link_objects, all_used_ids
 
 
-def _make_weighted_resptr_links(resptr_links: list[str]) -> dict[str, int]:
-    weight_dict = {link: 0 for link in set(resptr_links)}
-    for link in resptr_links:
-        weight_dict[link] += 1
-    return weight_dict
-
-
-def _get_all_links_one_resource(resource: etree._Element) -> list[str] | None and list[set[str]] | None:
-    resptr_links = []
-    xml_links = []
+def _get_all_links_one_resource(resource: etree._Element) -> tuple[list[str], list[set[str]]]:
+    resptr_links: list[str] = []
+    xml_links: list[set[str]] = []
     for prop in resource.getchildren():
         match prop.tag:
             case "{https://dasch.swiss/schema}resptr-prop":
-                links = _extract_id_one_resptr_prop(prop)
-                match links:
-                    case list():
-                        resptr_links.extend(links)
-                    case None:
-                        continue
+                resptr_links.extend(_extract_id_one_resptr_prop(prop))
             case "{https://dasch.swiss/schema}text-prop":
-                links = _extract_id_one_text_prop(prop)
-                match links:
-                    case list():
-                        xml_links.extend(links)
-                    case None:
-                        continue
-    if len(resptr_links) == 0:
-        resptr_links = None
-    if len(xml_links) == 0:
-        xml_links = None
+                xml_links.extend(_extract_id_one_text_prop(prop))
     return resptr_links, xml_links
 
 
 def _extract_id_one_resptr_prop(resptr_prop: etree._Element) -> list[str]:
-    return [x.text for x in resptr_prop.getchildren()]
+    return [x.text for x in resptr_prop.getchildren() if x.text]
 
 
-def _extract_id_one_text_prop(text_prop: etree._Element) -> list[set[str]] | None:
+def _extract_id_one_text_prop(text_prop: etree._Element) -> list[set[str]]:
     # the same ID is in several separate <text> in one <text-prop> are considered separate links
     xml_props = []
     for text in text_prop.getchildren():
         links = _extract_id_one_text(text)
-        match links:
-            case set():
-                xml_props.append(links)
-            case None:
-                continue
-    if len(xml_props) == 0:
-        return None
-    else:
-        return xml_props
+        if links:
+            xml_props.append(links)
+    return xml_props
 
 
-def _extract_id_one_text(text: etree._Element) -> set[str] | None:
+def _extract_id_one_text(text: etree._Element) -> set[str]:
     # the same id in one <text> only means one link to the resource
     all_links = set()
     for ele in text.iterdescendants():
         if href := ele.attrib.get("href"):
             searched = regex.search(r"IRI:(.*):IRI", href)
-            match searched:
-                case regex.Match():
-                    all_links.add(searched.group(1))
-                case None:
-                    continue
-    if len(all_links) == 0:
-        return None
-    else:
-        return all_links
+            if searched:
+                all_links.add(searched.group(1))
+    return all_links
+
+
+def _make_graph(
+    resptr_instances: list[ResptrLink], xml_instances: list[XMLLink], all_link_ids: set[str]
+) -> rx.PyDiGraph:  # type: ignore[type-arg] # pylint: disable=no-member
+    g: rx.PyDiGraph = rx.PyDiGraph()  # type: ignore[type-arg] # pylint: disable=no-member
+    nodes = [(id_, None, None) for id_ in all_link_ids]
+    node_ids = [x[0] for x in nodes]
+    node_inidices = g.add_nodes_from(nodes)
+    lookup = dict(zip(node_ids, node_inidices))
+    print(f"number of nodes: {len(nodes)}")
+    resptr_edges = [(lookup[x.subject_id], lookup[x.object_id], 1) for x in resptr_instances]
+    g.add_edges_from(resptr_edges)
+    print(f"number of resptr edges: {len(resptr_edges)}")
+    xml_edges = []
+    for xml in xml_instances:
+        xml_edges.extend([(lookup[xml.subject_id], lookup[x], xml.cost_links) for x in xml.object_link_ids])
+    g.add_edges_from(xml_edges)
+    print(f"number of xml edges: {len(xml_edges)}")
+    return g
+
+
+def _remove_leaf_nodes(g: rx.PyDiGraph) -> list[UploadResource]:  # type: ignore[type-arg] # pylint: disable=no-member
+    res: list[UploadResource] = []
+    while leaf_nodes := [x for x in g.node_indexes() if g.out_degree(x) == 0]:
+        print(f"number of leaf nodes removed: {len(leaf_nodes)}")
+        res.extend(UploadResource(g[n][0]) for n in leaf_nodes)
+        g.remove_nodes_from(leaf_nodes)
+    return res
+
+
+def _find_cheapest_node(
+    g: rx.PyDiGraph, cycle: rx.EdgeList  # type: ignore[type-arg] # pylint: disable=no-member
+) -> tuple[int, list[str]]:
+    costs = []
+    for source, _ in cycle:
+        edges_in = g.in_edges(source)
+        node_gain = len(edges_in)
+        edges_out = g.out_edges(source)
+        node_cost = sum(x[2] for x in edges_out)
+        node_value = node_cost / node_gain
+        costs.append((source, node_value, edges_out))
+    sorted_nodes = sorted(costs, key=lambda x: x[1])
+    cheapest_node, _, edges_out = sorted_nodes[0]
+    removed_target_ids = [g[x[1]][0] for x in edges_out]
+    return cheapest_node, removed_target_ids
+
+
+def _generate_upload_order(g: rx.PyDiGraph) -> list[UploadResource]:  # type: ignore[type-arg] # pylint: disable=no-member
+    removed_nodes = []
+    leaf_nodes = _remove_leaf_nodes(g)
+    removed_nodes.extend(leaf_nodes)
+    while g.num_nodes():
+        print(f"total number of nodes remaining: {g.num_nodes()}")
+        cycle = rx.digraph_find_cycle(g)  # type: ignore[attr-defined]  # pylint: disable=no-member
+        print("-" * 10)
+        print(f"cycle: {cycle}")
+        node = _find_cheapest_node(g, cycle)
+        source, targets = node
+        removed_nodes.append(UploadResource(g[source][0], targets))
+        g.remove_node(source)
+        print(f"removed link: {node}")
+        leaf_nodes = _remove_leaf_nodes(g)
+        removed_nodes.extend(leaf_nodes)
+    return removed_nodes
+
+
+def main() -> None:
+    """run the script"""
+    # - remove main() before merging to main branch
+    # - remove al print() statements before merging to main branch
+    print("-" * 20)
+    tree = etree.parse("testdata/xml-data/circular-references/test_circular_references_1.xml")
+    root = tree.getroot()
+    resptr_instances, xml_instances, all_link_ids = create_classes_from_root(root)
+    print(f"number of node ids: {len(all_link_ids)}")
+    print(f"number of resptr instances: {len(resptr_instances)}")
+    print(f"number of xml instances: {len(xml_instances)}")
+    print("-" * 20)
+    g = _make_graph(resptr_instances, xml_instances, all_link_ids)
+    print("=" * 80)
+    removed_nodes = _generate_upload_order(g)
+    print("=" * 80)
+    for n in removed_nodes:
+        print(n)
+    print("=" * 80)
+
+
+if __name__ == "__main__":
+    main()
--- src/dsp_tools/analyse_xml_data/models_xml_to_graph.py
+++ src/dsp_tools/analyse_xml_data/models_xml_to_graph.py
@@ -1,46 +1,37 @@
 from __future__ import annotations
 
-import uuid
 from dataclasses import dataclass, field
-from typing import Optional
-from uuid import UUID
-
-
-@dataclass
-class TripleGraph:
-    """
-    Contains all classes that express links between resources.
-    """
-
-    resptr_links: list[ResptrLink]
-    xml_links: list[XMLLink]
 
 
 @dataclass
 class ResptrLink:
-    """
-    This class represents a link between two resources.
-    """
+    """This class represents a link between two resources."""
 
     subject_id: str
     object_id: str
-    rx_edge_index: Optional[int] = None
-    edge_weight: float = 1
 
 
 @dataclass
 class XMLLink:
-    """
-    This class represents a link between a resource and an XMl text
-    which contains links to other resources.
-    """
+    """This class represents a link between a resource and an XMl text which contains links to other resources."""
 
     subject_id: str
     object_link_ids: set[str]
-    rx_edge_index: Optional[int] = None
-    edge_weight: float = 1
-    reified_object_id: UUID = field(init=False, default_factory=uuid.uuid4)
 
     @property
-    def cost_links(self):
+    def cost_links(self) -> float:
+        """The cost of this outgoing link (1 / number of links in the XML text)"""
         return 1 / len(self.object_link_ids)
+
+
+@dataclass
+class UploadResource:
+    """
+    Holds information about a resource that can be uploaded to the DSP.
+
+    May hold information about the links that need to be stashed from this resource before it can be uploaded.
+    A ordered list of UploadResources can be used to determine the order in which resources need to be uploaded.
+    """
+
+    res_id: str
+    stash_links_to: list[str] = field(default_factory=list)

4c5c7923b96db901e09e6816d49ff30cbd3f3160
Nora-Olivia-Ammann <103038637+Nora-Olivia-Ammann@users.noreply.github.com>
2023-10-16 16:58:03 +0200

    Update src/dsp_tools/analyse_xml_data/extract_links_from_XML.py
    Co-authored-by: Johannes Nussbaum <39048939+jnussbaum@users.noreply.github.com>

--- src/dsp_tools/analyse_xml_data/extract_links_from_XML.py
+++ src/dsp_tools/analyse_xml_data/extract_links_from_XML.py
@@ -61,7 +61,7 @@ def _extract_ids_from_one_resptr_prop(resptr_prop: etree._Element) -> list[str]:
 
 
 def _extract_id_one_text_prop(text_prop: etree._Element) -> list[set[str]]:
-    # the same ID is in several separate <text> in one <text-prop> are considered separate links
+    # if the same ID is in several separate <text> values of one <text-prop>, they are considered separate links
     xml_props = []
     for text in text_prop.getchildren():
         links = _extract_id_one_text(text)

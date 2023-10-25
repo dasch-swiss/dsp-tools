37a613cd69129f1cf0c52fb4a0651f100c459b21
Balduin Landolt <33053745+BalduinLandolt@users.noreply.github.com>
2023-10-25 20:26:33 +0200

    Update create_resource.py

--- src/dsp_tools/utils/xmlupload/create_resource.py
+++ src/dsp_tools/utils/xmlupload/create_resource.py
@@ -1,10 +1,12 @@
 import json
 import sys
-from typing import Any
+from typing import Any, assert_never
 
 from dsp_tools.connection.connection import Connection
 from dsp_tools.models.permission import Permissions
+from dsp_tools.models.value import KnoraStandoffXml
 from dsp_tools.models.xmlresource import BitstreamInfo, XMLResource
+from dsp_tools.models.xmlvalue import XMLValue
 from dsp_tools.utils.xmlupload.ark2iri import convert_ark_v0_to_resource_iri
 
 
@@ -18,7 +20,6 @@ def actually_crearte_resource(
     project_iri: str,
 ) -> tuple[str, str]:
     print("-" * 120)
-    print(resource)
     res = _make_resource(
         resource=resource,
         bitstream_information=bitstream_information,
@@ -26,12 +27,13 @@ def actually_crearte_resource(
         json_ld_context=json_ld_context,
         project_iri=project_iri,
     )
+    vals = _make_values(resource, id2iri_mapping, permissions_lookup)
+    res.update(vals)
     print(json.dumps(res, indent=2))
-    # TODO:
-    # - turn bitstream into something useful with the bitstream info object
-    # - add bitstream to json-ld object
     print("-" * 120)
-    sys.exit(0)
+    # TODO: upload resource
+    # sys.exit(0)
+    return "TODO", "TODO"
 
 
 def _make_resource(
@@ -61,7 +63,6 @@ def _make_resource(
         }
     if bitstream_information:
         res.update(_make_bitstream_file_value(bitstream_information))
-    # TODO: add bitstream
     return res
 
 
@@ -90,3 +91,42 @@ def _make_bitstream_file_value(bitstream_info: BitstreamInfo) -> dict[str, Any]:
             print(f"!!! No idea how to handle this file type: .{file_ending} !!!")
             print(f"({bitstream_info.local_file} - {bitstream_info.internal_file_name})")
             sys.exit(-1)
+
+
+def _make_values(
+    resource: XMLResource,
+    id2iri_mapping: dict[str, str],
+    permissions_lookup: dict[str, Permissions],
+) -> dict[str, Any]:
+    values: dict[str, Any] = {}
+    for prop in resource.properties:
+        values[prop.name] = [_make_value(v, prop.valtype) for v in prop.values]
+    return values
+
+
+def _make_value(value: XMLValue, value_type: str) -> dict[str, Any]:
+    match value_type:
+        case "text":
+            return _make_text_value(value)
+        case _:
+            print(f"!!! No idea how to handle this value type: {value_type} !!!")
+            sys.exit(-1)
+
+
+def _make_text_value(value: XMLValue) -> dict[str, Any]:
+    match value.value:
+        case str() as s:
+            return {
+                "@type": "knora-api:TextValue",
+                "knora-api:valueAsString": s,
+            }
+        case KnoraStandoffXml() as xml:
+            return {
+                "@type": "knora-api:TextValue",
+                "knora-api:valueAsXml": str(xml),
+                "knora-api:textValueHasMapping": {
+                    "@id": "http://rdfh.ch/standoff/mappings/StandardMapping",
+                },
+            }
+        case _:
+            assert_never(value.value)

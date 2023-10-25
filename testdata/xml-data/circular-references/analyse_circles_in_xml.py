f07b5aa8f054cc5645af6f09e2f96d9cdbb00b2d
Johannes Nussbaum <johannes.nussbaum@dasch.swiss>
2023-10-25 17:49:04 +0200

    Revert "move contents of analyse_xml_data --> utils.xmlupload/stash"
    This reverts commit 9e0ff32d7b7572a0e88d028973ccaadd2311deea.

--- testdata/xml-data/circular-references/analyse_circles_in_xml.py
+++ testdata/xml-data/circular-references/analyse_circles_in_xml.py
@@ -4,7 +4,7 @@ from pathlib import Path
 from lxml import etree
 from viztracer import VizTracer
 
-from dsp_tools.utils.xmlupload.stash.construct_and_analyze_graph import (
+from dsp_tools.analyse_xml_data.construct_and_analyze_graph import (
     create_info_from_xml_for_graph,
     generate_upload_order,
     make_graph,

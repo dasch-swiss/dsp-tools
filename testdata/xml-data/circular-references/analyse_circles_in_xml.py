20761401d14e8dc3506dd796c2eca4ebee49bba5
Johannes Nussbaum <johannes.nussbaum@dasch.swiss>
2023-10-26 09:29:50 +0200

    consider special tags!

--- testdata/xml-data/circular-references/analyse_circles_in_xml.py
+++ testdata/xml-data/circular-references/analyse_circles_in_xml.py
@@ -1,7 +1,6 @@
 from datetime import datetime
 from pathlib import Path
 
-from lxml import etree
 from viztracer import VizTracer
 
 from dsp_tools.analyse_xml_data.construct_and_analyze_graph import (
@@ -9,6 +8,7 @@ from dsp_tools.analyse_xml_data.construct_and_analyze_graph import (
     generate_upload_order,
     make_graph,
 )
+from dsp_tools.utils.xml_utils import parse_and_clean_xml_file
 
 
 def analyse_circles_in_data(xml_filepath: Path, tracer_output_file: str, save_tracer: bool = False) -> None:
@@ -30,8 +30,7 @@ def analyse_circles_in_data(xml_filepath: Path, tracer_output_file: str, save_tr
         max_stack_depth=3,
     )
     tracer.start()
-    tree = etree.parse(xml_filepath)
-    root = tree.getroot()
+    root = parse_and_clean_xml_file(xml_filepath)
     resptr_links, xml_links, all_resource_ids = create_info_from_xml_for_graph(root)
     print(f"Total Number of Resources: {len(all_resource_ids)}")
     print(f"Total Number of resptr Links: {len(resptr_links)}")
@@ -39,10 +38,10 @@ def analyse_circles_in_data(xml_filepath: Path, tracer_output_file: str, save_tr
     print("=" * 80)
     graph, node_to_id, edges = make_graph(resptr_links, xml_links, all_resource_ids)
     _, _, stash_counter = generate_upload_order(graph, node_to_id, edges)
-    print("Number of Links Stashed:", stash_counter)
     tracer.stop()
     if save_tracer:
         tracer.save(output_file=tracer_output_file)
+    print("Number of Links Stashed:", stash_counter)
     print("=" * 80)
     print("Start time:", start)
     print("End time:", datetime.now())

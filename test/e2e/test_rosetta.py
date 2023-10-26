24facdb50258d609b11c7b65174db988e51d362a
Johannes Nussbaum <johannes.nussbaum@dasch.swiss>
2023-10-26 17:35:19 +0200

    test out

--- test/e2e/test_rosetta.py
+++ test/e2e/test_rosetta.py
@@ -1,34 +1,56 @@
-# pylint: disable=protected-access,missing-class-docstring,missing-function-docstring
+# pylint: disable=protected-access,missing-function-docstring,redefined-outer-name
 
 import shutil
-import unittest
 from pathlib import Path
 
 import pytest
 
 from dsp_tools.utils import rosetta
 
+# @pytest.fixture
+# def setup_teardown():
+#     print("setup")
+#     yield
+#     print("teardown")
 
-class TestRosetta(unittest.TestCase):
-    def test_rosetta(self) -> None:
-        enclosing_folder = Path("tmp/.dsp-tools/rosetta")
-        enclosing_folder.mkdir(parents=True, exist_ok=True)
-        rosetta_folder = enclosing_folder / "082E-rosetta-scripts"
-        shutil.rmtree(rosetta_folder, ignore_errors=True)
+# @pytest.fixture
+# def temporary_dir() -> Path:
+#     with tempfile.TemporaryDirectory() as tmpdirname:
+#         yield Path(tmpdirname)
 
-        is_rosetta_up_to_date = rosetta._update_possibly_existing_repo(rosetta_folder=rosetta_folder)
-        self.assertFalse(is_rosetta_up_to_date)
 
-        rosetta._clone_repo(rosetta_folder=rosetta_folder, enclosing_folder=enclosing_folder)
-        is_rosetta_up_to_date = rosetta._update_possibly_existing_repo(rosetta_folder=rosetta_folder)
-        self.assertTrue(is_rosetta_up_to_date)
+@pytest.fixture
+def enclosing_folder() -> Path:
+    return Path("tmp/.dsp-tools/rosetta")
 
-        success1 = rosetta._create_json(rosetta_folder=rosetta_folder)
-        self.assertTrue(success1)
-        success2 = rosetta._upload_xml(rosetta_folder=rosetta_folder)
-        self.assertTrue(success2)
 
-        shutil.rmtree("tmp", ignore_errors=True)
+@pytest.fixture
+def rosetta_folder(enclosing_folder: Path) -> Path:
+    return enclosing_folder / "082E-rosetta-scripts"
+
+
+def test_repo_does_not_exist(enclosing_folder: Path, rosetta_folder: Path) -> None:
+    enclosing_folder.mkdir(parents=True, exist_ok=True)
+    shutil.rmtree(rosetta_folder, ignore_errors=True)
+    is_rosetta_up_to_date = rosetta._update_possibly_existing_repo(rosetta_folder=rosetta_folder)
+    assert not is_rosetta_up_to_date
+
+
+def test_update_repo(enclosing_folder: Path, rosetta_folder: Path) -> None:
+    rosetta._clone_repo(rosetta_folder=rosetta_folder, enclosing_folder=enclosing_folder)
+    is_rosetta_up_to_date = rosetta._update_possibly_existing_repo(rosetta_folder=rosetta_folder)
+    assert is_rosetta_up_to_date
+
+
+def test_create_data_model(rosetta_folder: Path) -> None:
+    success1 = rosetta._create_json(rosetta_folder=rosetta_folder)
+    assert success1
+
+
+def test_upload_data(rosetta_folder: Path) -> None:
+    success2 = rosetta._upload_xml(rosetta_folder=rosetta_folder)
+    assert success2
+    shutil.rmtree("tmp", ignore_errors=True)
 
 
 if __name__ == "__main__":

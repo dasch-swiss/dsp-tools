be09bdd38670f08a3764b4ed5ef0caf66f605032
Johannes Nussbaum <johannes.nussbaum@dasch.swiss>
2023-10-26 20:04:07 +0200

    improve

--- test/e2e/test_rosetta.py
+++ test/e2e/test_rosetta.py
@@ -10,35 +10,30 @@ from dsp_tools.utils import rosetta
 
 
 @pytest.fixture(scope="module", autouse=True)
-def temporary_dir() -> Iterator[Path]:
+def rosetta_folder() -> Iterator[Path]:
     with tempfile.TemporaryDirectory() as tmpdirname:
-        yield Path(tmpdirname)
+        yield Path(tmpdirname) / "082E-rosetta-scripts"
 
 
-@pytest.fixture
-def rosetta_folder(temporary_dir: Path) -> Path:
-    return temporary_dir / "082E-rosetta-scripts"
-
-
-def test_repo_does_not_exist(rosetta_folder: Path) -> None:
+def test_update_repo_before_cloning(rosetta_folder: Path) -> None:
     is_rosetta_up_to_date = rosetta._update_possibly_existing_repo(rosetta_folder=rosetta_folder)
     assert not is_rosetta_up_to_date
 
 
-def test_update_repo(rosetta_folder: Path) -> None:
+def test_update_repo_after_cloning(rosetta_folder: Path) -> None:
     rosetta._clone_repo(rosetta_folder=rosetta_folder, enclosing_folder=rosetta_folder.parent)
     is_rosetta_up_to_date = rosetta._update_possibly_existing_repo(rosetta_folder=rosetta_folder)
     assert is_rosetta_up_to_date
 
 
 def test_create_data_model(rosetta_folder: Path) -> None:
-    success1 = rosetta._create_json(rosetta_folder=rosetta_folder)
-    assert success1
+    success = rosetta._create_json(rosetta_folder=rosetta_folder)
+    assert success
 
 
 def test_upload_data(rosetta_folder: Path) -> None:
-    success2 = rosetta._upload_xml(rosetta_folder=rosetta_folder)
-    assert success2
+    success = rosetta._upload_xml(rosetta_folder=rosetta_folder)
+    assert success
 
 
 if __name__ == "__main__":

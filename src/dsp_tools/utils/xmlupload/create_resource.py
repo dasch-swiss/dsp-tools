7970c345910ab3ebec731ba8043a5e40cabc05d4
Balduin Landolt <33053745+BalduinLandolt@users.noreply.github.com>
2023-10-28 11:17:40 +0200

    Update create_resource.py

--- src/dsp_tools/utils/xmlupload/create_resource.py
+++ src/dsp_tools/utils/xmlupload/create_resource.py
@@ -3,6 +3,8 @@ import sys
 from pathlib import Path
 from typing import Any, assert_never
 
+import regex
+
 from dsp_tools.connection.connection import Connection
 from dsp_tools.models.exceptions import UserError
 from dsp_tools.models.permission import Permissions
@@ -139,23 +141,37 @@ def _make_values(
     values: dict[str, Any] = {}
     for prop in resource.properties:
         values[prop.name] = [_make_value(v, prop.valtype) for v in prop.values]
+    # TODO: comments
+    # TODO: permissions
     return values
 
 
 def _make_value(value: XMLValue, value_type: str) -> dict[str, Any]:
     match value_type:
+        case "boolean":
+            return _make_boolean_value(value)
         case "color":
             return _make_color_value(value)
+        case "date":
+            return _make_date_value(value)
+        case "decimal":
+            return _make_decimal_value(value)
         case "geometry":
             return _make_geometry_value(value)
+        case "geoname":
+            return _make_geoname_value(value)
         case "integer":
             return _make_integer_value(value)
         case "interval":
             return _make_interval_value(value)
         case "resptr":
             return _make_link_value(value)
+        case "list":
+            return _make_list_value(value)
         case "text":
             return _make_text_value(value)
+        case "time":
+            return _make_time_value(value)
         case "uri":
             return _make_uri_value(value)
         case _:
@@ -163,10 +179,83 @@ def _make_value(value: XMLValue, value_type: str) -> dict[str, Any]:
             sys.exit(-1)
 
 
+def _make_boolean_value(value: XMLValue) -> dict[str, Any]:
+    return {
+        "@type": "knora-api:BooleanValue",
+        "knora-api:booleanValueAsBoolean": bool(value.value),
+    }
+
+
 def _make_color_value(value: XMLValue) -> dict[str, Any]:
     return {
         "@type": "knora-api:ColorValue",
         "knora-api:colorValueAsColor": value.value,
+    }
+
+
+def _make_date_value(value: XMLValue) -> dict[str, Any]:
+    entire_pattern = r"""
+    ^
+    (?:(GREGORIAN|JULIAN|ISLAMIC):)?        # optional calendar
+    (?:(CE|BCE|BC|AD):)?                    # optional era
+    (\d{4}(?:-\d{1,2})?(?:-\d{1,2})?)       # date
+    (?::(CE|BCE|BC|AD))?                    # optional era
+    (?::(\d{4}(?:-\d{1,2})?(?:-\d{1,2})?))? # optional date
+    $
+"""
+    assert isinstance(value.value, str)
+    date_match = regex.search(entire_pattern, value.value, flags=regex.VERBOSE)
+    assert date_match
+    date_groups = date_match.groups()
+    match date_groups:
+        case (str() | None, str() | None, str(), str() | None, str() | None):
+            pass
+        case ("ISLAMIC", era, _, end_era, _) if era or end_era:
+            raise UserError(f"ISLAMIC calendar does not support eras: {value.value}")
+        case _:
+            raise UserError(f"Could not parse date: {value.value}")
+
+    res: dict[str, Any] = {"@type": "knora-api:DateValue"}
+    calendar, start_era, start_date, end_era, end_date = date_groups
+
+    year, month, day = _parse_date(start_date)
+    res["knora-api:dateValueHasStartYear"] = int(year)
+    if month:
+        res["knora-api:dateValueHasStartMonth"] = int(month)
+    if day:
+        res["knora-api:dateValueHasStartDay"] = int(day)
+    if end_era:
+        res["knora-api:dateValueHasEndEra"] = end_era
+
+    if calendar:
+        res["knora-api:dateValueHasCalendar"] = calendar
+    if start_era:
+        res["knora-api:dateValueHasStartEra"] = start_era
+    if end_date:
+        year, month, day = _parse_date(end_date)
+        res["knora-api:dateValueHasEndYear"] = int(year)
+        if month:
+            res["knora-api:dateValueHasEndMonth"] = int(month)
+        if day:
+            res["knora-api:dateValueHasEndDay"] = int(day)
+    return res
+
+
+def _parse_date(date: str) -> tuple[str, str | None, str | None]:
+    date_pattern = r"(\d{4})(?:-(\d{1,2}))?(?:-(\d{1,2}))?"
+    date_match = regex.search(date_pattern, date)
+    assert date_match
+    match date_match.groups():
+        case (year, month, day):
+            assert year
+            return year, month, day
+    raise UserError(f"Could not parse date: {date}")
+
+
+def _make_decimal_value(value: XMLValue) -> dict[str, Any]:
+    return {
+        "@type": "knora-api:DecimalValue",
+        "knora-api:decimalValueAsDecimal": value.value,  # XXX
     }
 
 
@@ -177,10 +266,17 @@ def _make_geometry_value(value: XMLValue) -> dict[str, Any]:
     }
 
 
+def _make_geoname_value(value: XMLValue) -> dict[str, Any]:
+    return {
+        "@type": "knora-api:GeonameValue",
+        "knora-api:geonameValueHasGeoname": value.value,  # XXX
+    }
+
+
 def _make_integer_value(value: XMLValue) -> dict[str, Any]:
     return {
         "@type": "knora-api:IntValue",
-        "knora-api:intValueAsInt": value.value,
+        "knora-api:intValueAsInt": value.value,  # XXX
     }
 
 
@@ -212,6 +308,13 @@ def _make_link_value(value: XMLValue) -> dict[str, Any]:
     }
 
 
+def _make_list_value(value: XMLValue) -> dict[str, Any]:
+    return {
+        "@type": "knora-api:ListValue",
+        "knora-api:listValueAsListNode": {"@id": value.value},  # XXX
+    }
+
+
 def _make_text_value(value: XMLValue) -> dict[str, Any]:
     match value.value:
         case str() as s:
@@ -229,6 +332,13 @@ def _make_text_value(value: XMLValue) -> dict[str, Any]:
             }
         case _:
             assert_never(value.value)
+
+
+def _make_time_value(value: XMLValue) -> dict[str, Any]:
+    return {
+        "@type": "knora-api:TimeValue",
+        "knora-api:timeValueAsTime": value.value,  # XXX
+    }
 
 
 def _make_uri_value(value: XMLValue) -> dict[str, Any]:

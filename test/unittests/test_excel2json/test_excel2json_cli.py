e78c87dc0a80a2e8f440200b1471ae07d54768b7
Johannes Nussbaum <johannes.nussbaum@dasch.swiss>
2023-10-30 10:39:31 +0100

    move excel2json tests into own file

---
+++ test/unittests/test_excel2json/test_excel2json_cli.py
@@ -0,0 +1,51 @@
+import json
+import unittest
+from pathlib import Path
+
+
+class TestExcel2JsonCli(unittest.TestCase):
+    testdata_tmp = Path("testdata/tmp")
+
+    def test_excel_to_json_project(self) -> None:
+        excel_folder = Path("testdata/excel2json/excel2json_files")
+        out_file = self.testdata_tmp / "_out_project.json"
+        self._make_cli_call(f"dsp-tools excel2json {excel_folder.absolute()} {out_file.absolute()}")
+        with open("testdata/excel2json/excel2json-expected-output.json", encoding="utf-8") as f:
+            output_expected = json.load(f)
+        with open(out_file, encoding="utf-8") as f:
+            output = json.load(f)
+        self.assertDictEqual(output, output_expected)
+        out_file.unlink()
+
+    def test_excel_to_json_list(self) -> None:
+        excel_folder = Path("testdata/excel2json/lists-multilingual")
+        out_file = self.testdata_tmp / "_lists-out.json"
+        self._make_cli_call(f"dsp-tools excel2lists {excel_folder.absolute()} {out_file.absolute()}")
+        with open(out_file, encoding="utf-8") as f:
+            output_actual = json.load(f)
+        with open("testdata/excel2json/lists-multilingual-output-expected.json", encoding="utf-8") as f:
+            output_expected = json.load(f)
+        self.assertListEqual(output_actual, output_expected)
+        out_file.unlink()
+
+    def test_excel_to_json_resources(self) -> None:
+        excel_file = Path("testdata/excel2json/excel2json_files/test-name (test_label)/resources.xlsx")
+        out_file = self.testdata_tmp / "_out_resources.json"
+        self._make_cli_call(f"dsp-tools excel2resources '{excel_file.absolute()}' {out_file.absolute()}")
+        with open(out_file, encoding="utf-8") as f:
+            output_actual = json.load(f)
+        with open("testdata/excel2json/resources-output-expected.json", encoding="utf-8") as f:
+            output_expected = json.load(f)
+        self.assertListEqual(output_actual, output_expected)
+        out_file.unlink()
+
+    def test_excel_to_json_properties(self) -> None:
+        excel_file = Path("testdata/excel2json/excel2json_files/test-name (test_label)/properties.xlsx")
+        out_file = self.testdata_tmp / "_out_properties.json"
+        self._make_cli_call(f"dsp-tools excel2properties '{excel_file.absolute()}' {out_file.absolute()}")
+        with open(out_file, encoding="utf-8") as f:
+            output_actual = json.load(f)
+        with open("testdata/excel2json/properties-output-expected.json", encoding="utf-8") as f:
+            output_expected = json.load(f)
+        self.assertListEqual(output_actual, output_expected)
+        out_file.unlink()

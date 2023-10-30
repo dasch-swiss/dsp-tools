e6aa47cfeb70136a1e5107bf49490168a13d5b96
Johannes Nussbaum <39048939+jnussbaum@users.noreply.github.com>
2023-10-30 14:01:06 +0100

    fix: don't create logging handler multiple times (DEV-2891) (#609)

--- src/dsp_tools/utils/create_logger.py
+++ src/dsp_tools/utils/create_logger.py
@@ -2,37 +2,34 @@ import logging
 import logging.handlers
 from pathlib import Path
 
+# the handler must live on module level, so that it is created only once
+_rotating_file_handler: logging.handlers.RotatingFileHandler | None = None
 
-def get_logger(
-    name: str,
-    level: int = logging.INFO,
-    filesize_mb: int = 5,
-    backupcount: int = 4,
-) -> logging.Logger:
+
+def _make_handler(
+    logfile_directory: Path,
+    filesize_mb: int,
+    backupcount: int,
+) -> logging.handlers.RotatingFileHandler:
     """
-    Create a logger instance,
-    set its level to INFO,
-    and configure it to write to a file in the user's home directory.
+    Create a rotating file handler.
+    A RotatingFileHandler fills "filename" until it is "maxBytes" big,
+    then appends ".1" to it and starts with a new file "filename",
+    fills it until it is "maxBytes" big,
+    then appends ".1" to it (replacing the old ".1" file)
 
     Args:
-        name: name of the logger
-        level: logging level, defaults to logging.INFO
-        filesize_mb: maximum size per log file in MB, defaults to 5
-        backupcount: number of log files to keep, defaults to 4
+        logfile_directory: directory to store the logfiles in
+        filesize_mb: maximum size of a logfile in MB
+        backupcount: number of logfiles to keep
 
     Returns:
-        the logger instance
+        handler instance
     """
-    _logger = logging.getLogger(name)
-    _logger.setLevel(level)
-    formatter = logging.Formatter(fmt="{asctime} {filename: <20} {levelname: <8} {message}", style="{")
+    formatter = logging.Formatter(fmt="{asctime} {filename: <30} {levelname: <8} {message}", style="{")
     formatter.default_time_format = "%Y-%m-%d %H:%M:%S"
     formatter.default_msec_format = "%s.%03d"
-    # a RotatingFileHandler fills "filename" until it is "maxBytes" big,
-    # then appends ".1" to it and starts with a new file "filename",
-    # fills it until it is "maxBytes" big,
-    # then appends ".1" to it (replacing the old ".1" file)
-    logfile_directory = Path.home() / Path(".dsp-tools")
+
     logfile_directory.mkdir(exist_ok=True)
     handler = logging.handlers.RotatingFileHandler(
         filename=logfile_directory / "logging.log",
@@ -41,5 +38,32 @@ def get_logger(
         backupCount=backupcount,
     )
     handler.setFormatter(formatter)
-    _logger.addHandler(handler)
-    return _logger
+    return handler
+
+
+def get_logger(
+    name: str,
+    level: int = logging.DEBUG,
+) -> logging.Logger:
+    """
+    Create a logger instance,
+    and configure it to write to a file in the user's home directory.
+
+    Args:
+        name: name of the logger
+        level: logging level, defaults to logging.DEBUG
+
+    Returns:
+        the logger instance
+    """
+    global _rotating_file_handler
+    if not _rotating_file_handler:
+        _rotating_file_handler = _make_handler(
+            Path.home() / Path(".dsp-tools"),
+            filesize_mb=100,
+            backupcount=30,
+        )
+    logger = logging.getLogger(name)
+    logger.setLevel(level)
+    logger.addHandler(_rotating_file_handler)
+    return logger

97a69d2b03ab68b4018e993b5e2d087eab071951
Johannes Nussbaum <johannes.nussbaum@dasch.swiss>
2023-10-31 16:00:55 +0100

    remove test_excel_to_json_list e2e test

--- test/e2e/test_cli.py
+++ test/e2e/test_cli.py
@@ -253,18 +253,6 @@ class TestCLI(unittest.TestCase):
         self.assertDictEqual(output, output_expected)
         out_file.unlink()
 
-    def test_excel_to_json_list(self) -> None:
-        """The unit tests test only if the deepest nested nodes are correct, and if the length of the list is correct"""
-        excel_folder = Path("testdata/excel2json/lists-multilingual")
-        out_file = self.testdata_tmp / "_lists-out.json"
-        self._make_cli_call(f"dsp-tools excel2lists {excel_folder.absolute()} {out_file.absolute()}")
-        with open(out_file, encoding="utf-8") as f:
-            output_actual = json.load(f)
-        with open("testdata/excel2json/lists-multilingual-output-expected.json", encoding="utf-8") as f:
-            output_expected = json.load(f)
-        self.assertListEqual(output_actual, output_expected)
-        out_file.unlink()
-
     def _get_original_project(self) -> dict[str, Any]:
         """
         Open the systematic JSON project file, expand all prefixes, and return the resulting Python object.

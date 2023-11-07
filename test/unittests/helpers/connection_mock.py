02b70692a8ee5baeadd2da4c0f8f144fbb21221e
Balduin Landolt <33053745+BalduinLandolt@users.noreply.github.com>
2023-11-07 17:42:07 +0100

    Update connection_mock.py

--- test/unittests/helpers/connection_mock.py
+++ test/unittests/helpers/connection_mock.py
@@ -12,19 +12,22 @@ class ConnectionMockBase:
     """
 
     def get(self, route: str, headers: dict[str, str] | None = None) -> dict[str, Any]:
-        raise NotImplementedError("GET not implemented in mock")
+        raise AssertionError("GET not implemented in mock")
 
     def put(self, route: str, jsondata: str | None = None, content_type: str = "application/json") -> dict[str, Any]:
-        raise NotImplementedError("PUT not implemented in mock")
+        raise AssertionError("PUT not implemented in mock")
 
     def post(self, route: str, jsondata: str | None = None, content_type: str = "application/json") -> dict[str, Any]:
-        raise NotImplementedError("POST not implemented in mock")
+        raise AssertionError("POST not implemented in mock")
 
     def delete(self, route: str, params: dict[str, Any] | None = None) -> dict[str, Any]:
-        raise NotImplementedError("DELETE not implemented in mock")
+        raise AssertionError("DELETE not implemented in mock")
 
     def get_token(self) -> str:
-        raise NotImplementedError("get_token not implemented in mock")
+        raise AssertionError("get_token not implemented in mock")
 
     def login(self, email: str, password: str) -> None:
-        raise NotImplementedError("login not implemented in mock")
+        raise AssertionError("login not implemented in mock")
+
+    def logout(self) -> None:
+        raise AssertionError("logout not implemented in mock")

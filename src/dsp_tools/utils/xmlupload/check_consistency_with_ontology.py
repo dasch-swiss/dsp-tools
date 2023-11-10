1c3b4d06e2714e968080c5443385f016b51af769
Nora-Olivia-Ammann <103038637+Nora-Olivia-Ammann@users.noreply.github.com>
2023-11-10 14:46:01 +0100

    Create check_consistency_with_ontology.py

---
+++ src/dsp_tools/utils/xmlupload/check_consistency_with_ontology.py
@@ -0,0 +1,30 @@
+from __future__ import annotations
+
+from typing import Any
+
+from dsp_tools.connection.connection import Connection
+from dsp_tools.utils.create_logger import get_logger
+
+logger = get_logger(__name__)
+
+
+def _get_project_ontology_names_from_server(con: Connection, project_shortcode: str) -> list[str]:
+    project_info = con.get(f"/admin/projects/shortcode/{project_shortcode}")
+    ontologies = project_info["project"]["ontologies"]
+    return [x.split("/")[-1] for x in ontologies]
+
+
+def _get_all_project_ontologies_from_server(
+    con: Connection, project_shortcode: str, ontology_names: list[str]
+) -> dict[str, list[dict[str, Any]]]:
+    onto_dict = dict()
+    for onto in ontology_names:
+        onto_dict[onto] = _get_single_ontology_from_server(con, project_shortcode, onto)
+    return onto_dict
+
+
+def _get_single_ontology_from_server(
+    con: Connection, project_shortcode: str, ontology_name: str
+) -> list[dict[str, Any]]:
+    onto = con.get(f"/ontology/{project_shortcode}/{ontology_name}/v2")
+    return onto["@graph"]

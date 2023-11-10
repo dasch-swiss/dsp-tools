1d8d66ef7e41ab4537b1ba2d47d737b65e994167
Nora-Olivia-Ammann <103038637+Nora-Olivia-Ammann@users.noreply.github.com>
2023-11-10 15:07:42 +0100

    Update check_consistency_with_ontology.py

--- src/dsp_tools/utils/xmlupload/check_consistency_with_ontology.py
+++ src/dsp_tools/utils/xmlupload/check_consistency_with_ontology.py
@@ -4,6 +4,7 @@ from typing import Any
 
 from dsp_tools.connection.connection import Connection
 from dsp_tools.utils.create_logger import get_logger
+from dsp_tools.utils.xmlupload.consistency_check_model import ProjectOntology
 
 logger = get_logger(__name__)
 
@@ -23,8 +24,6 @@ def _get_all_project_ontologies_from_server(
     return onto_dict
 
 
-def _get_single_ontology_from_server(
-    con: Connection, project_shortcode: str, ontology_name: str
-) -> list[dict[str, Any]]:
-    onto = con.get(f"/ontology/{project_shortcode}/{ontology_name}/v2")
-    return onto["@graph"]
+def _get_single_ontology_from_server(con: Connection, project_shortcode: str, ontology_name: str) -> ProjectOntology:
+    onto_list: dict[str, list[dict[str, Any]]] = con.get(f"/ontology/{project_shortcode}/{ontology_name}/v2")
+    return ProjectOntology.make(ontology_info=onto_list["@graph"], ontology_name=ontology_name)

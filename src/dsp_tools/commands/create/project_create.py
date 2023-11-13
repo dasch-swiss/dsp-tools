3f35f1dffd4edc7767a16e3366f8e2e519be3052
Johannes Nussbaum <johannes.nussbaum@dasch.swiss>
2023-11-13 09:47:21 +0100

    fix imports

--- src/dsp_tools/commands/create/project_create.py
+++ src/dsp_tools/commands/create/project_create.py
@@ -5,6 +5,8 @@ from typing import Any, Optional, Union, cast
 
 import regex
 
+from dsp_tools.commands.create.project_create_lists import create_lists_on_server
+from dsp_tools.commands.excel2json.lists import expand_lists_from_excel
 from dsp_tools.connection.connection import Connection
 from dsp_tools.models.exceptions import BaseError, UserError
 from dsp_tools.models.group import Group
@@ -16,8 +18,6 @@ from dsp_tools.models.propertyclass import PropertyClass
 from dsp_tools.models.resourceclass import ResourceClass
 from dsp_tools.models.user import User
 from dsp_tools.utils.create_logger import get_logger
-from dsp_tools.utils.excel2json.lists import expand_lists_from_excel
-from dsp_tools.utils.project_create_lists import create_lists_on_server
 from dsp_tools.utils.project_validate import validate_project
 from dsp_tools.utils.shared import login, parse_json_input, try_network_action
 

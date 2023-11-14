61ced6f19cc8df81f39ae335777bbaff876e1c54
Johannes Nussbaum <johannes.nussbaum@dasch.swiss>
2023-11-14 08:52:21 +0100

    move stuff out into excel2xml_lib

--- src/dsp_tools/commands/excel2xml/__init__.py
+++ src/dsp_tools/commands/excel2xml/__init__.py
@@ -1,4 +1,6 @@
-from dsp_tools.commands.excel2xml.excel2xml import *
+# pylint: disable-next=useless-import-alias
+from dsp_tools.commands.excel2xml.excel2xml import excel2xml as excel2xml
+from dsp_tools.commands.excel2xml.excel2xml_lib import *
 
 # pylint: disable-next=useless-import-alias
 from dsp_tools.commands.excel2xml.propertyelement import PropertyElement as PropertyElement

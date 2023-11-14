69136f93a501ba5a5782fefbe9055cfb55733b89
Johannes Nussbaum <johannes.nussbaum@dasch.swiss>
2023-11-14 17:46:23 +0100

    replace class IriTest by iri_util

--- src/dsp_tools/utils/iri_util.py
+++ src/dsp_tools/utils/iri_util.py
@@ -1,6 +1,18 @@
 import regex
 
+_iri_pattern = r"""
+    ^(http)s?://
+    [\w\.\-~]*
+    (:\d{,6})?
+    (/[\w\-~]+)*
+    (#[\w\-~]*)?
+"""
 _resource_iri_pattern = r"https?://rdfh.ch/[a-fA-F0-9]{4}/[\w-]{22}"
+
+
+def is_iri(s: str) -> bool:
+    """Checks whether a string is a valid IRI."""
+    return regex.fullmatch(_iri_pattern, s) is not None
 
 
 def is_resource_iri(s: str) -> bool:

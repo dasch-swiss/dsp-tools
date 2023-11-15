fad8e12f26a99a9b213de1b59dd58e6a20983523
Johannes Nussbaum <johannes.nussbaum@dasch.swiss>
2023-11-15 14:30:00 +0100

    refactor xmlupload command

--- src/dsp_tools/commands/xmlupload/resource_multimedia.py
+++ src/dsp_tools/commands/xmlupload/resource_multimedia.py
@@ -20,7 +20,7 @@ def _upload_bitstream(
     sipi_server: Sipi,
     imgdir: str,
     permissions_lookup: dict[str, Permissions],
-) -> BitstreamInfo | None:
+) -> BitstreamInfo | None:  # sourcery skip: inline-immediately-returned-variable
     """
     This function uploads a specified bitstream file to SIPI and then returns the file information from SIPI.
 

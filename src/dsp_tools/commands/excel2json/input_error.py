ae82749faedf8181379095557f61b5a699e0c239
Nora-Olivia-Ammann <103038637+Nora-Olivia-Ammann@users.noreply.github.com>
2023-12-01 09:15:02 +0100

    make darglint happy

--- src/dsp_tools/commands/excel2json/input_error.py
+++ src/dsp_tools/commands/excel2json/input_error.py
@@ -14,9 +14,6 @@ class Problem(Protocol):
     def execute_error_protocol(self) -> str:
         """
         This function initiates all the steps for successful problem communication with the user.
-
-        Returns:
-            message for the error
         """
 
 

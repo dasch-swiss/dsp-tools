3fbaccad879c3813073747c5964127f62dcf0718
Nora-Olivia-Ammann <103038637+Nora-Olivia-Ammann@users.noreply.github.com>
2023-12-06 10:59:26 +0100

    change user information

--- src/dsp_tools/commands/ingest_xmlupload/user_information.py
+++ src/dsp_tools/commands/ingest_xmlupload/user_information.py
@@ -16,14 +16,16 @@ class UserInformation(Protocol):
     Information about user messages.
     """
 
-    def __str__(self) -> str:
+    def all_good_msg(self) -> str:
         """
-        This function initiates all the steps for successful problem communication with the user.
+        If everything went as it should this function returns a success message for the user.
+        Returns:
+
         """
 
-    def make(self) -> str:
+    def execute_error_protocol(self) -> str:
         """
-        This function saves the information as a csv file.
+        This function initiates all the steps to communicate the problems to the user.
         """
 
 
@@ -37,51 +39,69 @@ class IngestInformation:
     unused_media_paths: list[str]
     media_no_uuid: list[tuple[str, str] | Any]
 
-    def make(self) -> str:
+    def all_good_msg(self) -> str | None:
         """
-        This function generates the user message and saves a file with the information
-        if a lot of resources affected.
+        This function checks if no media was unused or not uploaded.
+        If that is the case it returns the message,
+        if not, it ends without an effect.
 
         Returns:
-            User message
+            Message if all went well.
         """
-        save_path = Path.home()
-        msg = []
-        if unused_media_df := self._unused_media_to_df():
-            _save_as_csv(unused_media_df, "UnusedMediaUploadedInSipi.csv", save_path)
-            msg.append(
-                "Media was uploaded to Sipi which was not referenced in the XML file.\n"
-                f"The file 'UnusedMediaUploadedInSipi.csv' was saved in '{save_path}' with the filenames."
-            )
-        if no_uuid_df := self._no_uuid_to_df():
-            _save_as_csv(no_uuid_df, "NotUploadedFilesToSipi.csv", save_path)
-            msg.append(
-                "Media referenced in the file was not previously uploaded to sipi.\n"
-                f"The file 'NotUploadedFilesToSipi.csv' was saved in '{save_path}' with the resource IDs and filenames."
-            )
-        msg.append(str(self))
-        return separator.join(msg)
-
-    def __str__(self) -> str:
-        msg_list = []
         if not self.unused_media_paths and not self.media_no_uuid:
             return (
                 "All media referenced in the XML file were uploaded to sipi.\n"
                 "No media was uploaded to sipi that was not referenced in the XML file."
             )
-        if self.unused_media_paths:
+        return None
+
+    def execute_error_protocol(self, save_path: Path) -> str:
+        """
+        This function generates the user message and saves a file with the information
+        if a lot of resources affected.
+
+        Args:
+            save_path: Path where the file should be saved.
+
+        Returns:
+            User message
+        """
+        unused_media_filename = "UnusedMediaUploadedInSipi.csv"
+        no_uuid_filename = "NotUploadedFilesToSipi.csv"
+        self._check_save_csv(save_path, no_uuid_filename, unused_media_filename)
+        return self._get_error_msg(save_path, unused_media_filename, no_uuid_filename)
+
+    def _get_error_msg(self, save_path: Path, unused_media_filename: str, no_uuid_filename: str) -> str:
+        msg_list = ["The upload cannot continue as there are problems with the media referenced in the XML."]
+        if 0 < len(self.unused_media_paths) <= 10:
             msg_list.append(
-                "The following media were uploaded to sipi but not used in the data XML file:"
+                "The following media were uploaded to sipi but not referenced in the data XML file:"
                 + list_separator
                 + list_separator.join(self.unused_media_paths)
             )
-        if self.media_no_uuid:
+        elif len(self.unused_media_paths) > 10:
+            msg_list.append(
+                "Media was uploaded to Sipi which was not referenced in the XML file.\n"
+                f"    The file '{unused_media_filename}' was saved in '{save_path}' with the filenames.\n"
+            )
+        if 0 < len(self.media_no_uuid) <= 10:
             msg_list.append(
                 "The following media were not uploaded to sipi but referenced in the data XML file:"
                 + list_separator
                 + list_separator.join([f"Resource ID: '{x[0]}' | Filepath: '{x[1]}'" for x in self.media_no_uuid])
             )
+        elif len(self.media_no_uuid) > 10:
+            msg_list.append(
+                "Media was referenced in the XML file but not previously uploaded to sipi:\n"
+                f"    The file '{no_uuid_filename}' was saved in '{save_path}' with the resource IDs and filenames."
+            )
         return separator.join(msg_list)
+
+    def _check_save_csv(self, save_path: Path, no_uuid_filename: str, unused_media_filename: str) -> None:
+        if unused_media_df := self._unused_media_to_df():
+            _save_as_csv(unused_media_df, unused_media_filename, save_path)
+        if no_uuid_df := self._no_uuid_to_df():
+            _save_as_csv(no_uuid_df, no_uuid_filename, save_path)
 
     def _unused_media_to_df(self) -> pd.DataFrame | None:
         unused_media = None
--- test/unittests/commands/ingest_xmlupload/test_user_information.py
+++ test/unittests/commands/ingest_xmlupload/test_user_information.py
@@ -1,4 +1,9 @@
-# pylint: disable=missing-class-docstring,missing-function-docstring
+# pylint: disable=missing-class-docstring,missing-function-docstring,protected-access
+
+from pathlib import Path
+
+import pandas as pd
+from pandas.testing import assert_frame_equal
 
 from dsp_tools.commands.ingest_xmlupload.user_information import IngestInformation
 
@@ -9,15 +14,140 @@ class TestIngestInformation:
             "All media referenced in the XML file were uploaded to sipi.\n"
             "No media was uploaded to sipi that was not referenced in the XML file."
         )
-        assert str(IngestInformation([], [])) == expected
+        assert IngestInformation([], []).all_good_msg() == expected
+
+    def test_not_all_good(self) -> None:
+        assert not IngestInformation(["unused_media"], []).all_good_msg()
 
     def test_unused_media(self) -> None:
-        expected = "The following media were uploaded to sipi but not used in the data XML file:\n    - unused path"
-        assert str(IngestInformation(["unused path"], [])) == expected
+        expected = (
+            "The upload cannot continue as there are problems with the media referenced in the XML.\n"
+            "    The following media were uploaded to sipi but not referenced in the data XML file:\n"
+            "    - unused_media"
+        )
+        res_msg = IngestInformation(["unused_media"], [])._get_error_msg(Path(), "", "")
+        assert res_msg == expected
 
     def test_not_uploaded(self) -> None:
         expected = (
-            "The following media were not uploaded to sipi but referenced in the data XML file:\n"
+            "The upload cannot continue as there are problems with the media referenced in the XML.\n"
+            "    The following media were not uploaded to sipi but referenced in the data XML file:\n"
             "    - Resource ID: 'no upload id' | Filepath: 'media path'"
         )
-        assert str(IngestInformation([], [("no upload id", "media path")])) == expected
+        res_msg = IngestInformation([], [("no upload id", "media path")])._get_error_msg(Path(), "", "")
+        assert res_msg == expected
+
+    def test_all_problem_more_than_ten(self) -> None:
+        expected = (
+            "The upload cannot continue as there are problems with the media referenced in the XML.\n"
+            "    Media was uploaded to Sipi which was not referenced in the XML file.\n"
+            "    The file 'unused_media.csv' was saved in '.' with the filenames.\n\n"
+            "    Media was referenced in the XML file but not previously uploaded to sipi:\n"
+            "    The file 'no_upload.csv' was saved in '.' with the resource IDs and filenames."
+        )
+        res_msg = IngestInformation(
+            [
+                "unused_media0",
+                "unused_media1",
+                "unused_media2",
+                "unused_media3",
+                "unused_media4",
+                "unused_media5",
+                "unused_media6",
+                "unused_media7",
+                "unused_media8",
+                "unused_media9",
+                "unused_media10",
+            ],
+            [
+                "no_up0",
+                "no_up1",
+                "no_up2",
+                "no_up3",
+                "no_up4",
+                "no_up5",
+                "no_up6",
+                "no_up7",
+                "no_up8",
+                "no_up9",
+                "no_up10",
+            ],
+        )._get_error_msg(Path(), "unused_media.csv", "no_upload.csv")
+        assert res_msg == expected
+
+
+def test_unused_media_to_df() -> None:
+    unused_media_list = [
+        "unused_media0",
+        "unused_media1",
+        "unused_media2",
+        "unused_media3",
+        "unused_media4",
+        "unused_media5",
+        "unused_media6",
+        "unused_media7",
+        "unused_media8",
+        "unused_media9",
+        "unused_media10",
+    ]
+    expected = pd.DataFrame({"Media Filenames": unused_media_list})
+    res_df = IngestInformation(unused_media_list, [])._unused_media_to_df()
+    assert_frame_equal(res_df, expected)
+
+
+def test_unused_media_to_df_not_enough() -> None:
+    assert not IngestInformation(["unused_media"], [])._unused_media_to_df()
+
+
+def test_no_uuid_to_df() -> None:
+    expected = pd.DataFrame(
+        {
+            "Resource ID": [
+                "ID_no_up0",
+                "ID_no_up1",
+                "ID_no_up2",
+                "ID_no_up3",
+                "ID_no_up4",
+                "ID_no_up5",
+                "ID_no_up6",
+                "ID_no_up7",
+                "ID_no_up8",
+                "ID_no_up9",
+                "ID_no_up10",
+            ],
+            "Filepath": [
+                "fileno_up0.jpg",
+                "fileno_up1.jpg",
+                "fileno_up2.jpg",
+                "fileno_up3.jpg",
+                "fileno_up4.jpg",
+                "fileno_up5.jpg",
+                "fileno_up6.jpg",
+                "fileno_up7.jpg",
+                "fileno_up8.jpg",
+                "fileno_up9.jpg",
+                "fileno_up10.jpg",
+            ],
+        }
+    )
+    res_df = IngestInformation(
+        [],
+        [
+            ("ID_no_up0", "fileno_up0.jpg"),
+            ("ID_no_up1", "fileno_up1.jpg"),
+            ("ID_no_up2", "fileno_up2.jpg"),
+            ("ID_no_up3", "fileno_up3.jpg"),
+            ("ID_no_up4", "fileno_up4.jpg"),
+            ("ID_no_up5", "fileno_up5.jpg"),
+            ("ID_no_up6", "fileno_up6.jpg"),
+            ("ID_no_up7", "fileno_up7.jpg"),
+            ("ID_no_up8", "fileno_up8.jpg"),
+            ("ID_no_up9", "fileno_up9.jpg"),
+            ("ID_no_up10", "fileno_up10.jpg"),
+        ],
+    )._no_uuid_to_df()
+    assert_frame_equal(res_df, expected)
+
+
+def test_no_uuid_to_df_not_enough() -> None:
+    assert not IngestInformation([], [("ID_no_up0", "fileno_up0.jpg")])._unused_media_to_df()

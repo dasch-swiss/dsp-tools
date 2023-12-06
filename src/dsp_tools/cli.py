0229edbb25698197845e9192a11e6deb93326af2
Nora-Olivia-Ammann <103038637+Nora-Olivia-Ammann@users.noreply.github.com>
2023-12-06 16:18:41 +0100

    Update cli.py

--- src/dsp_tools/cli.py
+++ src/dsp_tools/cli.py
@@ -27,7 +27,7 @@ from dsp_tools.commands.start_stack import StackConfiguration, StackHandler
 from dsp_tools.commands.template import generate_template_repo
 from dsp_tools.commands.xmlupload.upload_config import DiagnosticsConfig, UploadConfig
 from dsp_tools.commands.xmlupload.xmlupload import xmlupload
-from dsp_tools.models.exceptions import InputError, InternalError, RetryError, UserError
+from dsp_tools.models.exceptions import BaseError, InternalError, UserError
 from dsp_tools.utils.create_logger import get_logger
 from dsp_tools.utils.shared import validate_xml_against_schema
 
@@ -611,9 +611,11 @@ def run(args: list[str]) -> None:
             default_sipi_url=default_sipi_url,
         )
         success = _call_requested_action(parsed_arguments)
-    except (UserError, InputError, InternalError, RetryError) as err:
+    except BaseError as err:
         logger.error(err)
-        raise err from None
+        print("\nThe process was terminated because of an Error:")
+        print(err.message)
+        sys.exit(1)
     except Exception as err:
         logger.error(err)
         raise InternalError from None

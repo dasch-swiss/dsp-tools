d8a0bbc5a2abb1eec973b3e794ce49a58fd75b1f
Nora-Olivia-Ammann <103038637+Nora-Olivia-Ammann@users.noreply.github.com>
2023-12-07 14:08:32 +0100

    fix test

--- test/e2e/commands/xmlupload/test_xmlupload.py
+++ test/e2e/commands/xmlupload/test_xmlupload.py
@@ -1,5 +1,6 @@
 # pylint: disable=missing-class-docstring,missing-function-docstring
 
+import re
 import unittest
 
 import pytest
@@ -16,7 +17,7 @@ class TestXMLUpload(unittest.TestCase):
     imgdir = "."
     sipi = "http://0.0.0.0:1024"
 
-    def test_error_on_nonexistant_shortcode(self) -> None:
+    def test_error_on_nonexistent_shortcode(self) -> None:
         with self.assertRaisesRegex(BaseError, r"A project with shortcode 9999 could not be found on the DSP server"):
             xmlupload(
                 input_file="testdata/invalid-testdata/xml-data/inexistent-shortcode.xml",
@@ -28,18 +29,21 @@ class TestXMLUpload(unittest.TestCase):
                 config=UploadConfig(),
             )
 
-    def test_error_on_nonexistant_onto_name(self) -> None:
-        expected = (
-            r"\nSome property and/or class type\(s\) used in the XML are unknown\:\n\n"
-            r"---------------------------------------\n\n"
-            r"The following resource\(s\) have an invalid resource type\:\n\n"
-            r"    Resource Type\: '\:minimalResource'\n"
-            r"    Problem\: 'Unknown ontology prefix'\n"
-            r"    Resource ID\(s\)\:\n"
-            r"    - the_only_resource\n\n"
-            r"---------------------------------------\n\n"
+    def test_error_on_nonexistent_onto_name(self) -> None:
+        expected = re.escape(
+            "\nSome property and/or class type(s) used in the XML are unknown.\n"
+            "The ontologies for your project on the server are:\n"
+            "    - testonto\n"
+            "    - knora-api\n\n"
+            "---------------------------------------\n\n"
+            "The following resource(s) have an invalid resource type:\n\n"
+            "    Resource Type: ':minimalResource'\n"
+            "    Problem: 'Unknown ontology prefix'\n"
+            "    Resource ID(s):\n"
+            "    - the_only_resource\n\n"
+            "---------------------------------------\n\n"
         )
-        with self.assertRaisesRegex(UserError, expected):
+        with pytest.raises(UserError, match=expected):
             xmlupload(
                 input_file="testdata/invalid-testdata/xml-data/inexistent-ontoname.xml",
                 server=self.server,

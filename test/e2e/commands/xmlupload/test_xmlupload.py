095ef234656884c161047e7d755b1fe5aa98e424
Nora-Olivia-Ammann <103038637+Nora-Olivia-Ammann@users.noreply.github.com>
2023-12-07 14:13:28 +0100

    Update test_xmlupload.py

--- test/e2e/commands/xmlupload/test_xmlupload.py
+++ test/e2e/commands/xmlupload/test_xmlupload.py
@@ -1,7 +1,6 @@
-# pylint: disable=missing-class-docstring,missing-function-docstring
+# pylint: disable=missing-function-docstring
 
 import re
-import unittest
 
 import pytest
 
@@ -9,50 +8,51 @@ from dsp_tools.commands.xmlupload.upload_config import UploadConfig
 from dsp_tools.commands.xmlupload.xmlupload import xmlupload
 from dsp_tools.models.exceptions import BaseError, UserError
 
+server = "http://0.0.0.0:3333"
+user = "root@example.com"
+password = "test"
+imgdir = "."
+sipi = "http://0.0.0.0:1024"
 
-class TestXMLUpload(unittest.TestCase):
-    server = "http://0.0.0.0:3333"
-    user = "root@example.com"
-    password = "test"
-    imgdir = "."
-    sipi = "http://0.0.0.0:1024"
 
-    def test_error_on_nonexistent_shortcode(self) -> None:
-        with self.assertRaisesRegex(BaseError, r"A project with shortcode 9999 could not be found on the DSP server"):
-            xmlupload(
-                input_file="testdata/invalid-testdata/xml-data/inexistent-shortcode.xml",
-                server=self.server,
-                user=self.user,
-                password=self.password,
-                imgdir=self.imgdir,
-                sipi=self.sipi,
-                config=UploadConfig(),
-            )
-
-    def test_error_on_nonexistent_onto_name(self) -> None:
-        expected = re.escape(
-            "\nSome property and/or class type(s) used in the XML are unknown.\n"
-            "The ontologies for your project on the server are:\n"
-            "    - testonto\n"
-            "    - knora-api\n\n"
-            "---------------------------------------\n\n"
-            "The following resource(s) have an invalid resource type:\n\n"
-            "    Resource Type: ':minimalResource'\n"
-            "    Problem: 'Unknown ontology prefix'\n"
-            "    Resource ID(s):\n"
-            "    - the_only_resource\n\n"
-            "---------------------------------------\n\n"
+def test_error_on_nonexistent_shortcode() -> None:
+    expected = "A project with shortcode 9999 could not be found on the DSP server"
+    with pytest.raises(BaseError, match=expected):
+        xmlupload(
+            input_file="testdata/invalid-testdata/xml-data/inexistent-shortcode.xml",
+            server=server,
+            user=user,
+            password=password,
+            imgdir=imgdir,
+            sipi=sipi,
+            config=UploadConfig(),
         )
-        with pytest.raises(UserError, match=expected):
-            xmlupload(
-                input_file="testdata/invalid-testdata/xml-data/inexistent-ontoname.xml",
-                server=self.server,
-                user=self.user,
-                password=self.password,
-                imgdir=self.imgdir,
-                sipi=self.sipi,
-                config=UploadConfig(),
-            )
+
+
+def test_error_on_nonexistent_onto_name() -> None:
+    expected = re.escape(
+        "\nSome property and/or class type(s) used in the XML are unknown.\n"
+        "The ontologies for your project on the server are:\n"
+        "    - testonto\n"
+        "    - knora-api\n\n"
+        "---------------------------------------\n\n"
+        "The following resource(s) have an invalid resource type:\n\n"
+        "    Resource Type: ':minimalResource'\n"
+        "    Problem: 'Unknown ontology prefix'\n"
+        "    Resource ID(s):\n"
+        "    - the_only_resource\n\n"
+        "---------------------------------------\n\n"
+    )
+    with pytest.raises(UserError, match=expected):
+        xmlupload(
+            input_file="testdata/invalid-testdata/xml-data/inexistent-ontoname.xml",
+            server=server,
+            user=user,
+            password=password,
+            imgdir=imgdir,
+            sipi=sipi,
+            config=UploadConfig(),
+        )
 
 
 if __name__ == "__main__":

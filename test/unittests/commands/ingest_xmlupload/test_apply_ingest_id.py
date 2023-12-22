3b447da12a13a136776ef22e8af32f18f2435260
Nora-Olivia-Ammann <103038637+Nora-Olivia-Ammann@users.noreply.github.com>
2023-12-22 13:19:23 +0100

    cosmetic changes

--- test/unittests/commands/ingest_xmlupload/test_apply_ingest_id.py
+++ test/unittests/commands/ingest_xmlupload/test_apply_ingest_id.py
@@ -5,13 +5,13 @@ from dsp_tools.commands.ingest_xmlupload.apply_ingest_id import replace_filepath
 
 class TestReplaceBitstreamPaths:
     def test_all_good(self) -> None:
-        xml = (
-            "<knora>"
-            '<resource label="Fluffy Image" restype=":Image2D" id="Fluffy1" permissions="res-default">'
-            '<bitstream permissions="prop-default">images/Fluffy.jpg</bitstream>'
-            "</resource>"
-            "</knora>"
-        )
+        xml = """
+        <knora>
+            <resource label="Fluffy Image" restype=":Image2D" id="Fluffy1" permissions="res-default">
+                <bitstream permissions="prop-default">images/Fluffy.jpg</bitstream>
+            </resource>
+        </knora>
+        """
         root = etree.ElementTree(etree.fromstring(xml))
         reference_dict = {"images/Fluffy.jpg": "fluffy_id"}
         res_tree, ingest_info = replace_filepath_with_sipi_id(root, reference_dict)
@@ -23,13 +23,13 @@ class TestReplaceBitstreamPaths:
         assert res_bitstream.tag == "bitstream"
 
     def test_extra_paths(self) -> None:
-        xml = (
-            "<knora>"
-            '<resource label="Fluffy Image" restype=":Image2D" id="Fluffy1" permissions="res-default">'
-            '<bitstream permissions="prop-default">images/Fluffy.jpg</bitstream>'
-            "</resource>"
-            "</knora>"
-        )
+        xml = """
+        <knora>
+            <resource label="Fluffy Image" restype=":Image2D" id="Fluffy1" permissions="res-default">
+                <bitstream permissions="prop-default">images/Fluffy.jpg</bitstream>
+            </resource>
+        </knora>
+        """
         root = etree.ElementTree(etree.fromstring(xml))
         reference_dict = {"images/Fluffy.jpg": "fluffy_id", "extra_media": "extra_id"}
         res_tree, ingest_info = replace_filepath_with_sipi_id(root, reference_dict)
@@ -41,13 +41,13 @@ class TestReplaceBitstreamPaths:
         assert res_bitstream.tag == "bitstream"
 
     def test_missing_paths(self) -> None:
-        xml = (
-            "<knora>"
-            '<resource label="Fluffy Image" restype=":Image2D" id="Fluffy1" permissions="res-default">'
-            '<bitstream permissions="prop-default">images/Fluffy.jpg</bitstream>'
-            "</resource>"
-            "</knora>"
-        )
+        xml = """
+        <knora>
+            <resource label="Fluffy Image" restype=":Image2D" id="Fluffy1" permissions="res-default">
+                <bitstream permissions="prop-default">images/Fluffy.jpg</bitstream>
+            </resource>
+        </knora>
+        """
         root = etree.ElementTree(etree.fromstring(xml))
         reference_dict = {"extra_media": "extra_id"}
         res_tree, ingest_info = replace_filepath_with_sipi_id(root, reference_dict)

f0642184025f1b7ca4febd08456cb7780650872a
Johannes Nussbaum <johannes.nussbaum@dasch.swiss>
2024-01-11 09:28:49 +0100

    remove stack dependency from test_error_on_nonexistent_shortcode

--- test/e2e/commands/xmlupload/test_xmlupload.py
+++ test/e2e/commands/xmlupload/test_xmlupload.py
@@ -1,7 +1,14 @@
 import re
+from dataclasses import dataclass
+from pathlib import Path
+from test.unittests.commands.xmlupload.connection_mock import ConnectionMockBase
+from typing import Any
 
 import pytest
+from lxml import etree
 
+from dsp_tools.commands.xmlupload.check_consistency_with_ontology import do_xml_consistency_check
+from dsp_tools.commands.xmlupload.ontology_client import OntologyClientLive
 from dsp_tools.commands.xmlupload.upload_config import UploadConfig
 from dsp_tools.commands.xmlupload.xmlupload import xmlupload
 from dsp_tools.models.exceptions import BaseError, UserError
@@ -13,18 +20,27 @@ imgdir = "."
 sipi = "http://0.0.0.0:1024"
 
 
+@dataclass
+class ConnectionMock(ConnectionMockBase):
+    def get(
+        self,
+        route: str,  # noqa: ARG002 (unused-method-argument)
+        headers: dict[str, str] | None = None,  # noqa: ARG002 (unused-method-argument)
+    ) -> dict[str, Any]:
+        raise BaseError("foo")
+
+
 def test_error_on_nonexistent_shortcode() -> None:
-    expected = "A project with shortcode 9999 could not be found on the DSP server"
-    with pytest.raises(BaseError, match=expected):
-        xmlupload(
-            input_file="testdata/invalid-testdata/xml-data/inexistent-shortcode.xml",
-            server=server,
-            user=user,
-            password=password,
-            imgdir=imgdir,
-            sipi=sipi,
-            config=UploadConfig(),
-        )
+    root = etree.parse("testdata/xml-data/test-data-minimal.xml").getroot()
+    con = ConnectionMock()
+    ontology_client = OntologyClientLive(
+        con=con,
+        shortcode="9999",
+        default_ontology="foo",
+        save_location=Path("bar"),
+    )
+    with pytest.raises(UserError, match="A project with shortcode 9999 could not be found on the DSP server"):
+        do_xml_consistency_check(ontology_client, root)
 
 
 def test_error_on_nonexistent_onto_name() -> None:

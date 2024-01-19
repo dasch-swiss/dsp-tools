b9aeb078fc6940facc0bf9b192ac8ec735db705f
Johannes Nussbaum <johannes.nussbaum@dasch.swiss>
2024-01-19 11:52:31 +0100

    edit

--- test/e2e/utils/test_connection_live.py
+++ test/e2e/utils/test_connection_live.py
@@ -1,93 +1,70 @@
-"""end to end tests for connection class"""
-
-import unittest
-
 import pytest
 
 from dsp_tools.models.exceptions import BaseError, UserError
 from dsp_tools.utils.connection_live import ConnectionLive
 
-# ruff: noqa: PT009 (pytest-unittest-assertion) (remove this line when pytest is used instead of unittest)
-# ruff: noqa: PT027 (pytest-unittest-raises-assertion) (remove this line when pytest is used instead of unittest)
+class SessionMock:
+    def post(self, url: str, data: bytes, *args, **kwargs) -> dict:
+        if url == "http://0.0.0.0:3333/v2/authentication" and data == b'{"email":"root@example.com","password":"test"}':
+            return {"token": "token"}
 
 
-class TestConnectionLive(unittest.TestCase):
-    con: ConnectionLive
+def test_log_in_and_out() -> None:
+    con = ConnectionLive("http://0.0.0.0:3333")
+    con.login("root@example.com", "test")
+    assertIsNotNone(con.token)
+    con.logout()
+    assertIsNone(con.token)
+    with assertRaises(UserError):
+        con.login("invalid", "invalid")
 
-    def setUp(self) -> None:
-        """
-        Creates a connection to DSP-API.
-        For each test method, a new TestCase instance is created, so setUp() is executed before each test method.
-        """
-        self.con = ConnectionLive(server="http://0.0.0.0:3333")
-        self.con.login(email="root@example.com", password="test")
+def test_get() -> None:
+    res = con.get("/ontology/0001/anything/simple/v2")
+    assertIsNotNone(res["@graph"])
+    assertRaises(BaseError, con.get, "/doesNotExist")
+    con.logout()
+    assertIsNotNone(res["@graph"])
+    assertRaises(BaseError, con.get, "/doesNotExist")
 
-    def tearDown(self) -> None:
-        """
-        Logs out from DSP-API.
-        For each test method, a new TestCase instance is created, so tearDown() is executed after each test method.
-        """
-        self.con.logout()
+def test_post() -> None:
+    res_info = {
+        "@type": "anything:Thing",
+        "knora-api:attachedToProject": {"@id": "http://rdfh.ch/projects/0001"},
+        "anything:hasBoolean": {"@type": "knora-api:BooleanValue", "knora-api:booleanValueAsBoolean": True},
+        "rdfs:label": "knora-py thing",
+        "knora-api:hasPermissions": "CR knora-admin:Creator|V http://rdfh.ch/groups/0001/thing-searcher",
+        "@context": {
+            "rdf": "http://www.w3.org/1999/02/22-rdf-syntax-ns#",
+            "knora-api": "http://api.knora.org/ontology/knora-api/v2#",
+            "rdfs": "http://www.w3.org/2000/01/rdf-schema#",
+            "xsd": "http://www.w3.org/2001/XMLSchema#",
+            "anything": "http://0.0.0.0:3333/ontology/0001/anything/v2#",
+        },
+    }
 
-    def test_Connection(self) -> None:
-        self.assertIsInstance(self.con, ConnectionLive)
+    res = con.post("/v2/resources", res_info)
 
-    def test_log_in_and_out(self) -> None:
-        con = ConnectionLive("http://0.0.0.0:3333")
-        con.login("root@example.com", "test")
-        self.assertIsNotNone(con.token)
-        con.logout()
-        self.assertIsNone(con.token)
-        with self.assertRaises(UserError):
-            con.login("invalid", "invalid")
+    assertIsNotNone(res["@id"])
+    assertEqual(res["@type"], "anything:Thing")
+    assertEqual(res["rdfs:label"], "knora-py thing")
 
-    def test_get(self) -> None:
-        res = self.con.get("/ontology/0001/anything/simple/v2")
-        self.assertIsNotNone(res["@graph"])
-        self.assertRaises(BaseError, self.con.get, "/doesNotExist")
-        self.con.logout()
-        self.assertIsNotNone(res["@graph"])
-        self.assertRaises(BaseError, self.con.get, "/doesNotExist")
+    res_id = res["@id"]
 
-    def test_post(self) -> None:
-        res_info = {
-            "@type": "anything:Thing",
-            "knora-api:attachedToProject": {"@id": "http://rdfh.ch/projects/0001"},
-            "anything:hasBoolean": {"@type": "knora-api:BooleanValue", "knora-api:booleanValueAsBoolean": True},
-            "rdfs:label": "knora-py thing",
-            "knora-api:hasPermissions": "CR knora-admin:Creator|V http://rdfh.ch/groups/0001/thing-searcher",
-            "@context": {
-                "rdf": "http://www.w3.org/1999/02/22-rdf-syntax-ns#",
-                "knora-api": "http://api.knora.org/ontology/knora-api/v2#",
-                "rdfs": "http://www.w3.org/2000/01/rdf-schema#",
-                "xsd": "http://www.w3.org/2001/XMLSchema#",
-                "anything": "http://0.0.0.0:3333/ontology/0001/anything/v2#",
-            },
-        }
+    erase_info = {
+        "@id": f"{res_id}",
+        "@type": "anything:Thing",
+        "@context": {
+            "rdf": "http://www.w3.org/1999/02/22-rdf-syntax-ns#",
+            "knora-api": "http://api.knora.org/ontology/knora-api/v2#",
+            "rdfs": "http://www.w3.org/2000/01/rdf-schema#",
+            "xsd": "http://www.w3.org/2001/XMLSchema#",
+            "anything": "http://0.0.0.0:3333/ontology/0001/anything/v2#",
+        },
+    }
 
-        res = self.con.post("/v2/resources", res_info)
-
-        self.assertIsNotNone(res["@id"])
-        self.assertEqual(res["@type"], "anything:Thing")
-        self.assertEqual(res["rdfs:label"], "knora-py thing")
-
-        res_id = res["@id"]
-
-        erase_info = {
-            "@id": f"{res_id}",
-            "@type": "anything:Thing",
-            "@context": {
-                "rdf": "http://www.w3.org/1999/02/22-rdf-syntax-ns#",
-                "knora-api": "http://api.knora.org/ontology/knora-api/v2#",
-                "rdfs": "http://www.w3.org/2000/01/rdf-schema#",
-                "xsd": "http://www.w3.org/2001/XMLSchema#",
-                "anything": "http://0.0.0.0:3333/ontology/0001/anything/v2#",
-            },
-        }
-
-        res = self.con.post("/v2/resources/erase", erase_info)
-        self.assertIsNotNone(res["knora-api:result"])
-        self.assertEqual(res["knora-api:result"], "Resource erased")
+    res = con.post("/v2/resources/erase", erase_info)
+    assertIsNotNone(res["knora-api:result"])
+    assertEqual(res["knora-api:result"], "Resource erased")
 
 
 if __name__ == "__main__":

258b40daf9baa09985fdef06a094db674fdf92b1
Johannes Nussbaum <johannes.nussbaum@dasch.swiss>
2024-01-23 16:27:16 +0100

    revert wrong changes

--- test/unittests/utils/test_connection_live.py
+++ test/unittests/utils/test_connection_live.py
@@ -1,20 +1,4 @@
-from unittest.mock import Mock
-
-import pytest
-
 from dsp_tools.utils.connection_live import ConnectionLive
-
-
-def test_log_in_log_out() -> None:
-    con = ConnectionLive("http://example.com/")
-    con.post = Mock(return_value={"token": "token"})  # type: ignore[method-assign]
-    con.login("root@example.com", "test")
-    assert con.token == "token"
-    con.post.assert_called_once_with(route="/v2/authentication", data={"email": "root@example.com", "password": "test"})
-    con.delete = Mock()  # type: ignore[method-assign]
-    con.logout()
-    assert con.token is None
-    con.delete.assert_called_once_with(route="/v2/authentication")
 
 
 def test_anonymize_different_keys() -> None:
@@ -38,7 +22,3 @@ def test_anonymize_different_lengths() -> None:
     assert con._anonymize({"token": "uk7m20-8gqn8ir7e30"}) == {"token": "uk7m2[+13]"}
     assert con._anonymize({"token": "uk7m2"}) == {"token": "*****"}
     assert con._anonymize({"token": "u"}) == {"token": "*"}
-
-
-if __name__ == "__main__":
-    pytest.main([__file__])

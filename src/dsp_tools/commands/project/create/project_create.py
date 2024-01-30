6abd68052ae38c0f4e3350b1f60d9cae9f9e4725
Nora-Olivia-Ammann <103038637+Nora-Olivia-Ammann@users.noreply.github.com>
2024-01-30 11:44:29 +0100

    Update project_create.py

--- src/dsp_tools/commands/project/create/project_create.py
+++ src/dsp_tools/commands/project/create/project_create.py
@@ -1136,7 +1136,7 @@ def _prepare_project_information_onto_lists(
     for onto in all_ontos:
         if onto.get("properties"):
             onto["properties"] = _rectify_hlist_of_properties(
-                lists=project_json["project"].get("lists", []),
+                lists=all_lists if all_lists else [],
                 properties=onto["properties"],
             )
 

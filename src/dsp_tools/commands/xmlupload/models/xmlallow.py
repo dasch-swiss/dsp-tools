1f881126a391569cf378aecab84bc84c29ffa54f
Nora-Olivia-Ammann <103038637+Nora-Olivia-Ammann@users.noreply.github.com>
2024-01-30 16:53:17 +0100

    chore: fix ruff PLR5501 (#774)

--- src/dsp_tools/commands/xmlupload/models/xmlallow.py
+++ src/dsp_tools/commands/xmlupload/models/xmlallow.py
@@ -23,6 +23,7 @@ class XmlAllow:
             XmlUploadError: If an upload fails
         """
         tmp = node.attrib["group"].split(":")
+
         sysgroups = ["UnknownUser", "KnownUser", "ProjectMember", "Creator", "ProjectAdmin", "SystemAdmin"]
         if len(tmp) > 1:
             if tmp[0]:
@@ -37,11 +38,11 @@ class XmlAllow:
                 if project_context.project_name is None:
                     raise XmlUploadError("Project shortcode has not been set in ProjectContext")
                 self._group = project_context.project_name + ":" + tmp[1]
+        elif tmp[0] in sysgroups:
+            self._group = "knora-admin:" + node.attrib["group"]
         else:
-            if tmp[0] in sysgroups:
-                self._group = "knora-admin:" + node.attrib["group"]
-            else:
-                raise XmlUploadError(f'Group "{node.attrib["group"]}" is not known: ')
+            raise XmlUploadError(f'Group "{node.attrib["group"]}" is not known: ')
+
         if not node.text:
             raise XmlUploadError("No permission set specified")
         self._permission = PermissionValue[node.text]

396adeace26473a99ce343f8dfb91e7951256496
Nora-Olivia-Ammann <103038637+Nora-Olivia-Ammann@users.noreply.github.com>
2024-02-05 16:37:45 +0100

    test json obj create

--- test/unittests/commands/project/test_models_project.py
+++ test/unittests/commands/project/test_models_project.py
@@ -12,20 +12,23 @@ def con() -> Connection:
     return ConnectionMockBase()
 
 
-def test_return_values(con: Connection) -> None:
-    project = Project(
+@pytest.fixture()
+def project(con: Connection) -> Project:
+    return Project(
         con=con,
         iri="http://rdfh.ch/test",
         shortcode="0FF0",
         shortname="test_project",
         longname="Test Project",
         description=LangString({Languages.EN: "This is a test project", Languages.DE: "Das ist ein Testprojekt"}),
-        keywords={"test", "project"},
+        keywords=set(),
         selfjoin=False,
         status=True,
         logo="logo.gif",
     )
 
+
+def test_return_values(project: Project) -> None:
     assert project.iri == "http://rdfh.ch/test"
     assert project.shortcode == "0FF0"
     assert project.shortname == "test_project"
@@ -34,6 +37,23 @@ def test_return_values(con: Connection) -> None:
     assert project.description["de"] == "Das ist ein Testprojekt"
     assert project.selfjoin is False
     assert project.status is True
+    assert project.keywords == set()
+
+
+def test_to_json_obj_create(project: Project) -> None:
+    res_json = project._toJsonObj_create()
+    expected = {
+        "shortcode": "0FF0",
+        "shortname": "test_project",
+        "longname": "Test Project",
+        "description": [
+            {"language": "en", "value": "This is a test project"},
+            {"language": "de", "value": "Das ist ein Testprojekt"},
+        ],
+        "selfjoin": False,
+        "status": True,
+    }
+    assert res_json == expected
 
 
 if __name__ == "__main__":

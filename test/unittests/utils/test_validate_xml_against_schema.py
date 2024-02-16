b3f3b352f2734dd6cec6a091f3bd4b44c55447c1
Nora-Olivia-Ammann <103038637+Nora-Olivia-Ammann@users.noreply.github.com>
2024-02-16 11:38:10 +0100

    fix import statement

--- test/unittests/utils/test_validate_xml_against_schema.py
+++ test/unittests/utils/test_validate_xml_against_schema.py
@@ -3,8 +3,8 @@ import unittest
 import pytest
 from lxml import etree
 
-import dsp_tools.utils.validate_xml_against_schema
 from dsp_tools.models.exceptions import InputError
+from dsp_tools.utils.validate_xml_against_schema import _find_xml_tags_in_simple_text_elements, validate_xml
 
 # ruff: noqa: PT009 (pytest-unittest-assertion) (remove this line when pytest is used instead of unittest)
 # ruff: noqa: PT027 (pytest-unittest-raises-assertion) (remove this line when pytest is used instead of unittest)
@@ -12,27 +12,17 @@ from dsp_tools.models.exceptions import InputError
 
 class TestValidateXMLAgainstSchema(unittest.TestCase):
     def test_data_systematic(self) -> None:
-        self.assertTrue(
-            dsp_tools.utils.validate_xml_against_schema.validate_xml(
-                input_file="testdata/xml-data/test-data-systematic.xml"
-            )
-        )
+        self.assertTrue(validate_xml(input_file="testdata/xml-data/test-data-systematic.xml"))
 
     def test_data_minimal(self) -> None:
-        self.assertTrue(
-            dsp_tools.utils.validate_xml_against_schema.validate_xml(
-                input_file=etree.parse(source="testdata/xml-data/test-data-minimal.xml")
-            )
-        )
+        self.assertTrue(validate_xml(input_file=etree.parse(source="testdata/xml-data/test-data-minimal.xml")))
 
     def test_invalid_resource_tag_line_twelve(self) -> None:
         with self.assertRaisesRegex(
             InputError,
             "Line 12: Element 'resource', attribute 'invalidtag': The attribute 'invalidtag' is not allowed",
         ):
-            dsp_tools.utils.validate_xml_against_schema.validate_xml(
-                input_file="testdata/invalid-testdata/xml-data/invalid-resource-tag.xml"
-            )
+            validate_xml(input_file="testdata/invalid-testdata/xml-data/invalid-resource-tag.xml")
 
     def test_invalid_resource_tag_problem(self) -> None:
         with self.assertRaisesRegex(
@@ -44,9 +34,7 @@ class TestValidateXMLAgainstSchema(unittest.TestCase):
             r"    - line 15: resource 'the_only_resource', property ':test'\n"
             r"    - line 16: resource 'the_only_resource', property ':test'",
         ):
-            dsp_tools.utils.validate_xml_against_schema.validate_xml(
-                input_file="testdata/invalid-testdata/xml-data/utf8-text-with-xml-tags.xml"
-            )
+            validate_xml(input_file="testdata/invalid-testdata/xml-data/utf8-text-with-xml-tags.xml")
 
     def test_data_duplicate_iri(self) -> None:
         with self.assertRaisesRegex(
@@ -54,9 +42,7 @@ class TestValidateXMLAgainstSchema(unittest.TestCase):
             "Line 19: Element 'resource': Duplicate key-sequence .+ "
             "in unique identity-constraint 'IRI_attribute_of_resource_must_be_unique'",
         ):
-            dsp_tools.utils.validate_xml_against_schema.validate_xml(
-                input_file="testdata/invalid-testdata/xml-data/duplicate-iri.xml"
-            )
+            validate_xml(input_file="testdata/invalid-testdata/xml-data/duplicate-iri.xml")
 
     def test_duplicate_ark(self) -> None:
         with self.assertRaisesRegex(
@@ -64,9 +50,7 @@ class TestValidateXMLAgainstSchema(unittest.TestCase):
             "Line 19: Element 'resource': Duplicate key-sequence .+ "
             "in unique identity-constraint 'ARK_attribute_of_resource_must_be_unique'",
         ):
-            dsp_tools.utils.validate_xml_against_schema.validate_xml(
-                input_file="testdata/invalid-testdata/xml-data/duplicate-ark.xml"
-            )
+            validate_xml(input_file="testdata/invalid-testdata/xml-data/duplicate-ark.xml")
 
     def test_empty_label(self) -> None:
         with self.assertRaisesRegex(
@@ -74,9 +58,7 @@ class TestValidateXMLAgainstSchema(unittest.TestCase):
             "Line 11: Element 'resource', attribute 'label': .+ "
             "The value '' has a length of '0'; this underruns the allowed minimum length of '1'",
         ):
-            dsp_tools.utils.validate_xml_against_schema.validate_xml(
-                input_file="testdata/invalid-testdata/xml-data/empty-label.xml"
-            )
+            validate_xml(input_file="testdata/invalid-testdata/xml-data/empty-label.xml")
 
 
 class TestFindXMLTagsInUTF8(unittest.TestCase):
@@ -102,11 +84,7 @@ class TestFindXMLTagsInUTF8(unittest.TestCase):
             for txt in allowed_html_escapes
         ]
         for xml in utf8_texts_with_allowed_html_escapes:
-            self.assertTrue(
-                dsp_tools.utils.validate_xml_against_schema._find_xml_tags_in_simple_text_elements(
-                    etree.fromstring(xml)
-                )
-            )
+            self.assertTrue(_find_xml_tags_in_simple_text_elements(etree.fromstring(xml)))
 
     def test_find_xml_tags_in_simple_text_elements_forbidden_escapes(self) -> None:
         test_ele = etree.fromstring(
@@ -125,7 +103,7 @@ class TestFindXMLTagsInUTF8(unittest.TestCase):
             "The following resources of your XML file violate this rule:\n"
             "    - line 5: resource 'id', property ':name'"
         )
-        all_good, res_msg = dsp_tools.utils.validate_xml_against_schema._find_xml_tags_in_simple_text_elements(test_ele)
+        all_good, res_msg = _find_xml_tags_in_simple_text_elements(test_ele)
         assert all_good is False
         assert res_msg == expected_msg
 
@@ -146,7 +124,7 @@ class TestFindXMLTagsInUTF8(unittest.TestCase):
             "The following resources of your XML file violate this rule:\n"
             "    - line 5: resource 'id', property ':propName'"
         )
-        all_good, res_msg = dsp_tools.utils.validate_xml_against_schema._find_xml_tags_in_simple_text_elements(test_ele)
+        all_good, res_msg = _find_xml_tags_in_simple_text_elements(test_ele)
         assert all_good is False
         assert res_msg == expected_msg
 

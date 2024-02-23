bae79f987289970779fa4c4d67f4ad182a670102
Nora-Olivia-Ammann <103038637+Nora-Olivia-Ammann@users.noreply.github.com>
2024-02-23 11:58:03 +0100

    Update test_xmlupload.py

--- test/unittests/commands/xmlupload/test_xmlupload.py
+++ test/unittests/commands/xmlupload/test_xmlupload.py
@@ -1,4 +1,5 @@
 import unittest
+from copy import deepcopy
 
 import pytest
 import regex
@@ -12,49 +13,37 @@ from dsp_tools.utils.xml_utils import parse_and_clean_xml_file
 # ruff: noqa: PT027 (pytest-unittest-raises-assertion) (remove this line when pytest is used instead of unittest)
 
 
-@pytest.fixture()
-def data_systematic_from_file() -> etree._Element:
-    return parse_and_clean_xml_file("testdata/xml-data/test-data-systematic.xml")
-
-
-@pytest.fixture()
-def data_systematic_from_tree(data_systematic_from_file) -> etree._Element:
-    return parse_and_clean_xml_file(data_systematic_from_file)
-
-
 def clean_resulting_tree(tree: etree._Element) -> etree._Element:
-    cleaned = regex.sub("\n", "", etree.tostring(tree, encoding=str))
+    cleaned = deepcopy(tree)
+    cleaned = regex.sub("\n", "", etree.tostring(cleaned, encoding=str))
     return regex.sub(" +", " ", cleaned)
 
 
-def test_parse_and_clean_xml_file_same_regardless_of_input(
-    data_systematic_from_file: etree._Element, data_systematic_from_tree: etree._Element
-) -> None:
-    cleaned_from_tree = clean_resulting_tree(data_systematic_from_tree)
-    cleaned_from_file = clean_resulting_tree(data_systematic_from_file)
-    assert (
-        cleaned_from_tree == cleaned_from_file
-    ), "The output must be equal, regardless if the input is a file path or a lxml tree."
-
-
-class TestXMLUpload(unittest.TestCase):
-    def test_parse_xml_file(self) -> None:
+class TestParseAndCleanXML(unittest.TestCase):
+    def test_parse_and_clean_xml_file_same_regardless_of_input(self) -> None:
         test_data_systematic_tree = etree.parse("testdata/xml-data/test-data-systematic.xml")
-        output1 = parse_and_clean_xml_file("testdata/xml-data/test-data-systematic.xml")
-        output2 = parse_and_clean_xml_file(test_data_systematic_tree)
-        result1 = regex.sub("\n", "", etree.tostring(output1, encoding=str))
-        result1 = regex.sub(" +", " ", result1)
-        result2 = regex.sub("\n", "", etree.tostring(output2, encoding=str))
-        result2 = regex.sub(" +", " ", result2)
-        self.assertEqual(result1, result2, msg="The output must be equal, regardless if the input is a path or parsed.")
+        from_file = parse_and_clean_xml_file("testdata/xml-data/test-data-systematic.xml")
+        from_tree = parse_and_clean_xml_file(test_data_systematic_tree)
+        cleaned_from_file = clean_resulting_tree(from_file)
+        cleaned_from_tree = clean_resulting_tree(from_tree)
+        self.assertEqual(
+            cleaned_from_file,
+            cleaned_from_tree,
+            msg="The output must be equal, regardless if the input is a path or parsed.",
+        )
 
+    def test_annotations_regions_links_before(self) -> None:
+        test_data_systematic_tree = etree.parse("testdata/xml-data/test-data-systematic.xml")
         annotations_regions_links_before = [
             e for e in test_data_systematic_tree.iter() if regex.search("annotation|region|link", str(e.tag))
         ]
-        annotations_regions_links_after = [
-            e for e in output1.iter() if regex.search("annotation|region|link", str(e.tag))
-        ]
         self.assertGreater(len(annotations_regions_links_before), 0)
+
+    def test_annotations_regions_links_after(self) -> None:
+        from_file = parse_and_clean_xml_file("testdata/xml-data/test-data-systematic.xml")
+        annotations_regions_links_after = [
+            e for e in from_file.iter() if regex.search("annotation|region|link", str(e.tag))
+        ]
         self.assertEqual(
             len(annotations_regions_links_after),
             0,
@@ -62,7 +51,9 @@ class TestXMLUpload(unittest.TestCase):
             '<resource restype="Annotation/Region/LinkObj">',
         )
 
-        comments = [e for e in output1.iter() if isinstance(e, etree._Comment)]
+    def test_comment_removal(self) -> None:
+        from_file = parse_and_clean_xml_file("testdata/xml-data/test-data-systematic.xml")
+        comments = [e for e in from_file.iter() if isinstance(e, etree._Comment)]
         self.assertEqual(
             len(comments),
             0,
@@ -70,26 +61,32 @@ class TestXMLUpload(unittest.TestCase):
             "if they are not removed in the parsing process",
         )
 
-    def test_convert_ark_v0_to_resource_iri(self) -> None:
+
+class TestConvertArkToResourceIRI(unittest.TestCase):
+    def test_good(self) -> None:
         ark = "ark:/72163/080c-779b9990a0c3f-6e"
         iri = convert_ark_v0_to_resource_iri(ark)
         self.assertEqual("http://rdfh.ch/080C/Ef9heHjPWDS7dMR_gGax2Q", iri)
 
+    def test_invalid_ark(self) -> None:
         with self.assertRaisesRegex(
             BaseError, r"converting ARK 'ark:/72163/080c-779b999-0a0c3f-6e'\. The ARK seems to be invalid"
         ):
             convert_ark_v0_to_resource_iri("ark:/72163/080c-779b999-0a0c3f-6e")
 
+    def test_invalid_shortcode(self) -> None:
         with self.assertRaisesRegex(
             BaseError, r"converting ARK 'ark:/72163/080X-779b9990a0c3f-6e'\. Invalid project shortcode '080X'"
         ):
             convert_ark_v0_to_resource_iri("ark:/72163/080X-779b9990a0c3f-6e")
 
+    def test_invalid_shortcode_long(self) -> None:
         with self.assertRaisesRegex(
             BaseError, r"converting ARK 'ark:/72163/080c1-779b9990a0c3f-6e'\. Invalid project shortcode '080C1'"
         ):
             convert_ark_v0_to_resource_iri("ark:/72163/080c1-779b9990a0c3f-6e")
 
+    def test_invalid_salsah_id(self) -> None:
         with self.assertRaisesRegex(
             BaseError, r"converting ARK 'ark:/72163/080c-779b99\+90a0c3f-6e'\. Invalid Salsah ID '779b99\+90a0c3f'"
         ):

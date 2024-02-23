f12ac5bff1a28fdf997cdfbf45292fc8a17e564e
Nora-Olivia-Ammann <103038637+Nora-Olivia-Ammann@users.noreply.github.com>
2024-02-23 14:09:47 +0100

    Update test_xml_utils.py

--- test/unittests/utils/test_xml_utils.py
+++ test/unittests/utils/test_xml_utils.py
@@ -1,20 +1,11 @@
-from pathlib import Path
+import unittest
 
-import pytest
 import regex
 from lxml import etree
 
 from dsp_tools.utils.xml_utils import parse_and_clean_xml_file
 
-
-@pytest.fixture()
-def data_systematic_uncleaned() -> etree._ElementTree[etree._Element]:
-    return etree.parse(Path("testdata/xml-data/test-data-systematic.xml"))
-
-
-@pytest.fixture()
-def data_systematic_cleaned() -> etree._Element:
-    return parse_and_clean_xml_file(Path("testdata/xml-data/test-data-systematic.xml"))
+# ruff: noqa: PT009 (pytest-unittest-assertion) (remove this line when pytest is used instead of unittest)
 
 
 def clean_resulting_tree(tree: etree._Element) -> str:
@@ -22,37 +13,44 @@ def clean_resulting_tree(tree: etree._Element) -> str:
     return regex.sub(" +", " ", cleaned_str)
 
 
-def test_parse_and_clean_xml_file_same_regardless_of_input(
-    data_systematic_uncleaned: etree._ElementTree[etree._Element], data_systematic_cleaned: etree._Element
-) -> None:
-    from_tree = parse_and_clean_xml_file(data_systematic_uncleaned)
-    cleaned_from_file = clean_resulting_tree(data_systematic_cleaned)
-    cleaned_from_tree = clean_resulting_tree(from_tree)
-    assert (
-        cleaned_from_file == cleaned_from_tree
-    ), "The output must be equal, regardless if the input is a path or parsed."
+class TestParseAndCleanXML(unittest.TestCase):
+    def test_parse_and_clean_xml_file_same_regardless_of_input(self) -> None:
+        test_data_systematic_tree = etree.parse("testdata/xml-data/test-data-systematic.xml")
+        from_file = parse_and_clean_xml_file("testdata/xml-data/test-data-systematic.xml")
+        from_tree = parse_and_clean_xml_file(test_data_systematic_tree)
+        cleaned_from_file = clean_resulting_tree(from_file)
+        cleaned_from_tree = clean_resulting_tree(from_tree)
+        self.assertEqual(
+            cleaned_from_file,
+            cleaned_from_tree,
+            msg="The output must be equal, regardless if the input is a path or parsed.",
+        )
 
+    def test_annotations_regions_links_before(self) -> None:
+        test_data_systematic_tree = etree.parse("testdata/xml-data/test-data-systematic.xml")
+        annotations_regions_links_before = [
+            e for e in test_data_systematic_tree.iter() if regex.search("annotation|region|link", str(e.tag))
+        ]
+        self.assertGreater(len(annotations_regions_links_before), 0)
 
-def test_annotations_regions_links_before_cleaning(data_systematic_uncleaned: etree._Element) -> None:
-    annotations_regions_links_before = [
-        e for e in data_systematic_uncleaned.iter() if regex.search("annotation|region|link", str(e.tag))
-    ]
-    assert len(annotations_regions_links_before) == 5
+    def test_annotations_regions_links_after(self) -> None:
+        from_file = parse_and_clean_xml_file("testdata/xml-data/test-data-systematic.xml")
+        annotations_regions_links_after = [
+            e for e in from_file.iter() if regex.search("annotation|region|link", str(e.tag))
+        ]
+        self.assertEqual(
+            len(annotations_regions_links_after),
+            0,
+            msg="The tags <annotation>, <region>, and <link> must be transformed to their technically correct form "
+            '<resource restype="Annotation/Region/LinkObj">',
+        )
 
-
-def test_annotations_regions_links_after_cleaning(data_systematic_cleaned: etree._Element) -> None:
-    annotations_regions_links_after = [
-        e for e in data_systematic_cleaned.iter() if regex.search("annotation|region|link", str(e.tag))
-    ]
-    assert len(annotations_regions_links_after) == 0, (
-        "The tags <annotation>, <region>, and <link> must be transformed to their technically "
-        'correct form <resource restype="Annotation/Region/LinkObj">'
-    )
-
-
-def test_comment_removal(data_systematic_cleaned: etree._Element) -> None:
-    comments = [e for e in data_systematic_cleaned.iter() if isinstance(e, etree._Comment)]
-    assert len(comments) == 0, (
-        "properties that are commented out would break the the constructor of the class XMLProperty, "
-        "if they are not removed in the parsing process"
-    )
+    def test_comment_removal(self) -> None:
+        from_file = parse_and_clean_xml_file("testdata/xml-data/test-data-systematic.xml")
+        comments = [e for e in from_file.iter() if isinstance(e, etree._Comment)]
+        self.assertEqual(
+            len(comments),
+            0,
+            msg="properties that are commented out would break the the constructor of the class XMLProperty, "
+            "if they are not removed in the parsing process",
+        )

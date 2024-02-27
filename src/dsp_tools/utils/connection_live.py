3e9b4c253ce8c309d31fdcccb7e332606932b2c9
Nora-Olivia-Ammann <103038637+Nora-Olivia-Ammann@users.noreply.github.com>
2024-02-27 16:19:06 +0100

    Update connection_live.py extract function

--- src/dsp_tools/utils/connection_live.py
+++ src/dsp_tools/utils/connection_live.py
@@ -252,7 +252,6 @@ class ConnectionLive:
         Returns:
             the return value of action
         """
-        logfiles = ", ".join([handler.baseFilename for handler in logger.handlers if isinstance(handler, FileHandler)])
         action = partial(self.session.request, **params.as_kwargs())
         for i in range(7):
             try:
@@ -269,20 +268,29 @@ class ConnectionLive:
             self._log_response(response)
             if response.status_code == HTTP_OK:
                 return response
-            elif "v2/authentication" in params.url and response.status_code == HTTP_UNAUTHORIZED:
-                raise BadCredentialsError("Bad credentials")
-            elif any(x in response.text for x in ["OntologyConstraintException", "DuplicateValueException"]):
-                msg = f"Permanently unable to execute the network action. See logs for more details: {logfiles}"
-                raise PermanentConnectionError(msg)
-            elif not self._in_testing_environment():
-                self._log_and_sleep(reason="Non-200 response code", retry_counter=i, exc_info=False)
-                continue
-            else:
-                msg = f"Permanently unable to execute the network action. See logs for more details: {logfiles}"
-                raise PermanentConnectionError(msg)
+
+            self._handle_non_ok_responses(response, params.url, i)
 
         # after 7 vain attempts to create a response, try it a last time and let it escalate
         return action()
+
+    def _handle_non_ok_responses(self, response: Response, request_url: str, retry_counter: int) -> None:
+        logfiles = ", ".join([handler.baseFilename for handler in logger.handlers if isinstance(handler, FileHandler)])
+
+        if "v2/authentication" in request_url and response.status_code == HTTP_UNAUTHORIZED:
+            raise BadCredentialsError("Bad credentials")
+
+        elif any(x in response.text for x in ["OntologyConstraintException", "DuplicateValueException"]):
+            msg = f"Permanently unable to execute the network action. See logs for more details: {logfiles}"
+            raise PermanentConnectionError(msg)
+
+        elif not self._in_testing_environment():
+            self._log_and_sleep(reason="Non-200 response code", retry_counter=retry_counter, exc_info=False)
+            return None
+
+        else:
+            msg = f"Permanently unable to execute the network action. See logs for more details: {logfiles}"
+            raise PermanentConnectionError(msg)
 
     def _renew_session(self) -> None:
         self.session.close()

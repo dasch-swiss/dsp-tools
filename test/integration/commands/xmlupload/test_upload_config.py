b3249c9eb195a257520aa344ed9174c319c3ffcf
Nora-Olivia-Ammann <103038637+Nora-Olivia-Ammann@users.noreply.github.com>
2024-02-29 14:21:13 +0100

    -

---
+++ test/integration/commands/xmlupload/test_upload_config.py
@@ -0,0 +1,30 @@
+import pytest
+
+from dsp_tools.commands.xmlupload.upload_config import UploadConfig
+
+
+def test_save_location() -> None:
+    server = "dasch.swiss"
+    shortcode = "9999"
+    onto_name = "testonto"
+    expected_path = f"/.dsp-tools/xmluploads/{server}/{shortcode}/{onto_name}"
+    config = UploadConfig()
+    config_with_save_location = config.with_server_info(
+        server=server,
+        shortcode=shortcode,
+        onto_name=onto_name,
+    )
+    diagnostics = config_with_save_location.diagnostics
+    result = str(diagnostics.save_location)
+    assert result.endswith(expected_path)
+    try:
+        diagnostics.save_location.rmdir()
+        diagnostics.save_location.parent.rmdir()
+        diagnostics.save_location.parent.parent.rmdir()
+    except OSError:
+        # there was already stuff in the folder before this test: do nothing
+        pass
+
+
+if __name__ == "__main__":
+    pytest.main([__file__])

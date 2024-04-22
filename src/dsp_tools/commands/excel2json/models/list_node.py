6f016e481fab8e5ff6539731347d3b12230077b5
Nora-Olivia-Ammann <103038637+Nora-Olivia-Ammann@users.noreply.github.com>
2024-04-22 17:25:25 +0200

    change into pydantic classes

--- src/dsp_tools/commands/excel2json/models/list_node.py
+++ src/dsp_tools/commands/excel2json/models/list_node.py
@@ -1,40 +1,19 @@
 from __future__ import annotations
 
-from dataclasses import dataclass
-from dataclasses import field
 from typing import Any
 from typing import Literal
+
+from pydantic import BaseModel
+from pydantic import Field
 
 LanguageTag = Literal["en", "de", "fr", "it", "rm"]
 
 
-@dataclass
-class ListRoot:
-    _id: str
+class ListNode(BaseModel):
+    id_: str
     labels: dict[LanguageTag, str]
-    nodes: list[ListNode]
-    comments: dict[LanguageTag, str] | None = None
-
-    def to_json(self) -> dict[str, Any]:
-        lst_root = self._make_myself()
-        lst_root["nodes"] = self._make_nodes()
-        return lst_root
-
-    def _make_nodes(self) -> list[dict[str, Any]]:
-        return [nd.to_json() for nd in self.nodes]
-
-    def _make_myself(self) -> dict[str, Any]:
-        lst = {"name": self._id, "labels": self.labels}
-        if self.comments:
-            lst.update({"comments": self.comments})
-        return lst
-
-
-@dataclass
-class ListNode:
-    _id: str
-    labels: dict[LanguageTag, str]
-    sub_nodes: list[ListNode] = field(default_factory=list)
+    row_number: int
+    sub_nodes: list[ListNode] = Field(default_factory=list)
 
     def to_json(self) -> dict[str, Any]:
         node = self._make_myself()
@@ -46,4 +25,25 @@ class ListNode:
         return [nd.to_json() for nd in self.sub_nodes]
 
     def _make_myself(self) -> dict[str, Any]:
-        return {"name": self._id, "labels": self.labels}
+        return {"name": self.id_, "labels": self.labels}
+
+
+class ListRoot(BaseModel):
+    id_: str
+    labels: dict[LanguageTag, str]
+    nodes: list[ListNode] = Field(default_factory=list)
+    comments: dict[LanguageTag, str] | None = None
+
+    def to_json(self) -> dict[str, Any]:
+        lst_root = self._make_myself()
+        lst_root["nodes"] = self._make_nodes()
+        return lst_root
+
+    def _make_nodes(self) -> list[dict[str, Any]]:
+        return [nd.to_json() for nd in self.nodes]
+
+    def _make_myself(self) -> dict[str, Any]:
+        lst = {"name": self.id_, "labels": self.labels}
+        if self.comments:
+            lst.update({"comments": self.comments})
+        return lst
--- test/unittests/commands/excel2json/test_list_node.py
+++ test/unittests/commands/excel2json/test_list_node.py
@@ -7,10 +7,10 @@ from dsp_tools.commands.excel2json.models.list_node import ListRoot
 class TestListRoot:
     def test_make_myself_comments(self) -> None:
         root = ListRoot(
-            "RootID",
-            {"en": "root_label_en", "de": "root_label_de"},
-            [],
-            {"en": "root_comment_en", "de": "root_comment_de"},
+            id_="RootID",
+            labels={"en": "root_label_en", "de": "root_label_de"},
+            nodes=[],
+            comments={"en": "root_comment_en", "de": "root_comment_de"},
         )
         expected = {
             "name": "RootID",
@@ -22,22 +22,22 @@ class TestListRoot:
 
     def test_make_myself_no_comments(self) -> None:
         root = ListRoot(
-            "RootID",
-            {"en": "root_label_en", "de": "root_label_de"},
-            [],
+            id_="RootID",
+            labels={"en": "root_label_en", "de": "root_label_de"},
+            nodes=[],
         )
         expected = {"name": "RootID", "labels": {"en": "root_label_en", "de": "root_label_de"}}
         res = root._make_myself()
         assert expected == res
 
     def test_to_json(self) -> None:
-        sub_1 = ListNode("Node_1", {"en": "Node_1_label_en"})
-        sub_21 = ListNode("SubNode_21", {"en": "SubNode_21_label_en"})
-        sub_2 = ListNode("Node_2", {"en": "Node_2_label_en"}, [sub_21])
+        sub_1 = ListNode(id_="Node_1", labels={"en": "Node_1_label_en"}, row_number=1)
+        sub_21 = ListNode(id_="SubNode_21", labels={"en": "SubNode_21_label_en"}, row_number=3)
+        sub_2 = ListNode(id_="Node_2", labels={"en": "Node_2_label_en"}, row_number=2, sub_nodes=[sub_21])
         root = ListRoot(
-            "RootID",
-            {"en": "root_label_en", "de": "root_label_de"},
-            [sub_1, sub_2],
+            id_="RootID",
+            labels={"en": "root_label_en", "de": "root_label_de"},
+            nodes=[sub_1, sub_2],
         )
         expected = {
             "name": "RootID",
@@ -57,16 +57,16 @@ class TestListRoot:
 
 class TestListNode:
     def test_make_myself(self) -> None:
-        nd = ListNode("NodeID", {"en": "node_label_en"})
+        nd = ListNode(id_="NodeID", labels={"en": "node_label_en"}, row_number=1)
         expected = {"name": "NodeID", "labels": {"en": "node_label_en"}}
         res = nd._make_myself()
         assert res == expected
 
     def test_to_json(self) -> None:
-        sub_1 = ListNode("SubNode_1", {"en": "SubNode_1_label_en"})
-        sub_21 = ListNode("SubNode_21", {"en": "SubNode_21_label_en"})
-        sub_2 = ListNode("SubNode_2", {"en": "SubNode_2_label_en"}, [sub_21])
-        test_nd = ListNode("NodeID", {"en": "node_label_en"}, [sub_1, sub_2])
+        sub_1 = ListNode(id_="SubNode_1", labels={"en": "SubNode_1_label_en"}, row_number=1)
+        sub_21 = ListNode(id_="SubNode_21", labels={"en": "SubNode_21_label_en"}, row_number=3)
+        sub_2 = ListNode(id_="SubNode_2", labels={"en": "SubNode_2_label_en"}, row_number=2, sub_nodes=[sub_21])
+        test_nd = ListNode(id_="NodeID", labels={"en": "node_label_en"}, row_number=0, sub_nodes=[sub_1, sub_2])
 
         expected = {
             "name": "NodeID",

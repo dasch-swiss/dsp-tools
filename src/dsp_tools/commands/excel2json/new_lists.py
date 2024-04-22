d7647cabd54401b2f361b45a7915f0b18ce0b970
Nora-Olivia-Ammann <103038637+Nora-Olivia-Ammann@users.noreply.github.com>
2024-04-22 16:25:22 +0200

    -

--- src/dsp_tools/commands/excel2json/new_lists.py
+++ src/dsp_tools/commands/excel2json/new_lists.py
@@ -4,10 +4,10 @@ from dsp_tools.commands.excel2json.models.list_node import ListNode
 from dsp_tools.utils.shared import simplify_name
 
 
-def _make_single_node(node_series: pd.Series, depth_number: int) -> ListNode:
+def _make_single_node(node_series: pd.Series[str], depth_number: int) -> ListNode:
     cols = [x for x in node_series.index if x.endswith(f"_{depth_number}")]
     labels = {x.rstrip(f"_{depth_number}"): node_series[x] for x in cols}
-    return ListNode(node_series.get("ID (optional)"), labels)
+    return ListNode(node_series["ID (optional)"], labels)
 
 
 def _fill_ids(df: pd.DataFrame) -> pd.DataFrame:

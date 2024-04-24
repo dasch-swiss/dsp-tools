b7e76dd9a781dd9e5df325059bf3eb3fc15c8f75
Nora-Olivia-Ammann <103038637+Nora-Olivia-Ammann@users.noreply.github.com>
2024-04-24 11:30:59 +0200

    get labels, langs, columns

--- src/dsp_tools/commands/excel2json/new_lists.py
+++ src/dsp_tools/commands/excel2json/new_lists.py
@@ -3,11 +3,66 @@ from __future__ import annotations
 import pandas as pd
 import regex
 
+from dsp_tools.commands.excel2json.models.list_node import ListNode
+from dsp_tools.commands.excel2json.models.list_node import ListRoot
 from dsp_tools.models.exceptions import InputError
 
 
-def _get_preferred_language_for_id(columns: pd.Series[str]) -> str:
-    match = [res.group(1) for x in columns if (res := regex.search(r"^(en|de|fr|it|rm)_\d+$", x))]
+def _make_one_list(df: pd.DataFrame, sheet_name: str, excel_name: str) -> ListRoot:
+    get_lang_col = _get_preferred_language_for_id(df.columns, "list")
+    groups = df.groupby(f"{get_lang_col}_list")
+    if len(groups) != 1:
+        raise InputError(
+            f"The Excel: '{excel_name}' and sheet: '{sheet_name}' must only have one unique value in the list column."
+        )
+    df = groups.get_group(groups.first().name)
+    node_cols = [res.group(1) for x in df.columns if (res := regex.search(r"^[en|de|fr|it|rm]_(\d)+$", x))]
+    nodes = []
+    for col in sorted(node_cols):
+        nodes.extend(_create_nodes(df, col))
+    root = ListRoot(
+        id_=_get_id(df.iloc[0], "list"),
+        labels=_get_labels(df.iloc[0], "list"),
+        nodes=nodes,
+    )
+    return root
+
+
+def _create_nodes(df: pd.DataFrame, col_number: str) -> list[ListNode]:
+    nodes = []
+    preferred_lang = _get_preferred_language_for_id(df.columns, "list")
+    groups = df.groupby(f"{preferred_lang}_{col_number}")
+    for name, group in groups:
+        nodes.append(_make_one_node(group, col_number))
+    # go through the nodes and add the children
+    return nodes
+
+
+def _make_one_node(row: pd.Series, col_number: str) -> ListNode:
+    node_id = _get_id(row, col_number)
+    labels = _get_labels(row, col_number)
+    return ListNode(node_id, labels, int(row.index))
+
+
+def _get_id(row: pd.Series, col_number: str) -> str:
+    return (
+        row.get("ID (optional)")
+        if pd.notna(row["ID (optional)"])
+        else row.get(f"{_get_preferred_language_for_id(row.index, str(col_number))}_{col_number}")
+    )
+
+
+def _get_labels(row: pd.Series, col_ending: str) -> dict[str, str]:
+    languages = _get_all_languages_for_columns(row.index, col_ending)
+    return {lang: row.get(f"{lang}_{col_ending}") for lang in languages if pd.notna(row[f"{lang}_{col_ending}"])}
+
+
+def _get_all_languages_for_columns(columns: pd.Index[str], ending: str) -> list[str]:
+    return [res.group(1) for x in columns if (res := regex.search(rf"^(en|de|fr|it|rm)_{ending}$", x))]
+
+
+def _get_preferred_language_for_id(columns: pd.Index[str], ending: str) -> str:
+    match = [res.group(1) for x in columns if (res := regex.search(rf"^(en|de|fr|it|rm)_{ending}+$", x))]
     if "en" in match:
         return "en"
     elif "de" in match:
--- test/unittests/commands/excel2json/test_lists.py
+++ test/unittests/commands/excel2json/test_lists.py
@@ -2,38 +2,102 @@ import pandas as pd
 import pytest
 import regex
 
+from dsp_tools.commands.excel2json.new_lists import _get_all_languages_for_columns
+from dsp_tools.commands.excel2json.new_lists import _get_labels
 from dsp_tools.commands.excel2json.new_lists import _get_preferred_language_for_id
 from dsp_tools.models.exceptions import InputError
 
 
+class TestGetLabels:
+    def test_correct_labels_for_all_languages(self) -> None:
+        row = pd.Series(
+            {
+                "en_1": "Hello",
+                "de_1": "Hallo",
+                "fr_1": "Bonjour",
+                "it_1": "Ciao",
+                "rm_1": "Bun di",
+                "en_2": pd.NA,
+                "de_2": pd.NA,
+            }
+        )
+        col_ending = "1"
+        expected = {"en": "Hello", "de": "Hallo", "fr": "Bonjour", "it": "Ciao", "rm": "Bun di"}
+        assert _get_labels(row, col_ending) == expected
+
+    def test_returns_correct_labels_for_some_languages(self) -> None:
+        row = pd.Series(
+            {
+                "en_1": "Hello",
+                "de_1": pd.NA,
+                "fr_1": "Bonjour",
+                "it_1": pd.NA,
+                "rm_1": "Bun di",
+                "en_2": "English_2",
+            }
+        )
+        col_ending = "1"
+        assert _get_labels(row, col_ending) == {"en": "Hello", "fr": "Bonjour", "rm": "Bun di"}
+
+    def test_returns_empty_dict_for_no_languages(self) -> None:
+        row = pd.Series(
+            {
+                "en_1": pd.NA,
+                "de_1": pd.NA,
+                "fr_1": pd.NA,
+                "it_1": pd.NA,
+                "rm_1": pd.NA,
+                "en_2": pd.NA,
+                "de_2": pd.NA,
+                "fr_2": pd.NA,
+                "it_2": pd.NA,
+                "rm_2": pd.NA,
+            }
+        )
+        col_ending = "1"
+        assert not _get_labels(row, col_ending)
+
+
+class TestGetAllLanguagesForColumns:
+    def test_get_all_languages_for_columns_returns_correct_languages(self) -> None:
+        columns = pd.Index(["en_1", "de_1", "fr_1", "it_1", "rm_1", "en_2"])
+        ending = "1"
+        assert _get_all_languages_for_columns(columns, ending) == ["en", "de", "fr", "it", "rm"]
+
+    def test_get_all_languages_for_columns_returns_empty_list_for_no_match(self) -> None:
+        columns = pd.Index(["en_1", "de_1", "fr_1", "it_1", "rm_1", "en_2", "de_2", "fr_2", "it_2", "rm_2"])
+        ending = "3"
+        assert not _get_all_languages_for_columns(columns, ending)
+
+
 class TestGetPreferredLanguageForId:
     def test_get_preferred_language_for_id_en(self) -> None:
-        columns = pd.Series(["en_1", "de_2", "fr_3", "it_4", "rm_5"])
-        assert _get_preferred_language_for_id(columns) == "en"
+        columns = pd.Index(["en_1", "de_1", "fr_1", "it_1", "rm_1"])
+        assert _get_preferred_language_for_id(columns, "1") == "en"
 
     def test_get_preferred_language_for_id_de(self) -> None:
-        columns = pd.Series(["de_1", "fr_2", "it_3", "rm_4"])
-        assert _get_preferred_language_for_id(columns) == "de"
+        columns = pd.Index(["de_1", "fr_1", "it_1", "rm_1"])
+        assert _get_preferred_language_for_id(columns, "1") == "de"
 
     def test_get_preferred_language_for_id_fr(self) -> None:
-        columns = pd.Series(["fr_1", "it_2", "rm_3"])
-        assert _get_preferred_language_for_id(columns) == "fr"
+        columns = pd.Index(["fr_1", "it_1", "rm_1"])
+        assert _get_preferred_language_for_id(columns, "1") == "fr"
 
     def test_get_preferred_language_for_id_it(self) -> None:
-        columns = pd.Series(["it_1", "rm_2"])
-        assert _get_preferred_language_for_id(columns) == "it"
+        columns = pd.Index(["it_1", "rm_1"])
+        assert _get_preferred_language_for_id(columns, "1") == "it"
 
     def test_get_preferred_language_for_id_rm(self) -> None:
-        columns = pd.Series(["rm_1"])
-        assert _get_preferred_language_for_id(columns) == "rm"
+        columns = pd.Index(["rm_1"])
+        assert _get_preferred_language_for_id(columns, "1") == "rm"
 
     def test_get_preferred_language_for_id_raises(self) -> None:
-        columns = pd.Series(["es_6"])
+        columns = pd.Index(["es_1"])
         msg = regex.escape(
-            "The columns may only contain the languages: 'en', 'de', 'fr', 'it', 'rm'.\n" "The columns are: es_6"
+            "The columns may only contain the languages: 'en', 'de', 'fr', 'it', 'rm'.\n" "The columns are: es_1"
         )
         with pytest.raises(InputError, match=msg):
-            _get_preferred_language_for_id(columns)
+            _get_preferred_language_for_id(columns, "1")
 
 
 if __name__ == "__main__":

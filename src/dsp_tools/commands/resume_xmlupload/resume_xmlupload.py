ce78f61e02ff6771ace0d075697f5a27c352b9ec
Johannes Nussbaum <johannes.nussbaum@dasch.swiss>
2024-04-30 14:02:26 +0200

    refactor resume_xmlupload()

--- src/dsp_tools/commands/resume_xmlupload/resume_xmlupload.py
+++ src/dsp_tools/commands/resume_xmlupload/resume_xmlupload.py
@@ -1,7 +1,5 @@
 import pickle
 import sys
-from copy import deepcopy
-from dataclasses import replace
 
 from loguru import logger
 from termcolor import colored
@@ -35,33 +33,9 @@ def resume_xmlupload(server: str, user: str, password: str, sipi: str, skip_firs
     """
     upload_state = _read_upload_state_from_disk(server)
     if skip_first_resource:
-        if len(upload_state.pending_resources) > 0:
-            new_pending = deepcopy(upload_state.pending_resources)
-            new_pending.pop(0)
-            upload_state = replace(upload_state, pending_resources=new_pending)
-        else:
-            msg = (
-                "The list of pending resources is empty.\n"
-                "It is not yet possible to skip the first item of the stashed properties.\n"
-                "Do you want to continue with the upload of the stashed properties anyway? [y/n]"
-            )
-            resp = None
-            while resp not in ["y", "n"]:
-                resp = input(colored(msg, color="red"))
-            if resp == "n":
-                sys.exit(1)
+        _skip_first_resource(upload_state)
 
-    previous_successful = len(upload_state.iri_resolver.lookup)
-    previous_failed = len(upload_state.failed_uploads)
-    previous_total = previous_successful + previous_failed
-    msg = (
-        f"Resuming upload for project {upload_state.config.shortcode} on server {server}. "
-        f"Number of resources uploaded until now: {previous_total}"
-    )
-    if previous_failed:
-        msg += f" ({previous_failed} of them failed)"
-    logger.info(msg)
-    print("\n==========================\n" + msg + "\n==========================\n")
+    _print_and_log(upload_state, server)
 
     con = ConnectionLive(server)
     con.login(user, password)
@@ -87,3 +61,33 @@ def _read_upload_state_from_disk(server: str) -> UploadState:
     with open(save_location, "rb") as f:
         saved_state: UploadState = pickle.load(f)  # noqa: S301 (deserialization of untrusted data)
     return saved_state
+
+
+def _skip_first_resource(upload_state: UploadState) -> None:
+    if len(upload_state.pending_resources) > 0:
+        upload_state.pending_resources.pop(0)
+    else:
+        msg = (
+            "The list of pending resources is empty.\n"
+            "It is not yet possible to skip the first item of the stashed properties.\n"
+            "Do you want to continue with the upload of the stashed properties anyway? [y/n]"
+        )
+        resp = None
+        while resp not in ["y", "n"]:
+            resp = input(colored(msg, color="red"))
+        if resp == "n":
+            sys.exit(1)
+
+
+def _print_and_log(upload_state: UploadState, server: str) -> None:
+    previous_successful = len(upload_state.iri_resolver.lookup)
+    previous_failed = len(upload_state.failed_uploads)
+    previous_total = previous_successful + previous_failed
+    msg = (
+        f"Resuming upload for project {upload_state.config.shortcode} on server {server}. "
+        f"Number of resources uploaded until now: {previous_total}"
+    )
+    if previous_failed:
+        msg += f" ({previous_failed} of them failed)"
+    logger.info(msg)
+    print("\n==========================\n" + msg + "\n==========================\n")

c5c42da926e9de94668451f4b11ff282c7c4fe60
Nora-Olivia-Ammann <103038637+Nora-Olivia-Ammann@users.noreply.github.com>
2024-05-07 10:45:44 +0200

    Update src/dsp_tools/commands/excel2json/project.py
    Co-authored-by: Johannes Nussbaum <39048939+jnussbaum@users.noreply.github.com>

--- src/dsp_tools/commands/excel2json/project.py
+++ src/dsp_tools/commands/excel2json/project.py
@@ -97,7 +97,7 @@ def new_excel2json(
     ::
 
         data_model_files
-        |-- lists (files must contain the word "list" in their name)
+        |-- lists
         |   |-- list.xlsx
         |   `-- list_2.xlsx
         `-- onto_name (onto_label)

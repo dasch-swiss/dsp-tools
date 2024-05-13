6d90b8fb401965dd2a651b1dc114ef0226bfd3a0
Nora-Olivia-Ammann <103038637+Nora-Olivia-Ammann@users.noreply.github.com>
2024-05-13 16:04:34 +0200

    refactor(xmlupload): reorganise models file (#953)

--- src/dsp_tools/commands/xmlupload/resource_create_client.py
+++ src/dsp_tools/commands/xmlupload/resource_create_client.py
@@ -14,6 +14,7 @@ from dsp_tools.commands.xmlupload.models.deserialise.deserialise_value import XM
 from dsp_tools.commands.xmlupload.models.deserialise.xmlresource import BitstreamInfo
 from dsp_tools.commands.xmlupload.models.deserialise.xmlresource import XMLResource
 from dsp_tools.commands.xmlupload.models.formatted_text_value import FormattedTextValue
+from dsp_tools.commands.xmlupload.models.namespace_context import get_json_ld_context_for_project
 from dsp_tools.commands.xmlupload.models.permission import Permissions
 from dsp_tools.models.exceptions import BaseError
 from dsp_tools.models.exceptions import UserError
@@ -29,7 +30,7 @@ class ResourceCreateClient:
     con: Connection
     project_iri: str
     iri_resolver: IriResolver
-    json_ld_context: dict[str, str]
+    project_onto_dict: dict[str, str]
     permissions_lookup: dict[str, Permissions]
     listnode_lookup: dict[str, str]
     media_previously_ingested: bool = False
@@ -69,11 +70,12 @@ class ResourceCreateClient:
         resource_iri = resource.iri
         if resource.ark:
             resource_iri = convert_ark_v0_to_resource_iri(resource.ark)
+        context = get_json_ld_context_for_project(self.project_onto_dict)
         res = {
             "@type": resource.restype,
             "rdfs:label": resource.label,
             "knora-api:attachedToProject": {"@id": self.project_iri},
-            "@context": self.json_ld_context,
+            "@context": context,
         }
         if resource_iri:
             res["@id"] = resource_iri

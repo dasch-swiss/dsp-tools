c69c96363b0184637d1a043c07f35194579d5b72
Nora-Olivia-Ammann <103038637+Nora-Olivia-Ammann@users.noreply.github.com>
2024-05-13 13:04:30 +0200

    refactor(xmlupload): value models (#949)

--- test/unittests/commands/xmlupload/test_value_deserialise.py
+++ test/unittests/commands/xmlupload/test_value_deserialise.py
@@ -3,8 +3,8 @@ import unittest
 import pytest
 from lxml import etree
 
+from dsp_tools.commands.xmlupload.models.deserialise.deserialise_value import XMLValue
 from dsp_tools.commands.xmlupload.models.formatted_text_value import FormattedTextValue
-from dsp_tools.commands.xmlupload.models.xmlvalue import XMLValue
 
 # ruff: noqa: PT009 (pytest-unittest-assertion) (remove this line when pytest is used instead of unittest)
 

e58108f718199c9adef06fa40f31878bcf573f83
Nora-Olivia-Ammann <103038637+Nora-Olivia-Ammann@users.noreply.github.com>
2024-05-13 16:15:52 +0200

    add serialise value

---
+++ src/dsp_tools/commands/xmlupload/models/serialise/serialise_value.py
@@ -0,0 +1,50 @@
+from __future__ import annotations
+
+from dataclasses import dataclass
+from typing import Any
+from typing import Optional
+from typing import Protocol
+
+
+@dataclass
+class PropertySerialise:
+    property_name: str
+    values: list[Value]
+
+    def serialise(self) -> dict[str, Any]:
+        """Serialise the property and all its values."""
+        return {self.property_name: [value.serialise() for value in self.values]}
+
+
+@dataclass
+class Value(Protocol):
+    """A value to be serialised."""
+
+    def serialise(self) -> dict[str, Any]:
+        """Serialise the value."""
+
+
+@dataclass
+class URIValue:
+    """A URI to be serialised."""
+
+    value: list[str]
+    permissions: str
+    comment: Optional[str]
+
+    def serialise(self) -> dict[str, Any]:
+        """Serialise the URI value."""
+        serialised = self._make_value()
+        if self.comment:
+            serialised["knora-api:valueHasComment"] = self.comment
+        return serialised
+
+    def _make_value(self) -> dict[str, Any]:
+        return {
+            "@type": "knora-api:UriValue",
+            "knora-api:hasPermissions": self.permissions,
+            "knora-api:uriValueAsUri": {
+                "@type": "xsd:anyURI",
+                "@value": self.value,
+            },
+        }

a1f41bb95b2e68b8702121e4fb3a07edc35558f5
Christian Kleinbölting <christian.kleinboelting@dasch.swiss>
2024-05-14 14:30:47 +0200

    add DSpIngestClient

--- src/dsp_tools/commands/xmlupload/models/sipi.py
+++ src/dsp_tools/commands/xmlupload/models/sipi.py
@@ -1,7 +1,8 @@
 from dataclasses import dataclass
 from pathlib import Path
-from typing import Any
 from typing import Protocol
+
+import requests
 
 from dsp_tools.utils.connection import Connection
 
@@ -12,7 +13,35 @@ class IngestResponse:
 
 
 class IngestClient(Protocol):
-    def ingest(self, shortcode: str, filepath: Path) -> IngestResponse: ...
+    def ingest(self, shortcode: str, filepath: Path) -> IngestResponse:
+        """
+        Ingests a file to the asset repository
+
+        Args:
+            shortcode: the shortcode of the project
+            filepath: path to the file, could be either absolute or relative
+
+        Returns:
+            API response
+        """
+        ...
+
+
+@dataclass(frozen=True)
+class DspIngestClient(IngestClient):
+    dsp_ingest_url: str
+    token: str
+
+    def ingest(self, shortcode: str, filepath: Path) -> IngestResponse:
+        with open(filepath, "rb") as binary_io:
+            url = f"{self.dsp_ingest_url}/projects/{shortcode}/assets/ingest/{filepath.name}"
+            res = requests.post(
+                url=url,
+                headers={"Authorization": f"Bearer {self.token}", "Content-Type": "application/octet-stream"},
+                data=binary_io,
+                timeout=60,
+            )
+            return IngestResponse(internal_filename=res.json()["internalFilename"])
 
 
 @dataclass(frozen=True)
@@ -25,21 +54,7 @@ class Sipi(IngestClient):
     con: Connection
 
     def ingest(self, shortcode: str, filepath: Path) -> IngestResponse:  # noqa: ARG002 (Unused method argument)
-        print("Ingesting file...")
-        res = self.upload_bitstream(filepath)
-        return IngestResponse(internal_filename=res["uploadedFiles"][0]["internalFilename"])
-
-    def upload_bitstream(self, filepath: Path) -> dict[str, Any]:
-        """
-        Uploads a bitstream to the Sipi server
-
-        Args:
-            filepath: path to the file, could be either absolute or relative
-
-        Returns:
-            API response
-        """
-        with open(filepath, "rb") as bitstream_file:
-            files = {"file": (filepath.name, bitstream_file)}
+        with open(filepath, "rb") as binary_io:
+            files = {"file": (filepath.name, binary_io)}
             res = self.con.post(route="/upload", files=files)
-            return res
+            return IngestResponse(internal_filename=res["uploadedFiles"][0]["internalFilename"])

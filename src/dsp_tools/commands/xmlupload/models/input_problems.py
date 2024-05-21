d7ab2299d8d64583cc5aa282115b4ae4cc19ba9f
Nora-Olivia-Ammann <103038637+Nora-Olivia-Ammann@users.noreply.github.com>
2024-05-21 09:45:34 +0200

    add input problem

--- src/dsp_tools/commands/xmlupload/models/input_problems.py
+++ src/dsp_tools/commands/xmlupload/models/input_problems.py
@@ -1,4 +1,22 @@
+from __future__ import annotations
+
 from dataclasses import dataclass
+
+list_separator = "\n    - "
+medium_separator = "\n----------------------------\n"
+
+
+@dataclass(frozen=True)
+class AllIIIFUriProblems:
+    """Information about all problems with IIIF URIs."""
+
+    problems: list[IIIFUriProblem]
+
+    def get_msg(self) -> str:
+        """Get a message describing all problems with the IIIF URIs."""
+        msg = "There were problems with the following IIIF URI(s):"
+        all_msg = [problem.get_msg() for problem in self.problems]
+        return msg + medium_separator + medium_separator.join(all_msg)
 
 
 @dataclass(frozen=True)
@@ -10,3 +28,29 @@ class IIIFUriProblem:
     status_code: int | None = None
     response_text: str | None = None
     thrown_exception: Exception | None = None
+
+    def get_msg(self) -> str:
+        """Get a message describing the problem with the IIIF URI."""
+        msg = [f"URI: {self.uri}"]
+        if self.passed_regex:
+            msg.append("Passed the internal regex check.")
+        else:
+            msg.append("Did not pass the internal regex check.")
+        if self.status_code is not None:
+            msg.extend(self._bad_status_code_msg())
+        else:
+            msg.extend(self._exception_msg())
+        return list_separator.join(msg)
+
+    def _exception_msg(self) -> list[str]:
+        return [
+            f"A connection error occurred during the network call: {self.thrown_exception.__class__.__name__}",
+            f"Original message: {self.thrown_exception}",
+        ]
+
+    def _bad_status_code_msg(self) -> list[str]:
+        return [
+            "The server did not respond as expected.",
+            f"Status code: {self.status_code}",
+            f"Response text: {self.response_text}",
+        ]
---
+++ test/unittests/commands/xmlupload/models/test_input_problems.py
@@ -0,0 +1,85 @@
+import pytest
+from requests.exceptions import RequestException
+
+from dsp_tools.commands.xmlupload.models.input_problems import AllIIIFUriProblems
+from dsp_tools.commands.xmlupload.models.input_problems import IIIFUriProblem
+
+
+@pytest.fixture()
+def iiif_exception() -> IIIFUriProblem:
+    return IIIFUriProblem(
+        uri="http://www.example.org/",
+        passed_regex=False,
+        thrown_exception=RequestException("This is the request exception."),
+    )
+
+
+@pytest.fixture()
+def iiif_uri_problem_ok_regex() -> IIIFUriProblem:
+    return IIIFUriProblem(
+        uri="https://iiif.dasch.swiss/0811/1Oi7mdiLsG7-FmFgp0xz2xU.jp2/full/837,530/0/default.jp2",
+        passed_regex=True,
+        status_code=404,
+        response_text="This is the response text.",
+    )
+
+
+@pytest.fixture()
+def iiif_uri_problem_bad_regex() -> IIIFUriProblem:
+    return IIIFUriProblem(
+        uri="http://www.example.org/",
+        passed_regex=False,
+        status_code=404,
+        response_text="This is the response text.",
+    )
+
+
+def test_iiif_uri_problem_ok_regex(iiif_uri_problem_ok_regex: IIIFUriProblem) -> None:
+    assert iiif_uri_problem_ok_regex.get_msg() == (
+        "URI: https://iiif.dasch.swiss/0811/1Oi7mdiLsG7-FmFgp0xz2xU.jp2/full/837,530/0/default.jp2\n"
+        "    - Passed the internal regex check.\n"
+        "    - The server did not respond as expected.\n"
+        "    - Status code: 404\n"
+        "    - Response text: This is the response text."
+    )
+
+
+def test_iiif_uri_problem_bad_regex(iiif_uri_problem_bad_regex: IIIFUriProblem) -> None:
+    assert iiif_uri_problem_bad_regex.get_msg() == (
+        "URI: http://www.example.org/\n"
+        "    - Did not pass the internal regex check.\n"
+        "    - The server did not respond as expected.\n"
+        "    - Status code: 404\n"
+        "    - Response text: This is the response text."
+    )
+
+
+def test_iiif_uri_problem_exception(iiif_exception: IIIFUriProblem) -> None:
+    assert iiif_exception.get_msg() == (
+        "URI: http://www.example.org/\n"
+        "    - Did not pass the internal regex check.\n"
+        "    - A connection error occurred during the network call: RequestException\n"
+        "    - Original message: This is the request exception."
+    )
+
+
+def test_all(iiif_exception: IIIFUriProblem, iiif_uri_problem_ok_regex: IIIFUriProblem) -> None:
+    problem = AllIIIFUriProblems(problems=[iiif_exception, iiif_uri_problem_ok_regex])
+    assert problem.get_msg() == (
+        "There were problems with the following IIIF URI(s):\n"
+        "----------------------------\n"
+        "URI: http://www.example.org/\n"
+        "    - Did not pass the internal regex check.\n"
+        "    - A connection error occurred during the network call: RequestException\n"
+        "    - Original message: This is the request exception."
+        "\n----------------------------\n"
+        "URI: https://iiif.dasch.swiss/0811/1Oi7mdiLsG7-FmFgp0xz2xU.jp2/full/837,530/0/default.jp2\n"
+        "    - Passed the internal regex check.\n"
+        "    - The server did not respond as expected.\n"
+        "    - Status code: 404\n"
+        "    - Response text: This is the response text."
+    )
+
+
+if __name__ == "__main__":
+    pytest.main([__file__])

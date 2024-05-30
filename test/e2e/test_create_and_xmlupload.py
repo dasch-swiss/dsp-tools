741f8ab49c3ae6d666f6fd67aa2f60e5ae07bb7c
Johannes Nussbaum <johannes.nussbaum@dasch.swiss>
2024-05-30 10:50:51 +0200

    factor out project_iri and onto_iri

--- test/e2e/test_create_and_xmlupload.py
+++ test/e2e/test_create_and_xmlupload.py
@@ -32,10 +32,21 @@ def token(creds: ServerCredentials, project_status: bool) -> str:  # noqa: ARG00
     return token
 
 
-def test_project(creds: ServerCredentials, token: str, project_status: bool) -> None:
+@pytest.fixture()
+def project_iri(creds: ServerCredentials, project_status: bool) -> str:  # noqa: ARG001
+    get_project_route = f"{creds.server}/admin/projects/shortcode/{PROJECT_SHORTCODE}"
+    project_iri: str = requests.get(get_project_route, timeout=3).json()["project"]["id"]
+    return project_iri
+
+
+@pytest.fixture()
+def onto_iri(creds: ServerCredentials) -> str:
+    return f"{creds.server}/ontology/{PROJECT_SHORTCODE}/testonto/v2"
+
+
+def test_project(creds: ServerCredentials, token: str, project_status: bool, onto_iri: str) -> None:
     assert project_status
     get_project_url = f"{creds.server}/admin/projects/shortcode/{PROJECT_SHORTCODE}"
-    onto_iri = f"{creds.server}/ontology/{PROJECT_SHORTCODE}/testonto/v2"
 
     get_project_response = requests.get(get_project_url, headers={"Authorization": f"Bearer {token}"}, timeout=3)
     project = get_project_response.json()["project"]
@@ -72,21 +83,19 @@ def test_project(creds: ServerCredentials, token: str, project_status: bool) -> 
     )
 
 
-def test_xmlupload(creds: ServerCredentials, token: str, project_status: bool) -> None:
+def test_xmlupload(creds: ServerCredentials, token: str, project_status: bool, project_iri: str, onto_iri: str) -> None:
     assert project_status
     xmlupload_status = xmlupload(Path("testdata/xml-data/test-data-e2e.xml"), creds, ".")
     assert xmlupload_status
 
-    img_resources = _get_resources("http://0.0.0.0:3333/ontology/4124/testonto/v2#ImageResource", creds, token)
+    img_resources = _get_resources(f"{onto_iri}#ImageResource", creds, token, project_iri)
     _analyze_img_resources(img_resources)
-    pdf_resources = _get_resources("http://0.0.0.0:3333/ontology/4124/testonto/v2#PDFResource", creds, token)
+    pdf_resources = _get_resources(f"{onto_iri}#PDFResource", creds, token, project_iri)
     _analyze_pdf_resources(pdf_resources)
 
 
-def _get_resources(resclass_iri: str, creds: ServerCredentials, token: str) -> list[dict[str, Any]]:
+def _get_resources(resclass_iri: str, creds: ServerCredentials, token: str, project_iri: str) -> list[dict[str, Any]]:
     resclass_iri_encoded = urllib.parse.quote_plus(resclass_iri)
-    get_project_route = f"{creds.server}/admin/projects/shortcode/{PROJECT_SHORTCODE}"
-    project_iri = requests.get(get_project_route, timeout=3).json()["project"]["id"]
     get_resources_route = f"{creds.server}/v2/resources?resourceClass={resclass_iri_encoded}&page=0"
     headers = {"X-Knora-Accept-Project": project_iri, "Authorization": f"Bearer {token}"}
     response = requests.get(get_resources_route, timeout=3, headers=headers).json()
@@ -97,14 +106,14 @@ def _get_resources(resclass_iri: str, creds: ServerCredentials, token: str) -> l
 def _analyze_img_resources(img_resources: list[dict[str, Any]]) -> None:
     res_labels = sorted([res["rdfs:label"] for res in img_resources])
     assert res_labels == ["Resource 1", "Resource 2"]
+
     res_1 = next(res for res in img_resources if res["rdfs:label"] == "Resource 1")
     assert res_1.get("knora-api:hasStillImageFileValue")
+
     res_2 = next(res for res in img_resources if res["rdfs:label"] == "Resource 2")
     assert res_2.get("knora-api:hasStillImageFileValue")
-    assert sorted([x["knora-api:valueAsString"] for x in res_2["testonto:hasText"]]) == [
-        "first text value",
-        "second text value",
-    ]
+    res_2_text_vals = sorted([x["knora-api:valueAsString"] for x in res_2["testonto:hasText"]])
+    assert res_2_text_vals == ["first text value", "second text value"]
 
 
 def _analyze_pdf_resources(pdf_resources: list[dict[str, Any]]) -> None:

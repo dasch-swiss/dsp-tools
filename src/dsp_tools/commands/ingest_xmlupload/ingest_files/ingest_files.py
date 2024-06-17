02dab20e531dc2f0157a5a7d4b647961a8dc46b3
Johannes Nussbaum <johannes.nussbaum@dasch.swiss>
2024-06-17 17:24:15 +0200

    continue

--- src/dsp_tools/commands/ingest_xmlupload/ingest_files/ingest_files.py
+++ src/dsp_tools/commands/ingest_xmlupload/ingest_files/ingest_files.py
@@ -2,9 +2,11 @@ from pathlib import Path
 from time import sleep
 from typing import Any
 
+import requests
 from loguru import logger
 
 from dsp_tools.cli.args import ServerCredentials
+from dsp_tools.models.exceptions import UserError
 from dsp_tools.utils.connection import Connection
 from dsp_tools.utils.connection_live import ConnectionLive
 
@@ -23,42 +25,48 @@ def ingest_files(creds: ServerCredentials, shortcode: str) -> bool:
     """
     con: Connection = ConnectionLive(creds.server)
     con.login(creds.user, creds.password)
-    _kick_off_ingest(con, creds, shortcode)
+    _kick_off_ingest(con.get_token(), creds, shortcode)
 
     success = False
     while not success:
-        success = _try_download(con, creds, shortcode)
+        success = _try_download(con.get_token(), creds, shortcode)
         sleep(10)
     return True
 
 
-def _kick_off_ingest(con: Connection, creds: ServerCredentials, shortcode: str) -> None:
-    kickoff_url = f"{creds.dsp_ingest_url}/projects/{shortcode}/bulk-ingest"
+def _kick_off_ingest(token: str, creds: ServerCredentials, shortcode: str) -> None:
+    url = f"{creds.dsp_ingest_url}/projects/{shortcode}/bulk-ingest"
     try:
-        con.post(kickoff_url)
-        print("Kicked off the ingest process on the server. Wait until it completes...")
-        logger.info("Kicked off the ingest process on the server. Wait until it completes...")
-    except Exception:  # TODO: catch a specific error
+        res: dict[str, Any] = requests.post(url, headers={"Authorization": f"Bearer {token}"}, timeout=5).json()
+    except Exception:  # TODO: catch a specific error  # noqa: BLE001
         print("Ingest process is already running. Wait until it completes...")
-        logger.info("Ingest process is already running. Wait until it completes...")
+        logger.error("Ingest process is already running. Wait until it completes...")
+        return
+    if res.get("id") != shortcode:
+        raise UserError("Failed to kick off the ingest process.")
+    print("Kicked off the ingest process on the server. Wait until it completes...")
+    logger.info("Kicked off the ingest process on the server. Wait until it completes...")
 
 
-def _try_download(con: Connection, creds: ServerCredentials, shortcode: str) -> bool:
+def _try_download(token: str, creds: ServerCredentials, shortcode: str) -> bool:
     url = f"{creds.dsp_ingest_url}/projects/{shortcode}/bulk-ingest/mapping.csv"
-    try:
-        mapping = con.get(url)
-        print("Ingest process completed.")
-        logger.info("Ingest process completed.")
-        _save_mapping(mapping, shortcode)
-        return True
-    except Exception:  # catch only 409 Conflict error
+    res = requests.get(url, headers={"Authorization": f"Bearer {token}"}, timeout=5)
+    if res.status_code == 409:
         print("Ingest process is still running. Wait until it completes...")
         logger.info("Ingest process is still running. Wait until it completes...")
         return False
+    elif not res.ok:
+        print("Dubious error")
+        logger.error("Dubious error")
+        return False
+    print("Ingest process completed.")
+    logger.info("Ingest process completed.")
+    _save_mapping(res.text, shortcode)
+    return True
 
 
-def _save_mapping(mapping: dict[str, Any], shortcode: str) -> None:
+def _save_mapping(mapping: str, shortcode: str) -> None:
     filepath = Path(f"mapping-{shortcode}.csv")
-    filepath.write_text(mapping["csv"])
+    filepath.write_text(mapping)
     print(f"Saved mapping CSV to '{filepath}'")
     logger.info(f"Saved mapping CSV to '{filepath}'")

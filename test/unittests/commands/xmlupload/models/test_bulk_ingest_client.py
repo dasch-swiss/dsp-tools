8bd48ba6de88af0eb8ba67c7cb34e4988350fa0e
Johannes Nussbaum <johannes.nussbaum@dasch.swiss>
2024-06-20 14:43:38 +0200

    remove redundant authentication test

--- test/unittests/commands/xmlupload/models/test_bulk_ingest_client.py
+++ test/unittests/commands/xmlupload/models/test_bulk_ingest_client.py
@@ -4,7 +4,6 @@ import pytest
 from requests_mock import Mocker
 
 from dsp_tools.commands.ingest_xmlupload.bulk_ingest_client import BulkIngestClient
-from dsp_tools.models.exceptions import BadCredentialsError
 from dsp_tools.models.exceptions import PermanentConnectionError
 
 DSP_INGEST_URL = "https://example.com"
@@ -38,15 +37,6 @@ def test_ingest_success(ingest_client: BulkIngestClient, requests_mock: Mocker, 
     assert req.headers["Authorization"] == "Bearer token"
     assert req.headers["Content-Type"] == "application/octet-stream"
     assert req.body.name == str(tmp_file)
-
-
-def test_ingest_failure_when_authentication_error(
-    ingest_client: BulkIngestClient, requests_mock: Mocker, tmp_file: Path
-) -> None:
-    tmp_file.write_text("<xml></xml>")
-    requests_mock.post(_make_url(tmp_file), status_code=401)
-    with pytest.raises(BadCredentialsError):
-        ingest_client._upload(tmp_file)
 
 
 def test_ingest_failure_when_other_error(

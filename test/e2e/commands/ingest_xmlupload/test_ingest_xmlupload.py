4b05f435eb23ea609673a83b8cd52cfd9572b0e1
Johannes Nussbaum <johannes.nussbaum@dasch.swiss>
2024-06-21 16:36:24 +0200

    remove test of ingest-xmlupload (belongs to next PR)

--- test/e2e/commands/ingest_xmlupload/test_ingest_xmlupload.py
+++ test/e2e/commands/ingest_xmlupload/test_ingest_xmlupload.py
@@ -3,11 +3,9 @@ from typing import Iterator
 
 import pandas as pd
 import pytest
-import regex
 from pytest_unordered import unordered
 
 from dsp_tools.cli.args import ServerCredentials
-from dsp_tools.commands.ingest_xmlupload.create_resources.upload_xml import ingest_xmlupload
 from dsp_tools.commands.ingest_xmlupload.ingest_files.ingest_files import ingest_files
 from dsp_tools.commands.ingest_xmlupload.upload_files.upload_files import upload_files
 from dsp_tools.commands.project.create.project_create import create_project
@@ -37,7 +35,6 @@ def _create_project() -> Iterator[None]:
 def test_ingest_upload(caplog: pytest.LogCaptureFixture, mapping_file: Path) -> None:
     _test_upload_step(caplog)
     _test_ingest_step(caplog, mapping_file)
-    _test_xmlupload_step(caplog)
 
 
 def _test_upload_step(caplog: pytest.LogCaptureFixture) -> None:
@@ -62,24 +59,3 @@ def _test_ingest_step(caplog: pytest.LogCaptureFixture, mapping_file: Path) -> N
 
     df = pd.read_csv(mapping_file)
     assert df["original"].tolist() == unordered(["testdata/bitstreams/test.jpg", "testdata/bitstreams/test.pdf"])
-
-
-def _test_xmlupload_step(caplog: pytest.LogCaptureFixture) -> None:
-    success = ingest_xmlupload(XML_FILE, CREDS)
-    assert success
-    logs = [rec.message for rec in caplog.records]
-    expected_logs = [
-        "The file 'mapping-4125.csv' is used to map the internal original filepaths to the internal image IDs.",
-        (
-            "All multimedia files referenced in the XML file were uploaded through dsp-ingest.\n"
-            "All multimedia files uploaded through dsp-ingest were referenced in the XML file."
-        ),
-    ]
-    assert all(expected_log in logs for expected_log in expected_logs)
-    expected_log_regexes = [
-        "Created resource 1/3",
-        "Created resource 2/3",
-        "Created resource 3/3",
-    ]
-    assert all(any(regex.search(exp, log) for log in logs) for exp in expected_log_regexes)
-    caplog.clear()

896b1503b3bddd7d9bbf8e203d274e3a4c6893ec
Nora-Olivia-Ammann <103038637+Nora-Olivia-Ammann@users.noreply.github.com>
2024-06-24 10:52:14 +0200

    change print message

--- src/dsp_tools/commands/project/create/project_create.py
+++ src/dsp_tools/commands/project/create/project_create.py
@@ -345,7 +345,10 @@ def _create_users(
         # skip the user if he already exists
         all_users = User.getAllUsers(con)
         if json_user_definition["email"] in [user.email for user in all_users]:
-            err_msg = f"User '{username}' already exists on the DSP server. Skipping..."
+            err_msg = (
+                f"User '{username}' already exists on the DSP server.\n"
+                f"The user has to be assigned as a project member manually on the APP."
+            )
             print(f"    WARNING: {err_msg}")
             logger.opt(exception=True).warning(err_msg)
             overall_success = False

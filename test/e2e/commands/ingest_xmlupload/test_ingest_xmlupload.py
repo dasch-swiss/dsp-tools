7c73b9167af073242c5bcea6f11ff9aabb979228
Johannes Nussbaum <johannes.nussbaum@dasch.swiss>
2024-07-03 15:37:42 +0200

    new testing strategy

--- test/e2e/commands/ingest_xmlupload/test_ingest_xmlupload.py
+++ test/e2e/commands/ingest_xmlupload/test_ingest_xmlupload.py
@@ -6,10 +6,12 @@ import pytest
 from dsp_tools.cli.args import ServerCredentials
 from dsp_tools.commands.ingest_xmlupload.upload_files.upload_files import upload_files
 from dsp_tools.commands.project.create.project_create import create_project
+from test.e2e.setup_testcontainers import SIPI_PATH_TMP_INGEST
 from test.e2e.setup_testcontainers import get_containers
 
 CREDS = ServerCredentials("root@example.com", "test", "http://0.0.0.0:3333")
 XML_FILE = Path("testdata/xml-data/test-data-e2e.xml")
+TMP_FOLDER = SIPI_PATH_TMP_INGEST / "import" / "4125" / "testdata" / "bitstreams"
 
 
 @pytest.fixture()
@@ -21,16 +23,11 @@ def _create_project() -> Iterator[None]:
 
 
 @pytest.mark.usefixtures("_create_project")
-def test_ingest_upload(caplog: pytest.LogCaptureFixture) -> None:
-    _test_upload_step(caplog)
+def test_ingest_upload() -> None:
+    _test_upload_step()
 
 
-def _test_upload_step(caplog: pytest.LogCaptureFixture) -> None:
+def _test_upload_step() -> None:
     success = upload_files(XML_FILE, CREDS)
     assert success
-    log_messages = [rec.message for rec in caplog.records]
-    assert "Found 2 files" in log_messages[-6]
-    assert "Uploaded file 'testdata/bitstreams/test.jpg'" in log_messages[-3]
-    assert "Uploaded file 'testdata/bitstreams/test.pdf'" in log_messages[-2]
-    assert "Uploaded all 2 files" in log_messages[-1]
-    caplog.clear()
+    assert set(TMP_FOLDER.iterdir()) == {TMP_FOLDER / "test.jpg", TMP_FOLDER / "test.pdf"}

5a76b59dea7430eba73c8c8f128a931abafce5ff
Nora-Olivia-Ammann <103038637+Nora-Olivia-Ammann@users.noreply.github.com>
2024-07-12 11:54:24 +0200

    use empty json header

--- src/dsp_tools/commands/excel2json/project.py
+++ src/dsp_tools/commands/excel2json/project.py
@@ -5,6 +5,7 @@ from typing import Any
 import regex
 
 from dsp_tools.commands.excel2json.lists import excel2lists
+from dsp_tools.commands.excel2json.models.json_header import EmptyJsonHeader
 from dsp_tools.commands.excel2json.new_lists import new_excel2lists
 from dsp_tools.commands.excel2json.properties import excel2properties
 from dsp_tools.commands.excel2json.resources import excel2resources
@@ -185,18 +186,7 @@ def _create_project_json(
     ontologies, success = _get_ontologies(data_model_files, onto_folders)
     if not success:
         overall_success = False
-    schema = "https://raw.githubusercontent.com/dasch-swiss/dsp-tools/main/src/dsp_tools/resources/schema/project.json"
-    project = {
-        "prefixes": {"": ""},
-        "$schema": schema,
-        "project": {
-            "shortcode": "",
-            "shortname": "",
-            "longname": "",
-            "descriptions": {"en": ""},
-            "keywords": [""],
-        },
-    }
+    project = EmptyJsonHeader().make()
     if lists:
         project["project"]["lists"] = lists  # type: ignore[index]
     project["project"]["ontologies"] = ontologies  # type: ignore[index]

59112d6dd9f2d5f16b71f16baac7a9348dcb6e9e
Nora-Olivia-Ammann <103038637+Nora-Olivia-Ammann@users.noreply.github.com>
2024-07-16 13:27:22 +0200

    Update input_error.py

--- src/dsp_tools/commands/excel2json/models/input_error.py
+++ src/dsp_tools/commands/excel2json/models/input_error.py
@@ -223,6 +223,7 @@ class DuplicateSheetProblem:
     def execute_error_protocol(self) -> str:
         """
         This function initiates all the steps for successful problem communication with the user.
+
         Returns:
             message for the error
         """

2eb5023aab82a507b01961e35fc782e739a93c6f
Nora-Olivia-Ammann <103038637+Nora-Olivia-Ammann@users.noreply.github.com>
2024-07-16 14:33:02 +0200

    Update test_project.py

--- test/integration/commands/excel2json/test_project.py
+++ test/integration/commands/excel2json/test_project.py
@@ -16,47 +16,47 @@ def expected_project() -> dict[str, Any]:
 
 
 @pytest.fixture()
-def result_project() -> dict[str, Any]:
+def returned_project() -> dict[str, Any]:
     excel_folder = "testdata/excel2json/excel2json_files"
     listfolder, onto_folders = _validate_folder_structure_get_filenames(excel_folder)
     _, project = _create_project_json(excel_folder, listfolder, onto_folders)
     return project
 
 
-def test_same_keys(result_project: dict[str, Any], expected_project: dict[str, Any]) -> None:
-    assert set(result_project["project"]) == set(expected_project["project"])
+def test_same_keys(returned_project, expected_project: dict[str, Any]) -> None:
+    assert set(returned_project["project"]) == set(expected_project["project"])
 
 
-def test_lists(result_project: dict[str, Any], expected_project: dict[str, Any]) -> None:
-    res_lists = sorted(result_project["project"]["lists"], key=lambda x: x["name"])
+def test_lists(returned_project, expected_project: dict[str, Any]) -> None:
+    res_lists = sorted(returned_project["project"]["lists"], key=lambda x: x["name"])
     expected_lists = sorted(expected_project["project"]["lists"], key=lambda x: x["name"])
     for r, e in zip(res_lists, expected_lists):
         assert r == e, r["name"]
 
 
-def test_ontology_number(result_project: dict[str, Any], expected_project: dict[str, Any]) -> None:
-    res_ontos = result_project["project"]["ontologies"]
+def test_ontology_number(returned_project, expected_project: dict[str, Any]) -> None:
+    res_ontos = returned_project["project"]["ontologies"]
     expected_onto = expected_project["project"]["ontologies"]
     assert len(res_ontos) == 1
     assert len(expected_onto) == 1
 
 
-def test_properties(result_project: dict[str, Any], expected_project: dict[str, Any]) -> None:
-    res_props = sorted(result_project["project"]["ontologies"][0]["properties"], key=lambda x: x["name"])
+def test_properties(returned_project, expected_project: dict[str, Any]) -> None:
+    res_props = sorted(returned_project["project"]["ontologies"][0]["properties"], key=lambda x: x["name"])
     ex_props = sorted(expected_project["project"]["ontologies"][0]["properties"], key=lambda x: x["name"])
     for r, e in zip(res_props, ex_props):
         assert r == e, r["name"]
 
 
-def test_resources(result_project: dict[str, Any], expected_project: dict[str, Any]) -> None:
-    res_resources = sorted(result_project["project"]["ontologies"][0]["resources"], key=lambda x: x["name"])
+def test_resources(returned_project, expected_project: dict[str, Any]) -> None:
+    res_resources = sorted(returned_project["project"]["ontologies"][0]["resources"], key=lambda x: x["name"])
     ex_resources = sorted(expected_project["project"]["ontologies"][0]["resources"], key=lambda x: x["name"])
     for r, e in zip(res_resources, ex_resources):
         assert r == e, r["name"]
 
 
-def test_entire_project(result_project: dict[str, Any], expected_project: dict[str, Any]) -> None:
-    assert result_project == expected_project
+def test_entire_project(returned_project, expected_project: dict[str, Any]) -> None:
+    assert returned_project == expected_project
 
 
 if __name__ == "__main__":

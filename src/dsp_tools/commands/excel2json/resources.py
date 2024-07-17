e48b4d0ee7ad5dc2ad00fe1ccd3a7ef3573f454a
Nora-Olivia-Ammann <103038637+Nora-Olivia-Ammann@users.noreply.github.com>
2024-07-17 15:11:07 +0200

    Update resources.py

--- src/dsp_tools/commands/excel2json/resources.py
+++ src/dsp_tools/commands/excel2json/resources.py
@@ -29,181 +29,6 @@ from dsp_tools.models.exceptions import InputError
 languages = ["en", "de", "fr", "it", "rm"]
 
 
-def _validate_resources(resources_list: list[dict[str, Any]]) -> None:
-    """
-    This function checks if the "resources" section of a JSON project file is valid according to the JSON schema,
-    and if the resource names are unique.
-
-    Args:
-        resources_list: the "resources" section of a JSON project as a list of dicts
-
-    Raises:
-        InputError: if the validation fails
-    """
-    with (
-        importlib.resources.files("dsp_tools")
-        .joinpath("resources/schema/resources-only.json")
-        .open(encoding="utf-8") as schema_file
-    ):
-        resources_schema = json.load(schema_file)
-    try:
-        jsonschema.validate(instance=resources_list, schema=resources_schema)
-    except jsonschema.ValidationError as err:
-        validation_problem = _find_validation_problem(
-            validation_error=err,
-            resources_list=resources_list,
-        )
-        msg = "\nThe Excel file 'resources.xlsx' did not pass validation." + validation_problem.execute_error_protocol()
-        raise InputError(msg) from None
-
-
-def _find_validation_problem(
-    validation_error: jsonschema.ValidationError, resources_list: list[dict[str, Any]]
-) -> JsonValidationResourceProblem:
-    if json_path_to_resource := regex.search(r"^\$\[(\d+)\]", validation_error.json_path):
-        # fmt: off
-        wrong_res_name = (
-            jsonpath_ng.ext.parse(json_path_to_resource.group(0))
-            .find(resources_list)[0]
-            .value["name"]
-        )
-        # fmt: on
-        if affected_field := regex.search(
-            r"name|labels|comments|super|cardinalities\[(\d+)\]", validation_error.json_path
-        ):
-            affected_value = affected_field.group(0)
-            problematic_resource, excel_sheet, excel_row, excel_column = "", None, None, None
-
-            if affected_value in ["name", "labels", "comments", "super"]:
-                excel_sheet = "classes"
-                problematic_resource = wrong_res_name
-                excel_row = int(json_path_to_resource.group(1)) + 2
-                excel_column = affected_value
-
-            elif "cardinalities" in affected_value:
-                excel_row = int(affected_field.group(1)) + 2
-                excel_sheet = wrong_res_name
-
-                if validation_error.json_path.endswith("cardinality"):
-                    excel_column = "Cardinality"
-
-                elif validation_error.json_path.endswith("propname"):
-                    excel_column = "Property"
-
-            return JsonValidationResourceProblem(
-                problematic_resource=problematic_resource,
-                excel_position=PositionInExcel(sheet=excel_sheet, column=excel_column, row=excel_row),
-                original_msg=validation_error.message,
-            )
-    return JsonValidationResourceProblem(
-        original_msg=validation_error.message,
-        message_path=validation_error.json_path,
-    )
-
-
-def _row2resource(
-    class_info_row: pd.Series[Any],
-    class_df_with_cardinalities: pd.DataFrame | None,
-) -> dict[str, Any]:
-    """
-    Method that reads one row from the "classes" DataFrame,
-    opens the corresponding details DataFrame,
-    and builds a dict object of the resource.
-
-    Args:
-        class_info_row: row from the "classes" DataFrame
-        class_df_with_cardinalities: Excel sheet of the individual class
-
-    Raises:
-        UserError: if the row or the details sheet contains invalid data
-
-    Returns:
-        dict object of the resource
-    """
-
-    class_name = class_info_row["name"]
-    labels = {
-        lang: class_info_row[f"label_{lang}"] for lang in languages if not pd.isna(class_info_row[f"label_{lang}"])
-    }
-    if not labels:
-        labels = {lang: class_info_row[lang] for lang in languages if not pd.isna(class_info_row[lang])}
-    supers = [s.strip() for s in class_info_row["super"].split(",")]
-
-    resource = {"name": class_name, "super": supers, "labels": labels}
-
-    comments = {
-        lang: class_info_row[f"comment_{lang}"] for lang in languages if not pd.isna(class_info_row[f"comment_{lang}"])
-    }
-    if comments:
-        resource["comments"] = comments
-
-    cards = _make_cardinality_section(class_name, class_df_with_cardinalities)
-    if cards:
-        resource["cardinalities"] = cards
-
-    return resource
-
-
-def _make_cardinality_section(
-    class_name: str, class_df_with_cardinalities: pd.DataFrame | None
-) -> list[dict[str, str | int]]:
-    if class_df_with_cardinalities is None:
-        return []
-    if len(class_df_with_cardinalities) == 0:
-        return []
-    cards = _create_all_cardinalities(class_name, class_df_with_cardinalities)
-    return cards
-
-
-def _create_all_cardinalities(class_name: str, class_df_with_cardinalities: pd.DataFrame) -> list[dict[str, str | int]]:
-    class_df_with_cardinalities = _check_complete_gui_order(class_name, class_df_with_cardinalities)
-    cards = [_make_one_property(detail_row) for _, detail_row in class_df_with_cardinalities.iterrows()]
-    return cards
-
-
-def _make_one_property(detail_row: pd.Series[str | int]) -> dict[str, str | int]:
-    return {
-        "propname": f':{detail_row["property"]}',
-        "cardinality": str(detail_row["cardinality"]).lower(),
-        "gui_order": detail_row["gui_order"],
-    }
-
-
-def _check_complete_gui_order(class_name: str, class_df_with_cardinalities: pd.DataFrame) -> pd.DataFrame:
-    detail_problem_msg = ""
-    attempt_conversion = False
-    if "gui_order" not in class_df_with_cardinalities:
-        pass
-    elif class_df_with_cardinalities["gui_order"].isna().all():
-        pass
-    elif class_df_with_cardinalities["gui_order"].isna().any():
-        detail_problem_msg = "some rows in the column 'gui_order' are empty."
-    elif not class_df_with_cardinalities["gui_order"].isna().all():
-        attempt_conversion = True
-
-    if attempt_conversion:
-        try:
-            class_df_with_cardinalities["gui_order"] = [int(float(x)) for x in class_df_with_cardinalities["gui_order"]]
-            return class_df_with_cardinalities
-        except ValueError:
-            detail_problem_msg = (
-                "some rows in the column 'gui_order' contain invalid characters "
-                "that could not be converted to an integer."
-            )
-
-    class_df_with_cardinalities["gui_order"] = list(range(1, len(class_df_with_cardinalities) + 1))
-
-    if detail_problem_msg:
-        complete_msg = (
-            f"In the sheet '{class_name}' of the file 'resources.xlsx', "
-            f"{detail_problem_msg}\n"
-            f"Values have been filled in automatically, "
-            f"so that the gui-order reflects the order of the properties in the file."
-        )
-        warnings.warn(complete_msg)
-    return class_df_with_cardinalities
-
-
 def excel2resources(
     excelfile: str,
     path_to_output_file: Optional[str] = None,
@@ -318,3 +143,178 @@ def _validate_individual_class_sheets(class_df_dict: dict[str, pd.DataFrame]) ->
     if problem_list:
         return [MissingValuesProblem(problem_list)]
     return []
+
+
+def _row2resource(
+    class_info_row: pd.Series[Any],
+    class_df_with_cardinalities: pd.DataFrame | None,
+) -> dict[str, Any]:
+    """
+    Method that reads one row from the "classes" DataFrame,
+    opens the corresponding details DataFrame,
+    and builds a dict object of the resource.
+
+    Args:
+        class_info_row: row from the "classes" DataFrame
+        class_df_with_cardinalities: Excel sheet of the individual class
+
+    Raises:
+        UserError: if the row or the details sheet contains invalid data
+
+    Returns:
+        dict object of the resource
+    """
+
+    class_name = class_info_row["name"]
+    labels = {
+        lang: class_info_row[f"label_{lang}"] for lang in languages if not pd.isna(class_info_row[f"label_{lang}"])
+    }
+    if not labels:
+        labels = {lang: class_info_row[lang] for lang in languages if not pd.isna(class_info_row[lang])}
+    supers = [s.strip() for s in class_info_row["super"].split(",")]
+
+    resource = {"name": class_name, "super": supers, "labels": labels}
+
+    comments = {
+        lang: class_info_row[f"comment_{lang}"] for lang in languages if not pd.isna(class_info_row[f"comment_{lang}"])
+    }
+    if comments:
+        resource["comments"] = comments
+
+    cards = _make_cardinality_section(class_name, class_df_with_cardinalities)
+    if cards:
+        resource["cardinalities"] = cards
+
+    return resource
+
+
+def _make_cardinality_section(
+    class_name: str, class_df_with_cardinalities: pd.DataFrame | None
+) -> list[dict[str, str | int]]:
+    if class_df_with_cardinalities is None:
+        return []
+    if len(class_df_with_cardinalities) == 0:
+        return []
+    cards = _create_all_cardinalities(class_name, class_df_with_cardinalities)
+    return cards
+
+
+def _create_all_cardinalities(class_name: str, class_df_with_cardinalities: pd.DataFrame) -> list[dict[str, str | int]]:
+    class_df_with_cardinalities = _check_complete_gui_order(class_name, class_df_with_cardinalities)
+    cards = [_make_one_property(detail_row) for _, detail_row in class_df_with_cardinalities.iterrows()]
+    return cards
+
+
+def _make_one_property(detail_row: pd.Series[str | int]) -> dict[str, str | int]:
+    return {
+        "propname": f':{detail_row["property"]}',
+        "cardinality": str(detail_row["cardinality"]).lower(),
+        "gui_order": detail_row["gui_order"],
+    }
+
+
+def _check_complete_gui_order(class_name: str, class_df_with_cardinalities: pd.DataFrame) -> pd.DataFrame:
+    detail_problem_msg = ""
+    attempt_conversion = False
+    if "gui_order" not in class_df_with_cardinalities:
+        pass
+    elif class_df_with_cardinalities["gui_order"].isna().all():
+        pass
+    elif class_df_with_cardinalities["gui_order"].isna().any():
+        detail_problem_msg = "some rows in the column 'gui_order' are empty."
+    elif not class_df_with_cardinalities["gui_order"].isna().all():
+        attempt_conversion = True
+
+    if attempt_conversion:
+        try:
+            class_df_with_cardinalities["gui_order"] = [int(float(x)) for x in class_df_with_cardinalities["gui_order"]]
+            return class_df_with_cardinalities
+        except ValueError:
+            detail_problem_msg = (
+                "some rows in the column 'gui_order' contain invalid characters "
+                "that could not be converted to an integer."
+            )
+
+    class_df_with_cardinalities["gui_order"] = list(range(1, len(class_df_with_cardinalities) + 1))
+
+    if detail_problem_msg:
+        complete_msg = (
+            f"In the sheet '{class_name}' of the file 'resources.xlsx', "
+            f"{detail_problem_msg}\n"
+            f"Values have been filled in automatically, "
+            f"so that the gui-order reflects the order of the properties in the file."
+        )
+        warnings.warn(complete_msg)
+    return class_df_with_cardinalities
+
+
+def _validate_resources(resources_list: list[dict[str, Any]]) -> None:
+    """
+    This function checks if the "resources" section of a JSON project file is valid according to the JSON schema,
+    and if the resource names are unique.
+
+    Args:
+        resources_list: the "resources" section of a JSON project as a list of dicts
+
+    Raises:
+        InputError: if the validation fails
+    """
+    with (
+        importlib.resources.files("dsp_tools")
+        .joinpath("resources/schema/resources-only.json")
+        .open(encoding="utf-8") as schema_file
+    ):
+        resources_schema = json.load(schema_file)
+    try:
+        jsonschema.validate(instance=resources_list, schema=resources_schema)
+    except jsonschema.ValidationError as err:
+        validation_problem = _find_validation_problem(
+            validation_error=err,
+            resources_list=resources_list,
+        )
+        msg = "\nThe Excel file 'resources.xlsx' did not pass validation." + validation_problem.execute_error_protocol()
+        raise InputError(msg) from None
+
+
+def _find_validation_problem(
+    validation_error: jsonschema.ValidationError, resources_list: list[dict[str, Any]]
+) -> JsonValidationResourceProblem:
+    if json_path_to_resource := regex.search(r"^\$\[(\d+)\]", validation_error.json_path):
+        # fmt: off
+        wrong_res_name = (
+            jsonpath_ng.ext.parse(json_path_to_resource.group(0))
+            .find(resources_list)[0]
+            .value["name"]
+        )
+        # fmt: on
+        if affected_field := regex.search(
+            r"name|labels|comments|super|cardinalities\[(\d+)\]", validation_error.json_path
+        ):
+            affected_value = affected_field.group(0)
+            problematic_resource, excel_sheet, excel_row, excel_column = "", None, None, None
+
+            if affected_value in ["name", "labels", "comments", "super"]:
+                excel_sheet = "classes"
+                problematic_resource = wrong_res_name
+                excel_row = int(json_path_to_resource.group(1)) + 2
+                excel_column = affected_value
+
+            elif "cardinalities" in affected_value:
+                excel_row = int(affected_field.group(1)) + 2
+                excel_sheet = wrong_res_name
+
+                if validation_error.json_path.endswith("cardinality"):
+                    excel_column = "Cardinality"
+
+                elif validation_error.json_path.endswith("propname"):
+                    excel_column = "Property"
+
+            return JsonValidationResourceProblem(
+                problematic_resource=problematic_resource,
+                excel_position=PositionInExcel(sheet=excel_sheet, column=excel_column, row=excel_row),
+                original_msg=validation_error.message,
+            )
+    return JsonValidationResourceProblem(
+        original_msg=validation_error.message,
+        message_path=validation_error.json_path,
+    )

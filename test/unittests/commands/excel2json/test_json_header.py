8e834a432aca505403749e4172951c03b8fb71ec
Nora-Olivia-Ammann <103038637+Nora-Olivia-Ammann@users.noreply.github.com>
2024-07-24 09:54:36 +0200

    add tests

--- test/unittests/commands/excel2json/test_json_header.py
+++ test/unittests/commands/excel2json/test_json_header.py
@@ -1,28 +1,16 @@
 import pandas as pd
 import pytest
 
-from dsp_tools.commands.excel2json.json_header import _check_email, _do_all_checks, _process_file
+from dsp_tools.commands.excel2json.json_header import _do_all_checks, _process_file, _extract_prefixes
 from dsp_tools.commands.excel2json.json_header import _check_if_sheets_are_filled_and_exist
 from dsp_tools.commands.excel2json.json_header import (
-    _check_lang,
     _check_keywords,
     _check_descriptions,
     _check_one_user,
     _check_all_users,
-    _check_role,
     _check_prefixes,
-    _check_project_sheet,
-    _check_email,
 )
 from dsp_tools.commands.excel2json.json_header import _check_project_sheet
-from dsp_tools.commands.excel2json.json_header import _extract_descriptions
-from dsp_tools.commands.excel2json.json_header import _extract_keywords
-from dsp_tools.commands.excel2json.json_header import _extract_one_user
-from dsp_tools.commands.excel2json.json_header import _extract_prefixes
-from dsp_tools.commands.excel2json.json_header import _extract_project
-from dsp_tools.commands.excel2json.json_header import _extract_users
-from dsp_tools.commands.excel2json.json_header import _get_description_cols
-from dsp_tools.commands.excel2json.json_header import _get_role
 from dsp_tools.commands.excel2json.models.input_error import AtLeastOneValueRequiredProblem
 from dsp_tools.commands.excel2json.models.input_error import EmptySheetsProblem
 from dsp_tools.commands.excel2json.models.input_error import ExcelFileProblem
@@ -35,228 +23,33 @@ from dsp_tools.commands.excel2json.models.input_error import RequiredColumnMissi
 from dsp_tools.commands.excel2json.models.json_header import Descriptions
 from dsp_tools.commands.excel2json.models.json_header import Keywords
 from dsp_tools.commands.excel2json.models.json_header import Prefixes
-from dsp_tools.commands.excel2json.models.json_header import Project
-from dsp_tools.commands.excel2json.models.json_header import User
-from dsp_tools.commands.excel2json.models.json_header import UserRole
 from dsp_tools.commands.excel2json.models.json_header import Users
-
-
-@pytest.fixture()
-def prefixes_good() -> pd.DataFrame:
-    return pd.DataFrame(
-        {"prefixes": ["foaf:", "sdh"], "uri": ["http://xmlns.com/foaf/0.1/", "https://ontome.net/ns/sdhss/"]}
-    )
-
-
-@pytest.fixture()
-def prefixes_missing_col() -> pd.DataFrame:
-    return pd.DataFrame({"uri": ["http://xmlns.com/foaf/0.1/", "https://ontome.net/ns/sdhss/"]})
-
-
-@pytest.fixture()
-def prefixes_missing_val() -> pd.DataFrame:
-    return pd.DataFrame(
-        {"prefixes": ["foaf:", pd.NA], "uri": ["http://xmlns.com/foaf/0.1/", "https://ontome.net/ns/sdhss/"]}
-    )
-
-
-@pytest.fixture()
-def prefixes_wrong_val() -> pd.DataFrame:
-    return pd.DataFrame({"prefixes": ["foaf:", "sdh"], "uri": ["http://xmlns.com/foaf/0.1/", "not a uri"]})
-
-
-@pytest.fixture()
-def project_good_missing_zero() -> pd.DataFrame:
-    return pd.DataFrame({"shortcode": [11], "shortname": ["name"], "longname": ["long"]})
-
-
-@pytest.fixture()
-def project_good_no_zero() -> pd.DataFrame:
-    return pd.DataFrame({"shortcode": [1111], "shortname": ["name"], "longname": ["long"]})
-
-
-@pytest.fixture()
-def project_missing_col() -> pd.DataFrame:
-    return pd.DataFrame({"shortname": ["name"], "longname": ["long"]})
-
-
-@pytest.fixture()
-def project_missing_val() -> pd.DataFrame:
-    return pd.DataFrame({"shortcode": [pd.NA], "shortname": ["name"], "longname": ["long"]})
-
-
-@pytest.fixture()
-def project_too_many_rows() -> pd.DataFrame:
-    return pd.DataFrame({"shortcode": [11, 0], "shortname": ["name", pd.NA], "longname": ["long", "other"]})
-
-
-@pytest.fixture()
-def description_good() -> pd.DataFrame:
-    return pd.DataFrame(
-        {
-            "description_en": ["english"],
-            "description_de": [pd.NA],
-            "fr": ["french"],
-            "description_rm": [pd.NA],
-        }
-    )
-
-
-@pytest.fixture()
-def description_too_many_rows() -> pd.DataFrame:
-    return pd.DataFrame(
-        {
-            "description_en": ["english", "en1"],
-            "description_de": [pd.NA, pd.NA],
-            "fr": ["french", "french2"],
-            "description_it": [pd.NA, "it"],
-            "description_rm": [pd.NA, pd.NA],
-        }
-    )
-
-
-@pytest.fixture()
-def description_missing_col() -> pd.DataFrame:
-    return pd.DataFrame({"other": ["english"]})
-
-
-@pytest.fixture()
-def description_missing_val() -> pd.DataFrame:
-    return pd.DataFrame({"other": ["english"], "description_en": [pd.NA]})
-
-
-@pytest.fixture()
-def keywords_good() -> pd.DataFrame:
-    return pd.DataFrame({"keywords": ["one", pd.NA, "three"]})
-
-
-@pytest.fixture()
-def keywords_missing_col() -> pd.DataFrame:
-    return pd.DataFrame({"other": [1]})
-
-
-@pytest.fixture()
-def keywords_missing_val() -> pd.DataFrame:
-    return pd.DataFrame({"other": [1], "keywords": [pd.NA]})
-
-
-@pytest.fixture()
-def users_good() -> pd.DataFrame:
-    return pd.DataFrame(
-        {
-            "username": ["Alice", "Caterpillar", "WhiteRabbit"],
-            "email": ["alice@dasch.swiss", "caterpillar@dasch.swiss", "white.rabbit@dasch.swiss"],
-            "givenname": ["Alice Pleasance", "Caterpillar", "White"],
-            "familyname": ["Liddell", "Wonderland", "Rabbit"],
-            "password": ["alice4322", "alice7652", "alice8711"],
-            "lang": ["en", "de", "fr"],
-            "role": ["systemadmin", "projectmember", "projectmember"],
-        }
-    )
-
-
-@pytest.fixture()
-def users_missing_col() -> pd.DataFrame:
-    return pd.DataFrame(
-        {
-            "username": ["Alice"],
-            "email": ["alice@dasch.swiss"],
-            "givenname": ["Alice Pleasance"],
-            "familyname": ["Liddell"],
-            "lang": ["fr"],
-            "role": ["systemadmin"],
-        }
-    )
-
-
-@pytest.fixture()
-def users_missing_val() -> pd.DataFrame:
-    return pd.DataFrame(
-        {
-            "username": [pd.NA],
-            "email": ["alice@dasch.swiss"],
-            "givenname": ["Alice Pleasance"],
-            "familyname": ["Liddell"],
-            "password": ["alice7652"],
-            "lang": ["fr"],
-            "role": ["systemadmin"],
-        }
-    )
-
-
-@pytest.fixture()
-def users_wrong_lang() -> pd.DataFrame:
-    return pd.DataFrame(
-        {
-            "username": ["Alice"],
-            "email": ["alice@dasch.swiss"],
-            "givenname": ["Alice Pleasance"],
-            "familyname": ["Liddell"],
-            "password": ["alice7652"],
-            "lang": ["other"],
-            "role": ["systemadmin"],
-        }
-    )
-
-
-@pytest.fixture()
-def user_good() -> pd.Series:
-    return pd.Series(
-        {
-            "username": "Alice",
-            "email": "alice@dasch.swiss",
-            "givenname": "Alice Pleasance",
-            "familyname": "Liddell",
-            "password": "alice7652",
-            "lang": "en",
-            "role": "systemadmin",
-        }
-    )
-
-
-@pytest.fixture()
-def user_wrong_lang() -> pd.Series:
-    return pd.Series(
-        {
-            "username": "Alice",
-            "email": "alice@dasch.swiss",
-            "givenname": "Alice Pleasance",
-            "familyname": "Liddell",
-            "password": "alice7652",
-            "lang": "other",
-            "role": "systemadmin",
-        }
-    )
-
-
-@pytest.fixture()
-def user_wrong_email() -> pd.Series:
-    return pd.Series(
-        {
-            "username": "Alice",
-            "email": "not an email",
-            "givenname": "Alice Pleasance",
-            "familyname": "Liddell",
-            "password": "alice7652",
-            "lang": "fr",
-            "role": "systemadmin",
-        }
-    )
-
-
-@pytest.fixture()
-def user_wrong_role() -> pd.Series:
-    return pd.Series(
-        {
-            "username": "Alice",
-            "email": "alice@dasch.swiss",
-            "givenname": "Alice Pleasance",
-            "familyname": "Liddell",
-            "password": "alice7652",
-            "lang": "fr",
-            "role": "other",
-        }
-    )
+from unittests.commands.excel2json.test_json_header_fixtures import (
+    prefixes_good,
+    prefixes_missing_col,
+    prefixes_missing_val,
+    prefixes_wrong_val,
+    project_good_missing_zero,
+    project_good_no_zero,
+    project_missing_col,
+    project_missing_val,
+    project_too_many_rows,
+    description_good,
+    description_too_many_rows,
+    description_missing_col,
+    description_missing_val,
+    keywords_good,
+    keywords_missing_col,
+    keywords_missing_val,
+    users_good,
+    users_missing_col,
+    users_missing_val,
+    users_wrong_lang,
+    user_good,
+    user_wrong_lang,
+    user_wrong_email,
+    user_wrong_role,
+)
 
 
 class TestCheckAll:
@@ -572,13 +365,13 @@ class TestProcessFile:
             "keywords": keywords_good,
         }
         result = _process_file(test_dict)
-        assert result.prefixes.prefixes == {"foaf": "http://xmlns.com/foaf/0.1/", "sdh": "https://ontome.net/ns/sdhss/"}
+        assert isinstance(result.prefixes, Prefixes)
         project = result.project
         assert project.shortcode == "1111"
         assert project.shortname == "name"
         assert project.longname == "long"
-        assert project.descriptions.descriptions == {"en": "english", "fr": "french"}
-        assert set(project.keywords.keywords) == {"one", "three"}
+        assert isinstance(project.descriptions, Descriptions)
+        assert isinstance(project.keywords, Keywords)
         assert not project.users
 
     def test_with_user(
@@ -597,18 +390,20 @@ class TestProcessFile:
             "users": users_good,
         }
         result = _process_file(test_dict)
-        assert result.prefixes.prefixes == {"foaf": "http://xmlns.com/foaf/0.1/", "sdh": "https://ontome.net/ns/sdhss/"}
+        assert isinstance(result.prefixes, Prefixes)
         project = result.project
         assert project.shortcode == "0011"
         assert project.shortname == "name"
         assert project.longname == "long"
-        assert project.descriptions.descriptions == {"en": "english", "fr": "french"}
-        assert set(project.keywords.keywords) == {"one", "three"}
+        assert isinstance(project.descriptions, Descriptions)
+        assert isinstance(project.keywords, Keywords)
         assert isinstance(project.users, Users)
 
 
 def test_extract_prefixes(prefixes_good: pd.DataFrame) -> None:
-    assert 1 == 0
+    result = _extract_prefixes(prefixes_good)
+    assert isinstance(result, Prefixes)
+    assert result.prefixes == {"foaf": "http://xmlns.com/foaf/0.1/", "sdh": "https://ontome.net/ns/sdhss/"}
 
 
 class TestExtractProject:

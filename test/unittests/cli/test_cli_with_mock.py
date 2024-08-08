a2dfb63580befdb70efc465563a8c9002f7ce2b5
Nora-Olivia-Ammann <103038637+Nora-Olivia-Ammann@users.noreply.github.com>
2024-08-08 15:15:08 +0200

    Apply suggestions from code review
    Co-authored-by: Johannes Nussbaum <39048939+jnussbaum@users.noreply.github.com>

--- test/unittests/cli/test_cli_with_mock.py
+++ test/unittests/cli/test_cli_with_mock.py
@@ -341,27 +341,27 @@ def test_excel2properties(excel2properties: Mock) -> None:
 
 
 @patch("dsp_tools.cli.call_action.new_excel2json")
-def test_new_excel2json(excel2json: Mock) -> None:
+def test_new_excel2json(new_excel2json: Mock) -> None:
     """Test the 'dsp-tools new-excel2json' command"""
     folder = "folder"
     out_file = "filename.json"
     args = f"new-excel2json {folder} {out_file}".split()
     entry_point.run(args)
-    excel2json.assert_called_once_with(
+    new_excel2json.assert_called_once_with(
         data_model_files=folder,
         path_to_output_file=out_file,
     )
 
 
 @patch("dsp_tools.cli.call_action.new_excel2lists")
-def test_new_excel2lists(excel2lists: Mock) -> None:
+def test_new_excel2lists(new_excel2lists: Mock) -> None:
     """Test the 'dsp-tools new-excel2lists' command"""
-    excel2lists.return_value = ([], True)
+    new_excel2lists.return_value = ([], True)
     file = "filename.xlsx"
     out_file = "filename.json"
     args = f"new-excel2lists {file} {out_file}".split()
     entry_point.run(args)
-    excel2lists.assert_called_once_with(
+    new_excel2lists.assert_called_once_with(
         excelfolder=file,
         path_to_output_file=out_file,
     )

6f312999fdd96141ebb9aada42d6c30bb13e02a2
Nora-Olivia-Ammann <103038637+Nora-Olivia-Ammann@users.noreply.github.com>
2024-08-12 16:15:18 +0200

    Update __init__.py

--- src/dsp_tools/commands/excel2xml/__init__.py
+++ src/dsp_tools/commands/excel2xml/__init__.py
@@ -3,5 +3,6 @@
 
 from dsp_tools.commands.excel2xml.excel2xml_lib import *
 from dsp_tools.commands.excel2xml.propertyelement import PropertyElement as PropertyElement
+from dsp_tools.commands.excel2xml.xml_models import *
 from dsp_tools.utils.shared import check_notna as check_notna
 from dsp_tools.utils.shared import simplify_name as simplify_name

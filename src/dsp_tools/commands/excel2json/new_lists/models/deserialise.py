fb19fa267a39d626ccbd8814cd17a9e0f3a9243e
Nora-Olivia-Ammann <103038637+Nora-Olivia-Ammann@users.noreply.github.com>
2024-08-13 10:54:47 +0200

    Update deserialise.py

--- src/dsp_tools/commands/excel2json/new_lists/models/deserialise.py
+++ src/dsp_tools/commands/excel2json/new_lists/models/deserialise.py
@@ -10,3 +10,33 @@ class ExcelSheet:
     excel_name: str
     sheet_name: str
     df: pd.DataFrame
+
+
+@dataclass
+class SheetDeserialised:
+    excel_name: str
+    sheet_name: str
+    lang_tags: set[str]
+    list_deserialised: ListDeserialised
+
+
+@dataclass
+class ListDeserialised:
+    list_id: str
+    excel_row: int
+    labels: LangColsDeserialised
+    comments: LangColsDeserialised | None
+
+
+@dataclass
+class NodeDeserialised:
+    node_id: str
+    parent_id: str
+    excel_row: int
+    labels: LangColsDeserialised
+    comments: LangColsDeserialised | None
+
+
+@dataclass
+class LangColsDeserialised:
+    content: dict[str, str]

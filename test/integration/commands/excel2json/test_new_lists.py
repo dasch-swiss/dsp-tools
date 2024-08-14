d18303afeb0eeb37c1bf206f21915abd69c544c3
Nora-Olivia-Ammann <103038637+Nora-Olivia-Ammann@users.noreply.github.com>
2024-08-14 11:57:21 +0200

    add integration test

--- test/integration/commands/excel2json/test_new_lists.py
+++ test/integration/commands/excel2json/test_new_lists.py
@@ -4,8 +4,10 @@ from typing import Any
 from typing import cast
 
 import pytest
+import regex
 
 from dsp_tools.commands.excel2json.new_lists.make_new_lists import new_excel2lists
+from dsp_tools.models.exceptions import InputError
 
 
 @pytest.fixture()
@@ -34,8 +36,49 @@ def test_second_list(expected_lists: list[dict[str, Any]], result_lists_correct:
     assert result_lists_correct[1] == expected_lists[1]
 
 
-def test_thrid_list(expected_lists: list[dict[str, Any]], result_lists_correct: list[dict[str, Any]]) -> None:
+def test_third_list(expected_lists: list[dict[str, Any]], result_lists_correct: list[dict[str, Any]]) -> None:
     assert result_lists_correct[2] == expected_lists[2]
+
+
+def test_duplicate_problems() -> None:
+    expected = regex.escape(
+        "\nThe excel file(s) used to create the list section have the following problem(s):"
+        "\n\n---------------------------------------\n\n"
+        "No duplicates are allowed in the 'ID (optional)' column. "
+        "At the following locations, IDs are duplicated:"
+        "\n----------------------------\n"
+        "ID: 'duplicateID'\n"
+        "    - Excel 'testdata/invalid-testdata/excel2json/new_lists_duplicate_ids/list_duplicate_1.xlsx' "
+        "| Sheet 'duplicate_list_id1' | Row 2\n"
+        "    - Excel 'testdata/invalid-testdata/excel2json/new_lists_duplicate_ids/list_duplicate_2.xlsx' "
+        "| Sheet 'duplicate_list_id2' | Row 2"
+        "\n----------------------------\n"
+        "ID: 'duplicate_node_id'\n"
+        "    - Excel 'testdata/invalid-testdata/excel2json/new_lists_duplicate_ids/list_duplicate_2.xlsx' "
+        "| Sheet 'duplicate_node_id1' | Row 3\n"
+        "    - Excel 'testdata/invalid-testdata/excel2json/new_lists_duplicate_ids/list_duplicate_2.xlsx' "
+        "| Sheet 'duplicate_node_id2' | Row 3"
+    )
+    with pytest.raises(InputError, match=expected):
+        new_excel2lists(Path("testdata/invalid-testdata/excel2json/new_lists_duplicate_ids"))
+
+
+def test_invalid_shape() -> None:
+    expected = "asdf"
+    with pytest.raises(InputError, match=expected):
+        new_excel2lists(Path("testdata/invalid-testdata/excel2json/new_lists_duplicate_listname"))
+
+
+def test_invalid_shape() -> None:
+    expected = "asdf"
+    with pytest.raises(InputError, match=expected):
+        new_excel2lists(Path("testdata/invalid-testdata/excel2json/new_lists_invalid_shape"))
+
+
+def test_missing_translation() -> None:
+    expected = "asdf"
+    with pytest.raises(InputError, match=expected):
+        new_excel2lists(Path("testdata/invalid-testdata/excel2json/new_lists_missing_translations"))
 
 
 if __name__ == "__main__":

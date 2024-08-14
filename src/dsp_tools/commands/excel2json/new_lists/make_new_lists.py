f2b05ac58856856bb0dfb6fec5aa1c41bf9874be
Nora-Olivia-Ammann <103038637+Nora-Olivia-Ammann@users.noreply.github.com>
2024-08-14 10:29:06 +0200

    refactor(excel2json-new-lists): extract fixtures for compliance tests (#1104)

--- src/dsp_tools/commands/excel2json/new_lists/make_new_lists.py
+++ src/dsp_tools/commands/excel2json/new_lists/make_new_lists.py
@@ -11,7 +11,7 @@ import regex
 from loguru import logger
 
 from dsp_tools.commands.excel2json.lists import validate_lists_section_with_schema
-from dsp_tools.commands.excel2json.new_lists.compliance_checks import make_all_formal_excel_compliance_checks
+from dsp_tools.commands.excel2json.new_lists.compliance_checks import make_all_excel_compliance_checks
 from dsp_tools.commands.excel2json.new_lists.models.deserialise import ExcelSheet
 from dsp_tools.commands.excel2json.new_lists.models.input_error import CollectedSheetProblems
 from dsp_tools.commands.excel2json.new_lists.models.input_error import ListCreationProblem
@@ -80,7 +80,7 @@ def _non_hidden(path: Path) -> bool:
 
 def _prepare_dfs(sheet_list: list[ExcelSheet]) -> list[ExcelSheet]:
     sheet_list = _add_id_optional_column_if_not_exists(sheet_list)
-    make_all_formal_excel_compliance_checks(sheet_list)
+    make_all_excel_compliance_checks(sheet_list)
     return _construct_ids(sheet_list)
 
 

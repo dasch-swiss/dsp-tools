ba797faab7da7eae8cc615175c7ff86519ef6fc9
Nora-Olivia-Ammann <103038637+Nora-Olivia-Ammann@users.noreply.github.com>
2024-08-19 15:51:01 +0200

    move code

--- src/dsp_tools/commands/xml_validate/prepare_input.py
+++ src/dsp_tools/commands/xml_validate/prepare_input.py
@@ -14,6 +14,46 @@ from dsp_tools.commands.xml_validate.models.deserialised import ValueDeserialise
 from dsp_tools.commands.xml_validate.models.xml_deserialised import PermissionsXML
 from dsp_tools.commands.xml_validate.models.xml_deserialised import ProjectXML
 from dsp_tools.commands.xml_validate.models.xml_deserialised import ResourceXML
+from dsp_tools.utils.xml_utils import parse_and_clean_xml_file
+from dsp_tools.utils.xml_validation import validate_xml
+
+
+def parse_file(file: Path) -> ProjectXML:
+    root = _parse_file_validate_with_schema(file)
+    return _transform_into_xml_deserialised(root)
+
+
+def _parse_file_validate_with_schema(file: Path) -> etree._Element:
+    root = parse_and_clean_xml_file(file)
+    validate_xml(root)
+    return root
+
+
+def _transform_into_xml_deserialised(root: etree._Element) -> ProjectXML:
+    shortcode = root.attrib["shortcode"]
+    default_ontology = root.attrib["default-ontology"]
+    permissions = [_get_permissions(x) for x in root.iterdescendants(tag="permissions")]
+    resources = [_get_resources(x) for x in root.iterdescendants(tag="resource")]
+    return ProjectXML(
+        shortcode=shortcode, default_onto=default_ontology, permissions=permissions, xml_resources=resources
+    )
+
+
+def _get_resources(ele: etree._Element) -> ResourceXML:
+    values = list(ele.iterchildren())
+    return ResourceXML(res_attrib=ele.attrib, values=values, file_value=_find_file_value(ele))
+
+
+def _find_file_value(ele: etree._Element) -> etree._Element | None:
+    if bitstream := ele.find(".//bitstream") is not None:
+        return bitstream
+    return ele.find(".//iiif-uri")
+
+
+def _get_permissions(permission_ele: etree._Element) -> PermissionsXML:
+    per_id = permission_ele.attrib["id"]
+    permissions = list(permission_ele.iterchildren())
+    return PermissionsXML(permission_id=per_id, permission_eles=permissions)
 
 
 def deserialise_xml_project(project: ProjectXML) -> ProjectDeserialised:

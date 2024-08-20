f1b539a03c9efe198fd1aa74dbb81052f0aa5841
Nora-Olivia-Ammann <103038637+Nora-Olivia-Ammann@users.noreply.github.com>
2024-08-20 10:20:26 +0200

    add messages

--- src/dsp_tools/commands/xml_validate/models/input_error.py
+++ src/dsp_tools/commands/xml_validate/models/input_error.py
@@ -1,12 +1,41 @@
 from __future__ import annotations
 
+from collections import defaultdict
 from dataclasses import dataclass
 from typing import Protocol
+
+INDENT = "    "
+MEDIUM_SEPARATOR = "\n\n"
+GRAND_SEPARATOR = "\n\n----------------------------\n\n"
+
+
+@dataclass
+class AllErrors:
+    errors: list[InputError]
+
+    def get_msg(self) -> str:
+        coll = self._make_collection()
+        msg = [x.get_msg() for x in coll]
+        return GRAND_SEPARATOR.join(msg)
+
+    def _make_collection(self) -> list[ResourceErrorCollection]:
+        d = defaultdict(list)
+        for e in self.errors:
+            d[e.res_id].append(e)
+        l = []
+        for k, v in d.items():
+            l.append(ResourceErrorCollection(k, v))
+        return sorted(l, key=lambda x: x.res_id)
 
 
 @dataclass
 class InputError(Protocol):
+    res_id: str
+
     def get_msg(self) -> str:
+        raise NotImplementedError
+
+    def sort_value(self) -> str:
         raise NotImplementedError
 
 
@@ -16,7 +45,12 @@ class ResourceErrorCollection:
     errors: list[InputError]
 
     def get_msg(self) -> str:
-        raise NotImplementedError
+        msg = [f"The resource with the ID '{self.res_id}' has the following problem(s):"]
+        msg.extend(sorted(self.errors, key=lambda x: x.sort_value()))
+        return "\n\n".join(msg)
+
+    def sort_value(self) -> str:
+        return self.res_id
 
 
 #######################
@@ -27,10 +61,15 @@ class ResourceErrorCollection:
 class MaxCardinalityViolation:
     res_id: str
     prop_name: str
-    number_used: str
 
     def get_msg(self) -> str:
-        raise NotImplementedError
+        return (
+            f"The following property should not be used more than once for one resource:\n"
+            f"{INDENT}Property: {self.prop_name}"
+        )
+
+    def sort_value(self) -> str:
+        return self.prop_name
 
 
 #######################
@@ -43,7 +82,13 @@ class PropNotFoundInOntology:
     prop_name: str
 
     def get_msg(self) -> str:
-        raise NotImplementedError
+        return (
+            f"The following property could not be found in the project ontology:\n"
+            f"{INDENT}Property: {self.prop_name}"
+        )
+
+    def sort_value(self) -> str:
+        return self.prop_name
 
 
 @dataclass
@@ -52,7 +97,13 @@ class ResClassNotFoundInOntology:
     cls_name: str
 
     def get_msg(self) -> str:
-        raise NotImplementedError
+        return (
+            f"The following resource class could not be found in the project ontology:\n"
+            f"{INDENT}Class: {self.cls_name}"
+        )
+
+    def sort_value(self) -> str:
+        return self.cls_name
 
 
 @dataclass
@@ -63,7 +114,15 @@ class PropTypeMismatch:
     prop_type_expected: str
 
     def get_msg(self) -> str:
-        raise NotImplementedError
+        return (
+            f"The following property does not have the same type in the ontology and the data\n"
+            f"{INDENT}Property: {self.prop_name}\n"
+            f"{INDENT}Type used in data: {self.prop_type_used}\n"
+            f"{INDENT}Type expected according to the ontology: {self.prop_type_expected}"
+        )
+
+    def sort_value(self) -> str:
+        return self.prop_name
 
 
 @dataclass
@@ -75,7 +134,16 @@ class LinkTargetMismatch:
     target_class_expected: str
 
     def get_msg(self) -> str:
-        raise NotImplementedError
+        return (
+            f"The following link property specifies a different resource type in object position:\n"
+            f"{INDENT}Property: {self.prop_name}\n"
+            f"{INDENT}Target resource ID: {self.target_id}\n"
+            f"{INDENT}Target class of resource: {self.target_class_used}\n"
+            f"{INDENT}Target class expected according to the ontology: {self.target_class_expected}"
+        )
+
+    def sort_value(self) -> str:
+        return self.prop_name
 
 
 #######################
@@ -90,7 +158,15 @@ class ListNodeNotFound:
     node_name: str
 
     def get_msg(self) -> str:
-        raise NotImplementedError
+        return (
+            f"The following list node was not found on the server:\n"
+            f"{INDENT}Property: {self.prop_name}\n"
+            f"{INDENT}List node used: {self.node_name}\n"
+            f"{INDENT}List name in data: {self.list_name}"
+        )
+
+    def sort_value(self) -> str:
+        return self.prop_name
 
 
 #######################
@@ -102,7 +178,14 @@ class DuplicateContent:
     res_id: str
     prop_name: str
     content: str
-    number_of_occurrence: int
 
     def get_msg(self) -> str:
-        raise NotImplementedError
+        return (
+            f"The following property and values are entered more than once in the data, but they are identical.\n"
+            f"Please remove all but one.\n"
+            f"{INDENT}Property: {self.prop_name}\n"
+            f"{INDENT}Content: {self.content}"
+        )
+
+    def sort_value(self) -> str:
+        return self.prop_name

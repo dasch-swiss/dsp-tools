eb4f45bc1c9548d57e8ffa006aee660255d4f95a
Nora-Olivia-Ammann <103038637+Nora-Olivia-Ammann@users.noreply.github.com>
2024-08-20 08:55:14 +0200

    requests

--- src/dsp_tools/commands/xml_validate/api_connection.py
+++ src/dsp_tools/commands/xml_validate/api_connection.py
@@ -1,52 +1,76 @@
 from dataclasses import dataclass
-from dataclasses import field
 from typing import Any
-from typing import Protocol
+
+import requests
 
 from dsp_tools.models.exceptions import BaseError
-from dsp_tools.models.exceptions import UserError
+from dsp_tools.models.exceptions import InputError
+
 
 @dataclass
-class OntologyClientLive:
+class Authentication:
+    server: str
+    email: str
+    password: str
+
+    def get_token(self) -> str:
+        """
+        Retrieve a session token and store it as class attribute.
+
+        Args:
+            email: email address of the user
+            password: password of the user
+
+        Raises:
+            UserError: if DSP-API returns no token with the provided user credentials
+        """
+        try:
+            response = requests.get(
+                url=f"{self.server}/v2/authentication",
+                data={"email": self.email, "password": self.password},
+                timeout=10,
+            )
+        except ConnectionError:
+            raise BaseError("ConnectionError during the authentication.")
+        except TimeoutError:
+            raise BaseError("TimeoutError during the authentication.")
+        if not (tnk := response.json().get("token")) and not response.ok:
+            raise InputError(
+                "Unable to retrieve a token from the server with the provided credentials.\n"
+                f"Status Code: {response.status_code}"
+            )
+        return f"Bearer {tnk}"
+
+
+@dataclass
+class OntologyClient:
     """Client handling ontology-related requests to the DSP-API."""
 
-    con:
+    server: str
+    token: str
     shortcode: str
     default_ontology: str
 
-    def get_all_project_ontologies_from_server(self) -> dict[str, list[dict[str, Any]]]:
+    def get_project_ontology(self, ontology_name: str) -> dict[list[dict[str, Any]]]:
         """
-        This function returns all the project ontologies that are on the server.
+        Retrieve the default ontology of a project from a server.
+
+        Args:
+            ontology_name: ontology name
 
         Returns:
-            a dictionary with the ontology name as key and the ontology as value.
+            The json response
         """
-        self._get_ontology_names_from_server()
-        return {onto: self._get_ontology_from_server(onto) for onto in self.ontology_names}
-
-    def _get_ontology_names_from_server(self) -> None:
         try:
-            url = f"/admin/projects/shortcode/{self.shortcode}"
-            res = self.con.get(url)
-        except BaseError as e:
-            raise UserError(f"A project with shortcode {self.shortcode} could not be found on the DSP server") from e
-        try:
-            onto_iris: list[str] = res["project"]["ontologies"]
-        except KeyError as e:
-            raise BaseError(f"Unexpected response from server: {res}") from e
-        onto_names: list[str] = [iri.split("/")[-2] for iri in onto_iris]
-        self.ontology_names = onto_names
-
-    def _get_ontology_from_server(self, ontology_name: str) -> list[dict[str, Any]]:
-        try:
-            url = f"/ontology/{self.shortcode}/{ontology_name}/v2"
-            res = self.con.get(url)
-        except BaseError:
+            url = f"{self.server}/ontology/{self.shortcode}/{ontology_name}/v2"
+            response = requests.get(url, timeout=10)
+            if not response.ok:
+                raise InputError(f"The ontology request returned a status code: {response.status_code}")
+            res: dict[list[dict[str, Any]]] = response.json()
+            return res
+        except ConnectionError:
             raise BaseError(
                 f"Ontologies for project {self.shortcode} could not be retrieved from the DSP server"
             ) from None
-        try:
-            onto_graph: list[dict[str, Any]] = res["@graph"]
-        except KeyError as e:
-            raise BaseError(f"Unexpected response from server: {res}") from e
-        return onto_graph
+        except TimeoutError:
+            raise BaseError("TimeoutError during the authentication.") from None

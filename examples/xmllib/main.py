971d535a8f558e1c59a30f98f3e137843b45aa09
Nora-Olivia-Ammann <103038637+Nora-Olivia-Ammann@users.noreply.github.com>
2024-08-21 14:51:23 +0200

    add files

--- examples/xmllib/main.py
+++ examples/xmllib/main.py
@@ -1,13 +1,17 @@
 # noqa: INP001 (ignore add __init__.py)
 
 
+from pathlib import Path
+
 import pandas as pd
 
 from dsp_tools import xmllib
 
+data_folder = Path("examples/xmllib/data")
+
 
 def _make_books(res_collection: xmllib.ResourceCollection) -> None:
-    df = pd.read_csv("examples/xmllib/data/Book.csv")
+    df = pd.read_csv(data_folder / "Book.csv")
     for _, row in df.iterrows():
         one_book = _make_one_book(row)
         res_collection.addResource(one_book)
@@ -24,7 +28,7 @@ def _make_one_book(row: pd.Series) -> xmllib.Resource:
 
 
 def _make_chapters(res_collection: xmllib.ResourceCollection) -> None:
-    df = pd.read_csv("examples/xmllib/data/Chapter.csv")
+    df = pd.read_csv(data_folder / "Chapter.csv")
     for _, row in df.iterrows():
         one_chapter = _make_one_chapter(row)
         res_collection.addResource(one_chapter)
@@ -38,11 +42,18 @@ def _make_one_chapter(row: pd.Series) -> xmllib.Resource:
     return res
 
 
+def _add_images(res_collection: xmllib.ResourceCollection) -> None:
+    files = [x for x in data_folder.glob("*.jpg")]
+    for f in files:
+        res_collection.addFileValueToResource(res_id=f.stem, filepath=str(f))
+
+
 def main() -> None:
     """Creates an XML file from the csv files in the data folder."""
     resources = xmllib.ResourceCollection()
     _make_books(resources)
     _make_chapters(resources)
+    _add_images(resources)
     root = xmllib.XMLRoot(shortcode="0001", default_ontology="onto", resources=resources.to_list())
     root.write_file("examples/xmllib/data.xml")
 

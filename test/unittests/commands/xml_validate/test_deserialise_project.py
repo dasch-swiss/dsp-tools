54463b1ad12d8353b9554b9239a23c50a5d91910
Nora-Olivia-Ammann <103038637+Nora-Olivia-Ammann@users.noreply.github.com>
2024-08-22 08:08:15 +0200

    Update test_deserialise_project.py

--- test/unittests/commands/xml_validate/test_deserialise_project.py
+++ test/unittests/commands/xml_validate/test_deserialise_project.py
@@ -2,7 +2,7 @@ from typing import Any
 
 import pytest
 
-from dsp_tools.commands.xml_validate.deserialise_api_responses import _deserialise_one_list
+from dsp_tools.commands.xml_validate.deserialise_project import _deserialise_one_list
 
 
 def test_deserialise_lists(list_from_api: dict[str, Any]) -> None:

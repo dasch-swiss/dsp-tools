c37f438a9dc3e9b83f6b9e7d7f4668ab09c4487f
Nora-Olivia-Ammann <103038637+Nora-Olivia-Ammann@users.noreply.github.com>
2024-09-04 12:53:21 +0200

    Update deserialise_project.py

--- src/dsp_tools/commands/xml_validate/deserialise_project.py
+++ src/dsp_tools/commands/xml_validate/deserialise_project.py
@@ -1,7 +1,3 @@
-import json
-from typing import Any
-
-from rdflib import Graph
 from rdflib import Literal
 from rdflib import Namespace
 
@@ -20,41 +16,7 @@ from dsp_tools.commands.xml_validate.models.data_rdf import ProjectNamespaces
 from dsp_tools.commands.xml_validate.models.data_rdf import ResourceRDF
 from dsp_tools.commands.xml_validate.models.data_rdf import SimpleTextRDF
 from dsp_tools.commands.xml_validate.models.data_rdf import ValueRDF
-from dsp_tools.commands.xml_validate.models.project_deserialised import ListDeserialised
 from dsp_tools.models.exceptions import InputError
-
-
-def get_project_ontology() -> Graph:
-    g = Graph()
-    return g.parse("testdata/xml-validate/from_api/onto.ttl")
-
-
-def get_deserialised_lists() -> ListDeserialised:
-    """Get objects which contain the pertinent information of the lists from the API."""
-    with open("testdata/xml-validate/from_api/onlyList.json", "r", encoding="utf-8") as file:
-        data: dict[str, Any] = json.load(file)
-    return _deserialise_one_list(data)
-
-
-def _deserialise_one_list(list_response: dict[str, Any]) -> ListDeserialised:
-    all_children = []
-    for child in list_response["list"]["children"]:
-        all_children.extend(_process_child_nodes(child))
-    return ListDeserialised(
-        list_name=list_response["list"]["listinfo"]["name"],
-        iri=list_response["list"]["listinfo"]["id"],
-        nodes=all_children,
-    )
-
-
-def _process_child_nodes(node: dict[str, Any]) -> list[str]:
-    children = []
-    all_nodes = [node]
-    while all_nodes:
-        current_node = all_nodes.pop(0)
-        children.append(current_node["name"])
-        all_nodes.extend(current_node.get("children", []))
-    return children
 
 
 def create_project_rdf(project: DataDeserialised) -> DataRDF:

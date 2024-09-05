fd03af181819b3afc009bce82ae9617e1d113aba
Nora-Olivia-Ammann <103038637+Nora-Olivia-Ammann@users.noreply.github.com>
2024-09-05 10:42:00 +0200

    add float checker

--- src/dsp_tools/commands/xmllib/value_checkers.py
+++ src/dsp_tools/commands/xmllib/value_checkers.py
@@ -19,6 +19,8 @@ def is_string(value: Any) -> bool:
     if pd.isna(value):
         return False
     value = str(value)
+    if len(value) == 0:
+        return False
     if regex.search(r"^(none|<NA>|-|n/a)$", value, flags=regex.IGNORECASE):
         return False
     return bool(regex.search(r"[\p{L}\d_!?]", value, flags=regex.UNICODE))
@@ -37,8 +39,28 @@ def is_integer(value: Any) -> bool:
         True if conforms to the above-mentioned criteria.
     """
     if isinstance(value, str):
-        if not regex.search(r"^\d+$", value):
-            return False
-    elif not isinstance(value, int):
-        return False
-    return True
+        if regex.search(r"^\d+$", value):
+            return True
+    elif isinstance(value, int):
+        return True
+    return False
+
+
+def is_float(value: Any) -> bool:
+    """
+    Checks if a value is a float.
+    A valid integer is if it is a string, which can be converted into an integer,
+    or a value of the type int.
+
+    Args:
+        value: value to check
+
+    Returns:
+        True if conforms to the above-mentioned criteria.
+    """
+    if isinstance(value, str):
+        if regex.search(r"^\d+(\.\d+)?$", value):
+            return True
+    elif isinstance(value, int) or isinstance(value, float):
+        return True
+    return False

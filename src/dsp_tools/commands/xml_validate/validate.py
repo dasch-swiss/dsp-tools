be1e7de0cfba767058dc74108e3ddd3aee21c3c1
Nora-Olivia-Ammann <103038637+Nora-Olivia-Ammann@users.noreply.github.com>
2024-09-16 14:38:12 +0200

    Update validate.py

--- src/dsp_tools/commands/xml_validate/validate.py
+++ src/dsp_tools/commands/xml_validate/validate.py
@@ -2,6 +2,6 @@ from pathlib import Path
 
 from dsp_tools.commands.xml_validate.xml_validate import xml_validate
 
-xml_validate(Path("testdata/xml-validate/data/valid-data.xml"))
+# xml_validate(Path("testdata/xml-validate/data/valid-data.xml"))
 
-# xml_validate(Path("testdata/xml-validate/data/invalid-data.xml"))
+xml_validate(Path("testdata/xml-validate/data/invalid-data.xml"))

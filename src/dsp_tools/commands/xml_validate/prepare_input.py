8062f3166b0f51b813fcf4b5eb9640e1e87b9c02
Nora-Olivia-Ammann <103038637+Nora-Olivia-Ammann@users.noreply.github.com>
2024-09-17 16:17:57 +0200

    Create prepare_input.py

---
+++ src/dsp_tools/commands/xml_validate/prepare_input.py
@@ -0,0 +1,110 @@
+from pathlib import Path
+from typing import cast
+
+from lxml import etree
+
+from dsp_tools.commands.xml_validate.models.data_deserialised import DataDeserialised
+from dsp_tools.commands.xml_validate.models.data_deserialised import IntValueData
+from dsp_tools.commands.xml_validate.models.data_deserialised import LinkValueData
+from dsp_tools.commands.xml_validate.models.data_deserialised import ListValueData
+from dsp_tools.commands.xml_validate.models.data_deserialised import ResourceData
+from dsp_tools.commands.xml_validate.models.data_deserialised import SimpleTextData
+from dsp_tools.commands.xml_validate.models.data_deserialised import ValueData
+from dsp_tools.utils.xml_utils import parse_and_clean_xml_file
+
+
+def parse_file(file: Path) -> DataDeserialised:
+    """Returns an object which follows the structure of the XML closely"""
+    root = parse_and_clean_xml_file(file)
+    return _transform_into_project_deserialised(root)
+
+
+def _transform_into_project_deserialised(root: etree._Element) -> DataDeserialised:
+    shortcode = root.attrib["shortcode"]
+    default_ontology = root.attrib["default-ontology"]
+    resources = [_deserialise_one_resource(x) for x in root.iterdescendants(tag="resource")]
+    return DataDeserialised(shortcode=shortcode, default_onto=default_ontology, resources=resources)
+
+
+def _deserialise_one_resource(resource: etree._Element) -> ResourceData:
+    res_id = resource.attrib["id"]
+    values: list[ValueData] = []
+    for val in resource.iterchildren():
+        values.extend(_deserialise_one_property(val))
+    return ResourceData(
+        res_id=res_id,
+        res_class=resource.attrib["restype"],
+        label=resource.attrib["label"],
+        values=values,
+    )
+
+
+def _deserialise_one_property(prop_ele: etree._Element) -> list[ValueData]:
+    match prop_ele.tag:
+        case "text-prop":
+            return _deserialise_text_prop(prop_ele)
+        case "list-prop":
+            return _deserialise_list_prop(prop_ele)
+        case "resptr-prop":
+            return _deserialise_resptr_prop(prop_ele)
+        case "integer-prop":
+            return _deserialise_int_prop(prop_ele)
+        case _:
+            return []
+
+
+def _deserialise_text_prop(prop: etree._Element) -> list[ValueData]:
+    prop_name = prop.attrib["name"]
+    all_vals: list[ValueData] = []
+    for child in prop.iterchildren():
+        val = cast(str, child.text)
+        match child.attrib["encoding"]:
+            case "utf8":
+                all_vals.append(SimpleTextData(prop_name=prop_name, prop_value=val))
+            case _:
+                pass
+    return all_vals
+
+
+def _deserialise_list_prop(prop: etree._Element) -> list[ValueData]:
+    prop_name = prop.attrib["name"]
+    list_name = prop.attrib["list"]
+    all_vals: list[ValueData] = []
+    for val in prop.iterchildren():
+        txt = cast(str, val.text)
+        all_vals.append(
+            ListValueData(
+                prop_name=prop_name,
+                prop_value=txt,
+                list_name=list_name,
+            )
+        )
+    return all_vals
+
+
+def _deserialise_resptr_prop(prop: etree._Element) -> list[ValueData]:
+    prop_name = prop.attrib["name"]
+    all_links: list[ValueData] = []
+    for val in prop.iterchildren():
+        txt = cast(str, val.text)
+        all_links.append(
+            LinkValueData(
+                prop_name=prop_name,
+                prop_value=txt,
+            )
+        )
+    return all_links
+
+
+def _deserialise_int_prop(prop: etree._Element) -> list[ValueData]:
+    prop_name = prop.attrib["name"]
+    all_links: list[ValueData] = []
+    for val in prop.iterchildren():
+        txt = cast(str, val.text)
+        all_links.append(
+            IntValueData(
+                prop_name=prop_name,
+                prop_value=txt,
+            )
+        )
+    return all_links

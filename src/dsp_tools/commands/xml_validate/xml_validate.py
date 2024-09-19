c1186ab73d8b07d037b8d1bd36813ac8c82d330c
Nora-Olivia-Ammann <103038637+Nora-Olivia-Ammann@users.noreply.github.com>
2024-09-19 17:06:03 +0200

    Update xml_validate.py

--- src/dsp_tools/commands/xml_validate/xml_validate.py
+++ src/dsp_tools/commands/xml_validate/xml_validate.py
@@ -1,13 +1,29 @@
 from pathlib import Path
 
+from lxml import etree
+
 from dsp_tools.commands.xml_validate.models.data_deserialised import DataDeserialised
 from dsp_tools.commands.xml_validate.reformat_input import transform_into_project_deserialised
 from dsp_tools.utils.xml_utils import parse_and_clean_xml_file
 from dsp_tools.utils.xml_validation import validate_xml
 
 
-def _deserialise_file(file: Path) -> DataDeserialised:
+def _deserialise_file(file: Path, ontology_name: str) -> DataDeserialised:
     """Returns an object which follows the structure of the XML closely"""
+    root = _parse_and_clean_file(file, ontology_name)
+    return transform_into_project_deserialised(root)
+
+
+def _parse_and_clean_file(file: Path, ontology_name: str) -> etree._Element:
     root = parse_and_clean_xml_file(file)
     validate_xml(root)
-    return transform_into_project_deserialised(root)
+    return _replace_namespaces(root, ontology_name)
+
+
+def _replace_namespaces(root: etree._Element, ontology_namespace: str) -> etree._Element:
+    with open("src/dsp_tools/resources/xml_validate/replace_namespace.xslt", "rb") as xslt_file:
+        xslt_data = xslt_file.read()
+    xslt_root = etree.XML(xslt_data)
+    transform = etree.XSLT(xslt_root)
+    replacement_value = etree.XSLT.strparam(ontology_namespace)
+    return transform(root, replacementValue=replacement_value).getroot()

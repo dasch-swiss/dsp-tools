1c1bcadebae444ba9a38bc71acec10ae5a1251d5
Johannes Nussbaum <johannes.nussbaum@dasch.swiss>
2024-09-19 16:43:09 +0200

    implement bump-version as python script

--- justfile
+++ justfile
@@ -76,21 +76,6 @@ legacy-e2e-tests *FLAGS:
     uv run pytest test/legacy_e2e/ {{FLAGS}}
 
 
-# Calculate the post-release number and write it into pyproject.toml (used for dev-releases)
-[no-exit-message]
-bump-version:
-    # get current version, append ".postN" (N=no. of commits since last release), write back into pyproject.toml
-    LATEST_TAG=$(git describe --tags --abbrev=0)          # get the latest release tag from git history (e.g. "v9.0.2")
-    COMMIT_CNT=$(git rev-list --count main ^$LATEST_TAG)  # get the number of commits since the last release (e.g. "5")
-    if [ $COMMIT_CNT -eq 0 ]; then exit 0; fi             # if this CI run was triggered by a release, do nothing
-    COMMIT_CNT=$((COMMIT_CNT - 1))                        # e.g. 5 -> 4 (N in ".postN" is zero-based)
-    NEW_VERSION=${LATEST_TAG:1}.post$COMMIT_CNT           # e.g. "9.0.2.post4" (no leading v in pyproject.toml)
-    OLD_TOML_LINE_RGX="version ?= ?\"${LATEST_TAG:1}\""   # e.g. 'version ?= ?"9.0.2"'
-    # exit ungracefully if the regex matches on more than 1 line within the 20 first lines
-    if [ $(grep -En $OLD_TOML_LINE_RGX pyproject.toml | awk -F ':' '{if ($1 <= 20) print $1}' | wc -l) != 1 ]; then exit 1; fi
-    sed -E -i "" "1,20s/$OLD_TOML_LINE_RGX/version = \"$NEW_VERSION\"/g" pyproject.toml  # write back into pyproject.toml
-
-
 # Remove artifact files
 [no-exit-message]
 clean:
---
+++ scripts/bump-version.py
@@ -0,0 +1,36 @@
+import subprocess  # noqa: INP001 (missing __init__.py)
+import sys
+from pathlib import Path
+
+import tomlkit
+
+
+def _make_sys_call(call: str) -> str:
+    return subprocess.run(call.split(), capture_output=True, check=True, encoding="utf-8").stdout.removesuffix("\n")
+
+
+def main() -> None:
+    """Get current version, append ".postN" (N=no. of commits since last release), write back into pyproject.toml"""
+    # get the latest release tag from git history (e.g. "v9.0.2")
+    latest_tag = _make_sys_call("git describe --tags --abbrev=0")
+    # get the number of commits since the last release (e.g. "5")
+    commit_count_str = _make_sys_call(f"git rev-list --count main {latest_tag}")
+    # if this CI run was triggered by a release, do nothing
+    if commit_count_str == "0":
+        sys.exit(0)
+    # e.g. 5 -> 4 (N in ".postN" is zero-based)
+    commit_count = int(commit_count_str) - 1
+    # e.g. "9.0.2.post4" (no leading v in pyproject.toml)
+    new_version = f"{latest_tag[1:]}.post{commit_count}"
+    # write new version into pyproject.toml
+    pyproject_pth = Path("pyproject.toml")
+    pyproject = tomlkit.parse(pyproject_pth.read_text(encoding="utf-8"))
+    if pyproject["project"]["version"] != latest_tag[1:]:
+        msg = f"Version in pyproject.toml is '{pyproject["project"]["version"]}', but expected {latest_tag[1:]}"
+        raise ValueError(msg)
+    pyproject["project"]["version"] = new_version
+    pyproject_pth.write_text(tomlkit.dumps(pyproject), encoding="utf-8")
+
+
+if __name__ == "__main__":
+    main()

6a0a38f335457174391c0a5ef75ce3a4af8d3ea5
Johannes Nussbaum <johannes.nussbaum@dasch.swiss>
2024-09-20 09:05:20 +0200

    refactor

--- scripts/bump-version.py
+++ scripts/bump-version.py
@@ -5,35 +5,57 @@ from pathlib import Path
 import tomlkit
 
 
-def _make_sys_call(call: str) -> str:
-    try:
-        return subprocess.run(call.split(), capture_output=True, check=True, encoding="utf-8").stdout.removesuffix("\n")
-    except subprocess.CalledProcessError as e:
-        print(f"Call: {call} | Returncode: {e.returncode} | Stdout: {e.stdout} | Stderr: {e.stderr}")
-        sys.exit(1)
-
-
 def main() -> None:
     """Get current version, append ".postN" (N=no. of commits since last release), write back into pyproject.toml"""
+
     # get the latest release tag from git history (e.g. "v9.0.2")
     latest_tag = _make_sys_call("git describe --tags --abbrev=0")
-    # get the number of commits since the last release (e.g. "5")
-    commit_count_str = _make_sys_call(f"git rev-list --count HEAD ^{latest_tag}")  # use main instead of HEAD!
+
+    new_version = _calculate_new_version(latest_tag)
+
+    _write_new_version_into_pyproject_toml(old_version=latest_tag[1:], new_version=new_version)
+
+
+def _calculate_new_version(latest_tag: str) -> str:
+    _check_if_on_main_branch()
+    # no. of commits since last release (e.g. "5")
+    commit_count_str = _make_sys_call(f"git rev-list --count main ^{latest_tag}")
     # if this CI run was triggered by a release, do nothing
     if commit_count_str == "0":
         sys.exit(0)
     # e.g. 5 -> 4 (N in ".postN" is zero-based)
     commit_count = int(commit_count_str) - 1
     # e.g. "9.0.2.post4" (no leading v in pyproject.toml)
-    new_version = f"{latest_tag[1:]}.post{commit_count}"
-    # write new version into pyproject.toml
+    return f"{latest_tag[1:]}.post{commit_count}"
+
+
+def _check_if_on_main_branch() -> None:
+    if _make_sys_call("git rev-parse --abbrev-ref HEAD") != "main":
+        msg = (
+            "The 'git rev-list' command will only work as expected on the main branch. "
+            "If this workflow is intentionally run on a branch other than the 'main', "
+            "run 'git rev-list' with 'HEAD' instead of 'main'. "
+            "This should only be done for testing purposes. In production, this will yield an unintended commit count."
+        )
+        raise ValueError(msg)
+
+
+def _write_new_version_into_pyproject_toml(old_version: str, new_version: str) -> None:
     pyproject_pth = Path("pyproject.toml")
     pyproject = tomlkit.parse(pyproject_pth.read_text(encoding="utf-8"))
-    if pyproject["project"]["version"] != latest_tag[1:]:  # type: ignore[index]
-        msg = f"Version in pyproject.toml is '{pyproject["project"]["version"]}', but expected {latest_tag[1:]}"  # type: ignore[index]
+    if pyproject["project"]["version"] != old_version:  # type: ignore[index]
+        msg = f"Version in pyproject.toml is '{pyproject["project"]["version"]}', but expected {old_version}"  # type: ignore[index]
         raise ValueError(msg)
     pyproject["project"]["version"] = new_version  # type: ignore[index]
     pyproject_pth.write_text(tomlkit.dumps(pyproject), encoding="utf-8")
+
+
+def _make_sys_call(call: str) -> str:
+    try:
+        return subprocess.run(call.split(), capture_output=True, check=True, encoding="utf-8").stdout.removesuffix("\n")
+    except subprocess.CalledProcessError as e:
+        print(f"Call: {call} | Returncode: {e.returncode} | Stdout: {e.stdout} | Stderr: {e.stderr}")
+        sys.exit(1)
 
 
 if __name__ == "__main__":

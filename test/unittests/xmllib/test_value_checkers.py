533b38549e2b0c0f38427add9062ae63301bcb9a
Nora-Olivia-Ammann <103038637+Nora-Olivia-Ammann@users.noreply.github.com>
2024-09-23 10:12:09 +0200

    Update test_value_checkers.py

--- test/unittests/xmllib/test_value_checkers.py
+++ test/unittests/xmllib/test_value_checkers.py
@@ -96,7 +96,7 @@ def test_is_integer_wrong(val: Any) -> None:
     assert not is_integer(val)
 
 
-@pytest.mark.parametrize("val", ["a", "None", "-", "1", "\n1", "עִבְרִית", "اَلْعَرَبِيَّةُ"])
+@pytest.mark.parametrize("val", ["a", "None", "-", "1", "\n1", "עִבְרִית", "اَلْعَرَبِيَّةُ", "hello world"])
 def test_is_string_correct(val: Any) -> None:
     assert is_string_like(val)
 

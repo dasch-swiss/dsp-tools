16b1672b670fb537f8be228e1279ae3f481d86d3
Nora-Olivia-Ammann <103038637+Nora-Olivia-Ammann@users.noreply.github.com>
2024-09-23 15:59:06 +0200

    Update api_connection.py

--- src/dsp_tools/commands/xml_validate/api_connection.py
+++ src/dsp_tools/commands/xml_validate/api_connection.py
@@ -1,18 +1,15 @@
-import json
+from dataclasses import dataclass
+from typing import Any
+from typing import cast
+
+import requests
 from loguru import logger
 from requests import ReadTimeout
 from requests import RequestException
 from requests import Response
-import requests
-from typing import cast, Any
-from dataclasses import dataclass
 
-from dsp_tools.commands.xml_validate.models.data_rdf import IntValueRDF
 from dsp_tools.models.exceptions import BadCredentialsError
-from dsp_tools.models.exceptions import BaseError, InternalError
-from dsp_tools.models.exceptions import InvalidInputError
-from dsp_tools.models.exceptions import PermanentConnectionError
-from dsp_tools.models.exceptions import PermanentTimeOutError
+from dsp_tools.models.exceptions import InternalError
 from dsp_tools.models.exceptions import UserError
 
 
@@ -52,10 +49,25 @@ class OntologyConnection:
     api_url: str
     shortcode: str
 
-    def get(self, url: str, token: str, headers: dict[str, Any]) -> dict[str, Any]:
+    def get(self, url: str, token: str, headers: dict[str, Any]) -> Response:
+        """
+        Sends a get request to the designated path
+
+        Args:
+            url: URL for the request
+            token: bearer token
+            headers: headers for the request
+
+        Returns:
+            Response of the request if it was a 200 response code
+
+        Raises:
+            InternalError: in case of errors raised by the requests library
+            UserError: If a non-200 response was given
+        """
         headers["Authorization"] = token
         try:
-            response = requests.request("POST", url, headers=headers)
+            response = requests.request("POST", url, headers=headers, timeout=100)
         except (TimeoutError, ReadTimeout) as err:
             logger.exception(err)
             raise InternalError("TimeoutError occurred. See logs for details.") from None
@@ -63,11 +75,43 @@ class OntologyConnection:
             logger.exception(err)
             raise InternalError("ConnectionError occurred. See logs for details.") from None
         if not response.ok:
-            msg = ""
+            msg = f"Non-ok response: {response.status_code}\nOriginal message: {response.text}"
+            logger.exception(msg)
+            raise UserError(msg)
+        return response
 
+    def get_ontologies(self, token: str) -> list[str]:
+        """
+        Returns a list of project ontologies as a string in turtle format.
 
-    def get_ontologies(self, token: str) -> dict[str, Any]:
-        pass
+        Args:
+            token: authentication token
 
-    def _get_ontology_names(self, token: str):
-        pass
+        Returns:
+            list of ontologies
+        """
+        ontology_iris = self._get_ontology_iris(token)
+        return [self._get_one_ontology(x, token) for x in ontology_iris]
+
+    def _get_one_ontology(self, ontology_iri: str, token: str) -> str:
+        header = {"Accept": "text/turtle"}
+        response = self.get(ontology_iri, token, header)
+        return response.text
+
+    def _get_ontology_iris(self, token: str) -> list[str]:
+        endpoint = f"{self.api_url}/admin/projects/shortcode/{self.shortcode}"
+        response = self.get(endpoint, token, {})
+        response_json = cast(dict[str, Any], response.json())
+        if not (ontos := response_json.get("ontologies")):
+            msg = f"The response from the API does not contain any ontologies.\nAPI response:{response}"
+            logger.exception(msg)
+            raise UserError(msg)
+        if isinstance(ontos, list):
+            out_onto = ontos
+        elif isinstance(ontos, str):
+            out_onto = [ontos]
+        else:
+            msg = f"The response from the API does not contain any ontologies.\nAPI response:{response}"
+            logger.exception(msg)
+            raise UserError(msg)
+        return out_onto

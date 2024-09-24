8fd62061eb91cde25a9fd2f7b23f2a106bdde2b9
Nora-Olivia-Ammann <103038637+Nora-Olivia-Ammann@users.noreply.github.com>
2024-09-24 13:06:16 +0200

    test get ontology

--- test/unittests/commands/xml_validate/test_api_connection.py
+++ test/unittests/commands/xml_validate/test_api_connection.py
@@ -12,30 +12,40 @@ def ontology_connection() -> OntologyConnection:
     return OntologyConnection("http://example.com", "9999")
 
 
-class TestOntologyConnection:
-    def test_get_ontology_iris_ok(self, ontology_connection: OntologyConnection) -> None:
-        mock_response = Mock()
-        mock_response.ok = True
-        mock_response.json.return_value = {"ontologies": ["onto_iri"]}
-        with patch.object(ontology_connection, "get", return_value=mock_response):
-            result = ontology_connection._get_ontology_iris()
-            assert result == ["onto_iri"]
+def test_get_ontology_iris_ok(ontology_connection: OntologyConnection) -> None:
+    mock_response = Mock()
+    mock_response.ok = True
+    mock_response.json.return_value = {"ontologies": ["onto_iri"]}
+    with patch.object(ontology_connection, "get", return_value=mock_response):
+        result = ontology_connection._get_ontology_iris()
+        assert result == ["onto_iri"]
 
-    def test_get_ontology_iris_non_ok_code(self, ontology_connection: OntologyConnection) -> None:
-        mock_response = Mock()
-        mock_response.ok = False
-        mock_response.json.return_value = {}
-        with patch.object(ontology_connection, "get", return_value=mock_response):
-            with pytest.raises(UserError):
-                ontology_connection._get_ontology_iris()
 
-    def test_get_ontology_iris_no_ontology_key(self, ontology_connection: OntologyConnection) -> None:
-        mock_response = Mock()
-        mock_response.ok = True
-        mock_response.json.return_value = {}
-        with patch.object(ontology_connection, "get", return_value=mock_response):
-            with pytest.raises(UserError):
-                ontology_connection._get_ontology_iris()
+def test_get_ontology_iris_non_ok_code(ontology_connection: OntologyConnection) -> None:
+    mock_response = Mock()
+    mock_response.ok = False
+    mock_response.json.return_value = {}
+    with patch.object(ontology_connection, "get", return_value=mock_response):
+        with pytest.raises(UserError):
+            ontology_connection._get_ontology_iris()
+
+
+def test_get_ontology_iris_no_ontology_key(ontology_connection: OntologyConnection) -> None:
+    mock_response = Mock()
+    mock_response.ok = True
+    mock_response.json.return_value = {}
+    with patch.object(ontology_connection, "get", return_value=mock_response):
+        with pytest.raises(UserError):
+            ontology_connection._get_ontology_iris()
+
+
+def test_get_one_ontology(ontology_connection: OntologyConnection) -> None:
+    mock_response = Mock()
+    mock_response.ok = True
+    mock_response.text = "Turtle Text"
+    with patch.object(ontology_connection, "get", return_value=mock_response):
+        result = ontology_connection._get_one_ontology("iri")
+        assert result == "Turtle Text"
 
 
 if __name__ == "__main__":

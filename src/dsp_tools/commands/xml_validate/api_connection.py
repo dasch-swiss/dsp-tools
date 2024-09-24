d57273362d87c447abeaa65c6177bf6f6a48c6ac
Nora-Olivia-Ammann <103038637+Nora-Olivia-Ammann@users.noreply.github.com>
2024-09-24 11:06:13 +0200

    remove auth

--- src/dsp_tools/commands/xml_validate/api_connection.py
+++ src/dsp_tools/commands/xml_validate/api_connection.py
@@ -8,7 +8,6 @@ from requests import ReadTimeout
 from requests import RequestException
 from requests import Response
 
-from dsp_tools.models.exceptions import BadCredentialsError
 from dsp_tools.models.exceptions import InternalError
 from dsp_tools.models.exceptions import UserError
 
@@ -16,58 +15,16 @@ STATUS_UNAUTHORIZED = 401
 
 
 @dataclass
-class Authentication:
-    api_url: str
-    user_email: str
-    password: str
-    bearer_tkn: str | None = None
-
-    def get_bearer_token(self) -> str:
-        response = self._request_token()
-        if response.status_code == STATUS_UNAUTHORIZED:
-            msg = f"Username and/or password are not valid on server '{self.api_url}'"
-            logger.exception(msg)
-            raise BadCredentialsError(msg)
-        if not response.ok:
-            msg = f"Non-ok response code: {response.status_code}\nOriginal Message: {response.text}"
-            logger.exception(msg)
-            raise UserError(msg)
-        json_response = cast(dict[str, Any], response.json())
-        if not json_response.get("token"):
-            msg = "Unable to retrieve a token from the server with the provided credentials."
-            logger.exception(msg)
-            raise InternalError(msg)
-        tkn = f"Bearer {json_response["token"]}"
-        self.bearer_tkn = tkn
-        return tkn
-
-    def _request_token(self) -> Response:
-        try:
-            return requests.post(
-                url=f"{self.api_url}/v2/authentication",
-                data={"email": self.user_email, "password": self.password},
-                timeout=10,
-            )
-        except (TimeoutError, ReadTimeout) as err:
-            logger.exception(err)
-            raise InternalError("TimeoutError occurred. See logs for details.") from None
-        except (ConnectionError, RequestException) as err:
-            logger.exception(err)
-            raise InternalError("ConnectionError occurred. See logs for details.") from None
-
-
-@dataclass
 class OntologyConnection:
     api_url: str
     shortcode: str
 
-    def get(self, url: str, token: str, headers: dict[str, Any]) -> Response:
+    def get(self, url: str, headers: dict[str, Any]) -> Response:
         """
         Sends a get request to the designated path
 
         Args:
             url: URL for the request
-            token: bearer token
             headers: headers for the request
 
         Returns:
@@ -77,7 +34,6 @@ class OntologyConnection:
             InternalError: in case of errors raised by the requests library
             UserError: If a non-200 response was given
         """
-        headers["Authorization"] = token
         try:
             response = requests.request("POST", url, headers=headers, timeout=100)
         except (TimeoutError, ReadTimeout) as err:
@@ -92,38 +48,28 @@ class OntologyConnection:
             raise UserError(msg)
         return response
 
-    def get_ontologies(self, token: str) -> list[str]:
+    def get_ontologies(self) -> list[str]:
         """
         Returns a list of project ontologies as a string in turtle format.
-
-        Args:
-            token: authentication token
 
         Returns:
             list of ontologies
         """
-        ontology_iris = self._get_ontology_iris(token)
-        return [self._get_one_ontology(x, token) for x in ontology_iris]
+        ontology_iris = self._get_ontology_iris()
+        return [self._get_one_ontology(x) for x in ontology_iris]
 
-    def _get_one_ontology(self, ontology_iri: str, token: str) -> str:
-        header = {"Accept": "text/turtle"}
-        response = self.get(ontology_iri, token, header)
-        return response.text
-
-    def _get_ontology_iris(self, token: str) -> list[str]:
+    def _get_ontology_iris(self) -> list[str]:
         endpoint = f"{self.api_url}/admin/projects/shortcode/{self.shortcode}"
-        response = self.get(endpoint, token, {})
+        response = self.get(endpoint, {})
         response_json = cast(dict[str, Any], response.json())
         if not (ontos := response_json.get("ontologies")):
             msg = f"The response from the API does not contain any ontologies.\nAPI response:{response}"
             logger.exception(msg)
             raise UserError(msg)
-        if isinstance(ontos, list):
-            out_onto = ontos
-        elif isinstance(ontos, str):
-            out_onto = [ontos]
-        else:
-            msg = f"The response from the API does not contain any ontologies.\nAPI response:{response}"
-            logger.exception(msg)
-            raise UserError(msg)
-        return out_onto
+        output = cast(list[str], ontos)
+        return output
+
+    def _get_one_ontology(self, ontology_iri: str) -> str:
+        header = {"Accept": "text/turtle"}
+        response = self.get(ontology_iri, header)
+        return response.text
--- test/unittests/commands/xml_validate/test_api_connection.py
+++ test/unittests/commands/xml_validate/test_api_connection.py
@@ -3,10 +3,7 @@ from unittest.mock import patch
 
 import pytest
 
-from dsp_tools.commands.xml_validate.api_connection import Authentication
 from dsp_tools.commands.xml_validate.api_connection import OntologyConnection
-from dsp_tools.models.exceptions import BadCredentialsError
-from dsp_tools.models.exceptions import InternalError
 from dsp_tools.models.exceptions import UserError
 
 
@@ -15,49 +12,30 @@ def ontology_connection() -> OntologyConnection:
     return OntologyConnection("http://example.com", "9999")
 
 
-class TestAuthentication:
-    def test_ok(self) -> None:
-        auth = Authentication("http://example.com", "email@test.ch", "pw1234")
-        mock_response = Mock()
-        mock_response.ok = True
-        mock_response.json.return_value = {"token": "token"}
-        with patch.object(auth, "_request_token", return_value=mock_response):
-            expected_tkn = "Bearer token"
-            assert auth.get_bearer_token() == expected_tkn
-            assert auth.bearer_tkn == expected_tkn
-
-    def test_bad_credentials(self) -> None:
-        auth = Authentication("http://example.com", "email@test.ch", "pw1234")
-        mock_response = Mock()
-        mock_response.status_code = 401
-        with patch.object(auth, "_request_token", return_value=mock_response):
-            with pytest.raises(BadCredentialsError):
-                auth.get_bearer_token()
-
-    def test_non_ok_code(self) -> None:
-        auth = Authentication("http://example.com", "email@test.ch", "pw1234")
-        mock_response = Mock()
-        mock_response.ok = False
-        with patch.object(auth, "_request_token", return_value=mock_response):
-            with pytest.raises(UserError):
-                auth.get_bearer_token()
-
-    def test_no_token(self) -> None:
-        auth = Authentication("http://example.com", "email@test.ch", "pw1234")
-        mock_response = Mock()
-        mock_response.ok = True
-        mock_response.json.return_value = {"other": "other"}
-        with patch.object(auth, "_request_token", return_value=mock_response):
-            with pytest.raises(InternalError):
-                auth.get_bearer_token()
-
-
 class TestOntologyConnection:
     def test_get_ontology_iris_ok(self, ontology_connection: OntologyConnection) -> None:
-        pass
+        mock_response = Mock()
+        mock_response.ok = True
+        mock_response.json.return_value = {"ontologies": ["onto_iri"]}
+        with patch.object(ontology_connection, "get", return_value=mock_response):
+            result = ontology_connection._get_ontology_iris()
+            assert result == ["onto_iri"]
 
     def test_get_ontology_iris_non_ok_code(self, ontology_connection: OntologyConnection) -> None:
-        pass
+        mock_response = Mock()
+        mock_response.ok = False
+        mock_response.json.return_value = {}
+        with patch.object(ontology_connection, "get", return_value=mock_response):
+            with pytest.raises(UserError):
+                ontology_connection._get_ontology_iris()
+
+    def test_get_ontology_iris_no_ontology_key(self, ontology_connection: OntologyConnection) -> None:
+        mock_response = Mock()
+        mock_response.ok = True
+        mock_response.json.return_value = {}
+        with patch.object(ontology_connection, "get", return_value=mock_response):
+            with pytest.raises(UserError):
+                ontology_connection._get_ontology_iris()
 
 
 if __name__ == "__main__":

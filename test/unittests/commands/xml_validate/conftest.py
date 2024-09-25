e4d9764dd72be4ce7e7842fc4e9b53aff6b17286
Nora-Olivia-Ammann <103038637+Nora-Olivia-Ammann@users.noreply.github.com>
2024-09-25 11:37:50 +0200

    Update conftest.py

--- test/unittests/commands/xml_validate/conftest.py
+++ test/unittests/commands/xml_validate/conftest.py
@@ -1,2 +1,3 @@
+from test.unittests.commands.xml_validate.data_deserialised_fixtures import *  # noqa: F403
 from test.unittests.commands.xml_validate.data_rdf_fixtures import *  # noqa: F403
 from test.unittests.commands.xml_validate.xml_fixtures import *  # noqa: F403

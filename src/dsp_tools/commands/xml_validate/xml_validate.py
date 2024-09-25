dc5eb444f9e50f9c6ce3bb8daf4ea17b54f51d7d
Nora-Olivia-Ammann <103038637+Nora-Olivia-Ammann@users.noreply.github.com>
2024-09-25 15:26:50 +0200

    Update xml_validate.py

--- src/dsp_tools/commands/xml_validate/xml_validate.py
+++ src/dsp_tools/commands/xml_validate/xml_validate.py
@@ -1,7 +1,9 @@
 from pathlib import Path
 
 from lxml import etree
+from rdflib import Graph
 
+from dsp_tools.commands.xml_validate.api_connection import OntologyConnection
 from dsp_tools.commands.xml_validate.deserialise_input import deserialise_xml
 from dsp_tools.commands.xml_validate.make_data_rdf import make_data_rdf
 from dsp_tools.commands.xml_validate.models.data_deserialised import ProjectDeserialised
@@ -13,18 +15,31 @@ from dsp_tools.utils.xml_utils import transform_into_localnames
 from dsp_tools.utils.xml_validation import validate_xml
 
 
-def _get_data_info_from_file(file: Path, namespace: str) -> tuple[ProjectInformation, DataRDF]:
-    cleaned_root = _parse_and_clean_file(file, namespace)
+def _get_project_ontos(onto_con: OntologyConnection) -> Graph:
+    all_ontos = onto_con.get_ontologies()
+    g = Graph()
+    for onto in all_ontos:
+        og = Graph()
+        og.parse(data=onto, format="ttl")
+        g += og
+    return g
+
+
+def _get_data_info_from_file(file: Path, api_url: str) -> tuple[ProjectInformation, DataRDF]:
+    cleaned_root = _parse_and_clean_file(file, api_url)
     deserialised: ProjectDeserialised = deserialise_xml(cleaned_root)
     rdf_data: DataRDF = make_data_rdf(deserialised.data)
     return deserialised.info, rdf_data
 
 
-def _parse_and_clean_file(file: Path, namespace: str) -> etree._Element:
+def _parse_and_clean_file(file: Path, api_url: str) -> etree._Element:
     root = parse_xml_file(file)
     root = remove_comments_from_element_tree(root)
     validate_xml(root)
     root = transform_into_localnames(root)
+    shortcode = root.attrib["shortcode"]
+    default_ontology = root.attrib["default-ontology"]
+    namespace = f"{api_url}/ontology/{shortcode}/{default_ontology}/v2#"
     return _replace_namespaces(root, namespace)
 
 

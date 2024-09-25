51a498abc06084519f187ff2f4bb1c0871c94e0b
Nora-Olivia-Ammann <103038637+Nora-Olivia-Ammann@users.noreply.github.com>
2024-09-25 14:34:10 +0200

    add sparql for resource nodeshape

---
+++ src/dsp_tools/commands/xml_validate/sparql/resource_shacl.py
@@ -0,0 +1,42 @@
+from rdflib import Graph
+
+
+def construct_resource_class_node_shape(onto: Graph) -> Graph:
+    """
+    Returns the sh:NodeShapes for the resource classes in the ontology.
+
+    Args:
+        onto: ontology graph
+
+    Returns:
+        Graph with the resource class node shapes
+    """
+    return _construct_resource_nodeshape(onto)
+
+
+def _construct_resource_nodeshape(onto_graph: Graph) -> Graph:
+    query_s = """
+    PREFIX owl: <http://www.w3.org/2002/07/owl#> 
+    PREFIX sh: <http://www.w3.org/ns/shacl#>
+    PREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema#>
+    PREFIX rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#>
+
+    PREFIX api-shapes: <http://api.knora.org/ontology/knora-api/shapes/v2#>
+    PREFIX knora-api:  <http://api.knora.org/ontology/knora-api/v2#>
+
+    CONSTRUCT {
+
+        ?shapesIRI a sh:NodeShape ;
+                sh:targetClass ?class ;
+                sh:property api-shapes:RDFS_label ;
+                sh:ignoredProperties ( rdf:type ) .
+
+    } WHERE {
+
+        ?class a owl:Class ;
+               knora-api:isResourceClass true .
+
+        BIND(IRI(CONCAT(str(?class), "_Shape")) AS ?shapesIRI)
+    }
+    """
+    return onto_graph.query(query_s).graph

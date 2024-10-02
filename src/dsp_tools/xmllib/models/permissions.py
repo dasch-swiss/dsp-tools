24b48786fac4e51272df89188b40780342b590a3
Nora-Olivia-Ammann <103038637+Nora-Olivia-Ammann@users.noreply.github.com>
2024-10-02 11:34:23 +0200

    Update permissions.py

--- src/dsp_tools/xmllib/models/permissions.py
+++ src/dsp_tools/xmllib/models/permissions.py
@@ -13,6 +13,49 @@ class Permissions(Enum):
     restricted_view = auto()
 
 
+class PermissionTypes(Enum):
+    RV = "RV"
+    V = "V"
+    D = "D"
+    CR = "CR"
+
+
 class XMLPermissions:
     def get(self) -> list[etree._Element]:
-        pass
+        return [self._get_open(), self._get_restricted(), self._get_restricted_view()]
+
+    def _get_open(self) -> etree._Element:
+        permissions = {
+            "UnknownUser": PermissionTypes.V,
+            "KnownUser": PermissionTypes.V,
+            "ProjectMember": PermissionTypes.D,
+            "ProjectAdmin": PermissionTypes.CR,
+        }
+        return self._get_one_permission_element("open", permissions)
+
+    def _get_restricted(self) -> etree._Element:
+        permissions = {
+            "ProjectMember": PermissionTypes.D,
+            "ProjectAdmin": PermissionTypes.CR,
+        }
+        return self._get_one_permission_element("restricted", permissions)
+
+    def _get_restricted_view(self) -> etree._Element:
+        permissions = {
+            "UnknownUser": PermissionTypes.RV,
+            "KnownUser": PermissionTypes.RV,
+            "ProjectMember": PermissionTypes.D,
+            "ProjectAdmin": PermissionTypes.CR,
+        }
+        return self._get_one_permission_element("restricted-view", permissions)
+
+    def _get_one_permission_element(self, permission_name: str, groups: dict[str, PermissionTypes]) -> etree._Element:
+        ele = etree.Element(f"{DASCH_SCHEMA}permissions", attrib={"id": permission_name}, nsmap=XML_NAMESPACE_MAP)
+        allows = [self._get_one_allow(g, name) for g, name in groups.items()]
+        ele.extend(allows)
+        return ele
+
+    def _get_one_allow(self, group: str, tag_text: PermissionTypes) -> etree._Element:
+        ele = etree.Element(f"{DASCH_SCHEMA}allow", attrib={"group": group}, nsmap=XML_NAMESPACE_MAP)
+        ele.text = tag_text.value
+        return ele

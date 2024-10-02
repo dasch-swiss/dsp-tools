e384c721d563eb281f37c88ca2c864982535abc0
Nora-Olivia-Ammann <103038637+Nora-Olivia-Ammann@users.noreply.github.com>
2024-10-02 13:32:14 +0200

    test cardinality reformatting

--- test/integration/commands/xml_validate/test_reformat_validaton_result.py
+++ test/integration/commands/xml_validate/test_reformat_validaton_result.py
@@ -3,6 +3,7 @@ from pathlib import Path
 import pytest
 from rdflib import Graph
 
+from dsp_tools.commands.xml_validate.reformat_validaton_result import _reformat_result_graph
 from dsp_tools.commands.xml_validate.xml_validate import _get_data_info_from_file
 
 LOCAL_API = "http://0.0.0.0:3333"
@@ -19,3 +20,12 @@ def result_cardinality_violation() -> Graph:
     g = Graph()
     g.parse("testdata/xml-validate/validation_results/cardinality_violation_result.ttl")
     return g
+
+
+def test_reformat_result_graph(result_cardinality_violation: Graph, data_cardinality_violation: Graph) -> None:
+    result = _reformat_result_graph(result_cardinality_violation, data_cardinality_violation)
+    assert len(result) == 4
+
+
+if __name__ == "__main__":
+    pytest.main([__file__])
--- test/unittests/commands/xml_validate/test_reformat_validaton_result.py
+++ test/unittests/commands/xml_validate/test_reformat_validaton_result.py
@@ -3,9 +3,14 @@ from rdflib import RDF
 from rdflib import SH
 from rdflib import Graph
 from rdflib import Namespace
-from rdflib import URIRef
 
+from dsp_tools.commands.xml_validate.models.input_problems import MaxCardinalityViolation
+from dsp_tools.commands.xml_validate.models.input_problems import MinCardinalityViolation
+from dsp_tools.commands.xml_validate.models.input_problems import NonExistentCardinalityViolation
+from dsp_tools.commands.xml_validate.models.input_problems import UnexpectedComponent
+from dsp_tools.commands.xml_validate.models.input_problems import ValidationResult
 from dsp_tools.commands.xml_validate.reformat_validaton_result import _extract_one_violation
+from dsp_tools.commands.xml_validate.reformat_validaton_result import _reformat_one_violation
 
 VALIDATION_PREFIXES = """
     @prefix onto: <http://0.0.0.0:3333/ontology/9999/onto/v2#> .
@@ -14,6 +19,7 @@ VALIDATION_PREFIXES = """
     """
 
 ONTO = Namespace("http://0.0.0.0:3333/ontology/9999/onto/v2#")
+DATA = Namespace("http://data/")
 
 
 @pytest.fixture
@@ -40,14 +46,88 @@ def data() -> Graph:
     return g
 
 
+@pytest.fixture
+def violation_min_card() -> ValidationResult:
+    return ValidationResult(
+        source_constraint_component=SH.MinCountConstraintComponent,
+        res_iri=DATA.id_min_card,
+        res_class=ONTO.ClassMixedCard,
+        property=ONTO.testGeoname,
+        results_message="1-n",
+    )
+
+
+@pytest.fixture
+def violation_max_card() -> ValidationResult:
+    return ValidationResult(
+        source_constraint_component=SH.MaxCountConstraintComponent,
+        res_iri=DATA.id_max_card,
+        res_class=ONTO.ClassMixedCard,
+        property=ONTO.testDecimalSimpleText,
+        results_message="0-1",
+    )
+
+
+@pytest.fixture
+def violation_closed() -> ValidationResult:
+    return ValidationResult(
+        source_constraint_component=SH.ClosedConstraintComponent,
+        res_iri=DATA.id_closed_constraint,
+        res_class=ONTO.CardOneResource,
+        property=ONTO.testIntegerSimpleText,
+        results_message="Predicate ns3:testIntegerSimpleText is not allowed (closed shape)",
+    )
+
+
+@pytest.fixture
+def violation_unknown() -> ValidationResult:
+    return ValidationResult(
+        source_constraint_component=SH.UniqueLangConstraintComponent,
+        res_iri=DATA.id,
+        res_class=ONTO.ClassMixedCard,
+        property=ONTO.testIntegerSimpleText,
+        results_message="This is a constraint that is not checked in the data and should never appear.",
+    )
+
+
 def test_extract_one_violation(min_count_violation: Graph, data: Graph) -> None:
     bn = next(min_count_violation.subjects(RDF.type, SH.ValidationResult))
     result = _extract_one_violation(bn, min_count_violation, data)
     assert result.source_constraint_component == SH.MinCountConstraintComponent
-    assert result.res_iri == URIRef("http://data/id_min_card")
+    assert result.res_iri == DATA.id_min_card
     assert result.res_class == ONTO.ClassMixedCard
     assert result.property == ONTO.testGeoname
     assert result.results_message == "1-n"
+
+
+class TestReformatViolation:
+    def test_min(self, violation_min_card: ValidationResult) -> None:
+        result = _reformat_one_violation(violation_min_card)
+        assert isinstance(result, MinCardinalityViolation)
+        assert result.res_id == "id_min_card"
+        assert result.res_type == "onto:ClassMixedCard"
+        assert result.prop_name == "onto:testGeoname"
+        assert result.expected_cardinality == "1-n"
+
+    def test_max(self, violation_max_card: ValidationResult) -> None:
+        result = _reformat_one_violation(violation_max_card)
+        assert isinstance(result, MaxCardinalityViolation)
+        assert result.res_id == "id_max_card"
+        assert result.res_type == "onto:ClassMixedCard"
+        assert result.prop_name == "onto:testDecimalSimpleText"
+        assert result.expected_cardinality == "0-1"
+
+    def test_closed(self, violation_closed: ValidationResult) -> None:
+        result = _reformat_one_violation(violation_closed)
+        assert isinstance(result, NonExistentCardinalityViolation)
+        assert result.res_id == "id_closed_constraint"
+        assert result.res_type == "onto:CardOneResource"
+        assert result.prop_name == "onto:testIntegerSimpleText"
+
+    def test_unknown(self, violation_unknown: ValidationResult) -> None:
+        result = _reformat_one_violation(violation_unknown)
+        assert isinstance(result, UnexpectedComponent)
+        assert result.component_type == str(SH.UniqueLangConstraintComponent)
 
 
 if __name__ == "__main__":

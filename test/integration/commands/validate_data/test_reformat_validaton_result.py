ac484d7f80dbac45e0b0af6dbe43b07030db4cbf
Nora-Olivia-Ammann <103038637+Nora-Olivia-Ammann@users.noreply.github.com>
2024-10-09 20:12:43 +0200

    everything once

--- test/integration/commands/validate_data/test_reformat_validaton_result.py
+++ test/integration/commands/validate_data/test_reformat_validaton_result.py
@@ -56,6 +56,27 @@ def result_content_violation() -> Graph:
     return g
 
 
+@pytest.fixture
+def data_every_constraint_once() -> Graph:
+    g = Graph()
+    g.parse("testdata/validate-data/validation_results/every_constraint_once_data.ttl")
+    return g
+
+
+@pytest.fixture
+def result_card_every_constraint_once() -> Graph:
+    g = Graph()
+    g.parse("testdata/validate-data/validation_results/every_constraint_once_result_cardinality.ttl")
+    return g
+
+
+@pytest.fixture
+def result_content_every_constraint_once() -> Graph:
+    g = Graph()
+    g.parse("testdata/validate-data/validation_results/every_constraint_once_result_content.ttl")
+    return g
+
+
 def test_reformat_cardinality_violation(result_cardinality_violation: Graph, data_cardinality_violation: Graph) -> None:
     val_rep = ValidationReports(
         conforms=False,
@@ -135,6 +156,35 @@ def test_reformat_content_violation(result_content_violation: Graph, data_conten
         assert one_result.res_id == expected_info[0]
         assert one_result.prop_name == expected_info[1]
         assert one_result.expected_format == expected_info[2]
+
+
+def test_reformat_every_constraint_once(
+    result_card_every_constraint_once: Graph,
+    result_content_every_constraint_once: Graph,
+    data_every_constraint_once: Graph,
+) -> None:
+    val_rep = ValidationReports(
+        conforms=False,
+        content_validation=result_content_every_constraint_once,
+        cardinality_validation=result_card_every_constraint_once,
+        shacl_graphs=Graph(),
+        data_graph=data_every_constraint_once,
+    )
+    result = reformat_validation_graph(val_rep)
+    expected_info_tuples = [
+        ("geoname_not_number", ContentRegexViolation),
+        ("id_card_one", MinCardinalityViolation),
+        ("id_closed_constraint", NonExistentCardinalityViolation),
+        ("id_max_card", MaxCardinalityViolation),
+        ("id_simpletext", ValueTypeViolation),
+        ("id_uri", ValueTypeViolation),
+    ]
+    assert not result.unexpected_results
+    assert len(result.problems) == 6
+    sorted_problems = sorted(result.problems, key=lambda x: x.res_id)
+    for one_result, expected_info in zip(sorted_problems, expected_info_tuples):
+        assert one_result.res_id == expected_info[0]
+        assert isinstance(one_result, expected_info[1])
 
 
 if __name__ == "__main__":

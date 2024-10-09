9a1eccf71c8cfb78a86b242a03a538940f6d7725
Nora-Olivia-Ammann <103038637+Nora-Olivia-Ammann@users.noreply.github.com>
2024-10-09 17:25:11 +0200

    add test data

--- test/integration/commands/validate_data/test_reformat_validaton_result.py
+++ test/integration/commands/validate_data/test_reformat_validaton_result.py
@@ -26,12 +26,53 @@ def result_cardinality_violation() -> Graph:
     return g
 
 
-def test_reformat_validation_graph(result_cardinality_violation: Graph, data_cardinality_violation: Graph) -> None:
+@pytest.fixture
+def data_type_violation() -> Graph:
+    data, _ = _get_data_info_from_file(Path("testdata/validate-data/data/value_type_violation.xml"), LOCAL_API)
+    return data.make_graph()
+
+
+@pytest.fixture
+def result_type_violation() -> Graph:
+    g = Graph()
+    g.parse("testdata/validate-data/validation_results/value_type_violation_result.ttl")
+    return g
+
+
+def test_reformat_validation_graph_cardinality(
+    result_cardinality_violation: Graph, data_cardinality_violation: Graph
+) -> None:
     val_rep = ValidationReport(
         conforms=False,
         validation_graph=result_cardinality_violation,
         shacl_graphs=Graph(),
         data_graph=data_cardinality_violation,
+    )
+
+    result = reformat_validation_graph(val_rep)
+    assert not result.unexpected_results
+    assert len(result.problems) == 4
+    sorted_problems = sorted(result.problems, key=lambda x: x.sort_value())
+    val_one = sorted_problems[0]
+    assert isinstance(val_one, MinCardinalityViolation)
+    assert val_one.res_id == "id_card_one"
+    val_one = sorted_problems[1]
+    assert isinstance(val_one, MinCardinalityViolation)
+    assert val_one.res_id == "id_min_card"
+    val_one = sorted_problems[2]
+    assert isinstance(val_one, MaxCardinalityViolation)
+    assert val_one.res_id == "id_max_card"
+    val_one = sorted_problems[3]
+    assert isinstance(val_one, NonExistentCardinalityViolation)
+    assert val_one.res_id == "id_closed_constraint"
+
+
+def test_reformat_validation_graph_type(result_type_violation: Graph, data_type_violation: Graph) -> None:
+    val_rep = ValidationReport(
+        conforms=False,
+        validation_graph=result_type_violation,
+        shacl_graphs=Graph(),
+        data_graph=data_type_violation,
     )
 
     result = reformat_validation_graph(val_rep)

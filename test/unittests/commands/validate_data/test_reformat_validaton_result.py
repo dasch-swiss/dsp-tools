bcf8a013c1082a087aa94864bc39eb49d1464f69
Nora-Olivia-Ammann <103038637+Nora-Olivia-Ammann@users.noreply.github.com>
2024-10-09 08:58:26 +0200

    Update test_reformat_validaton_result.py

--- test/unittests/commands/validate_data/test_reformat_validaton_result.py
+++ test/unittests/commands/validate_data/test_reformat_validaton_result.py
@@ -52,7 +52,7 @@ def data_min_count_violation() -> Graph:
 
 
 @pytest.fixture
-def data_class_constraint_component() -> Graph:
+def data_value_type_mismatch() -> Graph:
     gstr = """
     <http://data/id_2> a <http://0.0.0.0:3333/ontology/9999/onto/v2#ClassWithEverything> .
     <http://data/value-iri> a <http://api.knora.org/ontology/knora-api/v2#TextValue> .
@@ -63,7 +63,7 @@ def data_class_constraint_component() -> Graph:
 
 
 @pytest.fixture
-def class_constraint_component() -> Graph:
+def graph_value_type_mismatch() -> Graph:
     gstr = f'''{VALIDATION_PREFIXES}
     [] a sh:ValidationReport ;
     sh:result _:bn1 .
@@ -91,6 +91,50 @@ def class_constraint_component() -> Graph:
     sh:sourceShape <http://api.knora.org/ontology/knora-api/shapes/v2#ColorValue_ClassShape> ;
     sh:value <http://data/value-iri> .
     '''
+    g = Graph()
+    g.parse(data=gstr, format="ttl")
+    return g
+
+
+@pytest.fixture
+def data_wrong_regex_content() -> Graph:
+    gstr = """
+    <http://data/geoname_not_number> a <http://0.0.0.0:3333/ontology/9999/onto/v2#ClassWithEverything> .
+    <http://data/value-iri> a <http://api.knora.org/ontology/knora-api/v2#TextValue> .
+    """
+    g = Graph()
+    g.parse(data=gstr, format="ttl")
+    return g
+
+
+@pytest.fixture
+def graph_wrong_regex_content() -> Graph:
+    gstr = f"""{VALIDATION_PREFIXES}
+    [] a sh:ValidationReport ;
+    sh:result _:bn1 .
+    
+    [] a sh:ValidationReport ;
+    sh:conforms false ;
+    sh:result [ a sh:ValidationResult ;
+            sh:detail _:bn1 ;
+            sh:focusNode <http://data/geoname_not_number> ;
+            sh:resultMessage "Value does not have shape 
+                                        <http://api.knora.org/ontology/knora-api/shapes/v2#GeonameValue_ClassShape>" ;
+            sh:resultPath ns1:testGeoname ;
+            sh:resultSeverity sh:Violation ;
+            sh:sourceConstraintComponent sh:NodeConstraintComponent ;
+            sh:sourceShape ns1:testGeoname_PropShape ;
+            sh:value <http://data/value-iri> ] .
+            
+    _:bn1 a sh:ValidationResult ;
+    sh:focusNode <http://data/value-iri> ;
+    sh:resultMessage "The value must be a valid geoname code" ;
+    sh:resultPath ns3:geonameValueAsGeonameCode ;
+    sh:resultSeverity sh:Violation ;
+    sh:sourceConstraintComponent sh:PatternConstraintComponent ;
+    sh:sourceShape ns2:geonameValueAsGeonameCode_Shape ;
+    sh:value "llllllllllllllllll" .
+    """
     g = Graph()
     g.parse(data=gstr, format="ttl")
     return g
@@ -156,29 +200,17 @@ def violation_unknown_content() -> ContentValidationResult:
     )
 
 
-def test_query_for_one_content_validation_result(
-    class_constraint_component: Graph, data_class_constraint_component: Graph
-) -> None:
-    bn = next(class_constraint_component.subjects(SH.sourceConstraintComponent, SH.NodeConstraintComponent))
-    result = _query_for_one_content_validation_result(bn, class_constraint_component, data_class_constraint_component)
-    assert result.source_constraint_component == SH.NodeConstraintComponent
-    assert result.res_iri == DATA.id_2
-    assert result.res_class == ONTO.ClassWithEverything
-    assert result.property == ONTO.testColor
-    assert result.results_message == "ColorValue"
-    assert result.value_type == KNORA_API.TextValue
-
-
-def test_query_for_one_cardinality_validation_result(
-    min_count_violation: Graph, data_min_count_violation: Graph
-) -> None:
-    bn = next(min_count_violation.subjects(RDF.type, SH.ValidationResult))
-    result = _query_for_one_cardinality_validation_result(bn, min_count_violation, data_min_count_violation)
-    assert result.source_constraint_component == SH.MinCountConstraintComponent
-    assert result.res_iri == DATA.id_min_card
-    assert result.res_class == ONTO.ClassMixedCard
-    assert result.property == ONTO.testGeoname
-    assert result.results_message == "1-n"
+class TestQueryCardinality:
+    def test_query_for_one_cardinality_validation_result(
+        self, min_count_violation: Graph, data_min_count_violation: Graph
+    ) -> None:
+        bn = next(min_count_violation.subjects(RDF.type, SH.ValidationResult))
+        result = _query_for_one_cardinality_validation_result(bn, min_count_violation, data_min_count_violation)
+        assert result.source_constraint_component == SH.MinCountConstraintComponent
+        assert result.res_iri == DATA.id_min_card
+        assert result.res_class == ONTO.ClassMixedCard
+        assert result.property == ONTO.testGeoname
+        assert result.results_message == "1-n"
 
 
 class TestReformatCardinalityViolation:
@@ -204,6 +236,20 @@ class TestReformatCardinalityViolation:
         assert result.res_id == "id_closed_constraint"
         assert result.res_type == "onto:CardOneResource"
         assert result.prop_name == "onto:testIntegerSimpleText"
+
+
+class TestQueryContent:
+    def test_query_graph_value_type_mismatch(
+        self, graph_value_type_mismatch: Graph, data_value_type_mismatch: Graph
+    ) -> None:
+        bn = next(graph_value_type_mismatch.subjects(SH.sourceConstraintComponent, SH.NodeConstraintComponent))
+        result = _query_for_one_content_validation_result(bn, graph_value_type_mismatch, data_value_type_mismatch)
+        assert result.source_constraint_component == SH.NodeConstraintComponent
+        assert result.res_iri == DATA.id_2
+        assert result.res_class == ONTO.ClassWithEverything
+        assert result.property == ONTO.testColor
+        assert result.results_message == "ColorValue"
+        assert result.value_type == KNORA_API.TextValue
 
 
 class TestReformatContentViolation:

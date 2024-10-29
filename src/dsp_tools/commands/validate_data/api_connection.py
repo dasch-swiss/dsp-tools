be5b58b9719685034e7798523c2043018015f56a
Nora-Olivia-Ammann <103038637+Nora-Olivia-Ammann@users.noreply.github.com>
2024-10-29 13:41:03 +0100

    add api connection

---
+++ src/dsp_tools/commands/validate_data/api_connection.py
@@ -0,0 +1,62 @@
+from __future__ import annotations
+
+from dataclasses import dataclass
+from typing import Any
+
+import requests
+from loguru import logger
+from requests import RequestException
+from requests import Response
+from requests import Timeout
+
+from dsp_tools.models.exceptions import InternalError
+
+
+@dataclass
+class ApiConnection:
+    api_url: str
+
+    def get(self, endpoint: str, headers: dict[str, Any] | None = None, timeout: int = 42) -> Response:
+        url = f"{self.api_url}/{endpoint}"
+        logger.debug(f"REQUEST: GET to {url}, timeout: {timeout}, headers: {headers}")
+        try:
+            return requests.get(url=url, headers=headers, timeout=timeout)
+        except (TimeoutError, Timeout) as err:
+            logger.error(err)
+            raise InternalError("TimeoutError occurred. See logs for details.") from None
+        except (ConnectionError, RequestException) as err:
+            logger.error(err)
+            raise InternalError("ConnectionError occurred. See logs for details.") from None
+
+    def post_files(
+        self, endpoint: str, files: PostFiles, headers: dict[str, Any] | None = None, timeout: int = 42
+    ) -> Response:
+        file_dict = files.to_dict()
+        url = f"{self.api_url}/{endpoint}"
+        logger.debug(f"REQUEST: POST FILES to {url}, timeout: {timeout}, headers: {headers}")
+        try:
+            return requests.post(url, files=file_dict, timeout=timeout)
+        except (TimeoutError, Timeout) as err:
+            logger.error(err)
+            raise InternalError("TimeoutError occurred. See logs for details.") from None
+        except (ConnectionError, RequestException) as err:
+            logger.error(err)
+            raise InternalError("ConnectionError occurred. See logs for details.") from None
+
+
+@dataclass
+class PostFiles:
+    files: list[OneFile]
+
+    def to_dict(self) -> dict[str, tuple[str, str, str]]:
+        return {x.file_name: x.to_tuple() for x in self.files}
+
+
+@dataclass
+class OneFile:
+    file_name: str
+    file_content: str
+    file_format: str
+
+    def to_tuple(self) -> tuple[str, str, str]:
+        return self.file_name, self.file_content, self.file_format

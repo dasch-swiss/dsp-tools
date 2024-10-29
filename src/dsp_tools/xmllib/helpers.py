f81b37a1c09fe512a4e95c654244f166a4bf6aef
Nora-Olivia-Ammann <103038637+Nora-Olivia-Ammann@users.noreply.github.com>
2024-10-29 13:09:58 +0100

    Create helpers.py

---
+++ src/dsp_tools/xmllib/helpers.py
@@ -0,0 +1,63 @@
+import json
+from typing import Any
+from typing import Iterable
+
+
+def create_json_list_mapping(
+    path_to_json: str,
+    list_name: str,
+    language_label: str,
+) -> dict[str, str]:
+    """
+    Often, data sources contain list values named after the "label" of the JSON project list node, instead of the "name"
+    which is needed for the `dsp-tools xmlupload`. In order to create a correct XML, you need a dictionary that maps the
+    "labels" to their correct "names".
+
+    Alternatively, consider using the method create_json_excel_list_mapping(), which also creates a dictionary, but maps
+    values from your data source to list node names from the JSON project file, based on similarity.
+
+    Args:
+        path_to_json: path to a JSON project file (a.k.a. ontology)
+        list_name: name of a list in the JSON project (works also for nested lists)
+        language_label: which language of the label to choose
+
+    Returns:
+        a dictionary of the form {label: name}
+    """
+    with open(path_to_json, encoding="utf-8") as f:
+        json_file = json.load(f)
+    json_subset = [x for x in json_file["project"]["lists"] if x["name"] == list_name]
+    # json_subset is a list containing one item, namely the json object containing the entire json-list
+
+    res = {}
+    for label, name in _name_label_mapper_iterator(json_subset, language_label):
+        if name != list_name:
+            res[label] = name
+            res[label.strip().lower()] = name
+
+    return res
+
+
+def _name_label_mapper_iterator(
+    json_subset: list[dict[str, Any]],
+    language_label: str,
+) -> Iterable[tuple[str, str]]:
+    """
+    Go through list nodes of a JSON project and yield (label, name) pairs.
+
+    Args:
+        json_subset: list of DSP lists (a DSP list being a dictionary with the keys "name", "labels" and "nodes")
+        language_label: which language of the label to choose
+
+    Yields:
+        (label, name) pairs
+    """
+    for node in json_subset:
+        # node is the json object containing the entire json-list
+        if "nodes" in node:
+            # "nodes" is the json sub-object containing the entries of the json-list
+            yield from _name_label_mapper_iterator(node["nodes"], language_label)
+            # each yielded value is a (label, name) pair of a single list entry
+        if "name" in node:
+            yield (node["labels"][language_label], node["name"])
+            # the actual values of the name and the label

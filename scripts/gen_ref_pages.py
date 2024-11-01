fd7adb70418de57b64d4069c45f838bdbdf4401c
Johannes Nussbaum <johannes.nussbaum@dasch.swiss>
2024-11-01 11:51:16 +0100

    serve only xmllib

--- scripts/gen_ref_pages.py
+++ scripts/gen_ref_pages.py
@@ -9,7 +9,7 @@ nav = mkdocs_gen_files.Nav()
 root = Path(__file__).parent.parent
 src = root / "src"
 
-for path in sorted(src.rglob("*.py")):
+for path in sorted(src.glob("dsp_tools/xmllib/**/*.py")):
     module_path = path.relative_to(src).with_suffix("")
     doc_path = path.relative_to(src).with_suffix(".md")
     full_doc_path = Path("reference", doc_path)
@@ -21,7 +21,7 @@ for path in sorted(src.rglob("*.py")):
     elif parts[-1] == "__main__":
         continue
 
-    nav[parts] = doc_path.as_posix()  
+    nav[parts] = doc_path.as_posix()
 
     with mkdocs_gen_files.open(full_doc_path, "w") as fd:
         ident = ".".join(parts)
@@ -31,5 +31,5 @@ for path in sorted(src.rglob("*.py")):
     # See https://mkdocstrings.github.io/recipes/#generate-pages-on-the-fly
     mkdocs_gen_files.set_edit_path(full_doc_path, Path("../") / path)
 
-with mkdocs_gen_files.open("reference/SUMMARY.md", "w") as nav_file:  
+with mkdocs_gen_files.open("reference/xmllib-generated-api-doc.md", "w") as nav_file:
     nav_file.writelines(nav.build_literate_nav())

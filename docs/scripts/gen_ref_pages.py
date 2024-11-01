766b415e49dfe40fb2c48dcd9499e8eb2f1e5e50
Johannes Nussbaum <johannes.nussbaum@dasch.swiss>
2024-11-01 10:18:03 +0100

    failed attempt to use mkdocs-literate-nav

--- docs/scripts/gen_ref_pages.py
+++ docs/scripts/gen_ref_pages.py
@@ -7,13 +7,15 @@ from pathlib import Path
 
 import mkdocs_gen_files
 
+nav = mkdocs_gen_files.Nav()
+
 root = Path(__file__).parent.parent
 xmllib = root / "src/dsp_tools/xmllib"  
 
 for path in sorted(xmllib.glob("**/*.py")):  
     module_path = path.relative_to(xmllib).with_suffix("")  
     doc_path = path.relative_to(xmllib).with_suffix(".md")  
-    full_doc_path = Path("docs/xmllib", doc_path)  
+    full_doc_path = root / "docs/xmllib" / doc_path
 
     parts = tuple(module_path.parts)
 
@@ -22,8 +24,13 @@ for path in sorted(xmllib.glob("**/*.py")):
     elif parts[-1] == "__main__":
         continue
 
+    nav[parts] = doc_path.as_posix() 
+
     with mkdocs_gen_files.open(full_doc_path, "w") as fd:  
         identifier = ".".join(parts)  
         print("::: " + identifier, file=fd)  
 
     mkdocs_gen_files.set_edit_path(full_doc_path, path.relative_to(root))
+
+    with mkdocs_gen_files.open(root / "docs/xmllib/SUMMARY.md", "w") as nav_file:  
+        nav_file.writelines(nav.build_literate_nav())

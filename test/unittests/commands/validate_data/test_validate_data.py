2af8dcd6257be98146c3f516eddd63baf401b65a
Nora-Olivia-Ammann <103038637+Nora-Olivia-Ammann@users.noreply.github.com>
2024-11-11 09:15:43 +0100

    Create test_validate_data.py

---
+++ test/unittests/commands/validate_data/test_validate_data.py
@@ -0,0 +1,11 @@
+import pytest
+from rdflib import RDFS
+from rdflib import SH
+from rdflib import Graph
+from rdflib import Literal
+from rdflib import URIRef
+
+
+
+if __name__ == "__main__":
+    pytest.main([__file__])

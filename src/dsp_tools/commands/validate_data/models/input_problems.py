09cecd8710f47d97172f0a49753e5ef41392b10d
Nora-Olivia-Ammann <103038637+Nora-Olivia-Ammann@users.noreply.github.com>
2024-11-11 08:57:06 +0100

    Update input_problems.py

--- src/dsp_tools/commands/validate_data/models/input_problems.py
+++ src/dsp_tools/commands/validate_data/models/input_problems.py
@@ -25,7 +25,7 @@ class UnknownClassesUsed:
     def get_msg(self) -> str:
         if unknown := self._get_unknown_ontos_msg():
             return unknown
-
+        return self._get_unknown_classes_msg()
 
     def _get_unknown_ontos_msg(self) -> str:
         def split_prefix(relative_iri: str) -> str:
@@ -36,14 +36,21 @@ class UnknownClassesUsed:
         msg = ""
         if unknown := used_ontos - exising_ontos:
             msg = (
-                f"You data uses ontologies that don't exist in the database.\n"
-                f"The following ontologies are uploaded: {''.join(unknown)}\n"
+                f"Your data uses ontologies that don't exist in the database.\n"
                 f"The following ontologies that are used in the data are unknown: {''.join(exising_ontos)}"
+                f"The following ontologies are uploaded: {''.join(unknown)}\n"
             )
         return msg
 
     def _get_unknown_classes_msg(self) -> str:
-        pass
+        unknown_classes = sorted(list(self.unknown_classes))
+        known_classes = sorted(list(self.classes_onto))
+        return (
+            f"Your data uses resource classes that do not exist in the ontologies in the database.\n"
+            f"The following classes that are used in the data are unknown: {''.join(unknown_classes)}\n"
+            f"The following classes exist in the uploaded ontologies: {''.join(known_classes)}\n"
+        )
+
 
 @dataclass
 class UnexpectedResults:

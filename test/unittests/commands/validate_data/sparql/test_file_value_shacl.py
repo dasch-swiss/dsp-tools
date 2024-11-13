37d7104caa840f6736db8980dc7783f146c182a5
Nora-Olivia-Ammann <103038637+Nora-Olivia-Ammann@users.noreply.github.com>
2024-11-13 12:57:40 +0100

    Update test_file_value_shacl.py

--- test/unittests/commands/validate_data/sparql/test_file_value_shacl.py
+++ test/unittests/commands/validate_data/sparql/test_file_value_shacl.py
@@ -2,22 +2,17 @@ import pytest
 from rdflib import SH
 from rdflib import Graph
 
-from dsp_tools.commands.validate_data.sparql.file_value_shacl import _construct_generic_file_value_cardinality
 from dsp_tools.commands.validate_data.sparql.file_value_shacl import construct_file_value_cardinality
+from test.unittests.commands.validate_data.constants import API_SHAPES
+from test.unittests.commands.validate_data.constants import ONTO
 
 
 def test_construct_file_value_cardinality(onto_graph: Graph) -> None:
     res = construct_file_value_cardinality(onto_graph)
-    number_of_classes_with_files = 6
-    class_shapes = list(res.subjects(predicate=SH.property))
-    assert len(class_shapes) == number_of_classes_with_files
-
-
-def test_construct_generic_file_value_cardinality(onto_graph: Graph) -> None:
-    res = _construct_generic_file_value_cardinality(onto_graph)
-    number_of_classes_without_specific_sparql = 6
-    class_shapes = list(res.subjects(predicate=SH.property))
-    assert len(class_shapes) == number_of_classes_without_specific_sparql
+    number_of_classes_implemented = 1
+    assert len(list(res.subjects(SH.property))) == number_of_classes_implemented
+    moving_image = next(res.subjects(SH.property, API_SHAPES.hasMovingImageFileValue_PropShape))
+    assert moving_image == ONTO.TestMovingImageRepresentation
 
 
 if __name__ == "__main__":

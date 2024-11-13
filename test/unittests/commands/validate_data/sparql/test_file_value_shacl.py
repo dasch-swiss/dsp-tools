709c0874a410bbc5d8e738e531702e8db4b243fd
Nora-Olivia-Ammann <103038637+Nora-Olivia-Ammann@users.noreply.github.com>
2024-11-13 13:35:56 +0100

    Update test_file_value_shacl.py

--- test/unittests/commands/validate_data/sparql/test_file_value_shacl.py
+++ test/unittests/commands/validate_data/sparql/test_file_value_shacl.py
@@ -7,11 +7,20 @@ from test.unittests.commands.validate_data.constants import API_SHAPES
 from test.unittests.commands.validate_data.constants import ONTO
 
 
-def test_construct_file_value_cardinality(onto_graph: Graph) -> None:
-    res = construct_file_value_cardinality(onto_graph)
+@pytest.fixture
+def file_value_shacl(onto_graph: Graph) -> Graph:
+    return construct_file_value_cardinality(onto_graph)
+
+
+def test_construct_file_value_cardinality(file_value_shacl: Graph) -> None:
     number_of_classes_implemented = 1
-    assert len(list(res.subjects(SH.property))) == number_of_classes_implemented
-    moving_image = next(res.subjects(SH.property, API_SHAPES.hasMovingImageFileValue_PropShape))
+    assert len(list(file_value_shacl.subjects(SH.property))) == number_of_classes_implemented
+
+
+def test_construct_moving_image(file_value_shacl: Graph) -> None:
+    result_list = list(file_value_shacl.subjects(SH.property, API_SHAPES.hasMovingImageFileValue_PropShape))
+    assert len(result_list) == 1
+    moving_image = result_list[0]
     assert moving_image == ONTO.TestMovingImageRepresentation
 
 

0279615229ef00ca8ef230eed6a391e8f304392b
Nora-Olivia-Ammann <103038637+Nora-Olivia-Ammann@users.noreply.github.com>
2024-11-14 17:47:28 +0100

    refactor(xmlupload): serialise values in classes (#1272)
    Co-authored-by: Johannes Nussbaum <johannes.nussbaum@dasch.swiss>
    Co-authored-by: Johannes Nussbaum <39048939+jnussbaum@users.noreply.github.com>

--- src/dsp_tools/commands/xmlupload/models/serialise/serialise_value.py
+++ src/dsp_tools/commands/xmlupload/models/serialise/serialise_value.py
@@ -2,6 +2,7 @@ from __future__ import annotations
 
 from abc import ABC
 from abc import abstractmethod
+from collections.abc import Sequence
 from dataclasses import dataclass
 from typing import Any
 
@@ -9,13 +10,14 @@ from typing import Any
 @dataclass(frozen=True)
 class SerialiseProperty:
     property_name: str
-    values: list[SerialiseValue]
+    values: Sequence[SerialiseValue]
 
     def serialise(self) -> dict[str, Any]:
         """Serialise the property and all its values."""
         return {self.property_name: [value.serialise() for value in self.values]}
 
 
+@dataclass(frozen=True)
 class SerialiseValue(ABC):
     """A value to be serialised."""
 
@@ -34,13 +36,8 @@ class SerialiseValue(ABC):
         return optionals
 
 
-@dataclass(frozen=True)
 class SerialiseColor(SerialiseValue):
     """A ColorValue to be serialised."""
-
-    value: str
-    permissions: str | None
-    comment: str | None
 
     def serialise(self) -> dict[str, Any]:
         serialised = {
@@ -51,13 +48,89 @@ class SerialiseColor(SerialiseValue):
         return serialised
 
 
-@dataclass(frozen=True)
+class SerialiseDecimal(SerialiseValue):
+    """A DecimalValue to be serialised."""
+
+    def serialise(self) -> dict[str, Any]:
+        serialised = {
+            "@type": "knora-api:DecimalValue",
+            "knora-api:decimalValueAsDecimal": {
+                "@type": "xsd:decimal",
+                "@value": self.value,
+            },
+        }
+        serialised.update(self._get_optionals())
+        return serialised
+
+
+class SerialiseGeometry(SerialiseValue):
+    """A GeomValue to be serialised."""
+
+    def serialise(self) -> dict[str, Any]:
+        serialised = {
+            "@type": "knora-api:GeomValue",
+            "knora-api:geometryValueAsGeometry": self.value,
+        }
+        serialised.update(self._get_optionals())
+        return serialised
+
+
+class SerialiseGeoname(SerialiseValue):
+    """A GeonameValue to be serialised."""
+
+    def serialise(self) -> dict[str, Any]:
+        serialised = {
+            "@type": "knora-api:GeonameValue",
+            "knora-api:geonameValueAsGeonameCode": self.value,
+        }
+        serialised.update(self._get_optionals())
+        return serialised
+
+
+class SerialiseSimpletext(SerialiseValue):
+    """A Simpletext to be serialised."""
+
+    def serialise(self) -> dict[str, Any]:
+        serialised = {
+            "@type": "knora-api:TextValue",
+            "knora-api:valueAsString": self.value,
+        }
+        serialised.update(self._get_optionals())
+        return serialised
+
+
+class SerialiseRichtext(SerialiseValue):
+    """A Richtext to be serialised."""
+
+    def serialise(self) -> dict[str, Any]:
+        serialised = {
+            "@type": "knora-api:TextValue",
+            "knora-api:textValueAsXml": self.value,
+            "knora-api:textValueHasMapping": {
+                "@id": "http://rdfh.ch/standoff/mappings/StandardMapping",
+            },
+        }
+        serialised.update(self._get_optionals())
+        return serialised
+
+
+class SerialiseTime(SerialiseValue):
+    """A TimeValue to be serialised."""
+
+    def serialise(self) -> dict[str, Any]:
+        serialised = {
+            "@type": "knora-api:TimeValue",
+            "knora-api:timeValueAsTimeStamp": {
+                "@type": "xsd:dateTimeStamp",
+                "@value": self.value,
+            },
+        }
+        serialised.update(self._get_optionals())
+        return serialised
+
+
 class SerialiseURI(SerialiseValue):
     """A UriValue to be serialised."""
-
-    value: str
-    permissions: str | None
-    comment: str | None
 
     def serialise(self) -> dict[str, Any]:
         serialised = {

f424f265794e6ef97f7ccb161a69707b8092a20a
Nora-Olivia-Ammann <103038637+Nora-Olivia-Ammann@users.noreply.github.com>
2024-11-14 16:33:53 +0100

    Update serialise_file_value.py

--- src/dsp_tools/commands/xmlupload/models/serialise/serialise_file_value.py
+++ src/dsp_tools/commands/xmlupload/models/serialise/serialise_file_value.py
@@ -6,7 +6,7 @@ from dataclasses import dataclass
 from typing import Any
 
 
-@dataclass
+@dataclass(frozen=True)
 class SerialiseAbstractFileValue(ABC):
     file_name: str
     permissions: str | None
@@ -25,37 +25,31 @@ class SerialiseAbstractFileValue(ABC):
         return file_value
 
 
-@dataclass
 class SerialiseArchiveFileValue(SerialiseAbstractFileValue):
     def serialise(self) -> dict[str, Any]:
         return {"knora-api:hasArchiveFileValue": self._get_file_value("ArchiveFileValue")}
 
 
-@dataclass
 class SerialiseAudioFileValue(SerialiseAbstractFileValue):
     def serialise(self) -> dict[str, Any]:
         return {"knora-api:hasAudioFileValue": self._get_file_value("AudioFileValue")}
 
 
-@dataclass
 class SerialiseDocumentFileValue(SerialiseAbstractFileValue):
     def serialise(self) -> dict[str, Any]:
         return {"knora-api:hasDocumentFileValue": self._get_file_value("DocumentFileValue")}
 
 
-@dataclass
 class SerialiseMovingImageFileValue(SerialiseAbstractFileValue):
     def serialise(self) -> dict[str, Any]:
         return {"knora-api:hasMovingImageFileValue": self._get_file_value("MovingImageFileValue")}
 
 
-@dataclass
 class SerialiseStillImageFileValue(SerialiseAbstractFileValue):
     def serialise(self) -> dict[str, Any]:
         return {"knora-api:hasStillImageFileValue": self._get_file_value("StillImageFileValue")}
 
 
-@dataclass
 class SerialiseTextFileValue(SerialiseAbstractFileValue):
     def serialise(self) -> dict[str, Any]:
         return {"knora-api:hasTextFileValue": self._get_file_value("TextFileValue")}

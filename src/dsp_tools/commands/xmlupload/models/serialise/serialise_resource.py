ddb7b86602c9023dec5d3344e322a8368c5b3f63
Nora-Olivia-Ammann <103038637+Nora-Olivia-Ammann@users.noreply.github.com>
2024-11-15 11:38:20 +0100

    Create serialise_resource.py

---
+++ src/dsp_tools/commands/xmlupload/models/serialise/serialise_resource.py
@@ -0,0 +1,68 @@
+from __future__ import annotations
+
+from abc import ABC
+from abc import abstractmethod
+from dataclasses import dataclass
+from typing import Any
+
+from dsp_tools.commands.xmlupload.models.serialise.serialise_value import SerialiseProperty
+
+
+@dataclass(frozen=True)
+class ProjectContext:
+    context: dict[str, Any]
+    project_iri: str
+
+    def to_dict(self) -> dict[str, Any]:
+        return {
+            "knora-api:attachedToProject": {"@id": self.project_iri},
+            "@context": self.context,
+        }
+
+
+@dataclass(frozen=True)
+class SerialiseAbstractResource(ABC):
+    res_id: str
+    label: str
+    properties: list[SerialiseProperty]
+    project_context: ProjectContext
+    permissions: str | None
+    comment: str | None
+    migration_metadata: MigrationMetadata | None
+
+    @abstractmethod
+    def serialise_resource(self) -> dict[str, Any]: ...
+
+    def _get_base_info(self) -> dict[str, str]:
+        serialised = self.project_context.to_dict()
+        serialised["rdfs:label"] = self.label
+        if self.permissions:
+            serialised["knora-api:hasPermissions"] = self.permissions
+        return serialised
+
+
+@dataclass(frozen=True)
+class SerialiseResource(SerialiseAbstractResource):
+    res_type: str
+
+    def serialise_resource(self) -> dict[str, Any]:
+        serialised = self.migration_metadata.to_dict() if self.migration_metadata else {}
+        serialised.update(self._get_base_info())
+        serialised["@type"] = self.res_type
+
+        return serialised
+
+
+@dataclass(frozen=True)
+class MigrationMetadata:
+    creation_date: str | None
+    iri: str
+
+    def to_dict(self) -> dict[str, Any]:
+        info = {"@id": self.iri}
+        if self.creation_date:
+            info["knora-api:creationDate"] = {
+                "@type": "xsd:dateTimeStamp",
+                "@value": self.creation_date,
+            }
+        return info

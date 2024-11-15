4347cc7e5c34daef1ef5e0df25df8aa73bbe8c27
Nora-Olivia-Ammann <103038637+Nora-Olivia-Ammann@users.noreply.github.com>
2024-11-15 11:55:33 +0100

    Update serialise_resource.py

--- src/dsp_tools/commands/xmlupload/models/serialise/serialise_resource.py
+++ src/dsp_tools/commands/xmlupload/models/serialise/serialise_resource.py
@@ -1,11 +1,31 @@
 from __future__ import annotations
 
-from abc import ABC
-from abc import abstractmethod
 from dataclasses import dataclass
 from typing import Any
 
 from dsp_tools.models.datetimestamp import DateTimeStamp
+
+
+@dataclass(frozen=True)
+class SerialiseResource:
+    res_id: str
+    res_type: str
+    label: str
+    project_context: ProjectContext
+    permissions: str | None
+    migration_metadata: MigrationMetadata | None
+
+    def serialise_resource(self) -> dict[str, Any]:
+        serialised = self.migration_metadata.to_dict() if self.migration_metadata else {}
+        serialised.update(self._get_base_info())
+        return serialised
+
+    def _get_base_info(self) -> dict[str, Any]:
+        serialised = self.project_context.to_dict()
+        serialised.update({"rdfs:label": self.label, "@type": self.res_type})
+        if self.permissions:
+            serialised["knora-api:hasPermissions"] = self.permissions
+        return serialised
 
 
 @dataclass(frozen=True)
@@ -18,36 +38,6 @@ class ProjectContext:
             "knora-api:attachedToProject": {"@id": self.project_iri},
             "@context": self.context,
         }
-
-
-@dataclass(frozen=True)
-class SerialiseAbstractResource(ABC):
-    res_id: str
-    label: str
-    project_context: ProjectContext
-    permissions: str | None
-    migration_metadata: MigrationMetadata | None
-
-    @abstractmethod
-    def serialise_resource(self) -> dict[str, Any]: ...
-
-    def _get_base_info(self) -> dict[str, Any]:
-        serialised = self.project_context.to_dict()
-        serialised["rdfs:label"] = self.label
-        if self.permissions:
-            serialised["knora-api:hasPermissions"] = self.permissions
-        return serialised
-
-
-@dataclass(frozen=True)
-class SerialiseResource(SerialiseAbstractResource):
-    res_type: str
-
-    def serialise_resource(self) -> dict[str, Any]:
-        serialised = self.migration_metadata.to_dict() if self.migration_metadata else {}
-        serialised.update(self._get_base_info())
-        serialised["@type"] = self.res_type
-        return serialised
 
 
 @dataclass(frozen=True)

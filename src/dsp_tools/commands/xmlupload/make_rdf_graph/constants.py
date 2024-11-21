a140e1f054ebdb4113b8b3775c024a755fa6ed52
Nora-Olivia-Ammann <103038637+Nora-Olivia-Ammann@users.noreply.github.com>
2024-11-21 13:57:02 +0100

    move

---
+++ src/dsp_tools/commands/xmlupload/make_rdf_graph/constants.py
@@ -0,0 +1,3 @@
+from rdflib import Namespace
+
+KNORA_API = Namespace("http://api.knora.org/ontology/knora-api/v2#")
--- test/unittests/commands/xmlupload/test_transform_file_values.py
+++ test/unittests/commands/xmlupload/test_transform_file_values.py
@@ -6,14 +6,14 @@ from rdflib import BNode
 from rdflib import Namespace
 from rdflib import URIRef
 
+from dsp_tools.commands.xmlupload.make_rdf_graph.make_values import _make_bitstream_file_value
+from dsp_tools.commands.xmlupload.make_rdf_graph.make_values import _make_iiif_uri_value
 from dsp_tools.commands.xmlupload.models.deserialise.deserialise_value import IIIFUriInfo
 from dsp_tools.commands.xmlupload.models.deserialise.xmlresource import BitstreamInfo
 from dsp_tools.commands.xmlupload.models.deserialise.xmlresource import XMLResource
 from dsp_tools.commands.xmlupload.models.permission import Permissions
 from dsp_tools.commands.xmlupload.models.permission import PermissionValue
 from dsp_tools.commands.xmlupload.models.serialise.jsonld_serialiser import serialise_property_graph
-from dsp_tools.commands.xmlupload.transform_resource_and_values_for_upload import _make_bitstream_file_value
-from dsp_tools.commands.xmlupload.transform_resource_and_values_for_upload import _make_iiif_uri_value
 from dsp_tools.models.exceptions import PermissionNotExistsError
 
 ONTO = Namespace("http://0.0.0.0:3333/ontology/9999/onto/v2#")

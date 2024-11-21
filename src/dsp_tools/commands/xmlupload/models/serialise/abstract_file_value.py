e2cb46ccf9108fccdda6ed6ccb59416b6b05a54a
Nora-Olivia-Ammann <103038637+Nora-Olivia-Ammann@users.noreply.github.com>
2024-11-21 10:22:21 +0100

    make graphs

---
+++ src/dsp_tools/commands/xmlupload/models/serialise/abstract_file_value.py
@@ -0,0 +1,73 @@
+from __future__ import annotations
+
+from dataclasses import dataclass
+
+from rdflib import Namespace
+from rdflib import URIRef
+
+KNORA_API = Namespace("http://api.knora.org/ontology/knora-api/v2#")
+
+
+@dataclass
+class RDFPropTypeInfo:
+    knora_type: URIRef
+    knora_prop: URIRef
+
+
+@dataclass
+class AbstractFileValue:
+    internal_filename: str
+    metadata: FileValueMetadata
+
+
+@dataclass
+class FileValueMetadata:
+    permissions: str | None
+
+
+archive_file_value = RDFPropTypeInfo(KNORA_API.ArchiveFileValue, KNORA_API.hasArchiveFileValue)
+audio_file_value = RDFPropTypeInfo(KNORA_API.AudioFileValue, KNORA_API.hasAudioFileValue)
+document_file_value = RDFPropTypeInfo(KNORA_API.DocumentFileValue, KNORA_API.hasDocumentFileValue)
+moving_image_file_value = RDFPropTypeInfo(KNORA_API.MovingImageFileValue, KNORA_API.hasMovingImageFileValue)
+still_image_file_value = RDFPropTypeInfo(KNORA_API.StillImageFileValue, KNORA_API.hasStillImageFileValue)
+text_file_value = RDFPropTypeInfo(KNORA_API.TextFileValue, KNORA_API.hasTextFileValue)
+
+file_extension_to_type_mapper = {
+    # ArchiveFileValue
+    "zip": archive_file_value,
+    "tar": archive_file_value,
+    "gz": archive_file_value,
+    "z": archive_file_value,
+    "tgz": archive_file_value,
+    "gzip": archive_file_value,
+    "7z": archive_file_value,
+    # AudioFileValue
+    "mp3": audio_file_value,
+    "wav": audio_file_value,
+    # DocumentFileValue
+    "pdf": document_file_value,
+    "doc": document_file_value,
+    "docx": document_file_value,
+    "xls": document_file_value,
+    "xlsx": document_file_value,
+    "ppt": document_file_value,
+    "pptx": document_file_value,
+    # MovingImageFileValue
+    "mp4": moving_image_file_value,
+    # StillImageFileValue
+    "jpg": still_image_file_value,
+    "jpeg": still_image_file_value,
+    "jp2": still_image_file_value,
+    "png": still_image_file_value,
+    "tif": still_image_file_value,
+    "tiff": still_image_file_value,
+    "jpx": still_image_file_value,
+    # TextFileValue
+    "odd": text_file_value,
+    "rng": text_file_value,
+    "txt": text_file_value,
+    "xml": text_file_value,
+    "xsd": text_file_value,
+    "xsl": text_file_value,
+    "csv": text_file_value,
+}
---
+++ src/dsp_tools/commands/xmlupload/transform_file_value.py
@@ -0,0 +1,95 @@
+from pathlib import Path
+
+from rdflib import RDF
+from rdflib import XSD
+from rdflib import BNode
+from rdflib import Graph
+from rdflib import Literal
+from rdflib import Namespace
+from rdflib import URIRef
+
+from dsp_tools.commands.xmlupload.models.deserialise.deserialise_value import IIIFUriInfo
+from dsp_tools.commands.xmlupload.models.deserialise.xmlresource import BitstreamInfo
+from dsp_tools.commands.xmlupload.models.permission import Permissions
+from dsp_tools.commands.xmlupload.models.serialise.abstract_file_value import AbstractFileValue
+from dsp_tools.commands.xmlupload.models.serialise.abstract_file_value import FileValueMetadata
+from dsp_tools.commands.xmlupload.models.serialise.abstract_file_value import RDFPropTypeInfo
+from dsp_tools.commands.xmlupload.models.serialise.abstract_file_value import file_extension_to_type_mapper
+from dsp_tools.models.exceptions import BaseError
+from dsp_tools.models.exceptions import PermissionNotExistsError
+
+KNORA_API = Namespace("http://api.knora.org/ontology/knora-api/v2#")
+
+
+def make_iiif_uri_value_graph(
+    iiif_uri: IIIFUriInfo, res_bn: BNode, permissions_lookup: dict[str, Permissions]
+) -> tuple[Graph, URIRef]:
+    """
+    Creates a graph with the IIIF-URI Link
+
+    Args:
+        iiif_uri: Information about the IIIF URI
+        res_bn: Blank-node of the resource
+        permissions_lookup: to resolve the permissions
+
+    Returns:
+        Graph with the IIIF-URI Value
+    """
+    iiif_bn = BNode()
+    permissions = _get_permission_str(iiif_uri.permissions, permissions_lookup)
+    metadata = FileValueMetadata(permissions)
+    g = _add_metadata(iiif_bn, metadata)
+    knora_prop = KNORA_API.hasStillImageFileValue
+    g.add((res_bn, knora_prop, iiif_bn))
+    g.add((iiif_bn, RDF.type, KNORA_API.StillImageExternalFileValue))
+    g.add((iiif_bn, KNORA_API.fileValueHasExternalUrl, Literal(iiif_uri.value)))
+    return g, knora_prop
+
+
+def make_file_value_graph(
+    bitstream_info: BitstreamInfo, res_bn: BNode, permission_lookup: dict[str, Permissions]
+) -> tuple[Graph, URIRef]:
+    """
+    Creates a graph with the File Value information.
+
+    Args:
+        bitstream_info: Information about the previously uploaded file
+        res_bn: Blank-node of the resource
+        permission_lookup: to resolve the permissions
+
+    Returns:
+        Graph with the File Value
+    """
+    local_file = Path(bitstream_info.local_file)
+    file_ending = local_file.suffix[1:]
+    if not (file_type := file_extension_to_type_mapper.get(file_ending)):
+        raise BaseError(f"Unknown file ending '{file_ending}' for file '{local_file}'")
+    internal_filename = bitstream_info.internal_file_name
+    permissions = _get_permission_str(bitstream_info.permissions, permission_lookup)
+    metadata = FileValueMetadata(permissions)
+    file_value = AbstractFileValue(internal_filename, metadata)
+    return _make_file_value_graph(file_value, file_type, res_bn), file_type.knora_prop
+
+
+def _make_file_value_graph(file_value: AbstractFileValue, type_info: RDFPropTypeInfo, res_bn: BNode) -> Graph:
+    file_bn = BNode()
+    g = _add_metadata(file_bn, file_value.metadata)
+    g.add((res_bn, type_info.knora_prop, file_bn))
+    g.add((file_bn, RDF.type, type_info.knora_type))
+    g.add((file_bn, KNORA_API.fileValueHasFilename, Literal(file_value.internal_filename, datatype=XSD.string)))
+    return g
+
+
+def _add_metadata(file_bn: BNode, metadata: FileValueMetadata) -> Graph:
+    g = Graph()
+    if metadata.permissions:
+        g.add((file_bn, KNORA_API.hasPermissions, Literal(metadata.permissions, datatype=XSD.string)))
+    return g
+
+
+def _get_permission_str(permissions: str | None, permissions_lookup: dict[str, Permissions]) -> str | None:
+    if permissions:
+        if not (per := permissions_lookup.get(permissions)):
+            raise PermissionNotExistsError(f"Could not find permissions for value: {permissions}")
+        return str(per)
+    return None

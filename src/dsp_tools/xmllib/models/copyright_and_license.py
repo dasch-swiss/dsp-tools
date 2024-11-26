b2bb2dd35caf1638bfd9370b07052949cec91992
Nora-Olivia-Ammann <103038637+Nora-Olivia-Ammann@users.noreply.github.com>
2024-11-26 13:57:11 +0100

    Update copyright_and_license.py

--- src/dsp_tools/xmllib/models/copyright_and_license.py
+++ src/dsp_tools/xmllib/models/copyright_and_license.py
@@ -36,6 +36,17 @@ class CopyrightAndLicense:
         return CopyrightAndLicense(license=CREATIVE_COMMONS_LICENSES)
 
     def add_copyright_attribution(self, id_: str, text: str) -> CopyrightAndLicense:
+        """
+        Add a new copyright attribution element.
+        Please note that the id must be unique, this will raise an error if it is not.
+
+        Args:
+            id_: ID which is referenced in the attributes of the XML
+            text: Text that should be displayed in the APP.
+
+        Returns:
+            The original CopyrightAndLicense with the added copyright attribution.
+        """
         if id_ in self._get_copyright_attribution_ids():
             raise InputError(
                 f"Copyright attribution with the ID '{id_}' and the text '{text}' already exists. "
@@ -45,6 +56,16 @@ class CopyrightAndLicense:
         return self
 
     def add_copyright_attribution_with_dict(self, copyright_dict: dict[str, str]) -> CopyrightAndLicense:
+        """
+        Add a several new copyright attribution elements from a dictionary.
+        Please note that the id must be unique, this will raise an error if it is not.
+
+        Args:
+            copyright_dict: the dictionary should have the following structure: { id: text }
+
+        Returns:
+            The original CopyrightAndLicense with the added copyright attributions.
+        """
         if ids_exist := set(copyright_dict.keys()).intersection(self._get_copyright_attribution_ids()):
             raise InputError(
                 f"The following copyright IDs already exist: {", ".join(ids_exist)}. All IDs must be unique."
@@ -57,6 +78,19 @@ class CopyrightAndLicense:
         return {x.id_ for x in self.copyright_attribution}
 
     def add_license(self, id_: str, text: str, uri: Any = None) -> CopyrightAndLicense:
+        """
+        Add a new license element.
+        Please note that the id must be unique, this will raise an error if it is not.
+
+        Args:
+            id_: ID which is referenced in the attributes of the XML
+            text: Text that should be displayed in the APP.
+            uri: Optional URI liking to the license documentation.
+            A pd.isna() check is done before adding the URI, therefore any value is permissible.
+
+        Returns:
+            The original CopyrightAndLicense with the added license.
+        """
         if id_ in self._get_license_ids():
             raise InputError(
                 f"A license with the ID '{id_}' and the text '{text}' already exists. " f"All IDs must be unique."
@@ -68,6 +102,18 @@ class CopyrightAndLicense:
         return self
 
     def add_license_with_dict(self, license_dict: dict[str, tuple[str, Any]]) -> CopyrightAndLicense:
+        """
+        Add a new license element.
+        Please note that the id must be unique, this will raise an error if it is not.
+
+        Args:
+            license_dict: dictionary with the information for license elements.
+            It should have the following structure: { id: (text, uri) }
+            A pd.isna() check is done before adding the URI, therefore, any value is permissible.
+
+        Returns:
+            The original CopyrightAndLicense with the added licenses.
+        """
         for license_id, info_tuple in license_dict:
             self.add_license(license_id, info_tuple[0], info_tuple[1])
         return self

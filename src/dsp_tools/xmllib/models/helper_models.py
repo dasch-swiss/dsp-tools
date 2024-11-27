f8e8946e280d9ad2f11a83b2c33a233c87044fd3
Nora-Olivia-Ammann <103038637+Nora-Olivia-Ammann@users.noreply.github.com>
2024-11-27 08:18:19 +0100

    Update helper_models.py

--- src/dsp_tools/xmllib/models/helper_models.py
+++ src/dsp_tools/xmllib/models/helper_models.py
@@ -1,7 +1,11 @@
 from __future__ import annotations
 
+import warnings
 from dataclasses import dataclass
-from dataclasses import field
+from typing import Any
+
+from dsp_tools.models.custom_warnings import DspToolsUserWarning
+from dsp_tools.xmllib.value_checkers import is_decimal
 
 
 @dataclass
@@ -9,15 +13,34 @@ class GeometryPoint:
     x: float
     y: float
 
+    def __post_init__(self) -> None:
+        if not all([is_decimal(self.x), is_decimal(self.y)]):
+            msg = f"The entered geometry points are not floats. x: '{self.x}', y: '{self.y}'"
+            warnings.warn(DspToolsUserWarning(msg))
+
 
 @dataclass
 class Geometry:
     line_width: float
-    points: list[GeometryPoint] = field(default_factory=list)
-    status: str = "active"
-    type_: str = "rectangle"
+    points: list[GeometryPoint]
+    status: str
+    type_: str
 
     @staticmethod
     def create_new(
-        line_width: float, points: list[GeometryPoint] | None, status: str = "active", type_: str = "rectangle"
-    ) -> Geometry: ...
+        line_width: float,
+        points: list[GeometryPoint] | None,
+        status: str = "active",
+        type_: str = "rectangle",
+    ) -> Geometry:
+        pnt = points if points else []
+        return Geometry(line_width, pnt, status, type_)
+
+    def add_point(self, x: float, y: float) -> Geometry:
+        self.points.append(GeometryPoint(x, y))
+        return self
+
+    def add_point_optional(self, x: Any, y: Any) -> Geometry:
+        if all([is_decimal(x), is_decimal(y)]):
+            self.points.append(GeometryPoint(x, y))
+        return self

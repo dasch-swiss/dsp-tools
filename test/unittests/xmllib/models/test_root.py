368026655c7318096b7fc2edf305b2b4fc26eef5
Nora-Olivia-Ammann <103038637+Nora-Olivia-Ammann@users.noreply.github.com>
2024-11-28 09:55:26 +0100

    add statements

--- test/unittests/xmllib/models/test_root.py
+++ test/unittests/xmllib/models/test_root.py
@@ -1,3 +1,4 @@
+
 from dsp_tools.xmllib.models.config_options import Permissions
 from dsp_tools.xmllib.models.dsp_base_resources import AnnotationResource
 from dsp_tools.xmllib.models.resource import Resource
@@ -5,13 +6,13 @@ from dsp_tools.xmllib.models.root import XMLRoot
 
 
 def test_root() -> None:
-    root = XMLRoot.create_new("0000", "test")
+    xml_root = XMLRoot.create_new("0000", "test")
 
     # add_resource
     anno_res = AnnotationResource("annoID", "label", "id1", ["comment"], Permissions.OPEN)
     anno_res.add_comment("comment2")
-    root.add_resource(anno_res)
-    assert len(root.resources) == 1
+    xml_root.add_resource(anno_res)
+    assert len(xml_root.resources) == 1
 
     # add_resource_multiple
     many_resources = []
@@ -19,15 +20,21 @@ def test_root() -> None:
     res1.add_bool(":boolProp", True)
     many_resources.append(res1)
     res2 = Resource.create_new("id2", ":Restype", "label")
-    res2.add_color(":colorProp", "#87s3sb")
+    res2.add_color(":colorProp", "#5b24bf")
     many_resources.append(res2)
-    root.add_resource_multiple(many_resources)
-    assert len(root.resources) == 3
+    xml_root.add_resource_multiple(many_resources)
+    assert len(xml_root.resources) == 3
 
-    root.add_resource_multiple([])
-    assert len(root.resources) == 3
+    xml_root.add_resource_multiple([])
+    assert len(xml_root.resources) == 3
 
     # add_resource_optional
     no_resource = None
-    root.add_resource_optional(no_resource)
-    assert len(root.resources) == 3
+    xml_root.add_resource_optional(no_resource)
+    assert len(xml_root.resources) == 3
+
+    serialised = xml_root._serialise()
+    annotation = list(serialised.iterdescendants(tag="{https://dasch.swiss/schema}annotation"))
+    assert len(annotation) == 1
+    general_resources = list(serialised.iterdescendants(tag="{https://dasch.swiss/schema}resource"))
+    assert len(general_resources) == 2

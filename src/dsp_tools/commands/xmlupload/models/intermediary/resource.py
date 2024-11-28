521c2e79d8d1405006334a0560f630e2b540652d
Nora-Olivia-Ammann <103038637+Nora-Olivia-Ammann@users.noreply.github.com>
2024-11-28 11:36:03 +0100

    Update resource.py

--- src/dsp_tools/commands/xmlupload/models/intermediary/resource.py
+++ src/dsp_tools/commands/xmlupload/models/intermediary/resource.py
@@ -2,6 +2,23 @@ from __future__ import annotations
 
 from dataclasses import dataclass
 
+from dsp_tools.commands.xmlupload.models.intermediary.file_values import IntermediaryAbstractFileValue
+from dsp_tools.commands.xmlupload.models.intermediary.values import IntermediaryValue
+from dsp_tools.commands.xmlupload.models.permission import Permissions
+
 
 @dataclass
-class IntermediaryResource: ...
+class IntermediaryResource:
+    res_id: str
+    type_iri: str
+    label: str
+    permissions: Permissions | None
+    values: list[IntermediaryValue]
+    file_value: IntermediaryAbstractFileValue | None
+    migration_metadata: MigrationMetadata | None
+
+
+@dataclass
+class MigrationMetadata:
+    iri_str: str
+    creation_date: str

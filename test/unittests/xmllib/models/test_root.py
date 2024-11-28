f37d7623de0dd6798ada88bddcfb0c67557e568e
Nora-Olivia-Ammann <103038637+Nora-Olivia-Ammann@users.noreply.github.com>
2024-11-28 10:20:04 +0100

    Update test_root.py

--- test/unittests/xmllib/models/test_root.py
+++ test/unittests/xmllib/models/test_root.py
@@ -1,4 +1,3 @@
-from dsp_tools.xmllib.models.config_options import Permissions
 from dsp_tools.xmllib.models.dsp_base_resources import AnnotationResource
 from dsp_tools.xmllib.models.resource import Resource
 from dsp_tools.xmllib.models.root import XMLRoot
@@ -8,18 +7,15 @@ def test_root_add_resources() -> None:
     xml_root = XMLRoot.create_new("0000", "test")
 
     # add_resource
-    anno_res = AnnotationResource("annoID", "label", "id1", ["comment"], Permissions.OPEN)
-    anno_res.add_comment("comment2")
+    anno_res = AnnotationResource("annoID", "label", "id1", ["comment"])
     xml_root.add_resource(anno_res)
     assert len(xml_root.resources) == 1
 
     # add_resource_multiple
     many_resources = []
     res1 = Resource.create_new("id1", ":Restype", "label")
-    res1.add_bool(":boolProp", True)
     many_resources.append(res1)
     res2 = Resource.create_new("id2", ":Restype", "label")
-    res2.add_color(":colorProp", "#5b24bf")
     many_resources.append(res2)
     xml_root.add_resource_multiple(many_resources)
     assert len(xml_root.resources) == 3

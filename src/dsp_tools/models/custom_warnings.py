b99279faefd45a3a9f79a2d24d5d63061b59a6fe
Nora-Olivia-Ammann <103038637+Nora-Olivia-Ammann@users.noreply.github.com>
2024-12-03 16:00:56 +0100

    Update custom_warnings.py

--- src/dsp_tools/models/custom_warnings.py
+++ src/dsp_tools/models/custom_warnings.py
@@ -3,14 +3,18 @@ from typing import runtime_checkable
 
 # These are ANSI escape codes which can be used to configure the print output on the terminal
 # https://en.wikipedia.org/wiki/ANSI_escape_code
-# All codes must start with "\033"
 # The semicolon separates different configurations
+
+# All codes must start with "\033["
+SEQUENCE_START = "\033["
+
 # the "m" at the end signals,
 # that the configuration code it finished and after that the string that should be printed starts
+SEQUENCE_END = "m"
 
-BOLD_RED = "\033[1;31m"  # 1 (bold) ; 31 (red)
-YELLOW = "\033[0;33m"  # 0 (normal font) ; 33 (yellow)
-RESET_TO_DEFAULT = "\033[0m"  # reset the previous setting to the default of the console
+BOLD_RED = SEQUENCE_START + "1;31" + SEQUENCE_END  # 1 (bold) ; 31 (red)
+YELLOW = SEQUENCE_START + "0;33" + SEQUENCE_END  # 0 (normal font) ; 33 (yellow)
+RESET_TO_DEFAULT = SEQUENCE_START + "0" + SEQUENCE_END  # reset the previous setting to the default of the console
 
 
 @runtime_checkable

abbf4d3b4c584709e1f6e599631a9942d39879e3
Nora-Olivia-Ammann <103038637+Nora-Olivia-Ammann@users.noreply.github.com>
2024-12-04 08:36:28 +0100

    Update validate_data.py

--- src/dsp_tools/commands/validate_data/validate_data.py
+++ src/dsp_tools/commands/validate_data/validate_data.py
@@ -7,7 +7,6 @@ from rdflib import RDF
 from rdflib import Graph
 from rdflib import Literal
 from rdflib import URIRef
-from termcolor import cprint
 
 from dsp_tools.commands.validate_data.api_clients import ListClient
 from dsp_tools.commands.validate_data.api_clients import OntologyClient
@@ -33,6 +32,17 @@ from dsp_tools.utils.xml_validation import validate_xml
 LIST_SEPARATOR = "\n    - "
 KNORA_API = "http://api.knora.org/ontology/knora-api/v2#"
 
+# ANSI Color (see custom_warnings.py for more details)
+SEQUENCE_START = "\u001b["
+SEQUENCE_END = "m"
+BACKGROUND_BOLD_MAGENTA = f"{SEQUENCE_START}1;45{SEQUENCE_END}"  # 1 (bold) ; 45 (background magenta)
+BACKGROUND_BOLD_YELLOW = f"{SEQUENCE_START}1;43{SEQUENCE_END}"  # 1 (bold) ; 45 (background yellow)
+BACKGROUND_BOLD_GREEN = f"{SEQUENCE_START}1;42{SEQUENCE_END}"  # 1 (bold) ; 45 (background green)
+BOLD_CYAN = f"{SEQUENCE_START}1;36{SEQUENCE_END}"  # 1 (bold) ; 45 (cyan)
+RESET_TO_DEFAULT = f"{SEQUENCE_START}0{SEQUENCE_END}"  # reset to the default setting of the console
+
+VALIDATION_ERRORS_FOUND_MSG = BACKGROUND_BOLD_MAGENTA + "\n   Validation errors found!   " + RESET_TO_DEFAULT
+
 
 def validate_data(filepath: Path, api_url: str, dev_route: bool, save_graphs: bool) -> bool:  # noqa: ARG001 (unused argument)
     """
@@ -52,23 +62,22 @@ def validate_data(filepath: Path, api_url: str, dev_route: bool, save_graphs: bo
     graphs = _get_parsed_graphs(api_con, filepath)
     if unknown_classes := _check_for_unknown_resource_classes(graphs):
         msg = unknown_classes.get_msg()
-        cprint("\n   Validation errors found!   ", color="light_red", attrs=["bold", "reverse"])
+        print(VALIDATION_ERRORS_FOUND_MSG)
         print(msg)
         return True
     report = _get_validation_result(graphs, api_con, filepath, save_graphs)
     if report.conforms:
-        cprint("\n   Validation passed!   ", color="green", attrs=["bold", "reverse"])
+        print(BACKGROUND_BOLD_GREEN + "\n   Validation passed!   " + RESET_TO_DEFAULT)
     else:
         reformatted = reformat_validation_graph(report)
         problem_msg = reformatted.get_msg(filepath)
-        cprint("\n   Validation errors found!   ", color="light_red", attrs=["bold", "reverse"])
+        print(VALIDATION_ERRORS_FOUND_MSG)
         print(problem_msg)
         if reformatted.unexpected_results:
             if save_graphs:
-                cprint(
-                    "\n   Unexpected violations were found! Consult the saved graphs for details.   ",
-                    color="yellow",
-                    attrs=["bold", "reverse"],
+                print(
+                    BACKGROUND_BOLD_YELLOW + "\n   Unexpected violations were found! "
+                    "Consult the saved graphs for details.   " + RESET_TO_DEFAULT
                 )
                 return True
             reformatted.unexpected_results.save_inform_user(
@@ -87,7 +96,7 @@ def _inform_about_experimental_feature() -> None:
         "If the value type used matches the ontology",
         "Content of the values",
     ]
-    cprint(LIST_SEPARATOR.join(what_is_validated), color="magenta", attrs=["bold"])
+    print(BOLD_CYAN + LIST_SEPARATOR.join(what_is_validated) + RESET_TO_DEFAULT)
 
 
 def _get_parsed_graphs(api_con: ApiConnection, filepath: Path) -> RDFGraphs:
@@ -177,7 +186,7 @@ def _save_graphs(filepath: Path, rdf_graphs: RDFGraphs) -> Path:
     parent_directory = filepath.parent
     new_directory = parent_directory / "graphs"
     new_directory.mkdir(exist_ok=True)
-    cprint(f"\n   Saving graphs to {new_directory}   ", color="light_blue", attrs=["bold", "reverse"])
+    print(BOLD_CYAN + f"\n   Saving graphs to {new_directory}   " + RESET_TO_DEFAULT)
     generic_filepath = new_directory / filepath.stem
     rdf_graphs.ontos.serialize(f"{generic_filepath}_ONTO.ttl")
     shacl_onto = rdf_graphs.content_shapes + rdf_graphs.cardinality_shapes + rdf_graphs.ontos

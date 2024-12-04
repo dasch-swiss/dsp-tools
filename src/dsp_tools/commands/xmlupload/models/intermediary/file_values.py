207a04f54014b9b08f003ed8741020652521ee3d
Nora-Olivia-Ammann <103038637+Nora-Olivia-Ammann@users.noreply.github.com>
2024-12-04 10:42:33 +0100

    refactor(xmlupload): create intermediary class structure for data (#1304)

---
+++ src/dsp_tools/commands/xmlupload/models/intermediary/file_values.py
@@ -0,0 +1,22 @@
+from __future__ import annotations
+
+from dataclasses import dataclass
+
+from dsp_tools.commands.xmlupload.models.permission import Permissions
+
+
+@dataclass
+class IntermediaryFileMetadata:
+    permissions: Permissions | None
+
+
+@dataclass
+class IntermediaryFileValue:
+    value: str
+    metadata: IntermediaryFileMetadata | None
+
+
+@dataclass
+class IntermediaryIIIFUri:
+    value: str
+    metadata: IntermediaryFileMetadata | None
---
+++ src/dsp_tools/commands/xmlupload/models/intermediary/values.py
@@ -0,0 +1,97 @@
+from __future__ import annotations
+
+from dataclasses import dataclass
+from typing import TypeAlias
+from typing import Union
+
+from dsp_tools.commands.xmlupload.models.formatted_text_value import FormattedTextValue
+from dsp_tools.commands.xmlupload.models.permission import Permissions
+from dsp_tools.utils.date_util import Date
+
+
+@dataclass
+class IntervalFloats:
+    start: float
+    end: float
+
+
+IntermediaryValueTypes: TypeAlias = Union[bool, str, float, int, FormattedTextValue, Date, IntervalFloats]
+
+
+@dataclass
+class IntermediaryValue:
+    value: IntermediaryValueTypes
+    prop_iri: str
+    comment: str | None
+    permissions: Permissions | None
+
+
+@dataclass
+class IntermediaryBoolean(IntermediaryValue):
+    value: bool
+
+
+@dataclass
+class IntermediaryColor(IntermediaryValue):
+    value: str
+
+
+@dataclass
+class IntermediaryDate(IntermediaryValue):
+    value: Date
+
+
+@dataclass
+class IntermediaryDecimal(IntermediaryValue):
+    value: float
+
+
+@dataclass
+class IntermediaryGeoname(IntermediaryValue):
+    value: str
+
+
+@dataclass
+class IntermediaryGeometry(IntermediaryValue):
+    value: str
+
+
+@dataclass
+class IntermediaryInt(IntermediaryValue):
+    value: int
+
+
+@dataclass
+class IntermediaryInterval(IntermediaryValue):
+    value: IntervalFloats
+
+
+@dataclass
+class IntermediaryLink(IntermediaryValue):
+    value: str
+
+
+@dataclass
+class IntermediaryList(IntermediaryValue):
+    value: str
+
+
+@dataclass
+class IntermediarySimpleText(IntermediaryValue):
+    value: str
+
+
+@dataclass
+class IntermediaryRichtext(IntermediaryValue):
+    value: FormattedTextValue
+    resource_references: set[str]
+
+
+@dataclass
+class IntermediaryTime(IntermediaryValue):
+    value: str
+
+
+@dataclass
+class IntermediaryUri(IntermediaryValue):
+    value: str

f061e6648b175b9d6e7ffeda5e9c533681618096
Johannes Nussbaum <johannes.nussbaum@dasch.swiss>
2024-12-05 14:34:37 +0100

    fix ruff

--- src/dsp_tools/xmllib/conftest.py
+++ src/dsp_tools/xmllib/conftest.py
@@ -6,10 +6,10 @@ from dsp_tools import xmllib
 
 
 @pytest.fixture(autouse=True)
-def add_xmllib_namespace(doctest_namespace: dict[str, Any]) -> None:
+def _add_xmllib_namespace(doctest_namespace: dict[str, Any]) -> None:
     """
-    Add "xmllib" to the namespace of the doctests. 
-    This allows to use `xmllib.find_date_in_string()` in the docstring examples, 
+    Add "xmllib" to the namespace of the doctests.
+    This allows to use `xmllib.find_date_in_string()` in the docstring examples,
     instead of `find_date_in_string()`.
     See https://docs.pytest.org/en/stable/how-to/doctest.html#doctest-namespace-fixture.
 

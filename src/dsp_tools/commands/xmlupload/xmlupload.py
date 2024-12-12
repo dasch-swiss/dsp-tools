c797d787153737b8f74a111fc26f160f1f5f8325
Nora-Olivia-Ammann <103038637+Nora-Olivia-Ammann@users.noreply.github.com>
2024-12-12 11:53:12 +0100

    fix(xmlupload): inform user if resource transformation failed (DEV-4456) (#1336)

--- src/dsp_tools/commands/xmlupload/xmlupload.py
+++ src/dsp_tools/commands/xmlupload/xmlupload.py
@@ -236,6 +236,7 @@ def _upload_one_resource(
     transformation_result = transform_into_intermediary_resource(resource, intermediary_lookups)
     if transformation_result.resource_failure:
         _handle_resource_creation_failure(resource, transformation_result.resource_failure.failure_msg)
+        upload_state.failed_uploads.append(resource.res_id)
         return
 
     transformed_resource = cast(IntermediaryResource, transformation_result.resource_success)

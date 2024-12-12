259afb9154db5a57071cc61cb86e2267be695076
Nora-Olivia-Ammann <103038637+Nora-Olivia-Ammann@users.noreply.github.com>
2024-12-12 11:18:54 +0100

    Commit all changes

--- src/dsp_tools/commands/validate_data/validate_data.py
+++ src/dsp_tools/commands/validate_data/validate_data.py
@@ -88,10 +88,13 @@ def validate_data(filepath: Path, api_url: str, dev_route: bool, save_graphs: bo
 def _inform_about_experimental_feature() -> None:
     what_is_validated = [
         "This is an experimental feature, it will change and be extended continuously. "
+        "Please note that special characters may not be rendered correctly in the validation result. "
+        "This however has not influence on the validation itself."
         "The following information of your data is being validated:",
         "Cardinalities",
         "If the value type used matches the ontology",
         "Content of the values",
+        "Missing or wrong files",
     ]
     print(BOLD_CYAN + LIST_SEPARATOR.join(what_is_validated) + RESET_TO_DEFAULT)
 

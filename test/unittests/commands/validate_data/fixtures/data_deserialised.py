305805567f52f90ad11fc0f1d2995f4f0f4bd807
Nora-Olivia-Ammann <103038637+Nora-Olivia-Ammann@users.noreply.github.com>
2025-01-15 11:28:45 +0100

    refactor(validate-data): simplify internal Python representation of values (#1362)

--- test/unittests/commands/validate_data/fixtures/data_deserialised.py
+++ test/unittests/commands/validate_data/fixtures/data_deserialised.py
@@ -1,21 +1,11 @@
 import pytest
 
-from dsp_tools.commands.validate_data.models.data_deserialised import BooleanValueDeserialised
-from dsp_tools.commands.validate_data.models.data_deserialised import ColorValueDeserialised
-from dsp_tools.commands.validate_data.models.data_deserialised import DateValueDeserialised
-from dsp_tools.commands.validate_data.models.data_deserialised import DecimalValueDeserialised
-from dsp_tools.commands.validate_data.models.data_deserialised import GeonameValueDeserialised
-from dsp_tools.commands.validate_data.models.data_deserialised import IntValueDeserialised
-from dsp_tools.commands.validate_data.models.data_deserialised import LinkValueDeserialised
-from dsp_tools.commands.validate_data.models.data_deserialised import ListValueDeserialised
+from dsp_tools.commands.validate_data.models.data_deserialised import KnoraValueType
 from dsp_tools.commands.validate_data.models.data_deserialised import PropertyObject
 from dsp_tools.commands.validate_data.models.data_deserialised import ResourceDeserialised
-from dsp_tools.commands.validate_data.models.data_deserialised import RichtextDeserialised
-from dsp_tools.commands.validate_data.models.data_deserialised import SimpleTextDeserialised
-from dsp_tools.commands.validate_data.models.data_deserialised import TimeValueDeserialised
 from dsp_tools.commands.validate_data.models.data_deserialised import TripleObjectType
 from dsp_tools.commands.validate_data.models.data_deserialised import TriplePropertyType
-from dsp_tools.commands.validate_data.models.data_deserialised import UriValueDeserialised
+from dsp_tools.commands.validate_data.models.data_deserialised import ValueInformation
 
 LABEL_TRIPLE = PropertyObject(TriplePropertyType.RDFS_LABEL, "lbl", TripleObjectType.STRING)
 TYPE_TRIPLE = PropertyObject(
@@ -27,7 +17,7 @@ UNREIFIED_TRIPLE_OBJECTS = [LABEL_TRIPLE, TYPE_TRIPLE]
 
 @pytest.fixture
 def resource_deserialised_with_values(
-    boolean_value_deserialised_corr: BooleanValueDeserialised,
+    boolean_value_deserialised_corr: ValueInformation,
 ) -> ResourceDeserialised:
     return ResourceDeserialised(
         res_id="id",
@@ -46,132 +36,150 @@ def resource_deserialised_no_values() -> ResourceDeserialised:
 
 
 @pytest.fixture
-def boolean_value_deserialised_corr() -> BooleanValueDeserialised:
-    return BooleanValueDeserialised("http://0.0.0.0:3333/ontology/9999/onto/v2#testBoolean", "false")
-
-
-@pytest.fixture
-def boolean_value_deserialised_zero() -> BooleanValueDeserialised:
-    return BooleanValueDeserialised("http://0.0.0.0:3333/ontology/9999/onto/v2#testBoolean", "0")
-
-
-@pytest.fixture
-def boolean_value_deserialised_one() -> BooleanValueDeserialised:
-    return BooleanValueDeserialised("http://0.0.0.0:3333/ontology/9999/onto/v2#testBoolean", "1")
-
-
-@pytest.fixture
-def boolean_value_deserialised_none() -> BooleanValueDeserialised:
-    return BooleanValueDeserialised("http://0.0.0.0:3333/ontology/9999/onto/v2#testBoolean", None)
-
-
-@pytest.fixture
-def color_value_deserialised_corr() -> ColorValueDeserialised:
-    return ColorValueDeserialised("http://0.0.0.0:3333/ontology/9999/onto/v2#testColor", "#00ff00")
-
-
-@pytest.fixture
-def color_value_deserialised_none() -> ColorValueDeserialised:
-    return ColorValueDeserialised("http://0.0.0.0:3333/ontology/9999/onto/v2#testColor", None)
-
-
-@pytest.fixture
-def date_value_deserialised_corr() -> DateValueDeserialised:
-    return DateValueDeserialised("http://0.0.0.0:3333/ontology/9999/onto/v2#testSubDate1", "JULIAN:BCE:0700:BCE:0600")
-
-
-@pytest.fixture
-def date_value_deserialised_none() -> DateValueDeserialised:
-    return DateValueDeserialised("http://0.0.0.0:3333/ontology/9999/onto/v2#testSubDate1", None)
-
-
-@pytest.fixture
-def decimal_value_deserialised_corr() -> DecimalValueDeserialised:
-    return DecimalValueDeserialised("http://0.0.0.0:3333/ontology/9999/onto/v2#testDecimalSimpleText", "1.2")
-
-
-@pytest.fixture
-def decimal_value_deserialised_none() -> DecimalValueDeserialised:
-    return DecimalValueDeserialised("http://0.0.0.0:3333/ontology/9999/onto/v2#testDecimalSimpleText", None)
-
-
-@pytest.fixture
-def geoname_value_deserialised_corr() -> GeonameValueDeserialised:
-    return GeonameValueDeserialised("http://0.0.0.0:3333/ontology/9999/onto/v2#testGeoname", "1241345")
-
-
-@pytest.fixture
-def geoname_value_deserialised_none() -> GeonameValueDeserialised:
-    return GeonameValueDeserialised("http://0.0.0.0:3333/ontology/9999/onto/v2#testGeoname", None)
-
-
-@pytest.fixture
-def int_value_deserialised_corr() -> IntValueDeserialised:
-    return IntValueDeserialised("http://0.0.0.0:3333/ontology/9999/onto/v2#testIntegerSimpleText", "1")
-
-
-@pytest.fixture
-def int_value_deserialised_none() -> IntValueDeserialised:
-    return IntValueDeserialised("http://0.0.0.0:3333/ontology/9999/onto/v2#testIntegerSimpleText", None)
-
-
-@pytest.fixture
-def link_value_deserialised_corr() -> LinkValueDeserialised:
-    return LinkValueDeserialised("http://0.0.0.0:3333/ontology/9999/onto/v2#testHasLinkTo", "link-id")
-
-
-@pytest.fixture
-def link_value_deserialised_none() -> LinkValueDeserialised:
-    return LinkValueDeserialised("http://0.0.0.0:3333/ontology/9999/onto/v2#testHasLinkTo", None)
-
-
-@pytest.fixture
-def list_value_deserialised_corr() -> ListValueDeserialised:
-    return ListValueDeserialised("http://0.0.0.0:3333/ontology/9999/onto/v2#testListProp", "n1", "firstList")
-
-
-@pytest.fixture
-def list_value_deserialised_none() -> ListValueDeserialised:
-    return ListValueDeserialised("http://0.0.0.0:3333/ontology/9999/onto/v2#testListProp", None, "firstList")
-
-
-@pytest.fixture
-def simple_text_deserialised_corr() -> SimpleTextDeserialised:
-    return SimpleTextDeserialised("http://0.0.0.0:3333/ontology/9999/onto/v2#testTextarea", "simple text")
-
-
-@pytest.fixture
-def simple_text_deserialised_none() -> SimpleTextDeserialised:
-    return SimpleTextDeserialised("http://0.0.0.0:3333/ontology/9999/onto/v2#testTextarea", None)
-
-
-@pytest.fixture
-def richtext_deserialised_corr() -> RichtextDeserialised:
-    return RichtextDeserialised("http://0.0.0.0:3333/ontology/9999/onto/v2#testRichtext", "rich text")
-
-
-@pytest.fixture
-def richtext_deserialised_none() -> RichtextDeserialised:
-    return RichtextDeserialised("http://0.0.0.0:3333/ontology/9999/onto/v2#testRichtext", None)
-
-
-@pytest.fixture
-def time_value_deserialised_corr() -> TimeValueDeserialised:
-    return TimeValueDeserialised(
-        "http://0.0.0.0:3333/ontology/9999/onto/v2#testTimeValue", "2019-10-23T13:45:12.01-14:00"
+def boolean_value_deserialised_corr() -> ValueInformation:
+    return ValueInformation(
+        "http://0.0.0.0:3333/ontology/9999/onto/v2#testBoolean",
+        "false",
+        KnoraValueType.BOOLEAN_VALUE,
+        [],
     )
 
 
 @pytest.fixture
-def time_value_deserialised_none() -> TimeValueDeserialised:
-    return TimeValueDeserialised("http://0.0.0.0:3333/ontology/9999/onto/v2#testTimeValue", None)
+def boolean_value_deserialised_zero() -> ValueInformation:
+    return ValueInformation(
+        "http://0.0.0.0:3333/ontology/9999/onto/v2#testBoolean",
+        "0",
+        KnoraValueType.BOOLEAN_VALUE,
+        [],
+    )
 
 
 @pytest.fixture
-def uri_value_deserialised_corr() -> UriValueDeserialised:
-    return UriValueDeserialised("http://0.0.0.0:3333/ontology/9999/onto/v2#testUriValue", "https://dasch.swiss")
+def boolean_value_deserialised_one() -> ValueInformation:
+    return ValueInformation(
+        "http://0.0.0.0:3333/ontology/9999/onto/v2#testBoolean",
+        "1",
+        KnoraValueType.BOOLEAN_VALUE,
+        [],
+    )
 
 
 @pytest.fixture
-def uri_value_deserialised_none() -> UriValueDeserialised:
-    return UriValueDeserialised("http://0.0.0.0:3333/ontology/9999/onto/v2#testUriValue", None)
+def color_value_deserialised_corr() -> ValueInformation:
+    return ValueInformation(
+        "http://0.0.0.0:3333/ontology/9999/onto/v2#testColor",
+        "#00ff00",
+        KnoraValueType.COLOR_VALUE,
+        [],
+    )
+
+
+@pytest.fixture
+def date_value_deserialised_corr() -> ValueInformation:
+    return ValueInformation(
+        "http://0.0.0.0:3333/ontology/9999/onto/v2#testSubDate1",
+        "JULIAN:BCE:0700:BCE:0600",
+        KnoraValueType.DATE_VALUE,
+        [],
+    )
+
+
+@pytest.fixture
+def decimal_value_deserialised_corr() -> ValueInformation:
+    return ValueInformation(
+        "http://0.0.0.0:3333/ontology/9999/onto/v2#testDecimalSimpleText",
+        "1.2",
+        KnoraValueType.DECIMAL_VALUE,
+        [],
+    )
+
+
+@pytest.fixture
+def geoname_value_deserialised_corr() -> ValueInformation:
+    return ValueInformation(
+        "http://0.0.0.0:3333/ontology/9999/onto/v2#testGeoname",
+        "1241345",
+        KnoraValueType.GEONAME_VALUE,
+        [],
+    )
+
+
+@pytest.fixture
+def int_value_deserialised_corr() -> ValueInformation:
+    return ValueInformation(
+        "http://0.0.0.0:3333/ontology/9999/onto/v2#testIntegerSimpleText",
+        "1",
+        KnoraValueType.INT_VALUE,
+        [],
+    )
+
+
+@pytest.fixture
+def link_value_deserialised_corr() -> ValueInformation:
+    return ValueInformation(
+        "http://0.0.0.0:3333/ontology/9999/onto/v2#testHasLinkTo",
+        "link-id",
+        KnoraValueType.LINK_VALUE,
+        [],
+    )
+
+
+@pytest.fixture
+def link_value_deserialised_none() -> ValueInformation:
+    return ValueInformation(
+        "http://0.0.0.0:3333/ontology/9999/onto/v2#testHasLinkTo",
+        None,
+        KnoraValueType.LINK_VALUE,
+        [],
+    )
+
+
+@pytest.fixture
+def list_value_deserialised_corr() -> ValueInformation:
+    return ValueInformation(
+        "http://0.0.0.0:3333/ontology/9999/onto/v2#testListProp",
+        "n1",
+        KnoraValueType.LIST_VALUE,
+        [],
+    )
+
+
+@pytest.fixture
+def simple_text_deserialised_corr() -> ValueInformation:
+    return ValueInformation(
+        "http://0.0.0.0:3333/ontology/9999/onto/v2#testTextarea",
+        "simple text",
+        KnoraValueType.SIMPLETEXT_VALUE,
+        [],
+    )
+
+
+@pytest.fixture
+def richtext_deserialised_corr() -> ValueInformation:
+    return ValueInformation(
+        "http://0.0.0.0:3333/ontology/9999/onto/v2#testRichtext",
+        "rich text",
+        KnoraValueType.RICHTEXT_VALUE,
+        [],
+    )
+
+
+@pytest.fixture
+def time_value_deserialised_corr() -> ValueInformation:
+    return ValueInformation(
+        "http://0.0.0.0:3333/ontology/9999/onto/v2#testTimeValue",
+        "2019-10-23T13:45:12.01-14:00",
+        KnoraValueType.TIME_VALUE,
+        [],
+    )
+
+
+@pytest.fixture
+def uri_value_deserialised_corr() -> ValueInformation:
+    return ValueInformation(
+        "http://0.0.0.0:3333/ontology/9999/onto/v2#testUriValue",
+        "https://dasch.swiss",
+        KnoraValueType.URI_VALUE,
+        [],
+    )
--- test/unittests/commands/validate_data/test_make_data_rdf.py
+++ test/unittests/commands/validate_data/test_make_data_rdf.py
@@ -11,23 +11,10 @@ from dsp_tools.commands.validate_data.make_data_rdf import _make_one_rdflib_obje
 from dsp_tools.commands.validate_data.make_data_rdf import _make_one_resource
 from dsp_tools.commands.validate_data.make_data_rdf import _make_one_value
 from dsp_tools.commands.validate_data.models.data_deserialised import BitstreamDeserialised
-from dsp_tools.commands.validate_data.models.data_deserialised import BooleanValueDeserialised
-from dsp_tools.commands.validate_data.models.data_deserialised import ColorValueDeserialised
-from dsp_tools.commands.validate_data.models.data_deserialised import DateValueDeserialised
-from dsp_tools.commands.validate_data.models.data_deserialised import DecimalValueDeserialised
-from dsp_tools.commands.validate_data.models.data_deserialised import GeonameValueDeserialised
 from dsp_tools.commands.validate_data.models.data_deserialised import IIIFUriDeserialised
-from dsp_tools.commands.validate_data.models.data_deserialised import IntValueDeserialised
-from dsp_tools.commands.validate_data.models.data_deserialised import LinkValueDeserialised
-from dsp_tools.commands.validate_data.models.data_deserialised import ListValueDeserialised
-from dsp_tools.commands.validate_data.models.data_deserialised import PropertyObject
 from dsp_tools.commands.validate_data.models.data_deserialised import ResourceDeserialised
-from dsp_tools.commands.validate_data.models.data_deserialised import RichtextDeserialised
-from dsp_tools.commands.validate_data.models.data_deserialised import SimpleTextDeserialised
-from dsp_tools.commands.validate_data.models.data_deserialised import TimeValueDeserialised
 from dsp_tools.commands.validate_data.models.data_deserialised import TripleObjectType
-from dsp_tools.commands.validate_data.models.data_deserialised import TriplePropertyType
-from dsp_tools.commands.validate_data.models.data_deserialised import UriValueDeserialised
+from dsp_tools.commands.validate_data.models.data_deserialised import ValueInformation
 from test.unittests.commands.validate_data.constants import API_SHAPES
 from test.unittests.commands.validate_data.constants import DATA
 from test.unittests.commands.validate_data.constants import KNORA_API
@@ -37,28 +24,27 @@ RES_IRI = DATA["id"]
 
 
 @pytest.mark.parametrize(
-    ("property_object", "expected"),
+    ("object", "object_type", "expected"),
     [
         (
-            PropertyObject(TriplePropertyType.RDFS_LABEL, "label", TripleObjectType.STRING),
+            "label",
+            TripleObjectType.STRING,
             Literal("label", datatype=XSD.string),
         ),
         (
-            PropertyObject(
-                TriplePropertyType.RDF_TYPE,
-                "http://0.0.0.0:3333/ontology/9999/onto/v2#ClassWithEverything",
-                TripleObjectType.IRI,
-            ),
+            "http://0.0.0.0:3333/ontology/9999/onto/v2#ClassWithEverything",
+            TripleObjectType.IRI,
             URIRef("http://0.0.0.0:3333/ontology/9999/onto/v2#ClassWithEverything"),
         ),
         (
-            PropertyObject(TriplePropertyType.RDF_TYPE, None, TripleObjectType.IRI),
+            None,
+            TripleObjectType.IRI,
             Literal("", datatype=XSD.string),
         ),
     ],
 )
-def test_make_one_rdflib_object(property_object: PropertyObject, expected: Node) -> None:
-    result = _make_one_rdflib_object(property_object)
+def test_make_one_rdflib_object(object: str | None, object_type: TripleObjectType, expected: Node) -> None:
+    result = _make_one_rdflib_object(object, object_type)
     assert result == expected
 
 
@@ -79,52 +65,39 @@ class TestResource:
 
 
 class TestBooleanValue:
-    def test_corr(self, boolean_value_deserialised_corr: BooleanValueDeserialised) -> None:
+    def test_corr(self, boolean_value_deserialised_corr: ValueInformation) -> None:
         val_g = _make_one_value(boolean_value_deserialised_corr, RES_IRI)
         assert len(val_g) == 3
         bn = next(val_g.objects(RES_IRI, ONTO.testBoolean))
         assert next(val_g.objects(bn, RDF.type)) == KNORA_API.BooleanValue
         assert next(val_g.objects(bn, KNORA_API.booleanValueAsBoolean)) == Literal("false", datatype=XSD.boolean)
 
-    def test_one(self, boolean_value_deserialised_one: BooleanValueDeserialised) -> None:
+    def test_one(self, boolean_value_deserialised_one: ValueInformation) -> None:
         val_g = _make_one_value(boolean_value_deserialised_one, RES_IRI)
         assert len(val_g) == 3
         bn = next(val_g.objects(RES_IRI, ONTO.testBoolean))
         assert next(val_g.objects(bn, RDF.type)) == KNORA_API.BooleanValue
         assert next(val_g.objects(bn, KNORA_API.booleanValueAsBoolean)) == Literal("true", datatype=XSD.boolean)
 
-    def test_zero(self, boolean_value_deserialised_zero: BooleanValueDeserialised) -> None:
+    def test_zero(self, boolean_value_deserialised_zero: ValueInformation) -> None:
         val_g = _make_one_value(boolean_value_deserialised_zero, RES_IRI)
         assert len(val_g) == 3
         bn = next(val_g.objects(RES_IRI, ONTO.testBoolean))
         assert next(val_g.objects(bn, RDF.type)) == KNORA_API.BooleanValue
         assert next(val_g.objects(bn, KNORA_API.booleanValueAsBoolean)) == Literal("false", datatype=XSD.boolean)
 
-    def test_none(self, boolean_value_deserialised_none: BooleanValueDeserialised) -> None:
-        val_g = _make_one_value(boolean_value_deserialised_none, RES_IRI)
-        bn = next(val_g.objects(RES_IRI, ONTO.testBoolean))
-        assert next(val_g.objects(bn, RDF.type)) == KNORA_API.BooleanValue
-        assert next(val_g.objects(bn, KNORA_API.booleanValueAsBoolean)) == Literal("", datatype=XSD.string)
-
 
 class TestColorValue:
-    def test_corr(self, color_value_deserialised_corr: ColorValueDeserialised) -> None:
+    def test_corr(self, color_value_deserialised_corr: ValueInformation) -> None:
         val_g = _make_one_value(color_value_deserialised_corr, RES_IRI)
         assert len(val_g) == 3
         bn = next(val_g.objects(RES_IRI, ONTO.testColor))
         assert next(val_g.objects(bn, RDF.type)) == KNORA_API.ColorValue
         assert next(val_g.objects(bn, KNORA_API.colorValueAsColor)) == Literal("#00ff00", datatype=XSD.string)
 
-    def test_none(self, color_value_deserialised_none: ColorValueDeserialised) -> None:
-        val_g = _make_one_value(color_value_deserialised_none, RES_IRI)
-        assert len(val_g) == 3
-        bn = next(val_g.objects(RES_IRI, ONTO.testColor))
-        assert next(val_g.objects(bn, RDF.type)) == KNORA_API.ColorValue
-        assert next(val_g.objects(bn, KNORA_API.colorValueAsColor)) == Literal("", datatype=XSD.string)
-
 
 class TestDateValue:
-    def test_corr(self, date_value_deserialised_corr: DateValueDeserialised) -> None:
+    def test_corr(self, date_value_deserialised_corr: ValueInformation) -> None:
         val_g = _make_one_value(date_value_deserialised_corr, RES_IRI)
         assert len(val_g) == 3
         bn = next(val_g.objects(RES_IRI, ONTO.testSubDate1))
@@ -133,71 +106,43 @@ class TestDateValue:
             "JULIAN:BCE:0700:BCE:0600", datatype=XSD.string
         )
 
-    def test_none(self, date_value_deserialised_none: DateValueDeserialised) -> None:
-        val_g = _make_one_value(date_value_deserialised_none, RES_IRI)
-        assert len(val_g) == 3
-        bn = next(val_g.objects(RES_IRI, ONTO.testSubDate1))
-        assert next(val_g.objects(bn, RDF.type)) == KNORA_API.DateValue
-        assert next(val_g.objects(bn, KNORA_API.valueAsString)) == Literal("", datatype=XSD.string)
-
 
 class TestDecimalValue:
-    def test_corr(self, decimal_value_deserialised_corr: DecimalValueDeserialised) -> None:
+    def test_corr(self, decimal_value_deserialised_corr: ValueInformation) -> None:
         val_g = _make_one_value(decimal_value_deserialised_corr, RES_IRI)
         assert len(val_g) == 3
         bn = next(val_g.objects(RES_IRI, ONTO.testDecimalSimpleText))
         assert next(val_g.objects(bn, RDF.type)) == KNORA_API.DecimalValue
         assert next(val_g.objects(bn, KNORA_API.decimalValueAsDecimal)) == Literal("1.2", datatype=XSD.decimal)
 
-    def test_none(self, decimal_value_deserialised_none: DecimalValueDeserialised) -> None:
-        val_g = _make_one_value(decimal_value_deserialised_none, RES_IRI)
-        assert len(val_g) == 3
-        bn = next(val_g.objects(RES_IRI, ONTO.testDecimalSimpleText))
-        assert next(val_g.objects(bn, RDF.type)) == KNORA_API.DecimalValue
-        assert next(val_g.objects(bn, KNORA_API.decimalValueAsDecimal)) == Literal("", datatype=XSD.string)
-
 
 class TestGeonameValue:
-    def test_corr(self, geoname_value_deserialised_corr: GeonameValueDeserialised) -> None:
+    def test_corr(self, geoname_value_deserialised_corr: ValueInformation) -> None:
         val_g = _make_one_value(geoname_value_deserialised_corr, RES_IRI)
         assert len(val_g) == 3
         bn = next(val_g.objects(RES_IRI, ONTO.testGeoname))
         assert next(val_g.objects(bn, RDF.type)) == KNORA_API.GeonameValue
         assert next(val_g.objects(bn, KNORA_API.geonameValueAsGeonameCode)) == Literal("1241345", datatype=XSD.string)
 
-    def test_none(self, geoname_value_deserialised_none: GeonameValueDeserialised) -> None:
-        val_g = _make_one_value(geoname_value_deserialised_none, RES_IRI)
-        assert len(val_g) == 3
-        bn = next(val_g.objects(RES_IRI, ONTO.testGeoname))
-        assert next(val_g.objects(bn, RDF.type)) == KNORA_API.GeonameValue
-        assert next(val_g.objects(bn, KNORA_API.geonameValueAsGeonameCode)) == Literal("", datatype=XSD.string)
-
 
 class TestIntValue:
-    def test_corr(self, int_value_deserialised_corr: IntValueDeserialised) -> None:
+    def test_corr(self, int_value_deserialised_corr: ValueInformation) -> None:
         val_g = _make_one_value(int_value_deserialised_corr, RES_IRI)
         assert len(val_g) == 3
         bn = next(val_g.objects(RES_IRI, ONTO.testIntegerSimpleText))
         assert next(val_g.objects(bn, RDF.type)) == KNORA_API.IntValue
         assert next(val_g.objects(bn, KNORA_API.intValueAsInt)) == Literal("1", datatype=XSD.integer)
 
-    def test_none(self, int_value_deserialised_none: IntValueDeserialised) -> None:
-        val_g = _make_one_value(int_value_deserialised_none, RES_IRI)
-        assert len(val_g) == 3
-        bn = next(val_g.objects(RES_IRI, ONTO.testIntegerSimpleText))
-        assert next(val_g.objects(bn, RDF.type)) == KNORA_API.IntValue
-        assert next(val_g.objects(bn, KNORA_API.intValueAsInt)) == Literal("", datatype=XSD.string)
-
 
 class TestLinkValue:
-    def test_corr(self, link_value_deserialised_corr: LinkValueDeserialised) -> None:
+    def test_corr(self, link_value_deserialised_corr: ValueInformation) -> None:
         val_g = _make_one_value(link_value_deserialised_corr, RES_IRI)
         assert len(val_g) == 3
         bn = next(val_g.objects(RES_IRI, ONTO.testHasLinkTo))
         assert next(val_g.objects(bn, RDF.type)) == KNORA_API.LinkValue
         assert next(val_g.objects(bn, API_SHAPES.linkValueHasTargetID)) == DATA["link-id"]
 
-    def test_none(self, link_value_deserialised_none: LinkValueDeserialised) -> None:
+    def test_none(self, link_value_deserialised_none: ValueInformation) -> None:
         val_g = _make_one_value(link_value_deserialised_none, RES_IRI)
         assert len(val_g) == 3
         bn = next(val_g.objects(RES_IRI, ONTO.testHasLinkTo))
@@ -206,57 +151,34 @@ class TestLinkValue:
 
 
 class TestListValue:
-    def test_corr(self, list_value_deserialised_corr: ListValueDeserialised) -> None:
+    def test_corr(self, list_value_deserialised_corr: ValueInformation) -> None:
         val_g = _make_one_value(list_value_deserialised_corr, RES_IRI)
-        assert len(val_g) == 4
+        assert len(val_g) == 3
         bn = next(val_g.objects(RES_IRI, ONTO.testListProp))
         assert next(val_g.objects(bn, RDF.type)) == KNORA_API.ListValue
         assert next(val_g.objects(bn, API_SHAPES.listNodeAsString)) == Literal("n1", datatype=XSD.string)
-        assert next(val_g.objects(bn, API_SHAPES.listNameAsString)) == Literal("firstList", datatype=XSD.string)
-
-    def test_none(self, list_value_deserialised_none: ListValueDeserialised) -> None:
-        val_g = _make_one_value(list_value_deserialised_none, RES_IRI)
-        assert len(val_g) == 4
-        bn = next(val_g.objects(RES_IRI, ONTO.testListProp))
-        assert next(val_g.objects(bn, RDF.type)) == KNORA_API.ListValue
-        assert next(val_g.objects(bn, API_SHAPES.listNodeAsString)) == Literal("", datatype=XSD.string)
-        assert next(val_g.objects(bn, API_SHAPES.listNameAsString)) == Literal("firstList", datatype=XSD.string)
 
 
 class TestSimpleTextValue:
-    def test_corr(self, simple_text_deserialised_corr: SimpleTextDeserialised) -> None:
+    def test_corr(self, simple_text_deserialised_corr: ValueInformation) -> None:
         val_g = _make_one_value(simple_text_deserialised_corr, RES_IRI)
         assert len(val_g) == 3
         bn = next(val_g.objects(RES_IRI, ONTO.testTextarea))
         assert next(val_g.objects(bn, RDF.type)) == KNORA_API.TextValue
         assert next(val_g.objects(bn, KNORA_API.valueAsString)) == Literal("simple text", datatype=XSD.string)
 
-    def test_none(self, simple_text_deserialised_none: SimpleTextDeserialised) -> None:
-        val_g = _make_one_value(simple_text_deserialised_none, RES_IRI)
-        assert len(val_g) == 3
-        bn = next(val_g.objects(RES_IRI, ONTO.testTextarea))
-        assert next(val_g.objects(bn, RDF.type)) == KNORA_API.TextValue
-        assert next(val_g.objects(bn, KNORA_API.valueAsString)) == Literal("", datatype=XSD.string)
-
 
 class TestRichtextValue:
-    def test_corr(self, richtext_deserialised_corr: RichtextDeserialised) -> None:
+    def test_corr(self, richtext_deserialised_corr: ValueInformation) -> None:
         val_g = _make_one_value(richtext_deserialised_corr, RES_IRI)
         assert len(val_g) == 3
         bn = next(val_g.objects(RES_IRI, ONTO.testRichtext))
         assert next(val_g.objects(bn, RDF.type)) == KNORA_API.TextValue
         assert next(val_g.objects(bn, KNORA_API.textValueAsXml)) == Literal("rich text", datatype=XSD.string)
 
-    def test_none(self, richtext_deserialised_none: RichtextDeserialised) -> None:
-        val_g = _make_one_value(richtext_deserialised_none, RES_IRI)
-        assert len(val_g) == 3
-        bn = next(val_g.objects(RES_IRI, ONTO.testRichtext))
-        assert next(val_g.objects(bn, RDF.type)) == KNORA_API.TextValue
-        assert next(val_g.objects(bn, KNORA_API.textValueAsXml)) == Literal("", datatype=XSD.string)
-
 
 class TestTimeValue:
-    def test_corr(self, time_value_deserialised_corr: TimeValueDeserialised) -> None:
+    def test_corr(self, time_value_deserialised_corr: ValueInformation) -> None:
         val_g = _make_one_value(time_value_deserialised_corr, RES_IRI)
         assert len(val_g) == 3
         bn = next(val_g.objects(RES_IRI, ONTO.testTimeValue))
@@ -265,28 +187,14 @@ class TestTimeValue:
             "2019-10-23T13:45:12.01-14:00", datatype=XSD.dateTimeStamp
         )
 
-    def test_none(self, time_value_deserialised_none: TimeValueDeserialised) -> None:
-        val_g = _make_one_value(time_value_deserialised_none, RES_IRI)
-        assert len(val_g) == 3
-        bn = next(val_g.objects(RES_IRI, ONTO.testTimeValue))
-        assert next(val_g.objects(bn, RDF.type)) == KNORA_API.TimeValue
-        assert next(val_g.objects(bn, KNORA_API.timeValueAsTimeStamp)) == Literal("", datatype=XSD.string)
-
 
 class TestUriValue:
-    def test_corr(self, uri_value_deserialised_corr: UriValueDeserialised) -> None:
+    def test_corr(self, uri_value_deserialised_corr: ValueInformation) -> None:
         val_g = _make_one_value(uri_value_deserialised_corr, RES_IRI)
         assert len(val_g) == 3
         bn = next(val_g.objects(RES_IRI, ONTO.testUriValue))
         assert next(val_g.objects(bn, RDF.type)) == KNORA_API.UriValue
         assert next(val_g.objects(bn, KNORA_API.uriValueAsUri)) == Literal("https://dasch.swiss", datatype=XSD.anyURI)
-
-    def test_none(self, uri_value_deserialised_none: UriValueDeserialised) -> None:
-        val_g = _make_one_value(uri_value_deserialised_none, RES_IRI)
-        assert len(val_g) == 3
-        bn = next(val_g.objects(RES_IRI, ONTO.testUriValue))
-        assert next(val_g.objects(bn, RDF.type)) == KNORA_API.UriValue
-        assert next(val_g.objects(bn, KNORA_API.uriValueAsUri)) == Literal("", datatype=XSD.string)
 
 
 class TestTransformFileValue:

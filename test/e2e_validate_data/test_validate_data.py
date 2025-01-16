8eb66224c61ca6f977b39d414bf7636e75fbb590
Nora-Olivia-Ammann <103038637+Nora-Olivia-Ammann@users.noreply.github.com>
2025-01-16 09:39:14 +0100

    add test

--- test/e2e_validate_data/test_validate_data.py
+++ test/e2e_validate_data/test_validate_data.py
@@ -296,6 +296,8 @@ class TestReformatValidationGraph:
         assert not result.unexpected_results
         sorted_problems = sorted(result.problems, key=lambda x: x.res_id)
         expected_info_tuples = [
+            ("comment_on_value_empty", "knora-api:hasComment", "The comment on the value must be a non-empty string"),
+            ("comment_on_value_whitespace", "knora-api:hasComment", "The comment on the value must be a non-empty string"),
             ("empty_label", "rdfs:label", "The label must be a non-empty string"),
             ("empty_text_rich", "onto:testRichtext", "The value must be a non-empty string"),
             ("empty_text_simple", "onto:testTextarea", "The value must be a non-empty string"),

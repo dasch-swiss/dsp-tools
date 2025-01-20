46c395202fd806acad3c4583ff6ebba062f95ce0
Nora-Olivia-Ammann <103038637+Nora-Olivia-Ammann@users.noreply.github.com>
2025-01-20 08:19:38 +0100

    change validation

--- src/dsp_tools/commands/validate_data/api_clients.py
+++ src/dsp_tools/commands/validate_data/api_clients.py
@@ -180,11 +180,8 @@ class ShaclValidator:
         return self._parse_validation_result(response.text)
 
     def _prepare_cardinality_files(self) -> PostFiles:
-        shacl_str = self.rdf_graphs.get_cardinality_shacl_and_onto_str()
-        shacl_file = OneFile(file_name="shacl.ttl", file_content=shacl_str, file_format="text/turtle")
-        data_str = self.rdf_graphs.get_data_str()
-        data_file = OneFile(file_name="data.ttl", file_content=data_str, file_format="text/turtle")
-        return PostFiles([shacl_file, data_file])
+        shacl_graph = self.rdf_graphs.cardinality_shapes + self.rdf_graphs.ontos + self.rdf_graphs.knora_api
+        return self._prepare_validation_files_for_request(self.rdf_graphs.data, shacl_graph)
 
     def _validate_content(self) -> SHACLValidationReport:
         content_files = self._prepare_content_files()
@@ -199,11 +196,17 @@ class ShaclValidator:
         return self._parse_validation_result(response.text)
 
     def _prepare_content_files(self) -> PostFiles:
-        shacl_str = self.rdf_graphs.get_content_shacl_and_onto_str()
+        shacl_graph = self.rdf_graphs.content_shapes + self.rdf_graphs.ontos + self.rdf_graphs.knora_api
+        data_graph = self.rdf_graphs.data + self.rdf_graphs.ontos + self.rdf_graphs.knora_api
+        return self._prepare_validation_files_for_request(data_graph, shacl_graph)
+
+    @staticmethod
+    def _prepare_validation_files_for_request(data_graph: Graph, shacl_graph: Graph) -> PostFiles:
+        shacl_str = shacl_graph.serialize(format="ttl")
         shacl_file = OneFile(file_name="shacl.ttl", file_content=shacl_str, file_format="text/turtle")
-        data_str = self.rdf_graphs.get_data_and_onto_str()
-        data_onto_file = OneFile(file_name="data.ttl", file_content=data_str, file_format="text/turtle")
-        return PostFiles([shacl_file, data_onto_file])
+        data_str = data_graph.serialize(format="ttl")
+        data_file = OneFile(file_name="data.ttl", file_content=data_str, file_format="text/turtle")
+        return PostFiles([shacl_file, data_file])
 
     def _parse_validation_result(self, response_text: str) -> SHACLValidationReport:
         graph = Graph()

70723eaa89cdf3983e5a1ede301d449e16ea9488
Nora-Olivia-Ammann <103038637+Nora-Olivia-Ammann@users.noreply.github.com>
2025-01-29 08:26:58 +0100

    add test

--- test/unittests/cli/test_cli_with_mock.py
+++ test/unittests/cli/test_cli_with_mock.py
@@ -124,6 +124,25 @@ def test_xmlupload_no_iiif(xmlupload: Mock) -> None:
 
 
 @patch("dsp_tools.cli.call_action.xmlupload")
+def test_xmlupload_no_validation(xmlupload: Mock) -> None:
+    file = "filename.xml"
+    args = f"xmlupload {file} --no-validation".split()
+    creds = ServerCredentials(
+        server="http://0.0.0.0:3333",
+        user="root@example.com",
+        password="test",
+        dsp_ingest_url="http://0.0.0.0:3340",
+    )
+    entry_point.run(args)
+    xmlupload.assert_called_once_with(
+        input_file=Path(file),
+        creds=creds,
+        imgdir=".",
+        config=UploadConfig(skip_iiif_validation=False, interrupt_after=None, no_validation=True),
+    )
+
+
+@patch("dsp_tools.cli.call_action.xmlupload")
 def test_xmlupload_interrupt_after(xmlupload: Mock) -> None:
     file = "filename.xml"
     args = f"xmlupload --interrupt-after=1 {file}".split()

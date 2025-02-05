ae5411f6799d31585654038f92ad95d773812510
Nora-Olivia-Ammann <103038637+Nora-Olivia-Ammann@users.noreply.github.com>
2025-02-05 16:26:53 +0100

    Commit all changes

--- src/dsp_tools/commands/validate_data/get_user_validation_message.py
+++ src/dsp_tools/commands/validate_data/get_user_validation_message.py
@@ -11,7 +11,7 @@ def _get_message_details_dict(problem: InputProblem) -> dict[str, str]:
         "Resource Type": problem.res_type,
         "Property": problem.prop_name,
         "Problem": problem.message,
-        "Your Input": problem.actual_input,
+        "Your Input": _shorten_input(problem.actual_input),
         "Input Type": problem.actual_input_type,
         "Expected": problem.expected,
     }
@@ -19,4 +19,19 @@ def _get_message_details_dict(problem: InputProblem) -> dict[str, str]:
 
 
 def _get_message_detail_str(problem: InputProblem) -> str:
-    pass
+    msg = []
+    if problem.message:
+        msg.append(problem.message)
+    if problem.actual_input:
+        msg.append(f"Your input: '{_shorten_input(problem.actual_input)}'")
+    if problem.actual_input_type:
+        msg.append(f"Actual input type: '{problem.actual_input_type}'")
+    if problem.expected:
+        msg.append(f"Expected: '{problem.expected}'")
+    return " | ".join(msg)
+
+
+def _shorten_input(user_input: str | None) -> str | None:
+    if not user_input or len(user_input) < 15:
+        return user_input
+    return f"{user_input[:15]}[...]"

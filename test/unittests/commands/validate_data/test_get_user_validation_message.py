9b6a180158c914d72ffe75a5c960bd8354dd8a45
Nora-Olivia-Ammann <103038637+Nora-Olivia-Ammann@users.noreply.github.com>
2025-02-06 08:50:04 +0100

    Commit all changes

--- test/unittests/commands/validate_data/test_get_user_validation_message.py
+++ test/unittests/commands/validate_data/test_get_user_validation_message.py
@@ -7,10 +7,10 @@ from dsp_tools.commands.validate_data.models.input_problems import ProblemType
 @pytest.fixture
 def generic_problem() -> InputProblem:
     return InputProblem(
+        problem_type=ProblemType.GENERIC,
         res_id="res_id",
         res_type="onto:Class",
-        prop_name="onto:hasProp",
-        problem_type=ProblemType.GENERIC,
+        prop_name="onto:hasGenericProblem",
         message="This is a generic problem.",
     )
 
@@ -18,63 +18,63 @@ def generic_problem() -> InputProblem:
 @pytest.fixture
 def file_value() -> InputProblem:
     return InputProblem(
+        problem_type=ProblemType.FILE_VALUE,
         res_id="res_id",
         res_type="onto:Class",
-        prop_name="onto:hasProp",
-        problem_type=ProblemType.FILE_VALUE,
-        expected="expected message",
+        prop_name="onto:hasFileProblem",
+        expected="A MovingImageRepresentation requires a file with the extension 'mp4'.",
     )
 
 
 @pytest.fixture
 def max_card() -> InputProblem:
     return InputProblem(
+        problem_type=ProblemType.MAX_CARD,
         res_id="res_id",
         res_type="onto:Class",
-        prop_name="onto:hasProp",
-        problem_type=ProblemType.MAX_CARD,
-        expected="Expected Cardinality: 1",
+        prop_name="onto:hasMaxCardProblem",
+        expected="1",
     )
 
 
 @pytest.fixture
 def min_card() -> InputProblem:
     return InputProblem(
+        problem_type=ProblemType.MIN_CARD,
         res_id="res_id",
         res_type="onto:Class",
-        prop_name="onto:hasProp",
-        problem_type=ProblemType.MIN_CARD,
-        expected="Expected Cardinality: 1-n",
+        prop_name="onto:hasMinCardProblem",
+        expected="1-n",
     )
 
 
 @pytest.fixture
 def non_existing_card() -> InputProblem:
     return InputProblem(
+        problem_type=ProblemType.NON_EXISTING_CARD,
         res_id="res_id",
         res_type="onto:Class",
         prop_name="onto:hasProp",
-        problem_type=ProblemType.NON_EXISTING_CARD,
     )
 
 
 @pytest.fixture
 def file_value_prohibited() -> InputProblem:
     return InputProblem(
+        problem_type=ProblemType.FILE_VALUE_PROHIBITED,
         res_id="res_id",
         res_type="onto:Class",
-        prop_name="onto:hasProp",
-        problem_type=ProblemType.FILE_VALUE_PROHIBITED,
+        prop_name="bitstream / iiif-uri",
     )
 
 
 @pytest.fixture
 def value_type_mismatch() -> InputProblem:
     return InputProblem(
+        problem_type=ProblemType.VALUE_TYPE_MISMATCH,
         res_id="res_id",
         res_type="onto:Class",
         prop_name="onto:hasProp",
-        problem_type=ProblemType.VALUE_TYPE_MISMATCH,
         actual_input_type="LinkValue",
         expected="ListValue",
     )
@@ -83,11 +83,11 @@ def value_type_mismatch() -> InputProblem:
 @pytest.fixture
 def input_regex() -> InputProblem:
     return InputProblem(
+        problem_type=ProblemType.INPUT_REGEX,
         res_id="res_id",
         res_type="onto:Class",
         prop_name="onto:hasProp",
-        problem_type=ProblemType.INPUT_REGEX,
-        actual_input="Content",
+        actual_input="wrong input",
         expected="Expected format information",
     )
 
@@ -95,10 +95,10 @@ def input_regex() -> InputProblem:
 @pytest.fixture
 def link_target_mismatch() -> InputProblem:
     return InputProblem(
+        problem_type=ProblemType.LINK_TARGET_TYPE_MISMATCH,
         res_id="res_id",
         res_type="onto:Class",
         prop_name="onto:hasProp",
-        problem_type=ProblemType.LINK_TARGET_TYPE_MISMATCH,
         actual_input="link_target_id",
         actual_input_type="onto:Class",
         expected="onto:File",
@@ -108,10 +108,10 @@ def link_target_mismatch() -> InputProblem:
 @pytest.fixture
 def inexistent_linked_resource() -> InputProblem:
     return InputProblem(
+        problem_type=ProblemType.INEXISTENT_LINKED_RESOURCE,
         res_id="res_id",
         res_type="onto:Class",
         prop_name="onto:hasProp",
-        problem_type=ProblemType.INEXISTENT_LINKED_RESOURCE,
         actual_input="link_target_id",
     )
 
@@ -119,9 +119,9 @@ def inexistent_linked_resource() -> InputProblem:
 @pytest.fixture
 def duplicate_value() -> InputProblem:
     return InputProblem(
+        problem_type=ProblemType.DUPLICATE_VALUE,
         res_id="res_id",
         res_type="onto:Class",
         prop_name="onto:hasProp",
-        problem_type=ProblemType.DUPLICATE_VALUE,
-        actual_input="Duplicate Iinput",
+        actual_input="Duplicate input",
     )

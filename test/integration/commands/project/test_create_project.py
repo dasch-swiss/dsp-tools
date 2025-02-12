72268b84986ee0f5607df7049a2ca8e2a5c2befa
Nora-Olivia-Ammann <103038637+Nora-Olivia-Ammann@users.noreply.github.com>
2025-02-12 13:40:30 +0100

    Commit all changes

--- test/integration/commands/project/test_create_project.py
+++ test/integration/commands/project/test_create_project.py
@@ -4,6 +4,8 @@ from typing import Any
 import pytest
 import regex
 
+from dsp_tools.commands.project.create.project_create_ontologies import _sort_prop_classes
+from dsp_tools.commands.project.create.project_create_ontologies import _sort_resources
 from dsp_tools.commands.project.create.project_validate import _check_for_duplicate_names
 from dsp_tools.commands.project.create.project_validate import _check_for_undefined_cardinalities
 from dsp_tools.commands.project.create.project_validate import _check_for_undefined_super_property
@@ -147,3 +149,25 @@ def test_check_for_duplicate_properties() -> None:
 
 if __name__ == "__main__":
     pytest.main([__file__])
+
+
+def test_sort_resources(tp_systematic_ontology: dict[str, Any]) -> None:
+    onto_name: str = tp_systematic_ontology["name"]
+    unsorted_resources: list[dict[str, Any]] = tp_systematic_ontology["resources"]
+    sorted_resources = _sort_resources(unsorted_resources, onto_name)
+
+    unsorted_resources = sorted(unsorted_resources, key=lambda a: str(a["name"]))
+    sorted_resources = sorted(sorted_resources, key=lambda a: str(a["name"]))
+
+    assert unsorted_resources == sorted_resources
+
+
+def test_sort_prop_classes(tp_systematic_ontology: dict[str, Any]) -> None:
+    onto_name: str = tp_systematic_ontology["name"]
+    unsorted_props: list[dict[str, Any]] = tp_systematic_ontology["resources"]
+    sorted_props = _sort_prop_classes(unsorted_props, onto_name)
+
+    unsorted_props = sorted(unsorted_props, key=lambda a: str(a["name"]))
+    sorted_props = sorted(sorted_props, key=lambda a: str(a["name"]))
+
+    assert unsorted_props == sorted_props

1aa4c8b26d95c67234b2f4709465a4b6e77b69e2
Nora-Olivia-Ammann <103038637+Nora-Olivia-Ammann@users.noreply.github.com>
2025-02-12 13:41:11 +0100

    Commit all changes

--- test/integration/commands/project/test_create_project.py
+++ test/integration/commands/project/test_create_project.py
@@ -39,6 +39,28 @@ def tp_circular_ontology() -> dict[str, Any]:
     return tp_circular_ontology
 
 
+def test_sort_resources(tp_systematic_ontology: dict[str, Any]) -> None:
+    onto_name: str = tp_systematic_ontology["name"]
+    unsorted_resources: list[dict[str, Any]] = tp_systematic_ontology["resources"]
+    sorted_resources = _sort_resources(unsorted_resources, onto_name)
+
+    unsorted_resources = sorted(unsorted_resources, key=lambda a: str(a["name"]))
+    sorted_resources = sorted(sorted_resources, key=lambda a: str(a["name"]))
+
+    assert unsorted_resources == sorted_resources
+
+
+def test_sort_prop_classes(tp_systematic_ontology: dict[str, Any]) -> None:
+    onto_name: str = tp_systematic_ontology["name"]
+    unsorted_props: list[dict[str, Any]] = tp_systematic_ontology["resources"]
+    sorted_props = _sort_prop_classes(unsorted_props, onto_name)
+
+    unsorted_props = sorted(unsorted_props, key=lambda a: str(a["name"]))
+    sorted_props = sorted(sorted_props, key=lambda a: str(a["name"]))
+
+    assert unsorted_props == sorted_props
+
+
 def test_validate_project(tp_systematic: dict[str, Any], tp_circular_ontology: dict[str, Any]) -> None:
     assert validate_project(tp_systematic) is True
 
@@ -149,25 +171,3 @@ def test_check_for_duplicate_properties() -> None:
 
 if __name__ == "__main__":
     pytest.main([__file__])
-
-
-def test_sort_resources(tp_systematic_ontology: dict[str, Any]) -> None:
-    onto_name: str = tp_systematic_ontology["name"]
-    unsorted_resources: list[dict[str, Any]] = tp_systematic_ontology["resources"]
-    sorted_resources = _sort_resources(unsorted_resources, onto_name)
-
-    unsorted_resources = sorted(unsorted_resources, key=lambda a: str(a["name"]))
-    sorted_resources = sorted(sorted_resources, key=lambda a: str(a["name"]))
-
-    assert unsorted_resources == sorted_resources
-
-
-def test_sort_prop_classes(tp_systematic_ontology: dict[str, Any]) -> None:
-    onto_name: str = tp_systematic_ontology["name"]
-    unsorted_props: list[dict[str, Any]] = tp_systematic_ontology["resources"]
-    sorted_props = _sort_prop_classes(unsorted_props, onto_name)
-
-    unsorted_props = sorted(unsorted_props, key=lambda a: str(a["name"]))
-    sorted_props = sorted(sorted_props, key=lambda a: str(a["name"]))
-
-    assert unsorted_props == sorted_props

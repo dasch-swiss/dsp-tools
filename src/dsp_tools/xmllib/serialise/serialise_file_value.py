51b04144f8d3b0dae298cf982ff8bcebcb8773f4
Nora-Olivia-Ammann <103038637+Nora-Olivia-Ammann@users.noreply.github.com>
2025-02-18 10:14:55 +0100

    refactor(xmllib): move file value serialisation out of the classes (#1434)

---
+++ src/dsp_tools/xmllib/serialise/serialise_file_value.py
@@ -0,0 +1,44 @@
+from lxml import etree
+
+from dsp_tools.models.exceptions import BaseError
+from dsp_tools.xmllib.constants import DASCH_SCHEMA
+from dsp_tools.xmllib.constants import XML_NAMESPACE_MAP
+from dsp_tools.xmllib.models.config_options import Permissions
+from dsp_tools.xmllib.models.file_values import AbstractFileValue
+from dsp_tools.xmllib.models.file_values import FileValue
+from dsp_tools.xmllib.models.file_values import IIIFUri
+from dsp_tools.xmllib.value_checkers import is_string_like
+
+
+def serialise_file_value(file_value: AbstractFileValue) -> etree._Element:
+    """
+    Serialise the file value of a resource.
+
+    Args:
+        file_value: File Value
+
+    Returns:
+        File Value as etree
+
+    Raises:
+        BaseError: in case of an unknown class
+    """
+    if isinstance(file_value, FileValue):
+        return _serialise_file_value(file_value, "bitstream")
+    elif isinstance(file_value, IIIFUri):
+        return _serialise_file_value(file_value, "iiif-uri")
+    else:
+        raise BaseError(
+            f"file_value must be either a FileValue or a IIIFUri, but you provided {file_value.__class__.__name__}"
+        )
+
+
+def _serialise_file_value(value: AbstractFileValue, tag_name: str) -> etree._Element:
+    attribs = {}
+    if value.permissions != Permissions.PROJECT_SPECIFIC_PERMISSIONS:
+        attribs["permissions"] = value.permissions.value
+    if is_string_like(value.comment):
+        attribs["comment"] = str(value.comment)
+    ele = etree.Element(f"{DASCH_SCHEMA}{tag_name}", attrib=attribs, nsmap=XML_NAMESPACE_MAP)
+    ele.text = str(value.value)
+    return ele

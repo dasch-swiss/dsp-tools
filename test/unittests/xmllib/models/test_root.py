43d5c047cda5eb09d7f3c71c627d2c58d0d8e731
Nora-Olivia-Ammann <103038637+Nora-Olivia-Ammann@users.noreply.github.com>
2025-02-19 10:12:31 +0100

    Commit all changes

--- test/unittests/xmllib/models/test_root.py
+++ test/unittests/xmllib/models/test_root.py
@@ -36,6 +36,7 @@ def test_root_add_resources() -> None:
 def test_make_authorship_lookup() -> None:
     res1 = Resource.create_new("id1", ":Restype", "label").add_file("file.jpg", "lic", "copy", ["auth", "auth1"])
     res2 = Resource.create_new("id2", ":Restype", "label").add_file("file.jpg", "lic", "copy", ["auth2"])
-    result = _make_authorship_lookup([res1, res2])
+    region_res = RegionResource.create_new("regionID", "label", "id1")
+    result = _make_authorship_lookup([res1, res2, region_res])
     assert set(result.lookup.keys()) == {("auth", "auth1"), tuple(["auth2"])}
     assert set(result.lookup.values()) == {"authorship_1", "authorship_2"}

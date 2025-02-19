a6dc30dc446c414164a0c5377a12d76fe3db946a
Nora-Olivia-Ammann <103038637+Nora-Olivia-Ammann@users.noreply.github.com>
2025-02-19 10:11:15 +0100

    add test for lookup

--- test/unittests/xmllib/models/test_root.py
+++ test/unittests/xmllib/models/test_root.py
@@ -1,6 +1,7 @@
 from dsp_tools.xmllib.models.dsp_base_resources import RegionResource
 from dsp_tools.xmllib.models.res import Resource
 from dsp_tools.xmllib.models.root import XMLRoot
+from dsp_tools.xmllib.models.root import _make_authorship_lookup
 
 
 def test_root_add_resources() -> None:
@@ -30,3 +31,11 @@ def test_root_add_resources() -> None:
     assert len(region) == 1
     general_resources = list(serialised.iterdescendants(tag="{https://dasch.swiss/schema}resource"))
     assert len(general_resources) == 2
+
+
+def test_make_authorship_lookup() -> None:
+    res1 = Resource.create_new("id1", ":Restype", "label").add_file("file.jpg", "lic", "copy", ["auth", "auth1"])
+    res2 = Resource.create_new("id2", ":Restype", "label").add_file("file.jpg", "lic", "copy", ["auth2"])
+    result = _make_authorship_lookup([res1, res2])
+    assert set(result.lookup.keys()) == {("auth", "auth1"), tuple(["auth2"])}
+    assert set(result.lookup.values()) == {"authorship_1", "authorship_2"}

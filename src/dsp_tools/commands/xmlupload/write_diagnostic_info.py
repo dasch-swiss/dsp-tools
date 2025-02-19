dd079fff12c1c22db58becf5937b8c449bca9fd9
Nora-Olivia-Ammann <103038637+Nora-Olivia-Ammann@users.noreply.github.com>
2025-02-19 09:00:44 +0100

    feat(xmlupload): change name of id2iri mapping file (DEV-4601) (#1438)

--- src/dsp_tools/commands/xmlupload/write_diagnostic_info.py
+++ src/dsp_tools/commands/xmlupload/write_diagnostic_info.py
@@ -10,12 +10,13 @@ from dsp_tools.commands.xmlupload.upload_config import DiagnosticsConfig
 
 def write_id2iri_mapping(
     id2iri_mapping: dict[str, str],
+    shortcode: str,
     diagnostics: DiagnosticsConfig,
 ) -> None:
     """Writes the mapping of internal IDs to IRIs to a file."""
     timestamp = datetime.now().strftime("%Y-%m-%d_%H%M%S")
     servername = diagnostics.server_as_foldername
-    id2iri_filename = f"{timestamp}_id2iri_mapping_{servername}.json"
+    id2iri_filename = f"id2iri_{shortcode}_{servername}_{timestamp}.json"
     with open(id2iri_filename, "x", encoding="utf-8") as f:
         json.dump(id2iri_mapping, f, ensure_ascii=False, indent=4)
         print(f"{datetime.now()}: The mapping of internal IDs to IRIs was written to {id2iri_filename}")

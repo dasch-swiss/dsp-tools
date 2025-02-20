d9d9d5f3914d2449314de9d6c76c75d92dddae65
Nora-Olivia-Ammann <103038637+Nora-Olivia-Ammann@users.noreply.github.com>
2025-02-20 17:01:58 +0100

    remove

--- src/dsp_tools/utils/xml_parsing/xml_schema_validation.py
+++ src/dsp_tools/utils/xml_parsing/xml_schema_validation.py
@@ -2,8 +2,6 @@ from __future__ import annotations
 
 import importlib.resources
 import warnings
-from datetime import datetime
-from pathlib import Path
 
 import regex
 from loguru import logger
@@ -11,9 +9,6 @@ from lxml import etree
 
 from dsp_tools.models.custom_warnings import DspToolsUserWarning
 from dsp_tools.models.exceptions import InputError
-from dsp_tools.utils.xml_parsing.models.text_value_validation import InconsistentTextValueEncodings
-from dsp_tools.utils.xml_parsing.models.text_value_validation import TextValueData
-from dsp_tools.utils.xml_parsing.parse_and_transform_file import remove_namespaces_from_xml
 
 separator = "\n    "
 list_separator = "\n    - "
@@ -34,12 +29,8 @@ def validate_xml_with_schema(xml: etree._Element) -> bool:
     Returns:
         True if the XML file is valid
     """
-    xml_no_namespace = remove_namespaces_from_xml(xml)
-
-    problems = []
-
-    problems.extend(_validate_xml_against_schema(xml))
-    problems.extend(_validate_xml_contents(xml_no_namespace))
+    _warn_user_about_tags_in_simpletext(xml)
+    problems = _validate_xml_against_schema(xml)
 
     if problems:
         err_msg = grand_separator.join(problems)
@@ -62,15 +53,7 @@ def _validate_xml_against_schema(data_xml: etree._Element) -> list[str]:
     return []
 
 
-def _validate_xml_contents(xml_no_namespace: etree._Element) -> list[str]:
-    problems = []
-    _find_xml_tags_in_simple_text_elements(xml_no_namespace)
-    problems.extend(_find_mixed_encodings_in_one_text_prop(xml_no_namespace))
-    _check_for_deprecated_syntax(xml_no_namespace)
-    return problems
-
-
-def _find_xml_tags_in_simple_text_elements(xml_no_namespace: etree._Element) -> None:
+def _warn_user_about_tags_in_simpletext(xml_no_namespace: etree._Element) -> None:
     """
     Checks if there are angular brackets in simple text.
     It is possible that the user mistakenly added XML tags into a simple text field.
@@ -100,75 +83,3 @@ def _find_xml_tags_in_simple_text_elements(xml_no_namespace: etree._Element) -> 
             f"{list_separator.join(resources_with_potential_xml_tags)}"
         )
         warnings.warn(DspToolsUserWarning(err_msg))
-
-
-def _find_mixed_encodings_in_one_text_prop(xml_no_namespace: etree._Element) -> list[str]:
-    problems = check_if_only_one_encoding_is_used_per_prop_in_root(xml_no_namespace)
-    if not problems:
-        return []
-    msg, df = InconsistentTextValueEncodings(problems).execute_problem_protocol()
-    if df is not None:
-        csv_path = Path(f"XML_syntax_errors_{datetime.now().strftime('%Y-%m-%d_%H%M%S')}.csv")
-        msg = f"\nAll the problems are listed in the file: '{csv_path.absolute()}'{msg}"
-        df.to_csv(csv_path)
-    return [msg]
-
-
-def _check_for_deprecated_syntax(data_xml: etree._Element) -> None:
-    pass
-
-
-def check_if_only_one_encoding_is_used_per_prop_in_root(
-    root: etree._Element,
-) -> list[TextValueData]:
-    """
-    Check if all the encodings in the `<text>` elements are consistent within one `<text-prop>`
-
-    This is correct:
-    ```
-    <text-prop name=":hasSimpleText">
-        <text encoding="utf8">Text 1</text>
-        <text encoding="utf8">Text 2</text>
-    </text-prop>
-    ```
-
-    This is wrong:
-    ```
-    <text-prop name=":hasSimpleText">
-        <text encoding="utf8">Text 1</text>
-        <text encoding="xml">Text 2</text>
-    </text-prop>
-    ```
-
-    Args:
-        root: root of the data xml document
-
-    Returns:
-          A list of all the inconsistent `<text-props>`
-    """
-    text_props = _get_all_ids_and_encodings_from_root(root)
-    return _find_all_text_props_with_multiple_encodings(text_props)
-
-
-def _get_all_ids_and_encodings_from_root(
-    root: etree._Element,
-) -> list[TextValueData]:
-    res_list = []
-    for res_input in root.iterchildren(tag="resource"):
-        res_list.extend(_get_encodings_from_one_resource(res_input))
-    return res_list
-
-
-def _get_encodings_from_one_resource(resource: etree._Element) -> list[TextValueData]:
-    res_id = resource.attrib["id"]
-    return [_get_encodings_from_one_property(res_id, child) for child in list(resource.iterchildren(tag="text-prop"))]
-
-
-def _get_encodings_from_one_property(res_id: str, prop: etree._Element) -> TextValueData:
-    prop_name = prop.attrib["name"]
-    encodings = {x.attrib["encoding"] for x in prop.iterchildren()}
-    return TextValueData(res_id, prop_name, encodings)
-
-
-def _find_all_text_props_with_multiple_encodings(text_props: list[TextValueData]) -> list[TextValueData]:
-    return [x for x in text_props if len(x.encoding) != 1]

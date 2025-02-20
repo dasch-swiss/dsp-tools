87c4b1053ac56dced27683cdc66d99909b8868f6
Nora-Olivia-Ammann <103038637+Nora-Olivia-Ammann@users.noreply.github.com>
2025-02-20 13:01:49 +0100

    move get xmlproject

--- test/integration/commands/validate_data/xml_fixtures.py
+++ test/integration/commands/validate_data/xml_fixtures.py
@@ -3,9 +3,9 @@ from pathlib import Path
 import pytest
 from lxml import etree
 
-from dsp_tools.commands.validate_data.validate_data import _parse_and_clean_file
+from dsp_tools.utils.xml_parsing.get_xml_project import get_xml_project
 
 
 @pytest.fixture
 def data_xml() -> etree._Element:
-    return _parse_and_clean_file(Path("testdata/validate-data/generic/minimal_correct.xml"), "http://0.0.0.0:3333").root
+    return get_xml_project(Path("testdata/validate-data/generic/minimal_correct.xml"), "http://0.0.0.0:3333").root

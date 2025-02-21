4a01fd1a059fb04e7b335a87f8c695785d5ba624
Nora-Olivia-Ammann <103038637+Nora-Olivia-Ammann@users.noreply.github.com>
2025-02-21 11:48:14 +0100

    Update data_deserialised.py

--- src/dsp_tools/utils/xml_parsing/models/data_deserialised.py
+++ src/dsp_tools/utils/xml_parsing/models/data_deserialised.py
@@ -31,13 +31,20 @@ class ResourceDeserialised:
             For example, the label of a resource is here.
         values: a list of user-facing values (e.g. BooleanValue)
         asset_value: an asset value (all FileValues excluding the IIIFUri)
+        migration_metadata: Metadata used for salsah migration
     """
 
     res_id: str
     property_objects: list[PropertyObject]
     values: list[ValueInformation]
     asset_value: ValueInformation | None
+    migration_metadata: MigrationMetadata | None = None
 
+@dataclass
+class MigrationMetadata:
+    iri: str | None
+    ark: str | None
+    creation_date: str | None
 
 @dataclass
 class PropertyObject:

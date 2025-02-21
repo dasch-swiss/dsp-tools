6b648752481778cccc70062ad7f6cc95d6e7e0f8
Nora-Olivia-Ammann <103038637+Nora-Olivia-Ammann@users.noreply.github.com>
2025-02-21 09:22:32 +0100

    Commit all changes

--- src/dsp_tools/utils/xml_parsing/get_xml_project.py
+++ src/dsp_tools/utils/xml_parsing/get_xml_project.py
@@ -9,8 +9,8 @@ from dsp_tools.commands.validate_data.constants import KNORA_API_STR
 from dsp_tools.models.exceptions import InputError
 from dsp_tools.utils.xml_parsing.combinations import transform_special_tags_make_localname
 from dsp_tools.utils.xml_parsing.models.data_deserialised import XMLProject
+from dsp_tools.utils.xml_parsing.parse_and_clean import make_localnames_and_remove_comments
 from dsp_tools.utils.xml_parsing.parse_and_clean import parse_xml_file
-from dsp_tools.utils.xml_parsing.xx_parse_and_transform_file import remove_comments_from_element_tree
 from dsp_tools.utils.xml_parsing.xx_xml_schema_validation import validate_xml_with_schema
 
 
@@ -26,7 +26,7 @@ def get_xml_project(file: Path, api_url: str) -> XMLProject:
         Project Information and cleaned root
     """
     root = parse_xml_file(file)
-    root = remove_comments_from_element_tree(root)
+    root = make_localnames_and_remove_comments(root)
     validate_xml_with_schema(root)
     root = transform_special_tags_make_localname(root)
     return _replace_namespaces(root, api_url)

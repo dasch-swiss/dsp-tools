c86d95e4c15e3b12ad966d360ecf4987aa69e32e
Johannes Nussbaum <johannes.nussbaum@dasch.swiss>
2025-02-22 14:41:01 +0100

    fix formatting

--- test/unittests/xmllib/test_helpers.py
+++ test/unittests/xmllib/test_helpers.py
@@ -91,7 +91,7 @@ class TestFindDate:
         """template: 15 Jan 1927 | 15 January 1927"""
         assert find_date_in_string("x 15 Jan 1927 x") == "GREGORIAN:CE:1927-01-15:CE:1927-01-15"
         assert find_date_in_string("x 15 January 1927 x") == "GREGORIAN:CE:1927-01-15:CE:1927-01-15"
-    
+
     def test_find_date_in_string_iso(self) -> None:
         """template: 2021-01-01"""
         assert find_date_in_string("x 1492-10-12, x") == "GREGORIAN:CE:1492-10-12:CE:1492-10-12"

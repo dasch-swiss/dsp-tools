b36a4efe419b796e09f205b197e780a2b70e6393
Nora-Olivia-Ammann <103038637+Nora-Olivia-Ammann@users.noreply.github.com>
2025-02-24 10:39:39 +0100

    Commit all changes

--- src/dsp_tools/commands/xmlupload/stash/create_info_for_graph_from_deserialised_data.py
+++ src/dsp_tools/commands/xmlupload/stash/create_info_for_graph_from_deserialised_data.py
@@ -1,21 +1,22 @@
-
-from dsp_tools.commands.xmlupload.stash.graph_models import ResptrLink
-from dsp_tools.commands.xmlupload.stash.graph_models import XMLLink, InfoForGraph
-from dsp_tools.utils.iri_util import is_resource_iri
+from dsp_tools.commands.xmlupload.stash.graph_models import InfoForGraph
+from dsp_tools.commands.xmlupload.stash.graph_models import LinkValueLink
+from dsp_tools.commands.xmlupload.stash.graph_models import StandOffLink
 from dsp_tools.utils.xml_parsing.models.data_deserialised import DataDeserialised
-from dsp_tools.utils.xml_parsing.models.data_deserialised import KnoraValueType
-from dsp_tools.utils.xml_parsing.models.data_deserialised import MigrationMetadata
-from dsp_tools.utils.xml_parsing.models.data_deserialised import PropertyObject
 from dsp_tools.utils.xml_parsing.models.data_deserialised import ResourceDeserialised
-from dsp_tools.utils.xml_parsing.models.data_deserialised import TripleObjectType
-from dsp_tools.utils.xml_parsing.models.data_deserialised import TriplePropertyType
 from dsp_tools.utils.xml_parsing.models.data_deserialised import ValueInformation
 
 
 def create_info_for_graph_from_data(data: DataDeserialised) -> InfoForGraph:
+    """Extracts information to create the graph to analyse the circular references."""
+
+
+def _process_one_resource(resource: ResourceDeserialised) -> tuple[list[LinkValueLink], list[StandOffLink]]:
     pass
 
 
-def _process_one_resource(resource: ResourceDeserialised) -> list[XMLLink]:
+def _process_richtext_value(value: ValueInformation) -> list[StandOffLink]:
     pass
 
+
+def _process_link_value(value: ValueInformation) -> list[LinkValueLink]:
+    pass
---
+++ test/unittests/commands/xmlupload/stash/test_create_info_for_graph_from_deserialised_data.py
@@ -0,0 +1,41 @@
+import pytest
+
+
+def test_create_info_for_graph_from_data() -> None:
+    pass
+
+
+def test_process_one_resource_no_links() -> None:
+    pass
+
+
+def test_process_one_resource_stand_off() -> None:
+    pass
+
+
+def test_process_one_resource_link_values() -> None:
+    pass
+
+
+def test_process_one_resource_both_links() -> None:
+    pass
+
+
+def test_process_richtext_value_no_links() -> None:
+    pass
+
+
+def test_process_richtext_value_with_links() -> None:
+    pass
+
+
+def test_process_link_value_no_links() -> None:
+    pass
+
+
+def test_process_link_value_with_links() -> None:
+    pass
+
+
+if __name__ == "__main__":
+    pytest.main([__file__])

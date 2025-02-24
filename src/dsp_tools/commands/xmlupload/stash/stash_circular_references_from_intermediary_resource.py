2895502077e4ba29e3836a111eca08d619949068
Nora-Olivia-Ammann <103038637+Nora-Olivia-Ammann@users.noreply.github.com>
2025-02-24 16:19:41 +0100

    Commit all changes

--- src/dsp_tools/commands/xmlupload/stash/stash_circular_references_from_intermediary_resource.py
+++ src/dsp_tools/commands/xmlupload/stash/stash_circular_references_from_intermediary_resource.py
@@ -69,8 +69,8 @@ def _stash_link(
 
 
 def _stash_standoff(value: IntermediaryRichtext, res_id: str, res_type: str) -> StandoffStashItem:
-    # Replace the content with the UUID
     actual_text = value.value
+    # Replace the content with the UUID
     value.value = FormattedTextValue(value.value_uuid)
     # It is not necessary to add the permissions to the StandoffStashItem because it is an update request
     # If no new permissions are given during that request, the permissions of the previous value are taken

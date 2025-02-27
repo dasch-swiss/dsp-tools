de942a53c84db167f81be852aca50aa25e2e1819
Nora-Olivia-Ammann <103038637+Nora-Olivia-Ammann@users.noreply.github.com>
2025-02-27 13:25:31 +0100

    Commit all changes

--- test/unittests/utils/test_legal_info_live.py
+++ test/unittests/utils/test_legal_info_live.py
@@ -1,50 +1,50 @@
 # mypy: disable-error-code="method-assign,no-untyped-def"
+from unittest.mock import Mock
+from unittest.mock import patch
 
+import pytest
+
+from dsp_tools.models.exceptions import BadCredentialsError
+from dsp_tools.models.exceptions import BaseError
+from dsp_tools.utils.legal_info_client_live import HTTP_INSUFFICIENT_CREDENTIALS
+from dsp_tools.utils.legal_info_client_live import LegalInfoClientLive
 from dsp_tools.utils.legal_info_client_live import _segment_data
+from dsp_tools.utils.request_utils import RequestParameters
+
+AUTH = Mock()
 
 
-def test_client_ok():
-    pass
+class TestPostCopyrightHolders:
+    @patch("dsp_tools.utils.legal_info_client_live.log_request")
+    @patch("dsp_tools.utils.legal_info_client_live.log_response")
+    def test_client_post_copyright_holders_ok(self, log_request: Mock, log_response: Mock):
+        client = LegalInfoClientLive("http://api.com", "9999", AUTH)
+        expected_response = Mock(status_code=200, ok=True)
+        client._post_and_log_request = Mock(return_value=expected_response)
+        params = RequestParameters(
+            method="POST",
+            url="http://api.com/admin/projects/shortcode/9999/copyright-holders",
+            data={"data": ["1"]},
+            timeout=60,
+        )
 
+        client._post_and_log_request("copyright-holders", ["1"])
+        log_request.assert_called_once_with(params)
+        log_response.assert_called_once_with(expected_response)
 
+    def test_client_post_copyright_holders_insufficient_credentials(self):
+        client = LegalInfoClientLive("http://api.com", "9999", AUTH)
+        expected_response = Mock(status_code=HTTP_INSUFFICIENT_CREDENTIALS, ok=False)
+        client._post_and_log_request = Mock(return_value=expected_response)
+        with pytest.raises(BadCredentialsError):
+            client.post_copyright_holders(["1"])
 
-
-def test_client_timeout():
-    pass
-
-
-
-
-def test_client_insufficient_credentials():
-    pass
-
-
-
-
-def test_client_unknown_status_code():
-    pass
-
-
-
-
-def test_client_post_short():
-    pass
-
-
-
-
-def test_client_post_longer_than_max():
-    pass
-
-
-
-
-
-
-
-
-
-
+    def test_client_post_copyright_holders_unknown_status_code(self):
+        client = LegalInfoClientLive("http://api.com", "9999", AUTH)
+        expected_response = Mock(status_code=10000, ok=False)
+        client._post_and_log_request = Mock(return_value=expected_response)
+        with pytest.raises(BaseError):
+            client.post_copyright_holders(["1"])
 
 
 def test_segment_data_more_than_limit():
@@ -64,5 +64,3 @@ def test_segment_data_less_than_limit():
     result = _segment_data(data_list)
     assert len(result) == 1
     assert len(result[0]) == 4
-
-

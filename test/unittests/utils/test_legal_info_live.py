d8ad72cf67fe585ab473d8cd1a647689112b81d8
Nora-Olivia-Ammann <103038637+Nora-Olivia-Ammann@users.noreply.github.com>
2025-02-27 15:37:35 +0100

    Commit all changes

--- test/unittests/utils/test_legal_info_live.py
+++ test/unittests/utils/test_legal_info_live.py
@@ -11,12 +11,13 @@ from dsp_tools.utils.legal_info_client_live import LegalInfoClientLive
 from dsp_tools.utils.request_utils import RequestParameters
 
 AUTH = Mock()
+AUTH.get_token = Mock(return_value="tkn")
 
 
 class TestPostCopyrightHolders:
     @patch("dsp_tools.utils.legal_info_client_live.log_response")
     @patch("dsp_tools.utils.legal_info_client_live.log_request")
-    def test_client_post_copyright_holders_ok(self, log_request: Mock, log_response: Mock):  # noqa: ARG002
+    def test_log_request(self, log_request: Mock, log_response: Mock):  # noqa: ARG002
         client = LegalInfoClientLive("http://api.com", "9999", AUTH)
         params = RequestParameters(
             method="POST",
@@ -33,6 +34,24 @@ class TestPostCopyrightHolders:
         assert isinstance(log_request_call_params, RequestParameters)
         del log_request_call_params.headers
         assert log_request_call_params == params
+
+    @patch("dsp_tools.utils.legal_info_client_live.log_response")
+    @patch("dsp_tools.utils.legal_info_client_live.log_request")
+    def test_post_ok(self, log_request: Mock, log_response: Mock):  # noqa: ARG002
+        client = LegalInfoClientLive("http://api.com", "9999", AUTH)
+        params = RequestParameters(
+            method="POST",
+            url="http://api.com/admin/projects/shortcode/9999/legal-info/copyright-holders",
+            data={"data": ["1"]},
+            timeout=60,
+            headers={"Content-Type": "application/json", "Authorization": "Bearer tkn"},
+        )
+        with patch("dsp_tools.utils.legal_info_client_live.requests.post") as post_mock:
+            post_mock.return_value = Mock(status_code=200, ok=True)
+            client._post_and_log_request("copyright-holders", ["1"])
+            post_mock.assert_called_once_with(
+                url=params.url, headers=params.headers, data=params.data_serialized, timeout=params.timeout
+            )
 
     def test_client_post_copyright_holders_lacking_permissions(self):
         client = LegalInfoClientLive("http://api.com", "9999", AUTH)

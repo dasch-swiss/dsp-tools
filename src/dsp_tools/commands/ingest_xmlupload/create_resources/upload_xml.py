7e699a25d93f5449e93ba5f18e0b0cee9c11714a
Nora-Olivia-Ammann <103038637+Nora-Olivia-Ammann@users.noreply.github.com>
2025-02-27 16:24:33 +0100

    Commit all changes

--- src/dsp_tools/commands/ingest_xmlupload/create_resources/upload_xml.py
+++ src/dsp_tools/commands/ingest_xmlupload/create_resources/upload_xml.py
@@ -19,9 +19,11 @@ from dsp_tools.commands.xmlupload.project_client import ProjectClientLive
 from dsp_tools.commands.xmlupload.upload_config import UploadConfig
 from dsp_tools.commands.xmlupload.xmlupload import execute_upload
 from dsp_tools.models.exceptions import InputError
+from dsp_tools.utils.authentication_client import AuthenticationClient
 from dsp_tools.utils.authentication_client_live import AuthenticationClientLive
 from dsp_tools.utils.connection import Connection
 from dsp_tools.utils.connection_live import ConnectionLive
+from dsp_tools.utils.legal_info_client_live import LegalInfoClientLive
 
 
 def ingest_xmlupload(
@@ -100,8 +102,9 @@ def _parse_xml_and_replace_filepaths(xml_file: Path) -> tuple[str, etree._Elemen
     return default_ontology, root, shortcode
 
 
-def _get_live_clients(con: Connection, config: UploadConfig) -> UploadClients:
+def _get_live_clients(con: Connection, config: UploadConfig, auth: AuthenticationClient) -> UploadClients:
     ingest_client = BulkIngestedAssetClient()
     project_client = ProjectClientLive(con, config.shortcode)
     list_client = ListClientLive(con, project_client.get_project_iri())
-    return UploadClients(ingest_client, project_client, list_client)
+    legal_info_client = LegalInfoClientLive(con.server, config.shortcode, auth)
+    return UploadClients(ingest_client, project_client, list_client, legal_info_client)

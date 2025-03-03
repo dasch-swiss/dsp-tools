43b78a6dba85b301f661319c2836ffbcbffac9aa
Nora-Olivia-Ammann <103038637+Nora-Olivia-Ammann@users.noreply.github.com>
2025-03-03 15:42:36 +0100

    add shape

--- src/dsp_tools/commands/validate_data/sparql/file_value_shacl.py
+++ src/dsp_tools/commands/validate_data/sparql/file_value_shacl.py
@@ -52,3 +52,32 @@ def _construct_one_representation_shape(onto: Graph, representation_type: str, s
     if results_graph := onto.query(query_s).graph:
         return results_graph
     return Graph()
+
+
+def construct_allowed_licenses_shape() -> Graph:
+    """Create a constraint detailing the allowed licences."""
+    ttl_str = """
+    @prefix sh:         <http://www.w3.org/ns/shacl#> .
+    @prefix knora-api:  <http://api.knora.org/ontology/knora-api/v2#> .
+    @prefix api-shapes: <http://api.knora.org/ontology/knora-api/shapes/v2#> .
+    
+    api-shapes:FileValue_ClassShape
+      a sh:NodeShape ;
+      sh:targetClass knora-api:FileValue ;
+      sh:property [
+          a sh:PropertyShape ;
+          sh:path knora-api:hasLicense ;
+          sh:in (
+                  <http://rdfh.ch/licenses/cc-by-4.0> <http://rdfh.ch/licenses/cc-by-sa-4.0>
+                  <http://rdfh.ch/licenses/cc-by-nc-4.0> <http://rdfh.ch/licenses/cc-by-nc-sa-4.0>
+                  <http://rdfh.ch/licenses/cc-by-nd-4.0> <http://rdfh.ch/licenses/cc-by-nc-nd-4.0>
+                  <http://rdfh.ch/licenses/ai-generated> <http://rdfh.ch/licenses/unknown>
+                  <http://rdfh.ch/licenses/public-domain>
+                ) ;
+          sh:message "You are required to use one of the pre-defined licenses, please consult the documentation for details." ;
+          sh:severity sh:Violation
+                  ] .
+    """
+    g = Graph()
+    g.parse(data=ttl_str, format="turtle")
+    return g

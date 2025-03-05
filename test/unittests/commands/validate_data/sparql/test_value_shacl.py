09fd43f572efa0715345e49875c58cb1d008c9b2
Nora-Olivia-Ammann <103038637+Nora-Olivia-Ammann@users.noreply.github.com>
2025-03-05 10:26:19 +0100

    Commit all changes

--- test/unittests/commands/validate_data/sparql/test_value_shacl.py
+++ test/unittests/commands/validate_data/sparql/test_value_shacl.py
@@ -2,17 +2,21 @@ import pytest
 from rdflib import RDF
 from rdflib import SH
 from rdflib import Graph
+from rdflib import Literal
 from rdflib import URIRef
 
+from dsp_tools.commands.validate_data.constants import KNORA_API_STR
 from dsp_tools.commands.validate_data.models.api_responses import OneList
 from dsp_tools.commands.validate_data.models.api_responses import SHACLListInfo
 from dsp_tools.commands.validate_data.sparql.value_shacl import _add_property_shapes_to_class_shapes
+from dsp_tools.commands.validate_data.sparql.value_shacl import _add_value_type_shapes_to_class_shapes
 from dsp_tools.commands.validate_data.sparql.value_shacl import _construct_link_value_shape
 from dsp_tools.commands.validate_data.sparql.value_shacl import _construct_one_list_node_shape
 from dsp_tools.commands.validate_data.sparql.value_shacl import _construct_one_list_property_shape_with_collection
 from dsp_tools.commands.validate_data.sparql.value_shacl import _construct_one_property_type_shape_based_on_object_type
 from dsp_tools.commands.validate_data.sparql.value_shacl import _construct_one_property_type_text_value
 from test.unittests.commands.validate_data.constants import API_SHAPES
+from test.unittests.commands.validate_data.constants import KNORA_API
 from test.unittests.commands.validate_data.constants import ONTO
 
 
@@ -56,6 +60,15 @@ def test_add_property_shapes_to_class_shapes(card_1: Graph) -> None:
     }
     assert len(res) == 4
     assert set(res.objects(ONTO.ClassMixedCard, SH.property)) == expected_props
+
+
+def test_add_value_type_shapes_to_class_shapes(card_1: Graph) -> None:
+    res = _add_value_type_shapes_to_class_shapes(card_1)
+    prop_bn = next(res.objects(ONTO.ClassMixedCard, SH.property))
+    assert next(res.objects(prop_bn, SH.path)) == ONTO.testBoolean
+    assert next(res.objects(prop_bn, URIRef("http://www.w3.org/ns/shacl#class"))) == KNORA_API.BooleanValue
+    assert next(res.objects(prop_bn, SH.message)) == Literal(f"{KNORA_API_STR}BooleanValue")
+    assert len(res) == 6
 
 
 class TestConstructListNode:

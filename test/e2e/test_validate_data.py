e5b8a09c178cbcea95f359548f3b70223b7b9f3a
Johannes Nussbaum <johannes.nussbaum@dasch.swiss>
2025-03-10 17:08:31 +0100

    format

--- test/e2e/test_validate_data.py
+++ test/e2e/test_validate_data.py
@@ -52,7 +52,6 @@ def _create_projects(creds: ServerCredentials) -> None:
     assert create_project(Path("testdata/validate-data/inheritance/project_inheritance.json"), creds)
     assert create_project(Path("testdata/validate-data/erroneous_ontology/project_erroneous_ontology.json"), creds)
     assert create_project(Path("testdata/json-project/test-project-systematic.json"), creds)
-        
 
 
 @pytest.fixture(scope="module")

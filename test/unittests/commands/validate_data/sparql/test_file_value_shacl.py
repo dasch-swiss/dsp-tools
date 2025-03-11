44d51ebdbb02127081c5408dd311eb1bf78083b8
Nora-Olivia-Ammann <103038637+Nora-Olivia-Ammann@users.noreply.github.com>
2025-03-11 10:31:25 +0100

    Commit all changes

--- test/unittests/commands/validate_data/sparql/test_file_value_shacl.py
+++ test/unittests/commands/validate_data/sparql/test_file_value_shacl.py
@@ -18,14 +18,14 @@ def test_construct_file_value_cardinality(file_value_shacl: Graph) -> None:
 
 
 def test_construct_audio(file_value_shacl: Graph) -> None:
-    result_list = list(file_value_shacl.subjects(SH.property, API_SHAPES.hasAudioFileValue_PropShape))
+    result_list = list(file_value_shacl.subjects(SH.property, API_SHAPES.hasAudioFileValue_Cardinality))
     assert len(result_list) == 1
     moving_image = result_list[0]
     assert moving_image == ONTO.TestAudioRepresentation
 
 
 def test_construct_moving_image(file_value_shacl: Graph) -> None:
-    result_list = list(file_value_shacl.subjects(SH.property, API_SHAPES.hasMovingImageFileValue_PropShape))
+    result_list = list(file_value_shacl.subjects(SH.property, API_SHAPES.hasMovingImageFileValue_Cardinality))
     assert len(result_list) == 1
     moving_image = result_list[0]
     assert moving_image == ONTO.TestMovingImageRepresentation

a521222e9dad7389d757a743ac9d5c668d486791
Johannes Nussbaum <johannes.nussbaum@dasch.swiss>
2025-03-14 14:11:44 +0100

    edit

--- src/dsp_tools/commands/project/create/project_create.py
+++ src/dsp_tools/commands/project/create/project_create.py
@@ -1,5 +1,7 @@
 from typing import Any
 
+from loguru import logger
+
 from dsp_tools.commands.project.models.project_create_client import ProjectCreateClient
 from dsp_tools.commands.project.models.project_definition import ProjectMetadata
 from dsp_tools.models.exceptions import UserError
@@ -19,19 +21,24 @@ def create_project_on_server(
     Returns:
         success status (True if everything went smoothly, False otherwise)
     """
+    project_designation = f"{project_definition.shortcode} {project_definition.shortname}"
     existing_shortcodes = proj_client.get_all_project_shortcodes()
     if project_definition.shortcode in existing_shortcodes:
-        raise UserError(f"A project with the shortcode '{project_definition.shortcode}' already exists on the server.")
+        raise UserError(f"A project with the shortcode/shortname {project_designation} already exists on the server.")
     data: dict[str, Any] = {
         "shortcode": project_definition.shortcode,
         "shortname": project_definition.shortname,
         "longname": project_definition.longname,
         "keywords": project_definition.keywords,
         "selfjoin": False,  # was hardcoded already before
-        "status": True  # was hardcoded already before
+        "status": True,  # was hardcoded already before
     }
     if desc := project_definition.descriptions:
         data["description"] = [{"language": lang, "value": val} for lang, val in desc.items()]
 
-    proj_client.create_project(data)
-    return True
+    if not (success := proj_client.create_project(data)):
+        raise UserError(f"Cannot create project {project_designation} on DSP server")
+    print(f"    Created project {project_designation}")
+    logger.info(f"Created project {project_designation}")
+
+    return success
--- src/dsp_tools/commands/project/models/project_create_client.py
+++ src/dsp_tools/commands/project/models/project_create_client.py
@@ -14,7 +14,10 @@ class ProjectCreateClient:
 
     def get_all_project_shortcodes(self) -> set[str]:
         url = f"{self.auth.server}/admin/projects"
-        headers = {"User-Agent": f"DSP-TOOLS/{version('dsp-tools')}", "Authentication": f"Bearer {self.auth.get_token()}"}
+        headers = {
+            "User-Agent": f"DSP-TOOLS/{version('dsp-tools')}",
+            "Authentication": f"Bearer {self.auth.get_token()}",
+        }
         logger.debug(f"REQUEST: GET {url}")
         response = requests.get(url, headers=headers, timeout=10)
         logger.debug(f"RESPONSE {response.status_code}: {response.text}")
@@ -22,9 +25,13 @@ class ProjectCreateClient:
         shortcodes = [x.get("shortcode") for x in res_json["projects"]]
         return {x for x in shortcodes if x}
 
-    def create_project(self, payload: dict[str, Any]) -> None:
+    def create_project(self, payload: dict[str, Any]) -> bool:
         url = f"{self.auth.server}/admin/projects"
-        headers = {"User-Agent": f"DSP-TOOLS/{version('dsp-tools')}", "Authentication": f"Bearer {self.auth.get_token()}"}
+        headers = {
+            "User-Agent": f"DSP-TOOLS/{version('dsp-tools')}",
+            "Authentication": f"Bearer {self.auth.get_token()}",
+        }
         logger.debug(f"REQUEST: POST {url}. Payload: {payload}")
         response = requests.post(url, headers=headers, data=payload, timeout=10)
         logger.debug(f"RESPONSE {response.status_code}: {response.text}")
+        return response.ok

8dc28685e7fa7a4c070a8fd3165270c9e4056f91
Nora-Olivia-Ammann <103038637+Nora-Olivia-Ammann@users.noreply.github.com>
2025-03-28 15:12:54 +0100

    Commit all changes

--- src/dsp_tools/commands/xmlupload/stash/upload_stashed_xml_texts.py
+++ src/dsp_tools/commands/xmlupload/stash/upload_stashed_xml_texts.py
@@ -6,19 +6,19 @@ from typing import cast
 from urllib.parse import quote_plus
 
 from loguru import logger
+from rdflib import RDF
+from rdflib import Graph
 from rdflib import URIRef
 
 from dsp_tools.clients.connection import Connection
 from dsp_tools.commands.xmlupload.iri_resolver import IriResolver
+from dsp_tools.commands.xmlupload.make_rdf_graph.make_values import make_richtext_value_graph
 from dsp_tools.commands.xmlupload.models.formatted_text_value import FormattedTextValue
 from dsp_tools.commands.xmlupload.models.upload_state import UploadState
 from dsp_tools.commands.xmlupload.stash.stash_models import StandoffStash
 from dsp_tools.commands.xmlupload.stash.stash_models import StandoffStashItem
 from dsp_tools.commands.xmlupload.stash.stash_models import Stash
 from dsp_tools.error.exceptions import BaseError
-
-from dsp_tools.commands.xmlupload.make_rdf_graph.constants import RICHTEXT_PROP_TYPE_INFO
-from dsp_tools.commands.xmlupload.make_rdf_graph.make_values import make_richtext_value_graph
 
 
 def upload_stashed_xml_texts(upload_state: UploadState, con: Connection) -> None:
@@ -136,14 +136,21 @@ def _upload_stash_item(
 
 
 def _create_richtext_resource_for_update(
-    stash_item: StandoffStashItem,
-    res_iri_str: str,
-    value_iri_str: str,
-    iri_resolver: IriResolver) -> dict[str, Any]:
+    stash_item: StandoffStashItem, res_iri_str: str, value_iri_str: str, iri_resolver: IriResolver
+) -> dict[str, Any]:
+    graph = make_richtext_update_graph(stash_item, value_iri_str, res_iri_str, iri_resolver)
+
+
+def make_richtext_update_graph(
+    stash_item: StandoffStashItem, res_iri_str: str, value_iri_str: str, iri_resolver: IriResolver
+) -> Graph:
     res_iri = URIRef(res_iri_str)
     value_iri = URIRef(value_iri_str)
-    val_graph = make_richtext_value_graph(stash_item.value, RICHTEXT_PROP_TYPE_INFO, res_iri, iri_resolver)
-
+    val_graph = make_richtext_value_graph(
+        val=stash_item.value, val_node=value_iri, res_node=res_iri, iri_resolver=iri_resolver
+    )
+    val_graph.add((res_iri, RDF.type, URIRef(stash_item.res_type)))
+    return val_graph
 
 
 def _create_XMLResource_json_object_to_update(

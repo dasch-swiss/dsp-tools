ad81831d0ce3771acb79449ce763385eb80ba163
Nora-Olivia-Ammann <103038637+Nora-Olivia-Ammann@users.noreply.github.com>
2025-04-02 12:29:58 +0200

    Commit all changes

--- test/unittests/utils/xml_parsing/test_get_parsed_resources.py
+++ test/unittests/utils/xml_parsing/test_get_parsed_resources.py
@@ -1,4 +1,5 @@
 # mypy: disable-error-code="method-assign,no-untyped-def"
+from copy import deepcopy
 
 import pytest
 from lxml import etree
@@ -10,9 +11,20 @@ from dsp_tools.utils.xml_parsing.get_parsed_resources import _get_one_absolute_i
 API_URL = "http://url.ch"
 DEFAULT_ONTO_NAMESPACE = f"{API_URL}/ontology/0000/default/v2#"
 
+IRI_LOOKUP = {}
+
 
 @pytest.fixture
-def minimal_root():
+def root_no_resources() -> etree._Element:
+    return etree.fromstring("""
+    <knora shortcode="0000"
+       default-ontology="default">
+    </knora>
+    """)
+
+
+@pytest.fixture
+def minimal_root() -> etree._Element:
     return etree.fromstring("""
     <knora shortcode="0000"
        default-ontology="default">
@@ -28,6 +40,79 @@ def minimal_root():
         </link>
     </knora>
     """)
+
+
+@pytest.fixture
+def resource_no_values() -> etree._Element:
+    return etree.fromstring("""""")
+
+
+@pytest.fixture
+def resource_with_values() -> etree._Element:
+    return etree.fromstring("""""")
+
+
+@pytest.fixture
+def resource_with_file_values() -> etree._Element:
+    return etree.fromstring("""""")
+
+
+@pytest.fixture
+def resource_region() -> etree._Element:
+    return etree.fromstring("""""")
+
+
+@pytest.fixture
+def resource_link() -> etree._Element:
+    return etree.fromstring("""""")
+
+
+@pytest.fixture
+def resource_video_segment() -> etree._Element:
+    return etree.fromstring("""""")
+
+
+@pytest.fixture
+def resource_audio_segment() -> etree._Element:
+    return etree.fromstring("""""")
+
+
+class TestParseResource:
+    def test_empty(self, root_no_resources, resource_no_values):
+        root = deepcopy(root_no_resources)
+        root.append(resource_no_values)
+
+    def test_with_values(self, root_no_resources, resource_with_values):
+        root = deepcopy(root_no_resources)
+        root.append(resource_with_values)
+
+    def test_file_value(self, root_no_resources, resource_with_file_values):
+        root = deepcopy(root_no_resources)
+        root.append(resource_with_file_values)
+
+    def test_region(self, root_no_resources, resource_region):
+        root = deepcopy(root_no_resources)
+        root.append(resource_region)
+
+    def test_link(self, root_no_resources, resource_link):
+        root = deepcopy(root_no_resources)
+        root.append(resource_link)
+
+    def test_video_segment(self, root_no_resources, resource_video_segment):
+        root = deepcopy(root_no_resources)
+        root.append(resource_video_segment)
+
+    def test_audio_segment(self, root_no_resources, resource_audio_segment):
+        root = deepcopy(root_no_resources)
+        root.append(resource_audio_segment)
+
+
+class TestParseValues:
+    pass
+
+
+class TestParseFileValues:
+    pass
 
 
 def test_create_from_local_name_to_absolute_iri_lookup(minimal_root):

9a7d4081a81f328cdf40868c498f9a2200714317
Nora-Olivia-Ammann <103038637+Nora-Olivia-Ammann@users.noreply.github.com>
2025-04-03 11:46:38 +0200

    add assertions

--- test/integration/xmllib/test_helpers.py
+++ test/integration/xmllib/test_helpers.py
@@ -9,8 +9,28 @@ def list_lookup() -> ListLookup:
     return get_list_lookup("testdata/json-project/test-list-lookup.json", "en", "default")
 
 
-def test_get_list_lookup(list_lookup):
-    pass
+class TestGetListLookup:
+    def test_lookup(self, list_lookup):
+        assert list_lookup._label_language == "en"
+        assert set(list_lookup._lookup.keys()) == {"list1", "list2"}
+        expected_1 = {
+            "Label 1": "list1_node1",
+            "Label 2": "list1_node2",
+            "Label 2.1": "list1_node2.1",
+            "Label 2.2": "list1_node2.2",
+        }
+        assert list_lookup._lookup["list1"] == expected_1
+        expected_2 = {"Label 1": "list2_node1", "Label 2": "list2_node2"}
+        assert list_lookup._lookup["list2"] == expected_2
+
+    def test_prop_to_list_name(self, list_lookup):
+        expected = {
+            "default:defaultOntoHasListOne": "list1",
+            ":defaultOntoHasListOne": "list1",
+            "other-onto:otherOntoHasListOne": "list1",
+            "other-onto:otherOntoHasListTwo": "list2",
+        }
+        assert list_lookup._prop_to_list_name == expected
 
 
 class TestListLookup:

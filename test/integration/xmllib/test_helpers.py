2242fbc189e22914aba47d166e45a6b51d7b55a5
Nora-Olivia-Ammann <103038637+Nora-Olivia-Ammann@users.noreply.github.com>
2025-04-03 12:01:48 +0200

    change test data

--- test/integration/xmllib/test_helpers.py
+++ test/integration/xmllib/test_helpers.py
@@ -15,13 +15,14 @@ class TestGetListLookup:
         assert list_lookup._label_language == "en"
         assert set(list_lookup._lookup.keys()) == {"list1", "list2"}
         expected_1 = {
+            "label 1": "list1_node1",
             "Label 1": "list1_node1",
-            "Label 2": "list1_node2",
-            "Label 2.1": "list1_node2.1",
-            "Label 2.2": "list1_node2.2",
+            "label 2": "list1_node2",
+            "label 2.1": "list1_node2.1",
+            "label 2.2": "list1_node2.2",
         }
         assert list_lookup._lookup["list1"] == expected_1
-        expected_2 = {"Label 1": "list2_node1", "Label 2": "list2_node2"}
+        expected_2 = {"label 1": "list2_node1", "label 2": "list2_node2"}
         assert list_lookup._lookup["list2"] == expected_2
 
     def test_prop_to_list_name(self, list_lookup):

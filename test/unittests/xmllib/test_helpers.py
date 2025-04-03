1fb3b075fce73fe5cecbbe0c8d5b4b7050825e7a
Johannes Nussbaum <johannes.nussbaum@dasch.swiss>
2025-04-03 10:32:54 +0200

    parametrize

--- test/unittests/xmllib/test_helpers.py
+++ test/unittests/xmllib/test_helpers.py
@@ -224,13 +224,19 @@ class TestFindDate:
     def test_find_date_in_string_ce_different_notations(self, string: str) -> None:
         assert find_date_in_string(string) == "GREGORIAN:CE:9:CE:9"
 
-    def test_find_date_in_string_bc(self) -> None:
-        assert find_date_in_string("x 9 BC x") == "GREGORIAN:BC:9:BC:9"
-        assert find_date_in_string("x 10000 BC x") == "GREGORIAN:BC:10000:BC:10000"
-        assert find_date_in_string("x 170 BC - 90 BC x") == "GREGORIAN:BC:170:BC:90"
-        assert find_date_in_string("x 170-90 BCE x") == "GREGORIAN:BC:170:BC:90"
-        assert find_date_in_string("x 20 BCE-50 CE x") == "GREGORIAN:BC:20:CE:50"
-        assert find_date_in_string("x 20 BCE - 50 C.E. x") == "GREGORIAN:BC:20:CE:50"
+    @pytest.mark.parametrize(
+        ["string", "expected"],
+        [
+            ("x 9 BC x", "GREGORIAN:BC:9:BC:9"),
+            ("x 10000 BC x", "GREGORIAN:BC:10000:BC:10000"),
+            ("x 170 BC - 90 BC x", "GREGORIAN:BC:170:BC:90"),
+            ("x 170-90 BCE x", "GREGORIAN:BC:170:BC:90"),
+            ("x 20 BCE-50 CE x", "GREGORIAN:BC:20:CE:50"),
+            ("x 20 BCE - 50 C.E. x", "GREGORIAN:BC:20:CE:50"),
+        ],
+    )
+    def test_find_date_in_string_bc(self, string: str, expected: str) -> None:
+        assert find_date_in_string(string) == expected
 
     def test_find_date_in_string_french_bc(self) -> None:
         assert find_date_in_string("Text 12345 av. J.-C. text") == "GREGORIAN:BC:12345:BC:12345"

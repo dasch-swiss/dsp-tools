c1f7542598455215a9a504db54266bd61f3a1832
Nora-Olivia-Ammann <103038637+Nora-Olivia-Ammann@users.noreply.github.com>
2025-04-04 08:36:05 +0200

    rename

--- test/unittests/commands/xmlupload/transform_into_intermediary/conftest.py
+++ test/unittests/commands/xmlupload/transform_into_intermediary/conftest.py
@@ -1,1 +1,1 @@
-from test.unittests.commands.xmlupload.transform_into_intermediary.fixturesxmlproperty import *  # noqa: F403
+from test.unittests.commands.xmlupload.transform_into_intermediary.fixtures_xmlproperty import *  # noqa: F403

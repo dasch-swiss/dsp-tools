64eefe2cfe018e77a1801c8dfd87937cadedbd7b
Nora-Olivia-Ammann <103038637+Nora-Olivia-Ammann@users.noreply.github.com>
2025-04-10 07:47:15 +0200

    Update upload_xml.py

--- src/dsp_tools/commands/ingest_xmlupload/create_resources/upload_xml.py
+++ src/dsp_tools/commands/ingest_xmlupload/create_resources/upload_xml.py
@@ -17,8 +17,8 @@ from dsp_tools.commands.xmlupload.models.ingest import BulkIngestedAssetClient
 from dsp_tools.commands.xmlupload.models.upload_clients import UploadClients
 from dsp_tools.commands.xmlupload.models.upload_state import UploadState
 from dsp_tools.commands.xmlupload.prepare_xml_input.list_client import ListClientLive
-from dsp_tools.commands.xmlupload.prepare_xml_input.prepare_xml_input import get_intermediary_lookups
-from dsp_tools.commands.xmlupload.prepare_xml_input.prepare_xml_input import prepare_upload_from_root_ingest
+from dsp_tools.commands.xmlupload.prepare_xml_input.prepare_xml_input import get_resources_and_stash_for_upload
+from dsp_tools.commands.xmlupload.prepare_xml_input.read_validate_xml_file import parse_and_clean_xml_file
 from dsp_tools.commands.xmlupload.prepare_xml_input.read_validate_xml_file import (
     parse_and_validate_with_xsd_transform_special_tags,
 )
@@ -54,7 +54,8 @@ def ingest_xmlupload(
     Raises:
         InputError: if any media was not uploaded or uploaded media was not referenced.
     """
-    default_ontology, root, shortcode = _parse_xml_and_replace_filepaths(xml_file)
+    root = parse_and_clean_xml_file(xml_file)
+    shortcode = root.attrib["shortcode"]
 
     auth = AuthenticationClientLive(server=creds.server, email=creds.user, password=creds.password)
     con = ConnectionLive(creds.server, auth)
@@ -69,10 +70,7 @@ def ingest_xmlupload(
 
     preliminary_validation_of_root(root, con, config)
 
-    intermediary_lookups = get_intermediary_lookups(root=root, clients=clients)
-    transformed_resources, stash = prepare_upload_from_root_ingest(
-        root=root, default_ontology=default_ontology, intermediary_lookups=intermediary_lookups
-    )
+    transformed_resources, stash = get_resources_and_stash_for_upload(root, clients)
     state = UploadState(
         pending_resources=transformed_resources,
         pending_stash=stash,

4c8682d3292c699deae7272e22250ea971692ec9
Nora-Olivia-Ammann <103038637+Nora-Olivia-Ammann@users.noreply.github.com>
2025-04-10 14:29:12 +0200

    add test

--- src/dsp_tools/commands/xmlupload/prepare_xml_input/check_if_link_targets_exist.py
+++ src/dsp_tools/commands/xmlupload/prepare_xml_input/check_if_link_targets_exist.py
@@ -1,8 +1,5 @@
 from __future__ import annotations
 
-import regex
-from lxml import etree
-
 from dsp_tools.commands.xmlupload.models.intermediary.res import IntermediaryResource
 from dsp_tools.commands.xmlupload.models.intermediary.values import IntermediaryLink
 from dsp_tools.commands.xmlupload.models.intermediary.values import IntermediaryRichtext
@@ -10,23 +7,18 @@ from dsp_tools.error.exceptions import InputError
 from dsp_tools.utils.data_formats.iri_util import is_resource_iri
 
 
-def check_if_link_targets_exist_root(resources: list[IntermediaryResource]) -> None:
-    resptr_errors = _check_if_resptr_targets_exist(resources)
-    salsah_errors = _check_if_salsah_targets_exist(resources)
-    if errors := resptr_errors + salsah_errors:
+def check_if_link_targets_exist(resources: list[IntermediaryResource]) -> None:
+    if errors := _check_all_links(resources):
         sep = "\n - "
         msg = f"It is not possible to upload the XML file, because it contains invalid links:{sep}{sep.join(errors)}"
         raise InputError(msg)
 
 
-def check_if_link_targets_exist(resources: list[IntermediaryResource]) -> None:
+def _check_all_links(resources: list[IntermediaryResource]) -> list[str]:
     res_ids = {x.res_id for x in resources}
     link_value_errors = _check_if_link_value_targets_exist(resources, res_ids)
-    salsah_errors = _check_if_standoff_link_targets_exist(resources, res_ids)
-    if errors := link_value_errors + salsah_errors:
-        sep = "\n - "
-        msg = f"It is not possible to upload the XML file, because it contains invalid links:{sep}{sep.join(errors)}"
-        raise InputError(msg)
+    standoff_errors = _check_if_standoff_link_targets_exist(resources, res_ids)
+    return link_value_errors + standoff_errors
 
 
 def _check_if_link_value_targets_exist(resources: list[IntermediaryResource], resource_ids: set[str]) -> list[str]:
@@ -57,46 +49,3 @@ def _check_if_standoff_link_targets_exist(resources: list[IntermediaryResource],
                         f"has a invalid standoff link target(s) '{all_missing}'"
                     )
     return not_found
-
-
-def _get_resource_ids_from_root(root: etree._Element) -> list[str]:
-    resource_tags = ["resource", "link", "region", "video-segment", "audio-segment"]
-    return [x.attrib["id"] for x in root.iter() if str(x.tag) in resource_tags]
-
-
-def _get_id_of_ancestor_resource(invalid_link_val: etree._Element) -> str:
-    resource_tags = ["resource", "link", "region", "video-segment", "audio-segment"]
-    for tg in resource_tags:
-        try:
-            res_id = next(invalid_link_val.iterancestors(tag=tg)).attrib["id"]
-            return res_id
-        except StopIteration:
-            pass
-    return "Resource ID not found"
-
-
-def _check_if_resptr_targets_exist(root: etree._Element) -> list[str]:
-    link_values = [x for x in root.iter() if x.tag == "resptr"]
-    resource_ids = _get_resource_ids_from_root(root)
-    invalid_link_values = [x for x in link_values if x.text not in resource_ids]
-    invalid_link_values = [x for x in invalid_link_values if not is_resource_iri(str(x.text))]
-    errors = []
-    for inv in invalid_link_values:
-        prop_name = next(inv.iterancestors(tag="resptr-prop")).attrib["name"]
-        res_id = _get_id_of_ancestor_resource(inv)
-        errors.append(f"Resource '{res_id}', property '{prop_name}' has an invalid link target '{inv.text}'")
-    return errors
-
-
-def _check_if_salsah_targets_exist(root: etree._Element) -> list[str]:
-    link_values = [x for x in root.iter() if x.tag == "a"]
-    resource_ids = _get_resource_ids_from_root(root)
-    invalid_link_values = [x for x in link_values if regex.sub(r"IRI:|:IRI", "", x.attrib["href"]) not in resource_ids]
-    invalid_link_values = [x for x in invalid_link_values if x.attrib.get("class") == "salsah-link"]
-    invalid_link_values = [x for x in invalid_link_values if not is_resource_iri(x.attrib["href"])]
-    errors = []
-    for inv in invalid_link_values:
-        prop_name = next(inv.iterancestors(tag="text-prop")).attrib["name"]
-        res_id = _get_id_of_ancestor_resource(inv)
-        errors.append(f"Resource '{res_id}', property '{prop_name}' has an invalid link target '{inv.attrib['href']}'")
-    return errors
--- test/unittests/commands/xmlupload/test_read_validate_xml_file.py
+++ test/unittests/commands/xmlupload/test_read_validate_xml_file.py
@@ -1,100 +1,68 @@
 import pytest
-from lxml import etree
 
-from dsp_tools.commands.xmlupload.prepare_xml_input.check_if_link_targets_exist import _check_if_resptr_targets_exist
-from dsp_tools.commands.xmlupload.prepare_xml_input.check_if_link_targets_exist import _check_if_salsah_targets_exist
-
-
-def test_check_if_resptr_targets_exist() -> None:
-    """Check correct input"""
-    xml = """
-    <knora>
-        <resource id="resource1">
-            <resptr-prop name="resptr1"><resptr>resource2</resptr></resptr-prop>
-        </resource>
-        <resource id="resource2">
-            <resptr-prop name="resptr2"><resptr>resource1</resptr></resptr-prop>
-        </resource>
-    </knora>
-    """
-    root = etree.fromstring(xml)
-    errors_returned = _check_if_resptr_targets_exist(root)
-    assert not errors_returned
+from dsp_tools.commands.xmlupload.models.formatted_text_value import FormattedTextValue
+from dsp_tools.commands.xmlupload.models.intermediary.res import IntermediaryResource
+from dsp_tools.commands.xmlupload.models.intermediary.values import IntermediaryInt
+from dsp_tools.commands.xmlupload.models.intermediary.values import IntermediaryLink
+from dsp_tools.commands.xmlupload.models.intermediary.values import IntermediaryRichtext
+from dsp_tools.commands.xmlupload.prepare_xml_input.check_if_link_targets_exist import _check_all_links
 
 
-def test_check_if_resptr_targets_exist_invalid() -> None:
-    """Check invalid input"""
-    xml = """
-    <knora>
-        <resource id="resource1">
-            <resptr-prop name="resptr1"><resptr>resource3</resptr></resptr-prop>
-        </resource>
-        <resource id="resource2">
-            <resptr-prop name="resptr2"><resptr>resource4</resptr></resptr-prop>
-        </resource>
-    </knora>
-    """
-    root = etree.fromstring(xml)
-    errors_returned = _check_if_resptr_targets_exist(root)
-    errors_expected = [
-        "Resource 'resource1', property 'resptr1' has an invalid link target 'resource3'",
-        "Resource 'resource2', property 'resptr2' has an invalid link target 'resource4'",
-    ]
-    assert errors_returned == errors_expected
+@pytest.fixture
+def richtext() -> IntermediaryRichtext:
+    val = FormattedTextValue('<a class="salsah-link" href="IRI:target_resource_text:IRI">target_resource</a>')
+    return IntermediaryRichtext(val, "richtextProp", None, None, {"target_resource_text"}, "")
 
 
-def test_check_if_salsah_targets_exist() -> None:
-    """Check correct input"""
-    xml = """
-    <knora>
-        <resource id="resource1">
-            <text-prop name="text1">
-                <text encoding="xml">
-                    <a class="salsah-link" href="IRI:resource2:IRI">resource2</a>
-                </text>
-            </text-prop>
-        </resource>
-        <resource id="resource2">
-            <text-prop name="text2">
-                <text encoding="xml">
-                    <a class="salsah-link" href="IRI:resource1:IRI">resource1</a>
-                </text>
-            </text-prop>
-        </resource>
-    </knora>
-    """
-    root = etree.fromstring(xml)
-    errors_returned = _check_if_salsah_targets_exist(root)
-    assert not errors_returned
+@pytest.fixture
+def resource_richtext(richtext) -> IntermediaryResource:
+    return IntermediaryResource(
+        res_id="resource_richtext",
+        type_iri="type",
+        label="lbl",
+        permissions=None,
+        values=[richtext, IntermediaryInt(1, "intProp", None, None)],
+    )
 
 
-def test_check_if_salsah_targets_exist_invalid() -> None:
-    """Check invalid input"""
-    xml = """
-    <knora>
-        <resource id="resource1">
-            <text-prop name="text1">
-                <text encoding="xml">
-                    <a class="salsah-link" href="IRI:resource3:IRI">resource3</a>
-                </text>
-            </text-prop>
-        </resource>
-        <resource id="resource2">
-            <text-prop name="text2">
-                <text encoding="xml">
-                    <a class="salsah-link" href="IRI:resource4:IRI">resource4</a>
-                </text>
-            </text-prop>
-        </resource>
-    </knora>
-    """
-    root = etree.fromstring(xml)
-    errors_returned = _check_if_salsah_targets_exist(root)
-    errors_expected = [
-        "Resource 'resource1', property 'text1' has an invalid link target 'IRI:resource3:IRI'",
-        "Resource 'resource2', property 'text2' has an invalid link target 'IRI:resource4:IRI'",
-    ]
-    assert errors_returned == errors_expected
+@pytest.fixture
+def target_resource_text() -> IntermediaryResource:
+    return IntermediaryResource(
+        res_id="target_resource_text", type_iri="type", label="lbl", permissions=None, values=[]
+    )
+
+
+@pytest.fixture
+def resource_link_value() -> IntermediaryResource:
+    return IntermediaryResource(
+        res_id="resource_richtext",
+        type_iri="type",
+        label="lbl",
+        permissions=None,
+        values=[IntermediaryLink("target_resource_link", "linkProp", None, None, "")],
+    )
+
+
+@pytest.fixture
+def target_resource_link() -> IntermediaryResource:
+    return IntermediaryResource(
+        res_id="target_resource_link", type_iri="type", label="lbl", permissions=None, values=[]
+    )
+
+
+def test_check_all_links_good(richtext, resource_link_value, target_resource_text, target_resource_link):
+    resources = [richtext, resource_link_value, target_resource_text, target_resource_link]
+    assert len(_check_all_links(resources)) == 0
+
+
+def test_check_all_links_missing_text(richtext, resource_link_value, target_resource_link):
+    resources = [richtext, resource_link_value, target_resource_link]
+    assert len(_check_all_links(resources)) == 0
+
+
+def test_check_all_links_missing_link(richtext, resource_link_value, target_resource_text):
+    resources = [richtext, resource_link_value, target_resource_text]
+    assert len(_check_all_links(resources)) == 0
 
 
 if __name__ == "__main__":

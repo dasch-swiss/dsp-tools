d0b202ff484ca0bda59fc445af36fa82039b57df
Nora-Olivia-Ammann <103038637+Nora-Olivia-Ammann@users.noreply.github.com>
2025-04-14 11:08:22 +0200

    Update get_data_deserialised.py

--- src/dsp_tools/commands/validate_data/get_data_deserialised.py
+++ src/dsp_tools/commands/validate_data/get_data_deserialised.py
@@ -1,7 +1,12 @@
+import regex
+
 from dsp_tools.utils.xml_parsing.models.data_deserialised import DataDeserialised
 from dsp_tools.utils.xml_parsing.models.data_deserialised import PropertyObject
 from dsp_tools.utils.xml_parsing.models.data_deserialised import ResourceDeserialised
+from dsp_tools.utils.xml_parsing.models.data_deserialised import TripleObjectType
+from dsp_tools.utils.xml_parsing.models.data_deserialised import TriplePropertyType
 from dsp_tools.utils.xml_parsing.models.data_deserialised import ValueInformation
+from dsp_tools.utils.xml_parsing.models.parsed_resource import KnoraValueType
 from dsp_tools.utils.xml_parsing.models.parsed_resource import ParsedFileValue
 from dsp_tools.utils.xml_parsing.models.parsed_resource import ParsedFileValueMetadata
 from dsp_tools.utils.xml_parsing.models.parsed_resource import ParsedResource
@@ -16,7 +21,44 @@ def _get_one_resource(resource: ParsedResource) -> ResourceDeserialised:
     pass
 
 
+def _get_all_stand_off_links(values: list[ValueInformation]) -> list[PropertyObject]:
+    stand_offs = []
+    for val in values:
+        if val.knora_type.RICHTEXT_VALUE:
+            stand_offs.extend(_get_stand_off_links(val.user_facing_value))
+    return stand_offs
+
+
+def _get_stand_off_links(text: str | None) -> list[PropertyObject]:
+    if not text:
+        return []
+    links = set(regex.findall(pattern='href="IRI:(.*?):IRI"', string=text))
+    return [PropertyObject(TriplePropertyType.KNORA_STANDOFF_LINK, lnk, TripleObjectType.IRI) for lnk in links]
+
+
+def _get_one_value(value: ParsedValue) -> ValueInformation:
+    match value.value_type:
+        case KnoraValueType.LIST_VALUE:
+            return _get_list_value(value)
+        case KnoraValueType.INTERVAL_VALUE:
+            return _get_interval_value(value)
+        case _:
+            return _get_generic_value(value)
+
+
 def _get_generic_value(value: ParsedValue) -> ValueInformation:
+    pass
+
+
+def _get_list_value(value: ParsedValue) -> ValueInformation:
+    pass
+
+
+def _get_interval_value(value: ParsedValue) -> ValueInformation:
+    pass
+
+
+def _get_value_metadata(value: ParsedValue) -> list[PropertyObject]:
     pass
 
 

ee3d385b1fc47d881e9b45ec34acc1a12d27a6a9
Johannes Nussbaum <johannes.nussbaum@dasch.swiss>
2025-04-15 18:28:12 +0200

    adapt test

--- test/integration/xmllib/test_internal_helpers.py
+++ test/integration/xmllib/test_internal_helpers.py
@@ -1,4 +1,7 @@
 from pathlib import Path
+from typing import Iterator
+
+import pytest
 
 from dsp_tools.xmllib.models.res import Resource
 from dsp_tools.xmllib.models.root import XMLRoot
@@ -6,17 +9,29 @@ from dsp_tools.xmllib.models.root import XMLRoot
 SHORTCODE = "1234"
 DEFAULT_ONTO = "my_onto"
 RES_ID = "res_id"
-OUT_FILE = Path("testdata/xml-data/serialise_tags.xml")
+
+@pytest.fixture
+def out_file() -> Iterator[Path]:
+    out_file = Path("testdata/xml-data/tmp-serialise_tags.xml")
+    yield out_file
+    out_file.unlink()
 
 
-def test_serialisation_of_tags() -> None:
-    url = "https://www.google.com/"
-    expected = f'<text encoding="xml">Text<footnote content="Text &lt;a href=&#34;{url}&#34;&gt;Google&lt;/a&gt;" /></text>\n'
+@pytest.mark.parametrize(
+    ("_input", "expected"),
+    [
+        (
+            'Text <footnote content="Text &lt;a href=&quot;https://www.google.com/&quot;&gt;Google&lt;/a&gt;" />', 
+            'Text <footnote content="Text &lt;a href=&#34;https://www.google.com/&#34;&gt;Google&lt;/a&gt;" />'
+        ),
+    ],
+)
+def test_serialisation_of_tags(_input: str, expected: str, out_file: Path) -> None:
     root = XMLRoot.create_new(SHORTCODE, DEFAULT_ONTO)
     res = Resource.create_new(RES_ID, ":Type", "lbl")
-    res.add_richtext(":prop", f'Text <footnote content="Text &lt;a href=&quot;{url}&quot;&gt;URL&lt;/a&gt;" />')
-    root.add_resource(res).write_file(OUT_FILE)
-    with open(OUT_FILE, encoding="utf-8") as f:
+    res.add_richtext(":prop", _input)
+    root.add_resource(res).write_file(out_file)
+    with open(out_file, encoding="utf-8") as f:
         lines = f.readlines()
-    line = next(x for x in lines if url in x)
-    print(line)
+    line = next(x for x in lines if '<text encoding="xml">' in x).strip()
+    assert line == f'<text encoding="xml">{expected}</text>'

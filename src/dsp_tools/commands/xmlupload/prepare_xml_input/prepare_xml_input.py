8527143579a9e6b01f52dd7a87b189df332f0e6b
Nora-Olivia-Ammann <103038637+Nora-Olivia-Ammann@users.noreply.github.com>
2025-04-16 14:35:48 +0200

    move functions

--- src/dsp_tools/commands/xmlupload/prepare_xml_input/prepare_xml_input.py
+++ src/dsp_tools/commands/xmlupload/prepare_xml_input/prepare_xml_input.py
@@ -1,9 +1,7 @@
 from __future__ import annotations
 
 from datetime import datetime
-from typing import cast
 
-import regex
 from loguru import logger
 from lxml import etree
 
@@ -11,8 +9,6 @@ from dsp_tools.clients.connection import Connection
 from dsp_tools.commands.xmlupload.models.intermediary.res import IntermediaryResource
 from dsp_tools.commands.xmlupload.models.lookup_models import IntermediaryLookups
 from dsp_tools.commands.xmlupload.models.lookup_models import make_namespace_dict_from_onto_names
-from dsp_tools.commands.xmlupload.models.permission import Permissions
-from dsp_tools.commands.xmlupload.models.permissions_parsed import XmlPermission
 from dsp_tools.commands.xmlupload.models.upload_clients import UploadClients
 from dsp_tools.commands.xmlupload.prepare_xml_input.transform_into_intermediary_classes import (
     transform_all_resources_into_intermediary_resources,
@@ -24,6 +20,8 @@ from dsp_tools.commands.xmlupload.stash.stash_models import Stash
 from dsp_tools.error.exceptions import BaseError
 from dsp_tools.error.exceptions import InputError
 from dsp_tools.legacy_models.projectContext import ProjectContext
+from dsp_tools.utils.xml_parsing.get_lookups import get_authorship_lookup
+from dsp_tools.utils.xml_parsing.get_lookups import get_permissions_lookup
 from dsp_tools.utils.xml_parsing.get_parsed_resources import get_parsed_resources
 from dsp_tools.utils.xml_parsing.models.parsed_resource import ParsedResource
 
@@ -41,8 +39,8 @@ def _get_intermediary_lookups(root: etree._Element, clients: UploadClients) -> I
     proj_context = _get_project_context_from_server(
         connection=clients.project_client.con, shortcode=root.attrib["shortcode"]
     )
-    permissions_lookup = _get_permissions_lookup(root, proj_context)
-    authorship_lookup = _get_authorship_lookup(root)
+    permissions_lookup = get_permissions_lookup(root, proj_context)
+    authorship_lookup = get_authorship_lookup(root)
     listnode_lookup = clients.list_client.get_list_node_id_to_iri_lookup()
     project_onto_dict = clients.project_client.get_ontology_name_dict()
     namespaces = make_namespace_dict_from_onto_names(project_onto_dict)
@@ -54,14 +52,6 @@ def _get_intermediary_lookups(root: etree._Element, clients: UploadClients) -> I
     )
 
 
-def _get_permissions_lookup(root: etree._Element, proj_context: ProjectContext) -> dict[str, Permissions]:
-    permission_ele = list(root.iter(tag="permissions"))
-    permissions = [XmlPermission(permission, proj_context) for permission in permission_ele]
-    permissions_dict = {permission.permission_id: permission for permission in permissions}
-    permissions_lookup = {name: perm.get_permission_instance() for name, perm in permissions_dict.items()}
-    return permissions_lookup
-
-
 def _get_project_context_from_server(connection: Connection, shortcode: str) -> ProjectContext:
     try:
         proj_context = ProjectContext(con=connection, shortcode=shortcode)
@@ -69,21 +59,6 @@ def _get_project_context_from_server(connection: Connection, shortcode: str) -> 
         logger.exception("Unable to retrieve project context from DSP server")
         raise InputError("Unable to retrieve project context from DSP server") from None
     return proj_context
-
-
-def _get_authorship_lookup(root: etree._Element) -> dict[str, list[str]]:
-    def get_one_author(ele: etree._Element) -> str:
-        # The xsd file ensures that the body of the element contains valid non-whitespace characters
-        txt = cast(str, ele.text)
-        txt = regex.sub(r"[\n\t]", " ", txt)
-        txt = regex.sub(r" +", " ", txt)
-        return txt.strip()
-
-    authorship_lookup = {}
-    for auth in root.iter(tag="authorship"):
-        individual_authors = [get_one_author(child) for child in auth.iterchildren()]
-        authorship_lookup[auth.attrib["id"]] = individual_authors
-    return authorship_lookup
 
 
 def _get_intermediary_resources(

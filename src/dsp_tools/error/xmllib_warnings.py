b4b0fffe2c075e0cc4adea15b5fa5715a759870a
Nora-Olivia-Ammann <103038637+Nora-Olivia-Ammann@users.noreply.github.com>
2025-04-24 15:06:48 +0200

    make abstract method

--- src/dsp_tools/error/xmllib_warnings.py
+++ src/dsp_tools/error/xmllib_warnings.py
@@ -1,12 +1,16 @@
+from abc import ABC
+from abc import abstractmethod
+
 from dsp_tools.utils.ansi_colors import BOLD_RED
 from dsp_tools.utils.ansi_colors import RESET_TO_DEFAULT
 from dsp_tools.utils.ansi_colors import YELLOW
 
 
-class XmllibUserInfoBase(Warning):
+class XmllibUserInfoBase(Warning, ABC):
     """Base class for warnings that implement a custom showwarnings() function"""
 
     @classmethod
+    @abstractmethod
     def showwarning(cls, message: str) -> None:
         """Functionality that should be executed when a warning of this class is emitted"""
 

01b0cd343cbf8d8f024190f5eabb66bf71375cd9
Nora-Olivia-Ammann <103038637+Nora-Olivia-Ammann@users.noreply.github.com>
2025-04-25 11:53:42 +0200

    refactor(xmllib): change find function trace (#1658)

--- test/unittests/error/test_xmllib_warnings_util.py
+++ test/unittests/error/test_xmllib_warnings_util.py
@@ -1,11 +1,14 @@
 # mypy: disable-error-code="method-assign,no-untyped-def"
+
 import pytest
 import regex
 
 from dsp_tools.error.xmllib_warnings import MessageInfo
 from dsp_tools.error.xmllib_warnings import XmllibInputInfo
 from dsp_tools.error.xmllib_warnings import XmllibInputWarning
+from dsp_tools.error.xmllib_warnings_util import _filter_stack_frames
 from dsp_tools.error.xmllib_warnings_util import _get_calling_code_context
+from dsp_tools.error.xmllib_warnings_util import _get_stack_frame_number
 from dsp_tools.error.xmllib_warnings_util import emit_xmllib_input_info
 from dsp_tools.error.xmllib_warnings_util import emit_xmllib_input_warning
 from dsp_tools.error.xmllib_warnings_util import get_user_message_string
@@ -26,12 +29,6 @@ def test_emit_xmllib_input_warning(message_info):
     expected = regex.escape("Resource ID 'id' | msg")
     with pytest.warns(XmllibInputWarning, match=expected):
         emit_xmllib_input_warning(message_info)
-
-
-def test_get_calling_code_context():
-    result = _get_calling_code_context()
-    assert isinstance(result, str)
-    assert result.startswith("test_xmllib_warnings_util.py:")
 
 
 class TestGetMessageString:
@@ -62,3 +59,57 @@ class TestGetMessageString:
         result = get_user_message_string(message_info, None)
         expected = "Resource ID 'id' | msg"
         assert result == expected
+
+
+class TestStackFrame:
+    def test_get_calling_code_context(self):
+        result = _get_calling_code_context()
+        assert isinstance(result, str)
+        assert result.startswith("test_xmllib_warnings_util.py:")
+
+    def test_get_stack_frame_number(self):
+        file_paths = [
+            "/Users/UserName/repoLocation/dsp-tools/src/dsp_tools/error/xmllib_warnings_util.py",
+            "/Users/UserName/repoLocation/dsp-tools/src/dsp_tools/error/xmllib_warnings_util.py",
+            "/Users/UserName/repoLocation/dsp-tools/src/dsp_tools/xmllib/models/dsp_base_resources.py",
+            "/Users/UserName/repoLocation/dsp-tools/src/dsp_tools/xmllib/models/dsp_base_resources.py",
+            "<string>",
+            "/Users/UserName/repoLocation/dsp-tools/src/dsp_tools/xmllib/models/dsp_base_resources.py",
+            "/Users/UserName/repoLocation/dsp-tools/test/unittests/xmllib/models/test_dsp_base_resources.py",
+            "/Users/UserName/repoLocation/dsp-tools/.venv/lib/python3.12/site-packages/_pytest/python.py",
+            (
+                "/Users/UserName/Applications/PyCharm Community Edition.app"
+                "/Contents/plugins/python-ce/helpers/pydev/pydevd.py"
+            ),
+        ]
+        assert _get_stack_frame_number(file_paths) == 6
+
+    def test_get_stack_frame_number_none(self):
+        file_paths = [
+            "/Users/UserName/repoLocation/dsp-tools/.venv/lib/python3.12/site-packages/_pytest/python.py",
+            (
+                "/Users/UserName/Applications/PyCharm Community Edition.app"
+                "/Contents/plugins/python-ce/helpers/pydev/pydevd.py"
+            ),
+        ]
+        assert _get_stack_frame_number(file_paths) == -1
+
+    @pytest.mark.parametrize(
+        ("f_path", "expected"),
+        [
+            ("/Users/UserName/repoLocation/dsp-tools/src/dsp_tools/error/xmllib_warnings_util.py", True),
+            ("/Users/UserName/repoLocation/dsp-tools/src/dsp_tools/xmllib/models/dsp_base_resources.py", True),
+            ("<string>", True),
+            ("/Users/UserName/repoLocation/dsp-tools/test/unittests/xmllib/models/test_dsp_base_resources.py", True),
+            ("/Users/UserName/repoLocation/dsp-tools/.venv/lib/python3.12/site-packages/_pytest/python.py", False),
+            (
+                (
+                    "/Users/UserName/Applications/PyCharm Community Edition.app"
+                    "/Contents/plugins/python-ce/helpers/pydev/pydevd.py"
+                ),
+                False,
+            ),
+        ],
+    )
+    def test_filter_stack_frames(self, f_path, expected):
+        assert _filter_stack_frames(f_path) == expected

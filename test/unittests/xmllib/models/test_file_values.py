bcb27b1e4ea5d9ee97cd3372a4d5b97b3014bbb5
Nora-Olivia-Ammann <103038637+Nora-Olivia-Ammann@users.noreply.github.com>
2025-04-30 09:31:05 +0200

    Update test_file_values.py

--- test/unittests/xmllib/models/test_file_values.py
+++ test/unittests/xmllib/models/test_file_values.py
@@ -11,7 +11,7 @@ from dsp_tools.xmllib.models.licenses.recommended import LicenseRecommended
 
 
 class TestMetadata:
-    def good(self):
+    def test_good(self):
         with warnings.catch_warnings(record=True) as caught_warnings:
             Metadata.new(
                 LicenseRecommended.DSP.UNKNOWN,
@@ -22,7 +22,7 @@ class TestMetadata:
             )
         assert len(caught_warnings) == 0
 
-    def good_author_not_a_list(self):
+    def test_good_author_not_a_list(self):
         with warnings.catch_warnings(record=True) as caught_warnings:
             res = Metadata.new(
                 LicenseRecommended.DSP.UNKNOWN,
@@ -34,26 +34,36 @@ class TestMetadata:
         assert len(caught_warnings) == 0
         assert res.authorship == tuple(["authorship"])
 
-    def wrong_license(self):
-        expected = regex.escape("fds")
+    def test_wrong_license(self):
+        expected = regex.escape(
+            "Field 'license (bistream/iiif-uri)' | "
+            "The input should be a valid xmllib.License, your input 'license' does not match the type."
+        )
         with pytest.warns(XmllibInputWarning, match=expected):
-            Metadata.new("license", "copyright", ["authorship"], Permissions.PROJECT_SPECIFIC_PERMISSIONS, "id")
+            Metadata.new("license", "copyright", ["authorship"], Permissions.PROJECT_SPECIFIC_PERMISSIONS, "id")  # type: ignore[arg-type]
 
-    def wrong_copyright(self):
-        expected = regex.escape("fds")
+    def test_wrong_copyright(self):
+        expected = regex.escape(
+            "Field 'copyright_holder (bistream/iiif-uri)' | Your input '' is empty. Please enter a valid string."
+        )
         with pytest.warns(XmllibInputWarning, match=expected):
             Metadata.new(
                 LicenseRecommended.DSP.UNKNOWN, "", ["authorship"], Permissions.PROJECT_SPECIFIC_PERMISSIONS, "id"
             )
 
-    def wrong_authorship(self):
-        expected = regex.escape("fds")
+    def test_wrong_authorship(self):
+        expected = regex.escape(
+            "Field 'authorship (bistream/iiif-uri)' | Your input '' is empty. Please enter a valid string."
+        )
         with pytest.warns(XmllibInputWarning, match=expected):
             Metadata.new(
                 LicenseRecommended.DSP.UNKNOWN, "copyright", [""], Permissions.PROJECT_SPECIFIC_PERMISSIONS, "id"
             )
 
-    def wrong_permissions(self):
-        expected = regex.escape("fds")
+    def test_wrong_permissions(self):
+        expected = regex.escape(
+            "Field 'permissions (bistream/iiif-uri)' | "
+            "The input should be a valid xmllib.Permissions, your input 'string' does not match the type."
+        )
         with pytest.warns(XmllibInputWarning, match=expected):
-            Metadata.new(LicenseRecommended.DSP.UNKNOWN, "copyright", ["authorship"], "string", "id")
+            Metadata.new(LicenseRecommended.DSP.UNKNOWN, "copyright", ["authorship"], "string", "id")  # type: ignore[arg-type]

19d18e14c7e2a395ab99508af9f78c87876efb49
Johannes Nussbaum <johannes.nussbaum@dasch.swiss>
2025-05-07 16:34:18 +0200

    manually fix wrong import statements

--- src/dsp_tools/clients/openapi_ingest/openapi_client/api/__init__.py
+++ src/dsp_tools/clients/openapi_ingest/openapi_client/api/__init__.py
@@ -1,10 +1,9 @@
 # flake8: noqa
 
 # import apis into api package
-from openapi_client.api.assets_api import AssetsApi
-from openapi_client.api.bulk_ingest_api import BulkIngestApi
-from openapi_client.api.import_export_api import ImportExportApi
-from openapi_client.api.maintenance_api import MaintenanceApi
-from openapi_client.api.monitoring_api import MonitoringApi
-from openapi_client.api.projects_api import ProjectsApi
-
+from src.dsp_tools.clients.openapi_ingest.openapi_client.api.assets_api import AssetsApi
+from src.dsp_tools.clients.openapi_ingest.openapi_client.api.bulk_ingest_api import BulkIngestApi
+from src.dsp_tools.clients.openapi_ingest.openapi_client.api.import_export_api import ImportExportApi
+from src.dsp_tools.clients.openapi_ingest.openapi_client.api.maintenance_api import MaintenanceApi
+from src.dsp_tools.clients.openapi_ingest.openapi_client.api.monitoring_api import MonitoringApi
+from src.dsp_tools.clients.openapi_ingest.openapi_client.api.projects_api import ProjectsApi

ed6b0a6763651d6472847a686f2cfdef5bb3cfe1
Nora-Olivia-Ammann <103038637+Nora-Olivia-Ammann@users.noreply.github.com>
2025-05-08 14:16:27 +0200

    Commit all changes

--- test/e2e/commands/xmlupload/test_resources.py
+++ test/e2e/commands/xmlupload/test_resources.py
@@ -3,6 +3,7 @@
 
 import pytest
 
+from dsp_tools.utils.rdflib_constants import KNORA_API_STR
 from test.e2e.commands.xmlupload.conftest import _util_request_resources_by_class
 
 # ruff: noqa: ARG001 Unused function argument
@@ -36,5 +37,33 @@ def test_audio(onto_iri, auth_header, project_iri, creds):
 @pytest.mark.usefixtures("_xmlupload")
 def test_video(onto_iri, auth_header, project_iri, creds):
     cls_iri_str = f"{onto_iri}TestMovingImageRepresentation"
+    resources = _util_request_resources_by_class(cls_iri_str, auth_header, project_iri, creds)
+    assert len(resources) != 0
+
+
+@pytest.mark.usefixtures("_xmlupload")
+def test_region(auth_header, project_iri, creds):
+    cls_iri_str = f"{KNORA_API_STR}Region"
+    resources = _util_request_resources_by_class(cls_iri_str, auth_header, project_iri, creds)
+    assert len(resources) != 0
+
+
+@pytest.mark.usefixtures("_xmlupload")
+def test_link_obj(auth_header, project_iri, creds):
+    cls_iri_str = f"{KNORA_API_STR}LinkObj"
+    resources = _util_request_resources_by_class(cls_iri_str, auth_header, project_iri, creds)
+    assert len(resources) != 0
+
+
+@pytest.mark.usefixtures("_xmlupload")
+def test_audio_segment(auth_header, project_iri, creds):
+    cls_iri_str = f"{KNORA_API_STR}AudioSegment"
+    resources = _util_request_resources_by_class(cls_iri_str, auth_header, project_iri, creds)
+    assert len(resources) != 0
+
+
+@pytest.mark.usefixtures("_xmlupload")
+def test_video_segment(auth_header, project_iri, creds):
+    cls_iri_str = f"{KNORA_API_STR}VideoSegment"
     resources = _util_request_resources_by_class(cls_iri_str, auth_header, project_iri, creds)
     assert len(resources) != 0

1e99914bd3e371b2685f374ff8fd8145a25611dc
Nora-Olivia-Ammann <103038637+Nora-Olivia-Ammann@users.noreply.github.com>
2025-05-08 12:32:48 +0200

    Commit all changes

--- test/e2e/commands/xmlupload/test_xmlupload.py
+++ test/e2e/commands/xmlupload/test_xmlupload.py
@@ -9,6 +9,7 @@ import requests
 from rdflib import Graph
 
 from dsp_tools.commands.xmlupload.xmlupload import xmlupload
+from dsp_tools.utils.rdflib_constants import KNORA_API_STR
 
 PROJECT_SHORTCODE = "9999"
 ONTO_NAME = "onto"
@@ -24,17 +25,17 @@ def project_iri(_create_project: None, creds) -> str:
 
 @pytest.fixture(scope="module")
 def onto_iri(creds) -> str:
-    return f"{creds.server}/ontology/{PROJECT_SHORTCODE}/{ONTO_NAME}/v2"
+    return f"{creds.server}/ontology/{PROJECT_SHORTCODE}/{ONTO_NAME}/v2#"
 
 
 @pytest.fixture(scope="module")
-def class_with_everything_iri(creds) -> str:
-    return f"{creds.server}/ontology/{PROJECT_SHORTCODE}/{ONTO_NAME}/v2#ClassWithEverything"
+def class_with_everything_iri(onto_iri) -> str:
+    return f"{onto_iri}ClassWithEverything"
 
 
 @pytest.fixture(scope="module")
 def second_onto_iri(creds) -> str:
-    return f"{creds.server}/ontology/{PROJECT_SHORTCODE}/{SECOND_ONTO}/v2"
+    return f"{creds.server}/ontology/{PROJECT_SHORTCODE}/{SECOND_ONTO}/v2#"
 
 
 @pytest.fixture(scope="module")
@@ -53,21 +54,50 @@ def auth_header(_create_project: None, creds) -> dict[str, str]:
 def class_with_everything_resource_graph(
     _xmlupload, class_with_everything_iri, auth_header, project_iri, creds
 ) -> Graph:
-    resources = _util_request_resources_by_class(class_with_everything_iri, auth_header, project_iri, creds)
-    g = Graph()
-    g.parse(data=resources, format="json-ld")
-    return g
+    return _util_request_resources_by_class(class_with_everything_iri, auth_header, project_iri, creds)
 
 
-def _util_request_resources_by_class(resclass_iri: str, auth_header: dict[str, str], project_iri: str, creds) -> str:
+def _util_request_resources_by_class(resclass_iri: str, auth_header: dict[str, str], project_iri: str, creds) -> Graph:
     resclass_iri_encoded = urllib.parse.quote_plus(resclass_iri)
     get_resources_route = f"{creds.server}/v2/resources?resourceClass={resclass_iri_encoded}&page=0"
     headers = auth_header | {"X-Knora-Accept-Project": project_iri}
     response = requests.get(get_resources_route, timeout=3, headers=headers).json()
     resources = json.dumps(response)
-    return resources
+    g = Graph()
+    g.parse(data=resources, format="json-ld")
+    return g
 
 
 class TestResources:
     def test_class_with_everything_all_created(self, class_with_everything_resource_graph):
         assert len(class_with_everything_resource_graph) != 0
+
+    @pytest.mark.usefixtures("_xmlupload")
+    def test_second_onto_class(self, second_onto_iri, auth_header, project_iri, creds):
+        cls_iri_str = f"{second_onto_iri}SecondOntoClass"
+        resources = _util_request_resources_by_class(cls_iri_str, auth_header, project_iri, creds)
+        assert len(resources) != 0
+
+    @pytest.mark.usefixtures("_xmlupload")
+    def test_region(self, auth_header, project_iri, creds):
+        cls_iri_str = f"{KNORA_API_STR}Region"
+        resources = _util_request_resources_by_class(cls_iri_str, auth_header, project_iri, creds)
+        assert len(resources) != 0
+
+    @pytest.mark.usefixtures("_xmlupload")
+    def test_link_obj(self, auth_header, project_iri, creds):
+        cls_iri_str = f"{KNORA_API_STR}LinkObj"
+        resources = _util_request_resources_by_class(cls_iri_str, auth_header, project_iri, creds)
+        assert len(resources) != 0
+
+    @pytest.mark.usefixtures("_xmlupload")
+    def test_audio_segment(self, auth_header, project_iri, creds):
+        cls_iri_str = f"{KNORA_API_STR}AudioSegment"
+        resources = _util_request_resources_by_class(cls_iri_str, auth_header, project_iri, creds)
+        assert len(resources) != 0
+
+    @pytest.mark.usefixtures("_xmlupload")
+    def test_video_segment(self, auth_header, project_iri, creds):
+        cls_iri_str = f"{KNORA_API_STR}VideoSegment"
+        resources = _util_request_resources_by_class(cls_iri_str, auth_header, project_iri, creds)
+        assert len(resources) != 0

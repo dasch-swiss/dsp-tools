714b57d5071098a0ac17080fe3f59bca1f15bd4d
Nora-Olivia-Ammann <103038637+Nora-Olivia-Ammann@users.noreply.github.com>
2025-05-08 14:15:40 +0200

    Commit all changes

--- test/e2e/commands/xmlupload/test_dsp_resources.py
+++ test/e2e/commands/xmlupload/test_dsp_resources.py
@@ -3,38 +3,35 @@
 
 import pytest
 
+from dsp_tools.utils.rdflib_constants import KNORA_API_STR
 from test.e2e.commands.xmlupload.conftest import _util_request_resources_by_class
 
 # ruff: noqa: ARG001 Unused function argument
 
 
-def test_class_with_everything_all_created(class_with_everything_resource_graph):
-    assert len(class_with_everything_resource_graph) != 0
-
-
 @pytest.mark.usefixtures("_xmlupload")
-def test_second_onto_class(second_onto_iri, auth_header, project_iri, creds):
-    cls_iri_str = f"{second_onto_iri}SecondOntoClass"
+def test_region(auth_header, project_iri, creds):
+    cls_iri_str = f"{KNORA_API_STR}Region"
     resources = _util_request_resources_by_class(cls_iri_str, auth_header, project_iri, creds)
     assert len(resources) != 0
 
 
 @pytest.mark.usefixtures("_xmlupload")
-def test_still_image(onto_iri, auth_header, project_iri, creds):
-    cls_iri_str = f"{onto_iri}TestStillImageRepresentation"
+def test_link_obj(auth_header, project_iri, creds):
+    cls_iri_str = f"{KNORA_API_STR}LinkObj"
     resources = _util_request_resources_by_class(cls_iri_str, auth_header, project_iri, creds)
     assert len(resources) != 0
 
 
 @pytest.mark.usefixtures("_xmlupload")
-def test_audio(onto_iri, auth_header, project_iri, creds):
-    cls_iri_str = f"{onto_iri}TestAudioRepresentation"
+def test_audio_segment(auth_header, project_iri, creds):
+    cls_iri_str = f"{KNORA_API_STR}AudioSegment"
     resources = _util_request_resources_by_class(cls_iri_str, auth_header, project_iri, creds)
     assert len(resources) != 0
 
 
 @pytest.mark.usefixtures("_xmlupload")
-def test_video(onto_iri, auth_header, project_iri, creds):
-    cls_iri_str = f"{onto_iri}TestMovingImageRepresentation"
+def test_video_segment(auth_header, project_iri, creds):
+    cls_iri_str = f"{KNORA_API_STR}VideoSegment"
     resources = _util_request_resources_by_class(cls_iri_str, auth_header, project_iri, creds)
     assert len(resources) != 0

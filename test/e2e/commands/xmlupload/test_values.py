c42f81ecb96335f6d7640967b15d2f2ab5078163
Nora-Olivia-Ammann <103038637+Nora-Olivia-Ammann@users.noreply.github.com>
2025-05-09 08:45:21 +0200

    Update test_values.py

--- test/e2e/commands/xmlupload/test_values.py
+++ test/e2e/commands/xmlupload/test_values.py
@@ -1,8 +1,8 @@
 # mypy: disable-error-code="no-untyped-def"
 
 
+from rdflib import RDF
 from rdflib import Graph
-from rdflib import Literal
 from rdflib import URIRef
 
 from dsp_tools.utils.rdflib_constants import KNORA_API
@@ -20,15 +20,42 @@ def _assert_number_of_values_is_one_and_get_val_iri(g: Graph, label: str, prop_i
 
 
 def test_all_triples_of_a_value(class_with_everything_resource_graph, onto_iri):
-    prop_iri = URIRef(f"{onto_iri}")
-    val_iri = _assert_number_of_values_is_one_and_get_val_iri(class_with_everything_resource_graph, "", prop_iri)
-
-
-def test_(class_with_everything_resource_graph, onto_iri):
-    prop_iri = URIRef(f"{onto_iri}")
-    val_iri = _assert_number_of_values_is_one_and_get_val_iri(class_with_everything_resource_graph, "", prop_iri)
+    prop_iri = URIRef(f"{onto_iri}testBoolean")
+    val_iri = _assert_number_of_values_is_one_and_get_val_iri(
+        class_with_everything_resource_graph, "bool_true", prop_iri
+    )
     val_triples = list(class_with_everything_resource_graph.triples((val_iri, None, None)))
-    expected_val = Literal("")
-    actual_value = next(class_with_everything_resource_graph.objects(val_iri, KNORA_API))
-    assert actual_value == expected_val
-    assert len(val_triples) == BASE_NUMBER_OF_TRIPLES_PER_VALUE
+    props = [
+        KNORA_API.arkUrl,
+        KNORA_API.versionArkUrl,
+        KNORA_API.userHasPermission,
+        KNORA_API.attachedToUser,
+        KNORA_API.valueHasUUID,
+        KNORA_API.hasPermissions,
+        RDF.type,
+        KNORA_API.booleanValueAsBoolean,
+        KNORA_API.valueCreationDate,
+    ]
+    for prp in props:
+        assert len(list(class_with_everything_resource_graph.objects(val_iri, prp))) == 1
+    assert len(val_triples) == len(props)
+
+
+# def test_(class_with_everything_resource_graph, onto_iri):
+#     prop_iri = URIRef(f"{onto_iri}")
+#     val_iri = _assert_number_of_values_is_one_and_get_val_iri(class_with_everything_resource_graph, "", prop_iri)
+#     val_triples = list(class_with_everything_resource_graph.triples((val_iri, None, None)))
+#     expected_val = Literal("")
+#     actual_value = next(class_with_everything_resource_graph.objects(val_iri, KNORA_API))
+#     assert actual_value == expected_val
+#     assert len(val_triples) == BASE_NUMBER_OF_TRIPLES_PER_VALUE
+
+#
+# def test_(class_with_everything_resource_graph, onto_iri):
+#     prop_iri = URIRef(f"{onto_iri}")
+#     val_iri = _assert_number_of_values_is_one_and_get_val_iri(class_with_everything_resource_graph, "", prop_iri)
+#     val_triples = list(class_with_everything_resource_graph.triples((val_iri, None, None)))
+#     expected_val = Literal("")
+#     actual_value = next(class_with_everything_resource_graph.objects(val_iri, KNORA_API))
+#     assert actual_value == expected_val
+#     assert len(val_triples) == BASE_NUMBER_OF_TRIPLES_PER_VALUE

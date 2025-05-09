edf5b53eb3a263358839c16eab10fe8a44fca50e
Nora-Olivia-Ammann <103038637+Nora-Olivia-Ammann@users.noreply.github.com>
2025-05-09 09:08:34 +0200

    add correct assertions

--- test/e2e/commands/xmlupload/test_values.py
+++ test/e2e/commands/xmlupload/test_values.py
@@ -92,9 +92,9 @@ def test_color(class_with_everything_resource_graph, onto_iri):
     val_iri = _assert_number_of_values_is_one_and_get_val_iri(class_with_everything_resource_graph, "color", prop_iri)
     val_triples = list(class_with_everything_resource_graph.triples((val_iri, None, None)))
     expected_val = Literal("#00ff00")
-    actual_value = next(class_with_everything_resource_graph.objects(val_iri, KNORA_API))
+    actual_value = next(class_with_everything_resource_graph.objects(val_iri, KNORA_API.colorValueAsColor))
     assert actual_value == expected_val
-    assert next(class_with_everything_resource_graph.objects(val_iri, RDF.type)) == KNORA_API
+    assert next(class_with_everything_resource_graph.objects(val_iri, RDF.type)) == KNORA_API.ColorValue
     assert len(val_triples) == BASE_NUMBER_OF_TRIPLES_PER_VALUE
 
 
@@ -103,9 +103,9 @@ def test_date(class_with_everything_resource_graph, onto_iri):
     val_iri = _assert_number_of_values_is_one_and_get_val_iri(class_with_everything_resource_graph, "date", prop_iri)
     val_triples = list(class_with_everything_resource_graph.triples((val_iri, None, None)))
     expected_val = Literal("JULIAN:BCE:0700:BCE:0600")
-    actual_value = next(class_with_everything_resource_graph.objects(val_iri, KNORA_API))
+    actual_value = next(class_with_everything_resource_graph.objects(val_iri, KNORA_API.valueAsString))
     assert actual_value == expected_val
-    assert next(class_with_everything_resource_graph.objects(val_iri, RDF.type)) == KNORA_API
+    assert next(class_with_everything_resource_graph.objects(val_iri, RDF.type)) == KNORA_API.DateValue
     assert len(val_triples) == BASE_NUMBER_OF_TRIPLES_PER_VALUE
 
 
@@ -114,9 +114,9 @@ def test_decimal(class_with_everything_resource_graph, onto_iri):
     val_iri = _assert_number_of_values_is_one_and_get_val_iri(class_with_everything_resource_graph, "decimal", prop_iri)
     val_triples = list(class_with_everything_resource_graph.triples((val_iri, None, None)))
     expected_val = Literal(2.71)
-    actual_value = next(class_with_everything_resource_graph.objects(val_iri, KNORA_API))
+    actual_value = next(class_with_everything_resource_graph.objects(val_iri, KNORA_API.decimalValueAsDecimal))
     assert actual_value == expected_val
-    assert next(class_with_everything_resource_graph.objects(val_iri, RDF.type)) == KNORA_API
+    assert next(class_with_everything_resource_graph.objects(val_iri, RDF.type)) == KNORA_API.DecimalValue
     assert len(val_triples) == BASE_NUMBER_OF_TRIPLES_PER_VALUE
 
 
@@ -125,9 +125,9 @@ def test_geoname(class_with_everything_resource_graph, onto_iri):
     val_iri = _assert_number_of_values_is_one_and_get_val_iri(class_with_everything_resource_graph, "geoname", prop_iri)
     val_triples = list(class_with_everything_resource_graph.triples((val_iri, None, None)))
     expected_val = Literal("1111111")
-    actual_value = next(class_with_everything_resource_graph.objects(val_iri, KNORA_API))
+    actual_value = next(class_with_everything_resource_graph.objects(val_iri, KNORA_API.geonameValueAsGeonameCode))
     assert actual_value == expected_val
-    assert next(class_with_everything_resource_graph.objects(val_iri, RDF.type)) == KNORA_API
+    assert next(class_with_everything_resource_graph.objects(val_iri, RDF.type)) == KNORA_API.GeonameValue
     assert len(val_triples) == BASE_NUMBER_OF_TRIPLES_PER_VALUE
 
 
@@ -136,9 +136,9 @@ def test_integer(class_with_everything_resource_graph, onto_iri):
     val_iri = _assert_number_of_values_is_one_and_get_val_iri(class_with_everything_resource_graph, "integer", prop_iri)
     val_triples = list(class_with_everything_resource_graph.triples((val_iri, None, None)))
     expected_val = Literal(1)
-    actual_value = next(class_with_everything_resource_graph.objects(val_iri, KNORA_API))
+    actual_value = next(class_with_everything_resource_graph.objects(val_iri, KNORA_API.intValueAsInt))
     assert actual_value == expected_val
-    assert next(class_with_everything_resource_graph.objects(val_iri, RDF.type)) == KNORA_API
+    assert next(class_with_everything_resource_graph.objects(val_iri, RDF.type)) == KNORA_API.IntValue
     assert len(val_triples) == BASE_NUMBER_OF_TRIPLES_PER_VALUE
 
 
@@ -147,9 +147,9 @@ def test_list(class_with_everything_resource_graph, onto_iri):
     val_iri = _assert_number_of_values_is_one_and_get_val_iri(class_with_everything_resource_graph, "list", prop_iri)
     val_triples = list(class_with_everything_resource_graph.triples((val_iri, None, None)))
     expected_val = Literal("")
-    actual_value = next(class_with_everything_resource_graph.objects(val_iri, KNORA_API))
+    actual_value = next(class_with_everything_resource_graph.objects(val_iri, KNORA_API.listValueAsListNode))
     assert actual_value == expected_val
-    assert next(class_with_everything_resource_graph.objects(val_iri, RDF.type)) == KNORA_API
+    assert next(class_with_everything_resource_graph.objects(val_iri, RDF.type)) == KNORA_API.ListValue
     assert len(val_triples) == BASE_NUMBER_OF_TRIPLES_PER_VALUE
 
 
@@ -158,9 +158,9 @@ def test_link(class_with_everything_resource_graph, onto_iri):
     val_iri = _assert_number_of_values_is_one_and_get_val_iri(class_with_everything_resource_graph, "link", prop_iri)
     val_triples = list(class_with_everything_resource_graph.triples((val_iri, None, None)))
     target_iri = _util_get_res_iri_from_label(class_with_everything_resource_graph, "resource_no_values")
-    actual_value = next(class_with_everything_resource_graph.objects(val_iri, KNORA_API))
+    actual_value = next(class_with_everything_resource_graph.objects(val_iri, KNORA_API.linkValueHasTargetIri))
     assert actual_value == target_iri
-    assert next(class_with_everything_resource_graph.objects(val_iri, RDF.type)) == KNORA_API
+    assert next(class_with_everything_resource_graph.objects(val_iri, RDF.type)) == KNORA_API.LinkValue
     assert len(val_triples) == BASE_NUMBER_OF_TRIPLES_PER_VALUE
 
 
@@ -171,9 +171,9 @@ def test_richtext(class_with_everything_resource_graph, onto_iri):
     )
     val_triples = list(class_with_everything_resource_graph.triples((val_iri, None, None)))
     expected_val = Literal("")
-    actual_value = next(class_with_everything_resource_graph.objects(val_iri, KNORA_API))
+    actual_value = next(class_with_everything_resource_graph.objects(val_iri, KNORA_API.textValueAsXml))
     assert actual_value == expected_val
-    assert next(class_with_everything_resource_graph.objects(val_iri, RDF.type)) == KNORA_API
+    assert next(class_with_everything_resource_graph.objects(val_iri, RDF.type)) == KNORA_API.TextValue
     assert len(val_triples) == BASE_NUMBER_OF_TRIPLES_PER_VALUE + 1
 
 
@@ -184,9 +184,9 @@ def test_textarea(class_with_everything_resource_graph, onto_iri):
     )
     val_triples = list(class_with_everything_resource_graph.triples((val_iri, None, None)))
     expected_val = Literal("Line One\nLine Two")
-    actual_value = next(class_with_everything_resource_graph.objects(val_iri, KNORA_API))
+    actual_value = next(class_with_everything_resource_graph.objects(val_iri, KNORA_API.valueAsString))
     assert actual_value == expected_val
-    assert next(class_with_everything_resource_graph.objects(val_iri, RDF.type)) == KNORA_API
+    assert next(class_with_everything_resource_graph.objects(val_iri, RDF.type)) == KNORA_API.TextValue
     assert len(val_triples) == BASE_NUMBER_OF_TRIPLES_PER_VALUE
 
 
@@ -197,9 +197,9 @@ def test_simpletext(class_with_everything_resource_graph, onto_iri):
     )
     val_triples = list(class_with_everything_resource_graph.triples((val_iri, None, None)))
     expected_val = Literal("Text")
-    actual_value = next(class_with_everything_resource_graph.objects(val_iri, KNORA_API))
+    actual_value = next(class_with_everything_resource_graph.objects(val_iri, KNORA_API.valueAsString))
     assert actual_value == expected_val
-    assert next(class_with_everything_resource_graph.objects(val_iri, RDF.type)) == KNORA_API
+    assert next(class_with_everything_resource_graph.objects(val_iri, RDF.type)) == KNORA_API.TextValue
     assert len(val_triples) == BASE_NUMBER_OF_TRIPLES_PER_VALUE
 
 
@@ -208,9 +208,9 @@ def test_time(class_with_everything_resource_graph, onto_iri):
     val_iri = _assert_number_of_values_is_one_and_get_val_iri(class_with_everything_resource_graph, "time", prop_iri)
     val_triples = list(class_with_everything_resource_graph.triples((val_iri, None, None)))
     expected_val = Literal("2019-10-23T13:45:12.01-14:00", datatype=XSD.dateTimeStamp)
-    actual_value = next(class_with_everything_resource_graph.objects(val_iri, KNORA_API))
+    actual_value = next(class_with_everything_resource_graph.objects(val_iri, KNORA_API.timeValueAsTimeStamp))
     assert actual_value == expected_val
-    assert next(class_with_everything_resource_graph.objects(val_iri, RDF.type)) == KNORA_API
+    assert next(class_with_everything_resource_graph.objects(val_iri, RDF.type)) == KNORA_API.TimeValue
     assert len(val_triples) == BASE_NUMBER_OF_TRIPLES_PER_VALUE
 
 
@@ -219,7 +219,7 @@ def test_uri(class_with_everything_resource_graph, onto_iri):
     val_iri = _assert_number_of_values_is_one_and_get_val_iri(class_with_everything_resource_graph, "uri", prop_iri)
     val_triples = list(class_with_everything_resource_graph.triples((val_iri, None, None)))
     expected_val = Literal("https://dasch.swiss", datatype=XSD.anyURI)
-    actual_value = next(class_with_everything_resource_graph.objects(val_iri, KNORA_API))
+    actual_value = next(class_with_everything_resource_graph.objects(val_iri, KNORA_API.uriValueAsUri))
     assert actual_value == expected_val
-    assert next(class_with_everything_resource_graph.objects(val_iri, RDF.type)) == KNORA_API
+    assert next(class_with_everything_resource_graph.objects(val_iri, RDF.type)) == KNORA_API.UriValue
     assert len(val_triples) == BASE_NUMBER_OF_TRIPLES_PER_VALUE

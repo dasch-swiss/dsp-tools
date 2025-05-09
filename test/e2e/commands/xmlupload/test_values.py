603f0995f051c93df52904752f60975809c92f9b
Nora-Olivia-Ammann <103038637+Nora-Olivia-Ammann@users.noreply.github.com>
2025-05-09 08:51:00 +0200

    Commit all changes

--- test/e2e/commands/xmlupload/test_values.py
+++ test/e2e/commands/xmlupload/test_values.py
@@ -3,13 +3,18 @@
 
 from rdflib import RDF
 from rdflib import Graph
+from rdflib import Literal
 from rdflib import URIRef
 
 from dsp_tools.utils.rdflib_constants import KNORA_API
 from dsp_tools.utils.rdflib_constants import SubjectObjectTypeAlias
 from test.e2e.commands.xmlupload.conftest import _util_get_res_iri_from_label
 
-BASE_NUMBER_OF_TRIPLES_PER_VALUE = 0
+BASE_NUMBER_OF_TRIPLES_PER_VALUE = 9
+OPEN_PERMISSIONS = Literal(
+    "CR knora-admin:ProjectAdmin|D knora-admin:ProjectMember|V knora-admin:KnownUser,knora-admin:UnknownUser"
+)
+DOAP_PERMISSIONS = Literal("CR knora-admin:ProjectAdmin|D knora-admin:ProjectMember")
 
 
 def _assert_number_of_values_is_one_and_get_val_iri(g: Graph, label: str, prop_iri: URIRef) -> SubjectObjectTypeAlias:
@@ -41,16 +46,28 @@ def test_all_triples_of_a_value(class_with_everything_resource_graph, onto_iri):
     assert len(val_triples) == len(props)
 
 
-# def test_(class_with_everything_resource_graph, onto_iri):
-#     prop_iri = URIRef(f"{onto_iri}")
-#     val_iri = _assert_number_of_values_is_one_and_get_val_iri(class_with_everything_resource_graph, "", prop_iri)
-#     val_triples = list(class_with_everything_resource_graph.triples((val_iri, None, None)))
-#     expected_val = Literal("")
-#     actual_value = next(class_with_everything_resource_graph.objects(val_iri, KNORA_API))
-#     assert actual_value == expected_val
-#     assert len(val_triples) == BASE_NUMBER_OF_TRIPLES_PER_VALUE
+def test_doap(class_with_everything_resource_graph, onto_iri):
+    prop_iri = URIRef(f"{onto_iri}testBoolean")
+    val_iri = _assert_number_of_values_is_one_and_get_val_iri(
+        class_with_everything_resource_graph, "bool_true", prop_iri
+    )
+    permissions = next(class_with_everything_resource_graph.objects(val_iri, KNORA_API.hasPermissions))
+    assert permissions == DOAP_PERMISSIONS
 
-#
+
+def test_bool_true(class_with_everything_resource_graph, onto_iri):
+    prop_iri = URIRef(f"{onto_iri}testBoolean")
+    val_iri = _assert_number_of_values_is_one_and_get_val_iri(
+        class_with_everything_resource_graph, "bool_true", prop_iri
+    )
+    val_triples = list(class_with_everything_resource_graph.triples((val_iri, None, None)))
+    expected_val = Literal(True)
+    actual_value = next(class_with_everything_resource_graph.objects(val_iri, KNORA_API.booleanValueAsBoolean))
+    assert actual_value == expected_val
+    assert next(class_with_everything_resource_graph.objects(val_iri, RDF.type)) == KNORA_API.BooleanValue
+    assert len(val_triples) == BASE_NUMBER_OF_TRIPLES_PER_VALUE
+
+
 # def test_(class_with_everything_resource_graph, onto_iri):
 #     prop_iri = URIRef(f"{onto_iri}")
 #     val_iri = _assert_number_of_values_is_one_and_get_val_iri(class_with_everything_resource_graph, "", prop_iri)

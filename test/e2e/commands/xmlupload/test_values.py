ac7371464bb518c3a292c6e3bab8035238681390
Nora-Olivia-Ammann <103038637+Nora-Olivia-Ammann@users.noreply.github.com>
2025-05-09 08:22:33 +0200

    Update test_values.py

--- test/e2e/commands/xmlupload/test_values.py
+++ test/e2e/commands/xmlupload/test_values.py
@@ -2,16 +2,15 @@
 
 
 import pytest
+from rdflib import Literal
 
 from dsp_tools.utils.rdflib_constants import KNORA_API_STR, KNORA_API
 from test.e2e.commands.xmlupload.conftest import _util_request_resources_by_class, _util_get_res_iri_from_label
 
-
-def test_class_with_everything_all_created(self, class_with_everything_resource_graph):
-    assert len(class_with_everything_resource_graph) != 0
-
+BASE_NUMBER_OF_TRIPLES_PER_VALUE = 0
 
 def test_(class_with_everything_resource_graph):
     res_iri = _util_get_res_iri_from_label(class_with_everything_resource_graph, "")
     
+    expected_val = Literal("")
 

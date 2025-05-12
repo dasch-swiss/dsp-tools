17b4672207882f9a3a4f66a7fe79c617231fdd33
Nora-Olivia-Ammann <103038637+Nora-Olivia-Ammann@users.noreply.github.com>
2025-05-12 17:47:15 +0200

    feat(xmllib): add new license IRIs (DEV-4873) (#1707)
    Co-authored-by: Johannes Nussbaum <39048939+jnussbaum@users.noreply.github.com>

--- src/dsp_tools/xmllib/__init__.py
+++ src/dsp_tools/xmllib/__init__.py
@@ -1,10 +1,16 @@
 from dsp_tools.error.xmllib_warnings_util import initialise_warning_file
 
 from .helpers import ListLookup as ListLookup
+from .helpers import clean_whitespaces_from_string as clean_whitespaces_from_string
+from .helpers import create_footnote_element as create_footnote_element
+from .helpers import create_footnote_string as create_footnote_string
 from .helpers import create_list_from_string as create_list_from_string
 from .helpers import create_non_empty_list_from_string as create_non_empty_list_from_string
+from .helpers import create_standoff_link_to_resource as create_standoff_link_to_resource
+from .helpers import create_standoff_link_to_uri as create_standoff_link_to_uri
 from .helpers import escape_reserved_xml_characters as escape_reserved_xml_characters
 from .helpers import find_date_in_string as find_date_in_string
+from .helpers import find_license_in_string as find_license_in_string
 from .helpers import get_list_nodes_from_string_via_list_name as get_list_nodes_from_string_via_list_name
 from .helpers import get_list_nodes_from_string_via_property as get_list_nodes_from_string_via_property
 from .helpers import make_xsd_compatible_id as make_xsd_compatible_id
@@ -15,9 +21,11 @@ from .models.dsp_base_resources import AudioSegmentResource as AudioSegmentResou
 from .models.dsp_base_resources import LinkResource as LinkResource
 from .models.dsp_base_resources import RegionResource as RegionResource
 from .models.dsp_base_resources import VideoSegmentResource as VideoSegmentResource
+from .models.licenses.other import LicenseOther as LicenseOther
 from .models.licenses.recommended import LicenseRecommended as LicenseRecommended
 from .models.res import Resource as Resource
 from .models.root import XMLRoot as XMLRoot
+from .value_checkers import check_richtext_syntax as check_richtext_syntax
 from .value_checkers import is_bool_like as is_bool_like
 from .value_checkers import is_color as is_color
 from .value_checkers import is_date as is_date
---
+++ src/dsp_tools/xmllib/models/licenses/other.py
@@ -0,0 +1,57 @@
+from __future__ import annotations
+
+from dsp_tools.xmllib.models.licenses.recommended import License
+
+
+class LicenseOther:
+    """
+    Pre-defined licenses that are available in DSP.
+    If the user is free to choose a license, we recommend to choose from our [recommended licenses](https://docs.dasch.swiss/latest/DSP-TOOLS/xmllib-api-reference/licenses/recommended/).
+
+    - `Public`: [See `Public` for details](https://docs.dasch.swiss/latest/DSP-TOOLS/xmllib-api-reference/licenses/other/#xmllib.models.licenses.other.Public).
+    - `Various`: [See `Various` for details](https://docs.dasch.swiss/latest/DSP-TOOLS/xmllib-api-reference/licenses/other/#xmllib.models.licenses.other.Various).
+
+    Examples:
+        ```python
+        # adding a BORIS Standard License to a file
+        resource = resource.add_file(
+            filename="images/cat.jpg",
+            license=xmllib.LicenseOther.Various.BORIS_STANDARD,
+            copyright_holder="Meow University",
+            authorship=["Kitty Meow"],
+        )
+        ```
+    """
+
+    Public: type[Public]
+    Various: type[Various]
+
+
+class Public(License):
+    """
+    Pre-defined public domain licenses.
+    [See the API documentation for details about the licenses.](https://docs.dasch.swiss/latest/DSP-API/01-introduction/legal-info/#license)
+
+    - `CC_0_1_0`: CC0 1.0 Universal
+    - `CC_PDM_1_0`: Public Domain Mark 1.0 Universal
+    """
+
+    CC_0_1_0 = "http://rdfh.ch/licenses/cc-0-1.0"
+    CC_PDM_1_0 = "http://rdfh.ch/licenses/cc-pdm-1.0"
+
+
+class Various(License):
+    """
+    A collection of various, pre-defined licenses.
+    [See the API documentation for details about the licenses.](https://docs.dasch.swiss/latest/DSP-API/01-introduction/legal-info/#license)
+
+    - `BORIS_STANDARD`: BORIS Standard License
+    - `FRANCE_OUVERTE`: LICENCE OUVERTE 2.0
+    """
+
+    BORIS_STANDARD = "http://rdfh.ch/licenses/boris"
+    FRANCE_OUVERTE = "http://rdfh.ch/licenses/open-licence-2.0"
+
+
+LicenseOther.Public = Public
+LicenseOther.Various = Various

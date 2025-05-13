8240c7f551eab12b7a127c4052b6c77d5eb14d16
Nora-Olivia-Ammann <103038637+Nora-Olivia-Ammann@users.noreply.github.com>
2025-05-13 14:45:37 +0200

    add test

--- test/unittests/xmllib/models/test_root.py
+++ test/unittests/xmllib/models/test_root.py
@@ -1,11 +1,102 @@
 from lxml import etree
 
+from dsp_tools.xmllib.internal.constants import DASCH_SCHEMA
+from dsp_tools.xmllib.models.config_options import Permissions
+from dsp_tools.xmllib.models.dsp_base_resources import AudioSegmentResource
+from dsp_tools.xmllib.models.dsp_base_resources import LinkResource
 from dsp_tools.xmllib.models.dsp_base_resources import RegionResource
+from dsp_tools.xmllib.models.dsp_base_resources import VideoSegmentResource
 from dsp_tools.xmllib.models.licenses.recommended import LicenseRecommended
 from dsp_tools.xmllib.models.res import Resource
 from dsp_tools.xmllib.models.root import XMLRoot
 from dsp_tools.xmllib.models.root import _make_authorship_lookup
 from dsp_tools.xmllib.models.root import _serialise_authorship
+
+
+class TestSerialise:
+    def test_regular_resource(self):
+        xml_root = XMLRoot.create_new("0000", "test")
+        xml_root.add_resource(
+            Resource.create_new(
+                "with_open_permissions",
+                ":Restype",
+                "label",
+                Permissions.OPEN,
+            ).add_bool(":boolProp", True, comment="cmnt")
+        )
+        serialised = xml_root.serialise()
+        found = list(serialised.iterdescendants(tag=f"{DASCH_SCHEMA}resource"))
+        assert len(found) == 1
+        resource = found.pop(0)
+        vals = list(resource.iterchildren())
+        assert len(vals) == 0
+
+    def test_region(self):
+        xml_root = XMLRoot.create_new("0000", "test")
+        xml_root.add_resource(
+            RegionResource.create_new(
+                "region_no_permissions",
+                "label",
+                "region_of_val",
+            ).add_rectangle((0.1, 0.1), (0.2, 0.2))
+        )
+        serialised = xml_root.serialise()
+        found = list(serialised.iterdescendants(tag=f"{DASCH_SCHEMA}region"))
+        assert len(found) == 1
+        resource = found.pop(0)
+        vals = list(resource.iterchildren())
+        assert len(vals) == 0
+
+    def test_link_resource(self):
+        xml_root = XMLRoot.create_new("0000", "test")
+        xml_root.add_resource(
+            LinkResource.create_new(
+                "link_id",
+                "lbl",
+                ["link1", "link2"],
+            ).add_comment("cmnt", Permissions.RESTRICTED)
+        )
+        serialised = xml_root.serialise()
+        found = list(serialised.iterdescendants(tag=f"{DASCH_SCHEMA}link"))
+        assert len(found) == 1
+        resource = found.pop(0)
+        vals = list(resource.iterchildren())
+        assert len(vals) == 0
+
+    def test_audio_segment(self):
+        xml_root = XMLRoot.create_new("0000", "test")
+        xml_root.add_resource(
+            AudioSegmentResource.create_new("audio_id", "lbl", "segment_of", 1, 2)
+            .add_title("title")
+            .add_comment("cmnt")
+            .add_description("desc")
+            .add_keyword("keywrd")
+            .add_relates_to("relates_to")
+        )
+        serialised = xml_root.serialise()
+        found = list(serialised.iterdescendants(tag=f"{DASCH_SCHEMA}audio-segment"))
+        assert len(found) == 1
+        resource = found.pop(0)
+        vals = list(resource.iterchildren())
+        assert len(vals) == 0
+
+    def test_video_segment(self):
+        xml_root = XMLRoot.create_new("0000", "test")
+        xml_root.add_resource(
+            VideoSegmentResource.create_new(
+                "audio_id",
+                "lbl",
+                "segment_of",
+                1,
+                2,
+            )
+        )
+        serialised = xml_root.serialise()
+        found = list(serialised.iterdescendants(tag=f"{DASCH_SCHEMA}video-segment"))
+        assert len(found) == 1
+        resource = found.pop(0)
+        vals = list(resource.iterchildren())
+        assert len(vals) == 0
 
 
 def test_root_add_resources() -> None:
@@ -31,9 +122,9 @@ def test_root_add_resources() -> None:
     assert len(xml_root.resources) == 3
 
     serialised = xml_root.serialise()
-    region = list(serialised.iterdescendants(tag="{https://dasch.swiss/schema}region"))
+    region = list(serialised.iterdescendants(tag=f"{DASCH_SCHEMA}region"))
     assert len(region) == 1
-    general_resources = list(serialised.iterdescendants(tag="{https://dasch.swiss/schema}resource"))
+    general_resources = list(serialised.iterdescendants(tag=f"{DASCH_SCHEMA}resource"))
     assert len(general_resources) == 2
 
 

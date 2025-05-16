fa01d472b7b42ab78e1fd2bd0d11897547d1d04e
Nora-Olivia-Ammann <103038637+Nora-Olivia-Ammann@users.noreply.github.com>
2025-05-16 10:58:03 +0200

    Commit all changes

--- test/integration/commands/xmlupload/authentication_client_mock.py
+++ test/integration/commands/xmlupload/authentication_client_mock.py
@@ -2,5 +2,9 @@ from dsp_tools.clients.authentication_client import AuthenticationClient
 
 
 class AuthenticationClientMockBase(AuthenticationClient):
+    user: str
+    password: str
+    server: str
+
     def get_token(self) -> str:
         return "mocked_token"

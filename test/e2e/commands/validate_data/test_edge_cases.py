fd49429d16d754f19aaefac115be65f33ebcf29d
Nora-Olivia-Ammann <103038637+Nora-Olivia-Ammann@users.noreply.github.com>
2025-05-16 11:03:14 +0200

    Update test_edge_cases.py

--- test/e2e/commands/validate_data/test_edge_cases.py
+++ test/e2e/commands/validate_data/test_edge_cases.py
@@ -39,35 +39,35 @@ def authentication(creds: ServerCredentials) -> AuthenticationClient:
 
 @pytest.fixture(scope="module")
 def special_characters_violation(
-    _create_projects_edge_cases, api_url: str, shacl_validator: ShaclValidator
+    _create_projects_edge_cases, authentication: AuthenticationClient, shacl_validator: ShaclValidator
 ) -> ValidationReportGraphs:
     file = Path("testdata/validate-data/special_characters/special_characters_violation.xml")
-    graphs, _ = _prepare_data_for_validation_from_file(api_url, file)
+    graphs, _ = _prepare_data_for_validation_from_file(file, authentication)
     return _get_validation_result(graphs, shacl_validator, CONFIG)
 
 
 @pytest.fixture(scope="module")
 def inheritance_violation(
-    _create_projects_edge_cases, api_url: str, shacl_validator: ShaclValidator
+    _create_projects_edge_cases, authentication: AuthenticationClient, shacl_validator: ShaclValidator
 ) -> ValidationReportGraphs:
     file = Path("testdata/validate-data/inheritance/inheritance_violation.xml")
-    graphs, _ = _prepare_data_for_validation_from_file(api_url, file)
+    graphs, _ = _prepare_data_for_validation_from_file(file, authentication)
     return _get_validation_result(graphs, shacl_validator, CONFIG)
 
 
 @pytest.fixture(scope="module")
 def validate_ontology_violation(
-    _create_projects_edge_cases, api_url: str, shacl_validator: ShaclValidator
+    _create_projects_edge_cases, authentication: AuthenticationClient, shacl_validator: ShaclValidator
 ) -> OntologyValidationProblem | None:
     file = Path("testdata/validate-data/erroneous_ontology/erroneous_ontology.xml")
-    graphs, _ = _prepare_data_for_validation_from_file(api_url, file)
+    graphs, _ = _prepare_data_for_validation_from_file(file, authentication)
     return validate_ontology(graphs.ontos, shacl_validator, CONFIG)
 
 
 @pytest.mark.usefixtures("_create_projects_edge_cases")
-def test_special_characters_correct(api_url: str, shacl_validator: ShaclValidator) -> None:
+def test_special_characters_correct(authentication: AuthenticationClient, shacl_validator: ShaclValidator) -> None:
     file = Path("testdata/validate-data/special_characters/special_characters_correct.xml")
-    graphs, _ = _prepare_data_for_validation_from_file(api_url, file)
+    graphs, _ = _prepare_data_for_validation_from_file(file, authentication)
     special_characters_correct = _get_validation_result(graphs, shacl_validator, CONFIG)
     assert special_characters_correct.conforms
 
@@ -131,9 +131,9 @@ def test_reformat_special_characters_violation(special_characters_violation: Val
 
 
 @pytest.mark.usefixtures("_create_projects_edge_cases")
-def test_inheritance_correct(api_url: str, shacl_validator: ShaclValidator) -> None:
+def test_inheritance_correct(authentication: AuthenticationClient, shacl_validator: ShaclValidator) -> None:
     file = Path("testdata/validate-data/inheritance/inheritance_correct.xml")
-    graphs, _ = _prepare_data_for_validation_from_file(api_url, file)
+    graphs, _ = _prepare_data_for_validation_from_file(file, authentication)
     inheritance_correct = _get_validation_result(graphs, shacl_validator, CONFIG)
     assert inheritance_correct.conforms
 

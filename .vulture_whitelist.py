e4e5c66f0d4814c119f35793f4bdc29822d4e125
Nora-Olivia-Ammann <103038637+Nora-Olivia-Ammann@users.noreply.github.com>
2025-05-22 14:06:30 +0200

    vulture

--- .vulture_whitelist.py
+++ .vulture_whitelist.py
@@ -1,6 +1,7 @@
 from dsp_tools.commands.xmlupload.iri_resolver import IriResolver
 from dsp_tools.commands.xmlupload.models.permission import PermissionValue
 from dsp_tools.utils.ansi_colors import BOLD_GREEN
+from dsp_tools.utils.ansi_colors import YELLOW
 from dsp_tools.utils.data_formats.date_util import is_full_date
 from dsp_tools.utils.data_formats.shared import check_notna
 
@@ -11,6 +12,7 @@ PermissionValue.D
 PermissionValue.CR
 
 BOLD_GREEN
+YELLOW
 
 IriResolver.non_empty()
 
--- test/e2e/commands/validate_data/test_generic_violations.py
+++ test/e2e/commands/validate_data/test_generic_violations.py
@@ -26,7 +26,6 @@ from dsp_tools.commands.validate_data.query_validation_result import reformat_va
 from dsp_tools.commands.validate_data.validate_data import _check_for_unknown_resource_classes
 from dsp_tools.commands.validate_data.validate_data import _get_validation_result
 from dsp_tools.commands.validate_data.validate_data import _prepare_data_for_validation_from_file
-from dsp_tools.error.custom_warnings import DspToolsUserInfo
 
 # ruff: noqa: ARG001 Unused function argument
 
@@ -105,9 +104,7 @@ def value_type_violation(
     create_generic_project, authentication, shacl_validator: ShaclValidator
 ) -> ValidationReportGraphs:
     file = Path("testdata/validate-data/generic/value_type_violation.xml")
-    match = r"Angular brackets in the format of <text> were found in text properties with encoding=utf8"
-    with pytest.warns(DspToolsUserInfo, match=match):
-        graphs, _ = _prepare_data_for_validation_from_file(file, authentication)
+    graphs, _ = _prepare_data_for_validation_from_file(file, authentication)
     return _get_validation_result(graphs, shacl_validator, CONFIG)
 
 

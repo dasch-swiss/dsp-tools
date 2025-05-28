4531a1c69f2b42cdae81e9d98da17d4c782032ab
Nora-Olivia-Ammann <103038637+Nora-Olivia-Ammann@users.noreply.github.com>
2025-05-28 13:41:03 +0200

    add to test data

--- test/e2e/commands/validate_data/test_generic_correct.py
+++ test/e2e/commands/validate_data/test_generic_correct.py
@@ -10,9 +10,7 @@ from dsp_tools.cli.args import ValidationSeverity
 from dsp_tools.clients.authentication_client import AuthenticationClient
 from dsp_tools.clients.authentication_client_live import AuthenticationClientLive
 from dsp_tools.commands.validate_data.api_clients import ShaclValidator
-from dsp_tools.commands.validate_data.get_user_validation_message import sort_user_problems
 from dsp_tools.commands.validate_data.models.validation import RDFGraphs
-from dsp_tools.commands.validate_data.query_validation_result import reformat_validation_graph
 from dsp_tools.commands.validate_data.validate_data import _check_for_unknown_resource_classes
 from dsp_tools.commands.validate_data.validate_data import _get_validation_result
 from dsp_tools.commands.validate_data.validate_data import _prepare_data_for_validation_from_file
@@ -59,14 +57,7 @@ def test_content_correct(authentication, shacl_validator: ShaclValidator) -> Non
     file = Path("testdata/validate-data/generic/content_correct.xml")
     graphs, _ = _prepare_data_for_validation_from_file(file, authentication)
     content_correct = _get_validation_result(graphs, shacl_validator, CONFIG)
-    # The referenced absolute IRIs are perceived as a violation in SHACL
-    # because the resource does not exist in the graph
-    assert not content_correct.conforms
-    reformatted = reformat_validation_graph(content_correct)
-    sorted_messages = sort_user_problems(reformatted)
-    assert not sorted_messages.unique_violations
-    assert len(sorted_messages.user_info) == 1
-    assert not sorted_messages.unexpected_shacl_validation_components
+    assert content_correct.conforms
 
 
 @pytest.mark.usefixtures("create_generic_project")

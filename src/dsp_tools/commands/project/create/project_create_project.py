6285a44f2327075afea18341a554b613da8281e6
Johannes Nussbaum <johannes.nussbaum@dasch.swiss>
2025-06-05 16:00:58 +0200

    UserError -> InputError

--- src/dsp_tools/commands/project/create/project_create_project.py
+++ src/dsp_tools/commands/project/create/project_create_project.py
@@ -4,7 +4,7 @@ from loguru import logger
 
 from dsp_tools.commands.project.models.project_creation_client import ProjectCreationClient
 from dsp_tools.commands.project.models.project_definition import ProjectMetadata
-from dsp_tools.models.exceptions import UserError
+from dsp_tools.error.exceptions import InputError
 
 
 def create_project_on_server(
@@ -15,8 +15,8 @@ def create_project_on_server(
     Create the project on the DSP server.
 
     Raises:
-        UserError: if the project already exists on the server
-        UserError: if the project cannot be created on the DSP server
+        InputError: if the project already exists on the server
+        InputError: if the project cannot be created on the DSP server
 
     Returns:
         success status (True if everything went smoothly, False otherwise)
@@ -24,11 +24,11 @@ def create_project_on_server(
     project_designation = f"{project_definition.shortcode} {project_definition.shortname}"
     existing_shortcodes, existing_shortnames = proj_client.get_existing_shortcodes_and_shortnames()
     if project_definition.shortcode in existing_shortcodes or project_definition.shortname in existing_shortnames:
-        raise UserError(f"A project with the shortcode/shortname {project_designation} already exists on the server.")
+        raise InputError(f"A project with the shortcode/shortname {project_designation} already exists on the server.")
 
     data = _serialize_project(project_definition)
     if not (success := proj_client.create_project(data)):
-        raise UserError(f"Cannot create project {project_designation} on DSP server")
+        raise InputError(f"Cannot create project {project_designation} on DSP server")
     print(f"    Created project {project_designation}")
     logger.info(f"Created project {project_designation}")
 

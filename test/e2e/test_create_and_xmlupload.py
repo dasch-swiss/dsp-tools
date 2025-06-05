b43c405aa119b3b4d09440b6ff1ad57f946eaef5
Nora-Olivia-Ammann <103038637+Nora-Olivia-Ammann@users.noreply.github.com>
2025-06-05 10:08:35 +0200

    Update test_create_and_xmlupload.py

--- test/e2e/test_create_and_xmlupload.py
+++ test/e2e/test_create_and_xmlupload.py
@@ -20,7 +20,8 @@ from test.e2e.setup_testcontainers.ports import ExternalContainerPorts
 from test.e2e.setup_testcontainers.setup import get_containers
 
 PROJECT_SHORTCODE = "4125"
-ONTO_NAME = "e2e-testonto"
+E2E_TESTONTO_PREFIX = "e2e-testonto"
+SECOND_ONTO_PREFIX = "second-onto"
 PROPS_IN_ONTO_JSON = 1
 RESCLASSES_IN_ONTO_JSON = 2
 
@@ -42,8 +43,13 @@ def creds(container_ports: ExternalContainerPorts) -> ServerCredentials:
 
 
 @pytest.fixture(scope="module")
-def onto_iri(creds: ServerCredentials) -> str:
-    return f"{creds.server}/ontology/{PROJECT_SHORTCODE}/{ONTO_NAME}/v2"
+def e2e_testonto_iri(creds: ServerCredentials) -> str:
+    return f"{creds.server}/ontology/{PROJECT_SHORTCODE}/{E2E_TESTONTO_PREFIX}/v2"
+
+
+@pytest.fixture(scope="module")
+def second_onto_iri(creds: ServerCredentials) -> str:
+    return f"{creds.server}/ontology/{PROJECT_SHORTCODE}/{SECOND_ONTO_PREFIX}/v2"
 
 
 @pytest.fixture(scope="module")
@@ -74,12 +80,14 @@ def project_iri(_create_project: None, creds: ServerCredentials) -> str:
 
 
 @pytest.mark.usefixtures("_create_project")
-def test_project(auth_header: dict[str, str], creds: ServerCredentials, onto_iri: str) -> None:
+def test_project(
+    auth_header: dict[str, str], creds: ServerCredentials, e2e_testonto_iri: str, second_onto_iri: str
+) -> None:
     get_project_url = f"{creds.server}/admin/projects/shortcode/{PROJECT_SHORTCODE}"
     project = requests.get(get_project_url, headers=auth_header, timeout=3).json()["project"]
-    _check_project(project, onto_iri)
+    _check_project(project, e2e_testonto_iri, second_onto_iri)
 
-    onto = requests.get(onto_iri, headers=auth_header, timeout=3).json()["@graph"]
+    onto = requests.get(e2e_testonto_iri, headers=auth_header, timeout=3).json()["@graph"]
     _check_props([elem for elem in onto if elem.get("knora-api:isResourceProperty")])
     _check_resclasses([elem for elem in onto if elem.get("knora-api:isResourceClass")])
 
@@ -102,10 +110,12 @@ def _get_enabled_licenses(auth_header: dict[str, str], creds: ServerCredentials)
 
 
 @pytest.mark.usefixtures("_xmlupload")
-def test_xmlupload(auth_header: dict[str, str], project_iri: str, creds: ServerCredentials, onto_iri: str) -> None:
-    img_resources = _get_resources(f"{onto_iri}#ImageResource", auth_header, project_iri, creds)
+def test_xmlupload(
+    auth_header: dict[str, str], project_iri: str, creds: ServerCredentials, e2e_testonto_iri: str
+) -> None:
+    img_resources = _get_resources(f"{e2e_testonto_iri}#ImageResource", auth_header, project_iri, creds)
     _analyze_img_resources(img_resources)
-    pdf_resources = _get_resources(f"{onto_iri}#PDFResource", auth_header, project_iri, creds)
+    pdf_resources = _get_resources(f"{e2e_testonto_iri}#PDFResource", auth_header, project_iri, creds)
     _analyze_pdf_resources(pdf_resources)
 
 
@@ -127,18 +137,18 @@ def _get_copyright_holders(auth_header: dict[str, str], creds: ServerCredentials
     return dict(response)
 
 
-def _check_project(project: dict[str, Any], onto_iri: str) -> None:
+def _check_project(project: dict[str, Any], onto_iri: str, second_onto_iri: str) -> None:
     assert project["shortname"] == "e2e-tp"
     assert project["shortcode"] == PROJECT_SHORTCODE
     assert project["longname"] == "e2e test project"
     assert project["description"] == [{"value": "The e2e test project", "language": "en"}]
     assert project["keywords"] == ["e2e-test-projec-keyword"]
-    assert project["ontologies"] == [onto_iri]
+    assert project["ontologies"] == [onto_iri, second_onto_iri]
 
 
 def _check_props(props: list[dict[str, Any]]) -> None:
     assert len(props) == PROPS_IN_ONTO_JSON
-    assert props[0]["@id"] == f"{ONTO_NAME}:hasText"
+    assert props[0]["@id"] == f"{E2E_TESTONTO_PREFIX}:hasText"
     assert props[0]["rdfs:subPropertyOf"] == {"@id": "knora-api:hasValue"}
     assert props[0]["knora-api:objectType"] == {"@id": "knora-api:TextValue"}
     assert props[0]["rdfs:label"] == "Text"
@@ -148,16 +158,16 @@ def _check_resclasses(resclasses: list[dict[str, Any]]) -> None:
     assert len(resclasses) == RESCLASSES_IN_ONTO_JSON
     res_1, res_2 = resclasses
 
-    assert res_1["@id"] == f"{ONTO_NAME}:ImageResource"
+    assert res_1["@id"] == f"{E2E_TESTONTO_PREFIX}:ImageResource"
     assert res_1["rdfs:label"] == "Image Resource"
     cards_1 = res_1["rdfs:subClassOf"]
     fileval_card = [x for x in cards_1 if x.get("owl:onProperty", {}).get("@id") == "knora-api:hasStillImageFileValue"]
     assert len(fileval_card) == 1
     assert fileval_card[0].get("owl:cardinality") == 1
-    hasText_card = [x for x in cards_1 if x.get("owl:onProperty", {}).get("@id") == f"{ONTO_NAME}:hasText"]
+    hasText_card = [x for x in cards_1 if x.get("owl:onProperty", {}).get("@id") == f"{E2E_TESTONTO_PREFIX}:hasText"]
     assert len(hasText_card) == 1
 
-    assert res_2["@id"] == f"{ONTO_NAME}:PDFResource"
+    assert res_2["@id"] == f"{E2E_TESTONTO_PREFIX}:PDFResource"
     assert res_2["rdfs:label"] == "PDF Resource"
 
 

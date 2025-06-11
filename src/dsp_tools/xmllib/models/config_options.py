f656cb8b335bd9d6708ccd0cb4eba3b9b8418846
Nora-Olivia-Ammann <103038637+Nora-Olivia-Ammann@users.noreply.github.com>
2025-06-11 14:31:24 +0200

    add american order

--- src/dsp_tools/xmllib/models/config_options.py
+++ src/dsp_tools/xmllib/models/config_options.py
@@ -56,17 +56,43 @@ class NewlineReplacement(Enum):
 
 
 class DateOrder(Enum):
+    """
+    Date order options for the [`reformat_date`](https://docs.dasch.swiss/latest/DSP-TOOLS/xmllib-api-reference/helpers/#xmllib.helpers.reformat_date) function.
+
+    - `YYYY_MM_DD`
+    - `DD_MM_YYYY`
+    - `MM_DD_YYYY`
+    """  # noqa: E501
+
     YYYY_MM_DD = auto()
     DD_MM_YYYY = auto()
+    MM_DD_YYYY = auto()
 
 
 class Calendar(Enum):
+    """
+    Calendar options for the [`reformat_date`](https://docs.dasch.swiss/latest/DSP-TOOLS/xmllib-api-reference/helpers/#xmllib.helpers.reformat_date) function.
+
+    - `GREGORIAN`
+    - `JULIAN`
+    - `ISLAMIC`
+    """  # noqa: E501
+
     GREGORIAN = "GREGORIAN"
     JULIAN = "JULIAN"
     ISLAMIC = "ISLAMIC"
 
 
 class Era(Enum):
+    """
+    Era options for the [`reformat_date`](https://docs.dasch.swiss/latest/DSP-TOOLS/xmllib-api-reference/helpers/#xmllib.helpers.reformat_date) function.
+
+    - `CE`
+    - `BCE`
+    - `AD`
+    - `BC`
+    """  # noqa: E501
+
     CE = "CE"
     BCE = "BCE"
     AD = "AD"

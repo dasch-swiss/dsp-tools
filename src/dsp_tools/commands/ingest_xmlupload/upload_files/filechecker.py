007f64148b839f8113b00fdab3c28c56cfcdd29e
Johannes Nussbaum <39048939+jnussbaum@users.noreply.github.com>
2025-06-24 20:52:57 +0200

    feat(xmlupload): add support for HTML and EPUB files (DEV-5084) (#1805)

--- src/dsp_tools/commands/ingest_xmlupload/upload_files/filechecker.py
+++ src/dsp_tools/commands/ingest_xmlupload/upload_files/filechecker.py
@@ -6,8 +6,8 @@ from pathlib import Path
 from dsp_tools.commands.ingest_xmlupload.upload_files.input_error import FileProblems
 
 SUPPORTED_EXTENSIONS = (
-    "zip,tar,gz,z,tgz,gzip,7z,mp3,wav,pdf,doc,docx,xls,xlsx,ppt,pptx,"
-    "mp4,jpg,jpeg,jp2,png,tif,tiff,odd,rng,txt,xml,xsd,xsl,csv"
+    "zip,tar,gz,z,tgz,gzip,7z,mp3,wav,pdf,doc,docx,xls,xlsx,ppt,pptx,epub,"
+    "mp4,jpg,jpeg,jp2,png,tif,tiff,odd,rng,txt,xml,htm,html,xsd,xsl,csv"
 ).split(",")
 
 

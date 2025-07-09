a3415ae9a21b97fb9e88ed9089c71cd3261b1170
Nora-Olivia-Ammann <103038637+Nora-Olivia-Ammann@users.noreply.github.com>
2025-07-09 13:59:50 +0200

    Update conftest.py

--- test/e2e_validate_data/conftest.py
+++ test/e2e_validate_data/conftest.py
@@ -5,7 +5,7 @@ import pytest
 
 from dsp_tools.cli.args import ServerCredentials
 from dsp_tools.commands.project.create.project_create_all import create_project
-from dsp_tools.commands.validate_data.api_clients import ShaclValidator
+from dsp_tools.commands.validate_data.shacl_cli_validator import ShaclCliValidator
 from test.e2e.setup_testcontainers.ports import ExternalContainerPorts
 from test.e2e.setup_testcontainers.setup import get_containers
 
@@ -27,13 +27,8 @@ def creds(container_ports: ExternalContainerPorts) -> ServerCredentials:
 
 
 @pytest.fixture(scope="module")
-def api_url(container_ports: ExternalContainerPorts) -> str:
-    return f"http://0.0.0.0:{container_ports.api}"
-
-
-@pytest.fixture(scope="module")
-def shacl_validator(api_url: str) -> ShaclValidator:
-    return ShaclValidator(api_url)
+def shacl_validator() -> ShaclCliValidator:
+    return ShaclCliValidator()
 
 
 @pytest.fixture(scope="module")

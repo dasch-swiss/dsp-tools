56a5a097c236ea70ff026aed18806b9bc98b3dee
Nora-Olivia-Ammann <103038637+Nora-Olivia-Ammann@users.noreply.github.com>
2025-07-10 16:48:24 +0200

    Update test_generic_warning_and_info.py

--- test/e2e_validate_data/test_generic_warning_and_info.py
+++ test/e2e_validate_data/test_generic_warning_and_info.py
@@ -19,7 +19,6 @@ from dsp_tools.commands.validate_data.validate_data import _get_validation_repor
 from dsp_tools.commands.validate_data.validate_data import _get_validation_status
 from dsp_tools.commands.validate_data.validate_data import validate_parsed_resources
 from test.e2e_validate_data.util import prepare_data_for_validation_from_file
-from test.e2e_validate_data.util import prepare_data_for_validation_from_file
 
 # ruff: noqa: ARG001 Unused function argument
 

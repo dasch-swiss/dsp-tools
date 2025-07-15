ead336028c038247732ac7de01d6ef7cbf07047a
Nora-Olivia-Ammann <103038637+Nora-Olivia-Ammann@users.noreply.github.com>
2025-07-15 16:39:37 +0200

    Update write_diagnostic_info.py

--- src/dsp_tools/commands/xmlupload/write_diagnostic_info.py
+++ src/dsp_tools/commands/xmlupload/write_diagnostic_info.py
@@ -13,13 +13,15 @@ def write_id2iri_mapping(id2iri_mapping: dict[str, str], shortcode: str, diagnos
     """Writes the mapping of internal IDs to IRIs to a file."""
     timestamp = datetime.now().strftime("%Y-%m-%d_%H%M%S")
     servername = diagnostics.server_as_foldername
-    id2iri_filename = f"id2iri_{shortcode}_{servername}_{timestamp}.json"
+    json_str = json.dumps(id2iri_mapping, ensure_ascii=False, indent=4)
+    id2iri_filename_for_user = f"id2iri_{shortcode}_{servername}_{timestamp}.json"
+    with open(id2iri_filename_for_user, "w", encoding="utf-8") as f:
+        f.write(json_str)
+        print(f"{datetime.now()}: The mapping of internal IDs to IRIs was written to {id2iri_filename_for_user}")
+        logger.info(f"The mapping of internal IDs to IRIs was written to {id2iri_filename_for_user}")
+
     id_2_iri_folder = Path.home() / ".dsp-tools" / "id2iri"
     id_2_iri_folder.mkdir(parents=True, exist_ok=True)
-    json_str = json.dumps(id2iri_mapping, ensure_ascii=False, indent=4)
-    with open(id_2_iri_folder / id2iri_filename, "w", encoding="utf-8") as f:
+    id2iri_filename_for_logs = f"{timestamp}_id2iri_{shortcode}_{servername}.json"
+    with open(id_2_iri_folder / id2iri_filename_for_logs, "w", encoding="utf-8") as f:
         f.write(json_str)
-    with open(id2iri_filename, "w", encoding="utf-8") as f:
-        f.write(json_str)
-        print(f"{datetime.now()}: The mapping of internal IDs to IRIs was written to {id2iri_filename}")
-        logger.info(f"The mapping of internal IDs to IRIs was written to {id2iri_filename}")

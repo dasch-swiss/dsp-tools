b73485280347ed3a23969bddb6e593cd17255526
Nora-Olivia-Ammann <103038637+Nora-Olivia-Ammann@users.noreply.github.com>
2025-07-16 13:11:57 +0200

    fix tests

--- test/e2e/commands/validate_data/test_generic_warning_and_info.py
+++ test/e2e/commands/validate_data/test_generic_warning_and_info.py
@@ -12,14 +12,9 @@ from dsp_tools.clients.authentication_client_live import AuthenticationClientLiv
 from dsp_tools.commands.validate_data.models.input_problems import ProblemType
 from dsp_tools.commands.validate_data.models.input_problems import SortedProblems
 from dsp_tools.commands.validate_data.models.input_problems import ValidateDataResult
-from dsp_tools.commands.validate_data.prepare_data.prepare_data import get_info_and_parsed_resources_from_file
-from dsp_tools.commands.validate_data.process_validation_report.get_user_validation_message import sort_user_problems
-from dsp_tools.commands.validate_data.process_validation_report.query_validation_result import reformat_validation_graph
 from dsp_tools.commands.validate_data.shacl_cli_validator import ShaclCliValidator
 from dsp_tools.commands.validate_data.validate_data import _get_validation_status
 from dsp_tools.commands.validate_data.validate_data import _validate_data
-from dsp_tools.commands.validate_data.validate_data import validate_parsed_resources
-from dsp_tools.commands.validate_data.validation.get_validation_report import get_validation_report
 from test.e2e.commands.validate_data.util import prepare_data_for_validation_from_file
 
 # ruff: noqa: ARG001 Unused function argument
@@ -41,7 +36,7 @@ def authentication(creds: ServerCredentials) -> AuthenticationClient:
 
 
 @pytest.fixture(scope="module")
-def no_violations_with_warnings(
+def no_violations_with_warnings_do_not_ignore_duplicate_files(
     create_generic_project, authentication, shacl_validator: ShaclCliValidator
 ) -> ValidateDataResult:
     file = Path("testdata/validate-data/generic/no_violations_with_warnings.xml")
@@ -58,105 +53,18 @@ def no_violations_with_info(
     return _validate_data(graphs, used_iris, parsed_resources, CONFIG)
 
 
-@pytest.fixture(scope="module")
-def parsed_resources_51_duplicate_images(create_generic_project, authentication):
-    file = Path("testdata/validate-data/generic/51_duplicate_images.xml")
-    parsed_resources, shortcode, authorship_lookup, permission_ids = get_info_and_parsed_resources_from_file(
-        file, authentication.server
-    )
-    return parsed_resources, authorship_lookup, permission_ids, shortcode
-
-
-class TestGetDuplicateFileValidationResult:
-    def test_ignore_files_on_test_env(self, parsed_resources_51_duplicate_images, authentication):
-        parsed_resources, authorship_lookup, permission_ids, shortcode = parsed_resources_51_duplicate_images
-        config = ValidateDataConfig(
-            xml_file=Path(),
-            save_graph_dir=None,
-            severity=ValidationSeverity.INFO,
-            ignore_duplicate_files_warning=True,
-            is_on_prod_server=False,
-        )
-        validation_passed = validate_parsed_resources(
-            parsed_resources=parsed_resources,
-            authorship_lookup=authorship_lookup,
-            permission_ids=permission_ids,
-            shortcode=shortcode,
-            config=config,
-            auth=authentication,
-        )
-        assert validation_passed
-
-    def test_do_not_ignore_files_on_test_env(self, parsed_resources_51_duplicate_images, authentication):
-        parsed_resources, authorship_lookup, permission_ids, shortcode = parsed_resources_51_duplicate_images
-        config = ValidateDataConfig(
-            xml_file=Path(),
-            save_graph_dir=None,
-            severity=ValidationSeverity.INFO,
-            ignore_duplicate_files_warning=False,
-            is_on_prod_server=False,
-        )
-        validation_passed = validate_parsed_resources(
-            parsed_resources=parsed_resources,
-            authorship_lookup=authorship_lookup,
-            permission_ids=permission_ids,
-            shortcode=shortcode,
-            config=config,
-            auth=authentication,
-        )
-        assert validation_passed
-
-    def test_ignore_files_on_prod_env(self, parsed_resources_51_duplicate_images, authentication):
-        parsed_resources, authorship_lookup, permission_ids, shortcode = parsed_resources_51_duplicate_images
-        config = ValidateDataConfig(
-            xml_file=Path(),
-            save_graph_dir=None,
-            severity=ValidationSeverity.INFO,
-            ignore_duplicate_files_warning=True,
-            is_on_prod_server=True,
-        )
-        validation_passed = validate_parsed_resources(
-            parsed_resources=parsed_resources,
-            authorship_lookup=authorship_lookup,
-            permission_ids=permission_ids,
-            shortcode=shortcode,
-            config=config,
-            auth=authentication,
-        )
-        assert validation_passed
-
-    def test_do_not_ignore_files_on_prod_env(self, parsed_resources_51_duplicate_images, authentication):
-        parsed_resources, authorship_lookup, permission_ids, shortcode = parsed_resources_51_duplicate_images
-        config = ValidateDataConfig(
-            xml_file=Path(),
-            save_graph_dir=None,
-            severity=ValidationSeverity.INFO,
-            ignore_duplicate_files_warning=False,
-            is_on_prod_server=True,
-        )
-        validation_passed = validate_parsed_resources(
-            parsed_resources=parsed_resources,
-            authorship_lookup=authorship_lookup,
-            permission_ids=permission_ids,
-            shortcode=shortcode,
-            config=config,
-            auth=authentication,
-        )
-        assert validation_passed
-
-
 class TestGetCorrectValidationResult:
-    def test_no_violations_with_warnings_not_on_prod(self, no_violations_with_warnings):
+    def test_no_violations_with_warnings_not_on_prod(self, no_violations_with_warnings_do_not_ignore_duplicate_files):
         # this boolean carries the information if there are problems of any severity level,
         # but not if the validation will pass
-        assert not no_violations_with_warnings.no_problems
-        sorted_problems = no_violations_with_warnings.problems
+        assert not no_violations_with_warnings_do_not_ignore_duplicate_files.no_problems
+        sorted_problems = no_violations_with_warnings_do_not_ignore_duplicate_files.problems
         assert isinstance(sorted_problems, SortedProblems)
         result = _get_validation_status(sorted_problems, is_on_prod=False)
         assert result is True
 
-    def test_no_violations_with_warnings_on_prod(self, no_violations_with_warnings):
-        sorted_problems = no_violations_with_warnings.problems
+    def test_no_violations_with_warnings_on_prod(self, no_violations_with_warnings_do_not_ignore_duplicate_files):
+        sorted_problems = no_violations_with_warnings_do_not_ignore_duplicate_files.problems
         assert isinstance(sorted_problems, SortedProblems)
         result = _get_validation_status(sorted_problems, is_on_prod=True)
         assert result is False
@@ -178,8 +86,10 @@ class TestGetCorrectValidationResult:
 
 
 class TestSortedProblems:
-    def test_no_violations_with_warnings_problems(self, no_violations_with_warnings):
+    def test_no_violations_with_warnings_problems(self, no_violations_with_warnings_do_not_ignore_duplicate_files):
         expected_warnings = [
+            (None, ProblemType.FILE_DUPLICATE),  # triplicate_archive
+            (None, ProblemType.FILE_DUPLICATE),  # duplicate_iiif
             # each type of missing legal info (authorship, copyright, license) produces one violation
             ("archive_no_legal_info", ProblemType.GENERIC),
             ("archive_no_legal_info", ProblemType.GENERIC),
@@ -191,7 +101,7 @@ class TestSortedProblems:
             ("image_no_legal_info", ProblemType.GENERIC),
             ("image_no_legal_info", ProblemType.GENERIC),
         ]
-        sorted_problems = no_violations_with_warnings.problems
+        sorted_problems = no_violations_with_warnings_do_not_ignore_duplicate_files.problems
         assert isinstance(sorted_problems, SortedProblems)
         sorted_warnings = sorted(sorted_problems.user_warnings, key=lambda x: str(x.res_id))
         assert not sorted_problems.unique_violations
@@ -202,13 +112,23 @@ class TestSortedProblems:
             assert one_result.problem_type == expected_info[1]
             assert one_result.res_id == expected_info[0]
 
+    def test_no_violations_with_warnings_problems_ignore_duplicates(self, authentication):
+        # This only tests if the duplicates were ignored, the details of the result is tested separately
+        file = Path("testdata/validate-data/generic/no_violations_with_warnings.xml")
+        graphs, used_iris, parsed_resources = prepare_data_for_validation_from_file(file, authentication)
+        result = _validate_data(graphs, used_iris, parsed_resources, CONFIG)
+        expected_res_ids = {"archive_no_legal_info", "iiif_no_legal_info", "image_no_legal_info"}
+        sorted_problems = result.problems
+        assert isinstance(sorted_problems, SortedProblems)
+        warnings_ids = {x.res_id for x in sorted_problems.user_warnings}
+        assert not sorted_problems.unique_violations
+        assert len(sorted_problems.user_warnings) == 9
+        assert not sorted_problems.user_info
+        assert not sorted_problems.unexpected_shacl_validation_components
+        assert warnings_ids == expected_res_ids
+
     def test_no_violations_with_info(self, no_violations_with_info):
-        all_expected_info = [
-            (None, ProblemType.FILE_DUPLICATE),  # triplicate_archive
-            (None, ProblemType.FILE_DUPLICATE),  # duplicate_iiif
-            (None, ProblemType.FILE_DUPLICATE),  # duplicate_still_image
-            ("link_to_resource_in_db", ProblemType.INEXISTENT_LINKED_RESOURCE),
-        ]
+        all_expected_info = [("link_to_resource_in_db", ProblemType.INEXISTENT_LINKED_RESOURCE)]
         sorted_problems = no_violations_with_info.problems
         assert isinstance(sorted_problems, SortedProblems)
         sorted_info = sorted(sorted_problems.user_info, key=lambda x: str(x.res_id))
@@ -216,23 +136,6 @@ class TestSortedProblems:
         assert not sorted_problems.user_warnings
         assert len(sorted_problems.user_info) == len(all_expected_info)
         assert not sorted_problems.unexpected_shacl_validation_components
-        for one_result, expected_info in zip(sorted_info, all_expected_info):
-            assert one_result.problem_type == expected_info[1]
-            assert one_result.res_id == expected_info[0]
-
-    def test_no_violations_with_info_ignore_duplicate_files_warning(self, authentication, shacl_validator):
-        file = Path("testdata/validate-data/generic/no_violations_with_info.xml")
-        graphs, used_iris, parsed_resources = prepare_data_for_validation_from_file(file, authentication)
-        report = get_validation_report(graphs, shacl_validator)
-        reformatted = reformat_validation_graph(report)
-        # This is the behaviour if the flag `ignore_duplicate_files_warning` were set.
-        no_violations_with_info = sort_user_problems(reformatted, None)
-        all_expected_info = [("link_to_resource_in_db", ProblemType.INEXISTENT_LINKED_RESOURCE)]
-        sorted_info = sorted(no_violations_with_info.user_info, key=lambda x: str(x.res_id))
-        assert not no_violations_with_info.unique_violations
-        assert not no_violations_with_info.user_warnings
-        assert len(no_violations_with_info.user_info) == len(all_expected_info)
-        assert not no_violations_with_info.unexpected_shacl_validation_components
         for one_result, expected_info in zip(sorted_info, all_expected_info):
             assert one_result.problem_type == expected_info[1]
             assert one_result.res_id == expected_info[0]

86017ee0047e240bf33e54de95671a69c959f3a8
Johannes Nussbaum <39048939+jnussbaum@users.noreply.github.com>
2025-08-06 10:58:25 +0200

    fix(upload-files): enable extension ".json" (was forgotten) (#1899)

--- src/dsp_tools/commands/ingest_xmlupload/upload_files/filechecker.py
+++ src/dsp_tools/commands/ingest_xmlupload/upload_files/filechecker.py
@@ -7,7 +7,7 @@ from dsp_tools.commands.ingest_xmlupload.upload_files.input_error import FilePro
 
 SUPPORTED_EXTENSIONS = (
     "zip,tar,gz,z,tgz,gzip,7z,mp3,wav,pdf,doc,docx,xls,xlsx,ppt,pptx,epub,"
-    "mp4,jpg,jpeg,jp2,png,tif,tiff,odd,rng,txt,xml,htm,html,xsd,xsl,csv"
+    "mp4,jpg,jpeg,jp2,json,png,tif,tiff,odd,rng,txt,xml,htm,html,xsd,xsl,csv"
 ).split(",")
 
 

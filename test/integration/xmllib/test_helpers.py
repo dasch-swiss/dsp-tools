eb7b13f4ce7c7d94798af3b959724620b49b84cf
Nora-Olivia-Ammann <103038637+Nora-Olivia-Ammann@users.noreply.github.com>
2025-08-07 13:57:24 +0200

    Update test_helpers.py

--- test/integration/xmllib/test_helpers.py
+++ test/integration/xmllib/test_helpers.py
@@ -5,14 +5,19 @@ from dsp_tools.xmllib.helpers import ListLookup
 
 
 @pytest.fixture
-def list_lookup() -> ListLookup:
+def list_lookup_en() -> ListLookup:
     return ListLookup.create_new("testdata/json-project/test-list-lookup.json", "en", "default")
 
 
+@pytest.fixture
+def list_lookup_de() -> ListLookup:
+    return ListLookup.create_new("testdata/json-project/test-list-lookup.json", "de", "default")
+
+
 class TestGetListLookup:
-    def test_lookup(self, list_lookup):
-        assert list_lookup._label_language == "en"
-        assert set(list_lookup._lookup.keys()) == {"list1", "list2"}
+    def test_lookup_all_labels_have_lang(self, list_lookup_en):
+        assert list_lookup_en._label_language == "en"
+        assert set(list_lookup_en._lookup.keys()) == {"list1", "list2"}
         expected_1 = {
             "label 1": "list1_node1",
             "Label 1": "list1_node1",
@@ -21,15 +26,30 @@ class TestGetListLookup:
             "label 2.2": "list1_node2.2",
             "label 2.2.1": "list1_node2.2.1",
         }
-        assert list_lookup._lookup["list1"] == expected_1
+        assert list_lookup_en._lookup["list1"] == expected_1
         expected_2 = {"label 1": "list2_node1", "label 2": "list2_node2"}
-        assert list_lookup._lookup["list2"] == expected_2
+        assert list_lookup_en._lookup["list2"] == expected_2
 
-    def test_prop_to_list_name(self, list_lookup):
+    def test_lookup_some_language_labels_missing(self, list_lookup_de):
+        assert list_lookup_de._label_language == "de"
+        assert set(list_lookup_de._lookup.keys()) == {"list1", "list2"}
+        expected_1 = {
+            "Deutsch 1": "list1_node1",
+            "deutsch 1": "asdf",
+            "label 2": None,
+            "label 2.1": None,
+            "label 2.2": None,
+            "label 2.2.1": None,
+        }
+        assert list_lookup_de._lookup["list1"] == expected_1
+        expected_2 = {"label 1": "list2_node1", "label 2": "list2_node2"}
+        assert list_lookup_de._lookup["list2"] == expected_2
+
+    def test_prop_to_list_name(self, list_lookup_en):
         expected = {
             "default:defaultOntoHasListOne": "list1",
             ":defaultOntoHasListOne": "list1",
             "other-onto:otherOntoHasListOne": "list1",
             "other-onto:otherOntoHasListTwo": "list2",
         }
-        assert list_lookup._prop_to_list_name == expected
+        assert list_lookup_en._prop_to_list_name == expected

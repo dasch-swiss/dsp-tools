daadd041402cb0a0af640f85afd23af1ae264ccf
Nora-Olivia-Ammann <103038637+Nora-Olivia-Ammann@users.noreply.github.com>
2025-08-07 15:00:17 +0200

    add test

--- test/integration/xmllib/test_helpers.py
+++ test/integration/xmllib/test_helpers.py
@@ -35,14 +35,18 @@ class TestGetListLookup:
         assert set(list_lookup_de._lookup.keys()) == {"list1", "list2"}
         expected_1 = {
             "Deutsch 1": "list1_node1",
-            "deutsch 1": "asdf",
-            "label 2": None,
-            "label 2.1": None,
-            "label 2.2": None,
-            "label 2.2.1": None,
+            "deutsch 1": "list1_node1",
+            "deutsch 2": "list1_node2",
+            "deutsch 2.1": "list1_node2.1",
+            "deutsch 2.2": "list1_node2.2",
         }
         assert list_lookup_de._lookup["list1"] == expected_1
-        expected_2 = {"label 1": "list2_node1", "label 2": "list2_node2"}
+        expected_2 = {
+            "Deutsch 1": "list2_node1",
+            "Deutsch 2": "list2_node2",
+            "deutsch 1": "list2_node1",
+            "deutsch 2": "list2_node2",
+        }
         assert list_lookup_de._lookup["list2"] == expected_2
 
     def test_prop_to_list_name(self, list_lookup_en):

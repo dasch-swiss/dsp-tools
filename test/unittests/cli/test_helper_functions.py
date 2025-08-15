a833768ea331935dec051158021861ae174c01e7
Johannes Nussbaum <johannes.nussbaum@dasch.swiss>
2025-08-15 18:24:08 +0200

    introduce fixtures

--- test/unittests/cli/test_helper_functions.py
+++ test/unittests/cli/test_helper_functions.py
@@ -8,105 +8,116 @@ from dsp_tools.cli.entry_point import _derive_dsp_ingest_url
 from dsp_tools.cli.entry_point import _get_canonical_server_and_dsp_ingest_url
 from dsp_tools.error.exceptions import InputError
 
-default_dsp_api_url = "http://0.0.0.0:3333"
-default_dsp_ingest_url = "http://0.0.0.0:3340"
-root_user_email = "root@example.com"
-root_user_pw = "test"
 
-positive_testcases = {
-    "https://0.0.0.0:3333/": [
-        default_dsp_api_url,
-        default_dsp_ingest_url,
-    ],
-    "0.0.0.0:3333": [
-        default_dsp_api_url,
-        default_dsp_ingest_url,
-    ],
-    "localhost:3333": [
-        default_dsp_api_url,
-        default_dsp_ingest_url,
-    ],
-    "https://admin.dasch.swiss": [
-        "https://api.dasch.swiss",
-        "https://ingest.dasch.swiss",
-    ],
-    "https://api.dasch.swiss": [
-        "https://api.dasch.swiss",
-        "https://ingest.dasch.swiss",
-    ],
-    "https://app.dasch.swiss": [
-        "https://api.dasch.swiss",
-        "https://ingest.dasch.swiss",
-    ],
-    "https://ingest.dasch.swiss": [
-        "https://api.dasch.swiss",
-        "https://ingest.dasch.swiss",
-    ],
-    "https://dasch.swiss": [
-        "https://api.dasch.swiss",
-        "https://ingest.dasch.swiss",
-    ],
-    "dasch.swiss": [
-        "https://api.dasch.swiss",
-        "https://ingest.dasch.swiss",
-    ],
-    "http://admin.test.dasch.swiss/": [
-        "https://api.test.dasch.swiss",
-        "https://ingest.test.dasch.swiss",
-    ],
-    "http://app.staging.dasch.swiss/": [
-        "https://api.staging.dasch.swiss",
-        "https://ingest.staging.dasch.swiss",
-    ],
-    "https://demo.dasch.swiss/": [
-        "https://api.demo.dasch.swiss",
-        "https://ingest.demo.dasch.swiss",
-    ],
-    "http://api.dev.dasch.swiss/": [
-        "https://api.dev.dasch.swiss",
-        "https://ingest.dev.dasch.swiss",
-    ],
-    "dev-02.dasch.swiss": [
-        "https://api.dev-02.dasch.swiss",
-        "https://ingest.dev-02.dasch.swiss",
-    ],
-    "082a-test-server.dasch.swiss": [
-        "https://api.082a-test-server.dasch.swiss",
-        "https://ingest.082a-test-server.dasch.swiss",
-    ],
-    "admin.08F4-test-server.dasch.swiss": [
-        "https://api.08F4-test-server.dasch.swiss",
-        "https://ingest.08F4-test-server.dasch.swiss",
-    ],
-    "app.08F4-test-server.dasch.swiss": [
-        "https://api.08F4-test-server.dasch.swiss",
-        "https://ingest.08F4-test-server.dasch.swiss",
-    ],
-    "ingest.08F4-test-server.dasch.swiss": [
-        "https://api.08F4-test-server.dasch.swiss",
-        "https://ingest.08F4-test-server.dasch.swiss",
-    ],
-    "not-yet-0826-test-server.dasch.swiss": [
-        "https://api.not-yet-0826-test-server.dasch.swiss",
-        "https://ingest.not-yet-0826-test-server.dasch.swiss",
-    ],
-    "https://admin.ls-prod-server.dasch.swiss": [
-        "https://api.ls-prod-server.dasch.swiss",
-        "https://ingest.ls-prod-server.dasch.swiss",
-    ],
-    "https://ls-test-server.dasch.swiss": [
-        "https://api.ls-test-server.dasch.swiss",
-        "https://ingest.ls-test-server.dasch.swiss",
-    ],
-}
-
-negative_testcases = [
-    "https://0.0.0.0:1234",
-    "https://api.unkown-host.ch",
-]
+@pytest.fixture
+def default_dsp_api_url() -> str:
+    return "http://0.0.0.0:3333"
 
 
-def test_derive_dsp_ingest_url_without_server() -> None:
+@pytest.fixture
+def default_dsp_ingest_url() -> str:
+    return "http://0.0.0.0:3340"
+
+
+@pytest.fixture
+def positive_testcases(default_dsp_api_url: str, default_dsp_ingest_url: str) -> dict[str, list[str]]:
+    return {
+        "https://0.0.0.0:3333/": [
+            default_dsp_api_url,
+            default_dsp_ingest_url,
+        ],
+        "0.0.0.0:3333": [
+            default_dsp_api_url,
+            default_dsp_ingest_url,
+        ],
+        "localhost:3333": [
+            default_dsp_api_url,
+            default_dsp_ingest_url,
+        ],
+        "https://admin.dasch.swiss": [
+            "https://api.dasch.swiss",
+            "https://ingest.dasch.swiss",
+        ],
+        "https://api.dasch.swiss": [
+            "https://api.dasch.swiss",
+            "https://ingest.dasch.swiss",
+        ],
+        "https://app.dasch.swiss": [
+            "https://api.dasch.swiss",
+            "https://ingest.dasch.swiss",
+        ],
+        "https://ingest.dasch.swiss": [
+            "https://api.dasch.swiss",
+            "https://ingest.dasch.swiss",
+        ],
+        "https://dasch.swiss": [
+            "https://api.dasch.swiss",
+            "https://ingest.dasch.swiss",
+        ],
+        "dasch.swiss": [
+            "https://api.dasch.swiss",
+            "https://ingest.dasch.swiss",
+        ],
+        "http://admin.test.dasch.swiss/": [
+            "https://api.test.dasch.swiss",
+            "https://ingest.test.dasch.swiss",
+        ],
+        "http://app.staging.dasch.swiss/": [
+            "https://api.staging.dasch.swiss",
+            "https://ingest.staging.dasch.swiss",
+        ],
+        "https://demo.dasch.swiss/": [
+            "https://api.demo.dasch.swiss",
+            "https://ingest.demo.dasch.swiss",
+        ],
+        "http://api.dev.dasch.swiss/": [
+            "https://api.dev.dasch.swiss",
+            "https://ingest.dev.dasch.swiss",
+        ],
+        "dev-02.dasch.swiss": [
+            "https://api.dev-02.dasch.swiss",
+            "https://ingest.dev-02.dasch.swiss",
+        ],
+        "082a-test-server.dasch.swiss": [
+            "https://api.082a-test-server.dasch.swiss",
+            "https://ingest.082a-test-server.dasch.swiss",
+        ],
+        "admin.08F4-test-server.dasch.swiss": [
+            "https://api.08F4-test-server.dasch.swiss",
+            "https://ingest.08F4-test-server.dasch.swiss",
+        ],
+        "app.08F4-test-server.dasch.swiss": [
+            "https://api.08F4-test-server.dasch.swiss",
+            "https://ingest.08F4-test-server.dasch.swiss",
+        ],
+        "ingest.08F4-test-server.dasch.swiss": [
+            "https://api.08F4-test-server.dasch.swiss",
+            "https://ingest.08F4-test-server.dasch.swiss",
+        ],
+        "not-yet-0826-test-server.dasch.swiss": [
+            "https://api.not-yet-0826-test-server.dasch.swiss",
+            "https://ingest.not-yet-0826-test-server.dasch.swiss",
+        ],
+        "https://admin.ls-prod-server.dasch.swiss": [
+            "https://api.ls-prod-server.dasch.swiss",
+            "https://ingest.ls-prod-server.dasch.swiss",
+        ],
+        "https://ls-test-server.dasch.swiss": [
+            "https://api.ls-test-server.dasch.swiss",
+            "https://ingest.ls-test-server.dasch.swiss",
+        ],
+    }
+
+
+@pytest.fixture
+def negative_testcases() -> list[str]:
+    return [
+        "https://0.0.0.0:1234",
+        "https://api.unkown-host.ch",
+    ]
+
+
+def test_derive_dsp_ingest_url_without_server(default_dsp_api_url: str, default_dsp_ingest_url: str) -> None:
     """
     If the argparse.Namespace does not contain a server,
     the function should return the same object.
@@ -123,7 +134,7 @@ def test_derive_dsp_ingest_url_without_server() -> None:
     assert args_without_server == args_returned
 
 
-def test_derive_dsp_ingest_url_with_server() -> None:
+def test_derive_dsp_ingest_url_with_server(default_dsp_api_url: str, default_dsp_ingest_url: str) -> None:
     """
     If the argparse.Namespace contains a server,
     the function should return a modified argparse.Namespace,
@@ -152,7 +163,12 @@ def test_derive_dsp_ingest_url_with_server() -> None:
     assert args_expected == args_returned
 
 
-def test_get_canonical_server_and_dsp_ingest_url() -> None:
+def test_get_canonical_server_and_dsp_ingest_url(
+    positive_testcases: dict[str, list[str]],
+    negative_testcases: list[str],
+    default_dsp_api_url: str,
+    default_dsp_ingest_url: str,
+) -> None:
     """
     Test the method that canonicalizes the DSP URL and derives the SIPI URL from it.
     """

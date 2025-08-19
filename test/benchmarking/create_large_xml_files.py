832e89c3188e36f870a287ee14734b0046310070
Nora-Olivia-Ammann <103038637+Nora-Olivia-Ammann@users.noreply.github.com>
2025-08-19 14:26:05 +0200

    Update create_large_xml_files.py

--- test/benchmarking/create_large_xml_files.py
+++ test/benchmarking/create_large_xml_files.py
@@ -1,30 +1,32 @@
+from pathlib import Path
+
 from dsp_tools.xmllib import Resource
 from dsp_tools.xmllib import XMLRoot
 
 # This is to be used with the ontology: testdata/validate-data/generic/project.json
 
 
-def file_with_one_resource_and_many_values(value_counter: int) -> None:
+def file_with_one_resource_and_many_values(value_counter: int, save_dir: Path) -> None:
     root = XMLRoot.create_new("9999", "onto")
     res = create_one_resource(0)
     res = add_many_values(res, value_counter)
     root.add_resource(res)
-    root.write_file(f"size_test_one_res_with_{value_counter}_values.xml")
+    root.write_file(save_dir / f"size_test_one_res_with_{value_counter}_values.xml")
 
 
-def file_with_one_resource_and_large_text_value(text_length: int) -> None:
+def file_with_one_resource_and_large_text_value(text_length: int, save_dir: Path) -> None:
     root = XMLRoot.create_new("9999", "onto")
     res = create_one_resource(0)
     res = add_one_large_text_value(res, text_length)
     root.add_resource(res)
-    root.write_file(f"size_test_one_res_with_{text_length}_characters_of_text.xml")
+    root.write_file(save_dir / f"size_test_one_res_with_{text_length}_characters_of_text.xml")
 
 
-def file_with_many_resources_no_values(number_of_resources: int) -> None:
+def file_with_many_resources_no_values(number_of_resources: int, save_dir: Path) -> None:
     root = XMLRoot.create_new("9999", "onto")
     resources = [create_one_resource(i) for i in range(number_of_resources)]
     root.add_resource_multiple(resources)
-    root.write_file(f"size_test_{number_of_resources}_number_of_resources_no_values.xml")
+    root.write_file(save_dir / f"size_test_{number_of_resources}_number_of_resources_no_values.xml")
 
 
 def create_one_resource(id_counter: int) -> Resource:
@@ -43,8 +45,11 @@ def add_one_large_text_value(res: Resource, text_length: int) -> Resource:
 
 
 if __name__ == "__main__":
-    focus_number = 10000
-    file_with_one_resource_and_many_values(focus_number)
-    file_with_many_resources_no_values(focus_number)
+    save_dir = Path("x_fuseki_bloating_files")
+    save_dir.mkdir(exist_ok=True)
 
-    file_with_one_resource_and_large_text_value(100000)
+    focus_number = 999_999
+    file_with_one_resource_and_many_values(focus_number, save_dir)
+    file_with_many_resources_no_values(focus_number, save_dir)
+
+    file_with_one_resource_and_large_text_value(99_999_999, save_dir)

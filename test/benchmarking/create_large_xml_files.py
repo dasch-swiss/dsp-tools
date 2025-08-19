eace08916aeeda74e7870bfaf954f226e4ecb1a1
Nora-Olivia-Ammann <103038637+Nora-Olivia-Ammann@users.noreply.github.com>
2025-08-19 13:59:19 +0200

    Update create_large_xml_files.py

--- test/benchmarking/create_large_xml_files.py
+++ test/benchmarking/create_large_xml_files.py
@@ -4,21 +4,6 @@ from dsp_tools.xmllib import XMLRoot
 # This is to be used with the ontology: testdata/validate-data/generic/project.json
 
 
-def add_many_values(res: Resource, number_of_values: int) -> Resource:
-    for i in range(number_of_values):
-        res.add_integer(prop_name=":testIntegerSimpleText", value=i)
-    return res
-
-
-def add_one_large_text_value(res: Resource, text_length: int) -> Resource:
-    txt = "a" * text_length
-    return res.add_simpletext(prop_name=":testTextarea", value=txt)
-
-
-def create_one_resource(id_counter: int) -> Resource:
-    return Resource.create_new(res_id=f"id_{id_counter}", restype=":ClassWithEverything", label=str(id_counter))
-
-
 def create_one_res_many_values(value_counter: int) -> None:
     root = XMLRoot.create_new("9999", "onto")
     res = create_one_resource(0)
@@ -40,3 +25,18 @@ def create_many_res_no_values(number_of_resources: int) -> None:
     resources = [create_one_resource(i) for i in range(number_of_resources)]
     root.add_resource_multiple(resources)
     root.write_file(f"size_test_{number_of_resources}_number_of_resources_no_values.xml")
+
+
+def create_one_resource(id_counter: int) -> Resource:
+    return Resource.create_new(res_id=f"id_{id_counter}", restype=":ClassWithEverything", label=str(id_counter))
+
+
+def add_many_values(res: Resource, number_of_values: int) -> Resource:
+    for i in range(number_of_values):
+        res.add_integer(prop_name=":testIntegerSimpleText", value=i)
+    return res
+
+
+def add_one_large_text_value(res: Resource, text_length: int) -> Resource:
+    txt = "a" * text_length
+    return res.add_simpletext(prop_name=":testTextarea", value=txt)

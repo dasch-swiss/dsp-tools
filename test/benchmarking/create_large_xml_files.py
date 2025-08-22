8f355528f77cfd7445e1a551ccfcfe971a33ad54
Nora-Olivia-Ammann <103038637+Nora-Olivia-Ammann@users.noreply.github.com>
2025-08-22 11:47:35 +0200

    Update create_large_xml_files.py

--- test/benchmarking/create_large_xml_files.py
+++ test/benchmarking/create_large_xml_files.py
@@ -6,37 +6,39 @@ from dsp_tools.xmllib import XMLRoot
 # This is to be used with the ontology: testdata/validate-data/generic/project.json
 
 
-def file_with_one_resource_and_many_values(value_counter: int, save_dir: Path) -> None:
+def file_with_many_resources_no_values(res_counter: int, save_dir: Path) -> None:
     root = XMLRoot.create_new("9999", "onto")
-    res = create_one_resource(0)
-    res = add_many_values(res, value_counter)
-    root.add_resource(res)
-    root.write_file(save_dir / f"size_test_one_res_with_{value_counter}_values.xml")
-
-
-def file_with_one_resource_and_large_text_value(text_length: int, save_dir: Path) -> None:
-    root = XMLRoot.create_new("9999", "onto")
-    res = create_one_resource(0)
-    res = add_one_large_text_value(res, text_length)
-    root.add_resource(res)
-    root.write_file(save_dir / f"size_test_one_res_with_{text_length}_characters_of_text.xml")
-
-
-def file_with_many_resources_no_values(number_of_resources: int, save_dir: Path) -> None:
-    root = XMLRoot.create_new("9999", "onto")
-    resources = [create_one_resource(i) for i in range(number_of_resources)]
+    resources = [create_one_resource(i) for i in range(res_counter)]
     root.add_resource_multiple(resources)
-    root.write_file(save_dir / f"size_test_{number_of_resources}_number_of_resources_no_values.xml")
+    root.write_file(save_dir / f"res-{res_counter}_val-0.xml")
+
+
+def file_with_many_resources_and_small_values(res_counter: int, value_counter: int, save_dir: Path) -> None:
+    root = XMLRoot.create_new("9999", "onto")
+    resources = [create_one_resource(i) for i in range(res_counter)]
+    resources = [add_many_int_values(res, value_counter) for res in resources]
+    root.add_resource_multiple(resources)
+    root.write_file(save_dir / f"res-{res_counter}_val-{value_counter}_small_values.xml")
+
+
+def add_many_int_values(res: Resource, number_of_values: int) -> Resource:
+    for i in range(number_of_values):
+        res.add_integer(prop_name=":testIntegerSimpleText", value=i)
+    return res
+
+
+def file_with_many_resources_and_large_text_value(
+    res_counter: int, value_counter: int, text_length: int, save_dir: Path
+) -> None:
+    root = XMLRoot.create_new("9999", "onto")
+    resources = [create_one_resource(i) for i in range(res_counter)]
+    resources = [add_many_int_values(res, value_counter) for res in resources]
+    root.add_resource_multiple(resources)
+    root.write_file(save_dir / f"res-{res_counter}_val-{value_counter}_lenText-{text_length}.xml")
 
 
 def create_one_resource(id_counter: int) -> Resource:
     return Resource.create_new(res_id=f"id_{id_counter}", restype=":ClassWithEverything", label=str(id_counter))
-
-
-def add_many_values(res: Resource, number_of_values: int) -> Resource:
-    for i in range(number_of_values):
-        res.add_integer(prop_name=":testIntegerSimpleText", value=i)
-    return res
 
 
 def add_one_large_text_value(res: Resource, text_length: int) -> Resource:
@@ -48,8 +50,22 @@ if __name__ == "__main__":
     save_dir = Path("x_fuseki_bloating_files")
     save_dir.mkdir(exist_ok=True)
 
-    focus_number = 99_999
-    file_with_one_resource_and_many_values(focus_number, save_dir)
-    file_with_many_resources_no_values(focus_number, save_dir)
+    res_number = 10_000
+    val_number = 10
+    text_size = 999_999
 
-    file_with_one_resource_and_large_text_value(9_999_999, save_dir)
+    file_with_many_resources_no_values(
+        res_counter=res_number,
+        save_dir=save_dir,
+    )
+    file_with_many_resources_and_small_values(
+        res_counter=res_number,
+        value_counter=val_number,
+        save_dir=save_dir,
+    )
+    file_with_many_resources_and_large_text_value(
+        res_counter=res_number,
+        value_counter=val_number,
+        text_length=text_size,
+        save_dir=save_dir,
+    )

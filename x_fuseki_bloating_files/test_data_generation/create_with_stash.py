b3e5ac4595386bbda7076398957b576a6c86741f
Nora-Olivia-Ammann <103038637+Nora-Olivia-Ammann@users.noreply.github.com>
2025-09-02 15:46:45 +0200

    Update create_with_stash.py

--- x_fuseki_bloating_files/test_data_generation/create_with_stash.py
+++ x_fuseki_bloating_files/test_data_generation/create_with_stash.py
@@ -1,8 +1,12 @@
+from pathlib import Path
 from typing import TypeVar
 
 from dsp_tools.xmllib import Resource
+from dsp_tools.xmllib import XMLRoot
 from dsp_tools.xmllib import create_standoff_link_to_resource
 from dsp_tools.xmllib.models.internal.values import Value
+
+# This is to be used with the ontology: testdata/validate-data/generic/project.json
 
 T = TypeVar("T", bound=Value)
 
@@ -16,23 +20,47 @@ def _create_link_target_resource(id_counter: int) -> Resource:
 
 
 def _add_richtext(res_1: Resource, res_2: Resource) -> tuple[Resource, Resource]:
-    val_1 = create_standoff_link_to_resource(res_2.res_id, "Text")
-    res_1.add_richtext(":testRichtext", val_1)
-    val_2 = create_standoff_link_to_resource(res_1.res_id, "Text")
-    res_2.add_richtext(":testRichtext", val_2)
+    for _ in range(6):
+        val_1 = create_standoff_link_to_resource(res_2.res_id, "Text")
+        res_1.add_richtext(":testRichtext", val_1)
+        val_2 = create_standoff_link_to_resource(res_1.res_id, "Text")
+        res_2.add_richtext(":testRichtext", val_2)
     return res_1, res_2
 
 
 def _add_link(res_1: Resource, res_2: Resource) -> tuple[Resource, Resource]:
-    res_1.add_link(":testHasLinkTo", res_2.res_id)
-    res_2.add_link(":testHasLinkTo", res_1.res_id)
+    for _ in range(6):
+        res_1.add_link(":testHasLinkTo", res_2.res_id)
+        res_2.add_link(":testHasLinkTo", res_1.res_id)
     return res_1, res_2
 
 
 if __name__ == "__main__":
 
-    def create_link_circles():
-        pass
+    def main(total_res: int):
+        res_count = int(total_res / 2)
+        save_dir = Path("x_fuseki_bloating_files/files")
+        save_dir.mkdir(exist_ok=True)
 
-    def create_stand_off_circles():
-        pass
+        def create_link_circles():
+            root = XMLRoot.create_new("9999", "onto")
+            res_1 = [_create_one_resource(i) for i in range(res_count)]
+            res_2 = [_create_link_target_resource(i) for i in range(res_count)]
+            for r1, r2 in zip(res_1, res_2):
+                r = _add_richtext(r1, r2)
+                root.add_resource_multiple(r)
+            root.write_file(save_dir / f"res-{res_count}_val-12_link_{res_count}-stash.xml")
+
+        def create_stand_off_circles():
+            root = XMLRoot.create_new("9999", "onto")
+            res_1 = [_create_one_resource(i) for i in range(res_count)]
+            res_2 = [_create_link_target_resource(i) for i in range(res_count)]
+            for r1, r2 in zip(res_1, res_2):
+                r = _add_link(r1, r2)
+                root.add_resource_multiple(r)
+            root.write_file(save_dir / f"res-{res_count}_val-12_richtext_{res_count}-stash.xml")
+
+        create_link_circles()
+        create_stand_off_circles()
+
+    main(10_000)

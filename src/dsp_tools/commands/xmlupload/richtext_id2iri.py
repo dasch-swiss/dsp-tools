62945f766ececf805d7be6a1d33a181ef3e0efd4
Nora-Olivia-Ammann <103038637+Nora-Olivia-Ammann@users.noreply.github.com>
2025-10-07 10:10:48 +0200

    Update richtext_id2iri.py

--- src/dsp_tools/commands/xmlupload/richtext_id2iri.py
+++ src/dsp_tools/commands/xmlupload/richtext_id2iri.py
@@ -1,12 +1,12 @@
 import regex
 
 from dsp_tools.commands.xmlupload.iri_resolver import IriResolver
-from dsp_tools.commands.xmlupload.models.formatted_text_value import FormattedTextValue
 from dsp_tools.error.exceptions import BaseError
 
 
-def replace_internal_ids_with_iris_in_richtext(val: FormattedTextValue, iri_resolver: IriResolver) -> str:
-    pass
+def replace_internal_ids_with_iris_in_richtext(richtext_str: str, iri_resolver: IriResolver) -> str:
+    ids_used = _find_internal_ids(richtext_str)
+    return _replace_ids(richtext_str, ids_used, iri_resolver)
 
 
 def _replace_ids(txt: str, ids_used: set[str], iri_resolver: IriResolver) -> str:

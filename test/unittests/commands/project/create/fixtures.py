7ae1989894a98904dddf0b647963220007b0e102
Nora-Olivia-Ammann <103038637+Nora-Olivia-Ammann@users.noreply.github.com>
2025-10-14 16:05:28 +0200

    Update fixtures.py

--- test/unittests/commands/project/create/fixtures.py
+++ test/unittests/commands/project/create/fixtures.py
@@ -1,3 +1,5 @@
 from rdflib import Namespace
+from rdflib import URIRef
 
+ONTO_IRI = URIRef("http://0.0.0.0:3333/ontology/9999/onto/v2")
 ONTO = Namespace("http://0.0.0.0:3333/ontology/9999/onto/v2#")

5ecfef054b7b8a3c35cf1d29c728a7a558b79904
Nora-Olivia-Ammann <103038637+Nora-Olivia-Ammann@users.noreply.github.com>
2025-10-16 15:37:29 +0200

    Update fixtures.py

--- test/unittests/commands/create/fixtures.py
+++ test/unittests/commands/create/fixtures.py
@@ -12,6 +12,19 @@ ONTO_PREFIX = "http://0.0.0.0:3333/ontology/8888/onto/v2#"
 
 
 @pytest.fixture
+def prefixes() -> dict[str, str]:
+    return {
+        "dcterms": "http://purl.org/dc/terms/",
+        "externalOnto": "http://otherOntology.com/onto/",
+        "foaf": "http://xmlns.com/foaf/0.1/",
+        "in-built": "http://0.0.0.0:3333/ontology/8888/in-built/v2#",
+        "knora-api": KNORA_API,
+        "onto": ONTO_PREFIX,
+        "second-onto": "http://0.0.0.0:3333/ontology/8888/second-onto/v2#",
+    }
+
+
+@pytest.fixture
 def project_json_systematic() -> dict[str, Any]:
     return parse_json_file(Path("testdata/json-project/test-project-systematic.json"))
 
@@ -78,18 +91,6 @@ def minimal_project_json() -> dict[str, Any]:
                 "resources": [],
             }
         ],
-    }
-
-
-@pytest.fixture
-def prefixes() -> dict[str, str]:
-    return {
-        "foaf": "http://xmlns.com/foaf/0.1/",
-        "externalOnto": "http://otherOntology.com/onto/",
-        "dcterms": "http://purl.org/dc/terms/",
-        "knora-api": KNORA_API,
-        "onto": ONTO_PREFIX,
-        "second-onto": "http://0.0.0.0:3333/ontology/8888/second-onto/v2#",
     }
 
 

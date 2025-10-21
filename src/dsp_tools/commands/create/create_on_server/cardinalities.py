bd7e6eff6e2bba8b367a63ea2a9a319a73899fca
Nora-Olivia-Ammann <103038637+Nora-Olivia-Ammann@users.noreply.github.com>
2025-10-21 16:24:24 +0200

    move

--- src/dsp_tools/commands/create/create_on_server/cardinalities.py
+++ src/dsp_tools/commands/create/create_on_server/cardinalities.py
@@ -1,20 +1,13 @@
-from rdflib import OWL
-from rdflib import RDF
-from rdflib import RDFS
-from rdflib import BNode
-from rdflib import Graph
 from rdflib import Literal
 from rdflib import URIRef
 
 from dsp_tools.clients.ontology_client import OntologyClient
-from dsp_tools.commands.create.constants import SALSAH_GUI_NAMESPACE
-from dsp_tools.commands.create.create_on_server.mappers import PARSED_CARDINALITY_TO_RDF
-from dsp_tools.commands.create.create_on_server.ontology import make_ontology_base_graph
 from dsp_tools.commands.create.models.input_problems import CollectedProblems
 from dsp_tools.commands.create.models.input_problems import ProblemType
 from dsp_tools.commands.create.models.input_problems import UploadProblem
 from dsp_tools.commands.create.models.parsed_ontology import ParsedClassCardinalities
 from dsp_tools.commands.create.models.parsed_ontology import ParsedPropertyCardinality
+from dsp_tools.commands.create.serialisation.ontology import make_cardinality_graph_for_request
 
 
 def add_all_cardinalities(
@@ -58,31 +51,10 @@ def _add_one_cardinality(
     last_modification_date: Literal,
     onto_client: OntologyClient,
 ) -> tuple[Literal, UploadProblem | None]:
-    card_g = _make_cardinality_graph_for_request(card, res_iri, onto_iri, last_modification_date)
+    card_g = make_cardinality_graph_for_request(card, res_iri, onto_iri, last_modification_date)
     new_mod_date = onto_client.post_resource_cardinalities(card_g)
     if not new_mod_date:
         return last_modification_date, UploadProblem(
             f"{res_iri!s} / {card.propname}", ProblemType.CARDINALITY_COULD_NOT_BE_ADDED
         )
     return new_mod_date, None
-
-
-def _make_cardinality_graph_for_request(
-    card: ParsedPropertyCardinality, res_iri: URIRef, onto_iri: URIRef, last_modification_date: Literal
-) -> Graph:
-    g = make_ontology_base_graph(onto_iri, last_modification_date)
-    g += _make_one_cardinality_graph(card, res_iri)
-    return g
-
-
-def _make_one_cardinality_graph(card: ParsedPropertyCardinality, res_iri: URIRef) -> Graph:
-    card_info = PARSED_CARDINALITY_TO_RDF[card.cardinality]
-    g = Graph()
-    bn_card = BNode()
-    g.add((res_iri, RDFS.subClassOf, bn_card))
-    g.add((bn_card, RDF.type, OWL.Restriction))
-    g.add((bn_card, card_info.owl_property, card_info.cardinality_value))
-    g.add((bn_card, OWL.onProperty, URIRef(card.propname)))
-    if card.gui_order is not None:
-        g.add((bn_card, SALSAH_GUI_NAMESPACE.guiOrder, Literal(card.gui_order)))
-    return g
--- test/unittests/commands/create/create_on_server/test_cardinalities.py
+++ test/unittests/commands/create/create_on_server/test_cardinalities.py
@@ -1,16 +1,14 @@
 from unittest.mock import Mock
 
-from rdflib import OWL
-from rdflib import RDF
-from rdflib import RDFS
 from rdflib import XSD
 from rdflib import Graph
 from rdflib import Literal
+from unittests.commands.create.constants import LAST_MODIFICATION_DATE
+from unittests.commands.create.constants import ONTO
+from unittests.commands.create.constants import ONTO_IRI
 
 from dsp_tools.clients.ontology_client import OntologyClient
 from dsp_tools.commands.create.create_on_server.cardinalities import _add_one_cardinality
-from dsp_tools.commands.create.create_on_server.cardinalities import _make_cardinality_graph_for_request
-from dsp_tools.commands.create.create_on_server.cardinalities import _make_one_cardinality_graph
 from dsp_tools.commands.create.create_on_server.cardinalities import add_all_cardinalities
 from dsp_tools.commands.create.models.input_problems import CollectedProblems
 from dsp_tools.commands.create.models.input_problems import ProblemType
@@ -19,82 +17,9 @@ from dsp_tools.commands.create.models.parsed_ontology import Cardinality
 from dsp_tools.commands.create.models.parsed_ontology import ParsedClassCardinalities
 from dsp_tools.commands.create.models.parsed_ontology import ParsedPropertyCardinality
 from dsp_tools.utils.rdflib_constants import KNORA_API
-from test.unittests.commands.create.create_on_server.fixtures import LAST_MODIFICATION_DATE
-from test.unittests.commands.create.create_on_server.fixtures import ONTO
-from test.unittests.commands.create.create_on_server.fixtures import ONTO_IRI
 
 RESOURCE_IRI = ONTO.Resource
 PROP_IRI = ONTO.hasText
-
-
-class TestMakeOneCardinalityGraph:
-    def test_creates_correct_graph_structure_with_cardinality_1(self) -> None:
-        property_card = ParsedPropertyCardinality(
-            propname=str(PROP_IRI),
-            cardinality=Cardinality.C_1,
-            gui_order=None,
-        )
-        result_graph = _make_one_cardinality_graph(property_card, RESOURCE_IRI)
-        assert len(result_graph) == 4
-        blank_nodes = list(result_graph.objects(RESOURCE_IRI, RDFS.subClassOf))
-        assert len(blank_nodes) == 1
-        bn = blank_nodes[0]
-        assert (bn, RDF.type, OWL.Restriction) in result_graph
-        assert (bn, OWL.cardinality, Literal(1)) in result_graph
-        assert (bn, OWL.onProperty, PROP_IRI) in result_graph
-
-    def test_creates_correct_graph_with_max_cardinality(self) -> None:
-        property_card = ParsedPropertyCardinality(
-            propname=str(PROP_IRI),
-            cardinality=Cardinality.C_0_1,
-            gui_order=None,
-        )
-        result_graph = _make_one_cardinality_graph(property_card, RESOURCE_IRI)
-        bn = next(result_graph.objects(RESOURCE_IRI, RDFS.subClassOf))
-        assert (bn, OWL.maxCardinality, Literal(1)) in result_graph
-
-    def test_creates_correct_graph_with_min_cardinality(self) -> None:
-        property_card = ParsedPropertyCardinality(
-            propname=str(PROP_IRI),
-            cardinality=Cardinality.C_0_N,
-            gui_order=None,
-        )
-        result_graph = _make_one_cardinality_graph(property_card, RESOURCE_IRI)
-        bn = next(result_graph.objects(RESOURCE_IRI, RDFS.subClassOf))
-        assert (bn, OWL.minCardinality, Literal(0)) in result_graph
-
-
-class TestMakeCardinalityGraphForRequest:
-    def test_combines_base_graph_and_cardinality(self) -> None:
-        property_card = ParsedPropertyCardinality(
-            propname=str(PROP_IRI),
-            cardinality=Cardinality.C_1,
-            gui_order=None,
-        )
-        result_graph = _make_cardinality_graph_for_request(
-            property_card, RESOURCE_IRI, ONTO_IRI, LAST_MODIFICATION_DATE
-        )
-        assert (ONTO_IRI, RDF.type, OWL.Ontology) in result_graph
-        assert (ONTO_IRI, KNORA_API.lastModificationDate, LAST_MODIFICATION_DATE) in result_graph
-        blank_nodes = list(result_graph.objects(RESOURCE_IRI, RDFS.subClassOf))
-        assert len(blank_nodes) == 1
-        bn = blank_nodes[0]
-        assert (bn, RDF.type, OWL.Restriction) in result_graph
-        assert (bn, OWL.cardinality, Literal(1)) in result_graph
-        assert (bn, OWL.onProperty, PROP_IRI) in result_graph
-
-    def test_graph_has_correct_total_triples(self) -> None:
-        property_card = ParsedPropertyCardinality(
-            propname=str(PROP_IRI),
-            cardinality=Cardinality.C_1,
-            gui_order=None,
-        )
-        result_graph = _make_cardinality_graph_for_request(
-            property_card, RESOURCE_IRI, ONTO_IRI, LAST_MODIFICATION_DATE
-        )
-        ontology_graph_triples = 2
-        cardinality_graph_triples = 4
-        assert len(result_graph) == ontology_graph_triples + cardinality_graph_triples
 
 
 class TestAddOneCardinality:

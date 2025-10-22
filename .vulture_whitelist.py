eb24a4396095b530ce949164c8a8a56f73ae893f
Nora-Olivia-Ammann <103038637+Nora-Olivia-Ammann@users.noreply.github.com>
2025-10-22 07:58:58 +0200

    vulture

--- .vulture_whitelist.py
+++ .vulture_whitelist.py
@@ -8,6 +8,7 @@ from dsp_tools.utils.ansi_colors import YELLOW
 from dsp_tools.utils.data_formats.date_util import is_full_date
 from dsp_tools.utils.data_formats.shared import check_notna
 from dsp_tools.utils.xml_parsing.parse_clean_validate_xml import validate_root_emit_user_message
+from dsp_tools.utils.rdflib_utils import serialise_json
 
 PermissionValue.RV
 PermissionValue.V
@@ -29,3 +30,5 @@ validate_root_emit_user_message()
 XmllibInputError()
 XmllibInternalError()
 XmllibFileNotFoundError()
+
+serialise_json()

6619577f66b355f494549a27c4526ac7225b2f47
Nora-Olivia-Ammann <103038637+Nora-Olivia-Ammann@users.noreply.github.com>
2025-10-22 17:43:11 +0200

    Update server_project_info.py

--- src/dsp_tools/commands/create/models/server_project_info.py
+++ src/dsp_tools/commands/create/models/server_project_info.py
@@ -1,18 +1,11 @@
 from dataclasses import dataclass
 from dataclasses import field
 
-from dsp_tools.error.exceptions import InternalError
-
 
 @dataclass
 class ProjectIriLookup:
     project_iri: str
     onto_iris: dict[str, str] = field(default_factory=dict)
-
-    def get_onto_iri(self, onto_name: str) -> str:
-        if found := self.onto_iris.get(onto_name):
-            return found
-        raise InternalError(f"Ontology with name '{onto_name}' does not have an IRI.")
 
 
 @dataclass

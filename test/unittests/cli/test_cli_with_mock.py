80c119f1a984d4ca20ecb08699b6f46e864a140d
Nora-Olivia-Ammann <103038637+Nora-Olivia-Ammann@users.noreply.github.com>
2025-10-22 13:09:08 +0200

    Update test_cli_with_mock.py

--- test/unittests/cli/test_cli_with_mock.py
+++ test/unittests/cli/test_cli_with_mock.py
@@ -935,6 +935,7 @@ def test_check_api_health_not_healthy_local(mock_get: Mock) -> None:
     mock_get.assert_called_once_with("http://0.0.0.0:3333/health", timeout=2)
 
 
+@patch("requests.get")
 def test_check_api_health_not_healthy_server(mock_get: Mock) -> None:
     mock_response = Mock()
     mock_response.ok = False
@@ -942,8 +943,7 @@ def test_check_api_health_not_healthy_server(mock_get: Mock) -> None:
     mock_get.return_value = mock_response
 
     expected_msg = regex.escape(
-        "The DSP-API could not be reached. Please check if your stack is healthy "
-        "or start a stack with 'dsp-tools start-stack' if none is running."
+        "The DSP-API could not be reached (returned status 503). Please contact the DaSCH engineering team for help."
     )
     with pytest.raises(DspApiNotReachableError, match=expected_msg):
         _check_api_health("https://api.dasch.swiss")

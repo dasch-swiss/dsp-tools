86d793cbf17c43e38e4b1dd20d7bea082421989d
Nora-Olivia-Ammann <103038637+Nora-Olivia-Ammann@users.noreply.github.com>
2025-10-24 16:35:07 +0200

    Update test_metadata_client_live.py

--- test/unittests/clients/test_metadata_client_live.py
+++ test/unittests/clients/test_metadata_client_live.py
@@ -1,6 +1,9 @@
+# mypy: disable-error-code="no-untyped-def"
+
 from unittest.mock import Mock
 
 import pytest
+from requests import Response
 
 from dsp_tools.clients.authentication_client import AuthenticationClient
 from dsp_tools.clients.metadata_client import MetadataClient
@@ -15,7 +18,7 @@ def mock_auth_client() -> Mock:
 
 
 @pytest.fixture
-def ontology_client(mock_auth_client: Mock) -> MetadataClient:
+def metadata_client(mock_auth_client: Mock) -> MetadataClient:
     return MetadataClient(
         server="http://0.0.0.0:3333",
         authentication_client=mock_auth_client,
@@ -24,50 +27,37 @@ def ontology_client(mock_auth_client: Mock) -> MetadataClient:
 
 @pytest.fixture
 def ok_response():
-    return {
-        "status_code": 200,
-        "headers": {
-            "Server": "webapi/v32.3.2",
-            "Content-Disposition": 'attachment; filename="project_4124_metadata_resources_20251024_142527Z.json"',
-            "Date": "Fri, 24 Oct 2025 14:25:27 GMT",
-            "Content-Type": "application/json",
-            "Content-Length": "463",
-        },
-        "content": [
-            {
-                "resourceClassIri": "http://0.0.0.0:3333/ontology/4124/testonto/v2#minimalResource",
-                "resourceIri": "http://rdfh.ch/4124/bPs-3bjqSr2uIJFGO3Joyw",
-                "arkUrl": "http://0.0.0.0:3336/ark:/72163/1/4124/bPs=3bjqSr2uIJFGO3Joywb",
-                "arkUrlWithTimestamp": "http://0.0.0.0:3336/ark:/72163/1/4124/bPs=3bjqSr2uIJFGO3Joywb.20251024T142527080662792Z",
-                "label": "The only resource",
-                "resourceCreatorIri": "http://rdfh.ch/users/root",
-                "resourceCreationDate": "2025-10-24T14:25:05.092534796Z",
-            }
-        ],
-    }
+    return [
+        {
+            "resourceClassIri": "http://0.0.0.0:3333/ontology/4124/testonto/v2#minimalResource",
+            "resourceIri": "http://rdfh.ch/4124/bPs-3bjqSr2uIJFGO3Joyw",
+            "arkUrl": "http://0.0.0.0:3336/ark:/72163/1/4124/bPs=3bjqSr2uIJFGO3Joywb",
+            "arkUrlWithTimestamp": "http://0.0.0.0:3336/ark:/72163/1/4124/bPs=3bjqSr2uIJFGO3Joywb.20251024T142527080662792Z",
+            "label": "The only resource",
+            "resourceCreatorIri": "http://rdfh.ch/users/root",
+            "resourceCreationDate": "2025-10-24T14:25:05.092534796Z",
+        }
+    ]
 
 
 @pytest.fixture
 def non_ok_response():
-    return {
-        "status_code": 403,
-        "headers": {
-            "Server": "webapi/v32.3.2",
-            "Date": "Fri, 24 Oct 2025 14:27:38 GMT",
-            "Content-Type": "application/json",
-            "Content-Length": "52",
-        },
-        "content": {"message": "Project with shortcode 9999 not found."},
-    }
+    return {"message": "Project with shortcode 9999 not found."}
 
 
-def test_get_resource_metadata_ok(ok_response):
-    pass
+def test_get_resource_metadata_ok(metadata_client, ok_response):
+    mock_response = Mock(spec=Response)
+    mock_response.ok = True
+    mock_response.status_code = 200
+    mock_response.text = ok_response
 
 
-def test_get_resource_metadata_non_ok(non_ok_response):
-    pass
+def test_get_resource_metadata_non_ok(metadata_client, non_ok_response):
+    mock_response = Mock(spec=Response)
+    mock_response.ok = False
+    mock_response.status_code = 403
+    mock_response.text = non_ok_response
 
 
-def test_get_resource_metadata_error_raised():
+def test_get_resource_metadata_error_raised(metadata_client):
     pass

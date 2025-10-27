0e41dfc3de4588f5c1f389525313fce668f8cee7
Nora-Olivia-Ammann <103038637+Nora-Olivia-Ammann@users.noreply.github.com>
2025-10-27 11:29:20 +0100

    include

--- src/dsp_tools/commands/validate_data/prepare_data/prepare_data.py
+++ src/dsp_tools/commands/validate_data/prepare_data/prepare_data.py
@@ -6,9 +6,12 @@ from rdflib import Graph
 
 from dsp_tools.clients.authentication_client import AuthenticationClient
 from dsp_tools.clients.legal_info_client_live import LegalInfoClientLive
+from dsp_tools.clients.metadata_client import MetadataRetrieval
+from dsp_tools.clients.metadata_client_live import MetadataClientLive
 from dsp_tools.commands.validate_data.api_clients import ListClient
 from dsp_tools.commands.validate_data.api_clients import OntologyClient
 from dsp_tools.commands.validate_data.models.api_responses import EnabledLicenseIris
+from dsp_tools.commands.validate_data.models.api_responses import InfoForResourceInDB
 from dsp_tools.commands.validate_data.models.api_responses import ListLookup
 from dsp_tools.commands.validate_data.models.api_responses import OneList
 from dsp_tools.commands.validate_data.models.api_responses import ProjectDataFromApi
@@ -42,13 +45,13 @@ def prepare_data_for_validation_from_parsed_resource(
     permission_ids: list[str],
     auth: AuthenticationClient,
     shortcode: str,
-) -> tuple[RDFGraphs, set[str]]:
+) -> tuple[RDFGraphs, set[str], MetadataRetrieval]:
     used_iris = {x.res_type for x in parsed_resources}
-    proj_info = _get_project_specific_information_from_api(auth, shortcode)
+    proj_info, metadata_retrieval_success = _get_project_specific_information_from_api(auth, shortcode)
     list_lookup = _make_list_lookup(proj_info.all_lists)
     data_rdf = _make_data_graph_from_parsed_resources(parsed_resources, authorship_lookup, list_lookup)
     rdf_graphs = _create_graphs(data_rdf, shortcode, auth, proj_info, permission_ids)
-    return rdf_graphs, used_iris
+    return rdf_graphs, used_iris, metadata_retrieval_success
 
 
 def _make_list_lookup(project_lists: list[OneList]) -> ListLookup:
@@ -60,11 +63,23 @@ def _make_list_lookup(project_lists: list[OneList]) -> ListLookup:
     return ListLookup(lookup)
 
 
-def _get_project_specific_information_from_api(auth: AuthenticationClient, shortcode: str) -> ProjectDataFromApi:
+def _get_project_specific_information_from_api(
+    auth: AuthenticationClient, shortcode: str
+) -> tuple[ProjectDataFromApi, MetadataRetrieval]:
     list_client = ListClient(auth.server, shortcode)
     all_lists = list_client.get_lists()
     enabled_licenses = _get_license_iris(shortcode, auth)
-    return ProjectDataFromApi(all_lists, enabled_licenses)
+    retrieval_status, formatted_metadata = _get_metadata_info(auth, shortcode)
+    return ProjectDataFromApi(all_lists, enabled_licenses, formatted_metadata), retrieval_status
+
+
+def _get_metadata_info(
+    auth: AuthenticationClient, shortcode: str
+) -> tuple[MetadataRetrieval, list[InfoForResourceInDB]]:
+    metadata_client = MetadataClientLive(auth.server, auth)
+    retrieval_status, metadata = metadata_client.get_resource_metadata(shortcode)
+    formatted_metadata = [InfoForResourceInDB(x["resourceIri"], x["resourceClassIri"]) for x in metadata]
+    return retrieval_status, formatted_metadata
 
 
 def _make_data_graph_from_parsed_resources(

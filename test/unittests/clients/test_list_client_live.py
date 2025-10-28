935db7be6c585184a626e93ec30ff887ada1da43
Nora-Olivia-Ammann <103038637+Nora-Olivia-Ammann@users.noreply.github.com>
2025-10-28 12:08:48 +0100

    move fixtures

--- test/unittests/clients/test_list_client_live.py
+++ test/unittests/clients/test_list_client_live.py
@@ -9,6 +9,92 @@ from dsp_tools.error.exceptions import InternalError
 
 
 @pytest.fixture
+def response_all_list_one_project() -> dict[str, Any]:
+    return {
+        "lists": [
+            {
+                "id": "http://rdfh.ch/lists/9999/list1",
+                "projectIri": "http://rdfh.ch/projects/projectIRI",
+                "name": "firstList",
+                "labels": [{"value": "List 1", "language": "en"}],
+                "comments": [{"value": "This is the first list", "language": "en"}],
+                "isRootNode": True,
+            },
+            {
+                "id": "http://rdfh.ch/lists/9999/list2",
+                "projectIri": "http://rdfh.ch/projects/projectIRI",
+                "name": "secondList",
+                "labels": [{"value": "List", "language": "en"}],
+                "comments": [{"value": "This is the second list", "language": "en"}],
+                "isRootNode": True,
+            },
+        ]
+    }
+
+
+@pytest.fixture
+def response_all_list_one_project_no_lists() -> dict[str, Any]:
+    return {"lists": []}
+
+
+@pytest.fixture
+def response_one_list() -> dict[str, Any]:
+    return {
+        "type": "ListGetResponseADM",
+        "list": {
+            "listinfo": {
+                "id": "http://rdfh.ch/lists/9999/list1",
+                "projectIri": "http://rdfh.ch/projects/projectIRI",
+                "name": "firstList",
+                "labels": [{"value": "List 1", "language": "en"}],
+                "comments": [{"value": "This is the first list", "language": "en"}],
+                "isRootNode": True,
+            },
+            "children": [
+                {
+                    "id": "http://rdfh.ch/lists/9999/n1",
+                    "name": "n1",
+                    "labels": [{"value": "Node 1", "language": "en"}],
+                    "comments": [],
+                    "position": 0,
+                    "hasRootNode": "http://rdfh.ch/lists/9999/list1",
+                    "children": [
+                        {
+                            "id": "http://rdfh.ch/lists/9999/n11",
+                            "name": "n1.1",
+                            "labels": [{"value": "Node 1.1", "language": "en"}],
+                            "comments": [],
+                            "position": 0,
+                            "hasRootNode": "http://rdfh.ch/lists/9999/list1",
+                            "children": [
+                                {
+                                    "id": "http://rdfh.ch/lists/9999/n111",
+                                    "name": "n1.1.1",
+                                    "labels": [{"value": "Node 1.1.1", "language": "en"}],
+                                    "comments": [],
+                                    "position": 0,
+                                    "hasRootNode": "http://rdfh.ch/lists/9999/list1",
+                                    "children": [],
+                                },
+                                {
+                                    "id": "http://rdfh.ch/lists/9999/n112",
+                                    "name": "n1.1.2",
+                                    "labels": [{"value": "Node 1.1.2", "language": "en"}],
+                                    "comments": [],
+                                    "position": 1,
+                                    "hasRootNode": "http://rdfh.ch/lists/9999/list1",
+                                    "children": [],
+                                },
+                            ],
+                        }
+                    ],
+                }
+            ],
+        },
+    }
+
+
+@pytest.fixture
 def api_url() -> str:
     return "http://0.0.0.0:3333"
 
@@ -22,7 +108,7 @@ class TestListClient:
     def test_get_all_list_iris(self, list_client: ListGetClientLive) -> None:
         mock_response = Mock(status_code=200, ok=True, headers={})
         mock_response.json.return_value = {"lists": []}
-        with patch("dsp_tools.commands.validate_data.api_clients.requests.get", return_value=mock_response) as mock_get:
+        with patch("dsp_tools.clients.list_client_live.requests.get", return_value=mock_response) as mock_get:
             result = list_client._get_all_list_iris()
         assert result == {"lists": []}
         assert mock_get.call_args_list[0][1]["url"] == f"{list_client.api_url}/admin/lists?projectShortcode=9999"
@@ -30,14 +116,14 @@ class TestListClient:
     def test_get_all_list_iris_non_ok_code(self, list_client: ListGetClientLive) -> None:
         mock_response = Mock(status_code=404, ok=False, headers={})
         mock_response.json.return_value = {}
-        with patch("dsp_tools.commands.validate_data.api_clients.requests.get", return_value=mock_response):
+        with patch("dsp_tools.clients.list_client_live.requests.get", return_value=mock_response):
             with pytest.raises(InternalError):
                 list_client._get_all_list_iris()
 
     def test_get_one_list(self, list_client: ListGetClientLive) -> None:
         mock_response = Mock(status_code=200, ok=True, headers={})
         mock_response.json.return_value = {"type": "ListGetResponseADM", "list": {}}
-        with patch("dsp_tools.commands.validate_data.api_clients.requests.get", return_value=mock_response) as mock_get:
+        with patch("dsp_tools.clients.list_client_live.requests.get", return_value=mock_response) as mock_get:
             result = list_client._get_one_list("http://rdfh.ch/lists/9999/WWqeCEj8R_qrK5djsVcHvg")
         assert result == {"type": "ListGetResponseADM", "list": {}}
         url_expected = f"{list_client.api_url}/admin/lists/http%3A%2F%2Frdfh.ch%2Flists%2F9999%2FWWqeCEj8R_qrK5djsVcHvg"
@@ -46,7 +132,7 @@ class TestListClient:
     def test_get_one_list_non_ok_code(self, list_client: ListGetClientLive) -> None:
         mock_response = Mock(status_code=404, ok=False, headers={})
         mock_response.json.return_value = {}
-        with patch("dsp_tools.commands.validate_data.api_clients.requests.get", return_value=mock_response):
+        with patch("dsp_tools.clients.list_client_live.requests.get", return_value=mock_response):
             with pytest.raises(InternalError):
                 list_client._get_one_list("http://rdfh.ch/lists/9999/WWqeCEj8R_qrK5djsVcHvg")
 
--- test/unittests/clients/test_ontology_get_client_live.py
+++ test/unittests/clients/test_ontology_get_client_live.py
@@ -21,7 +21,7 @@ class TestOntologyClient:
     def test_get_ontology_iris_ok(self, ontology_client: OntologyGetClientLive) -> None:
         mock_response = Mock(status_code=200, ok=True, headers={})
         mock_response.json.return_value = {"project": {"ontologies": ["onto_iri"]}}
-        with patch("dsp_tools.commands.validate_data.api_clients.requests.get", return_value=mock_response) as mock_get:
+        with patch("dsp_tools.clients.ontology_get_client_live.requests.get", return_value=mock_response) as mock_get:
             result = ontology_client._get_ontology_iris()
         assert result == ["onto_iri"]
         assert mock_get.call_args_list[0][1]["url"] == f"{ontology_client.api_url}/admin/projects/shortcode/9999"
@@ -29,21 +29,21 @@ class TestOntologyClient:
     def test_get_ontology_iris_non_ok_code(self, ontology_client: OntologyGetClientLive) -> None:
         mock_response = Mock(status_code=404, ok=False, headers={})
         mock_response.json.return_value = {}
-        with patch("dsp_tools.commands.validate_data.api_clients.requests.get", return_value=mock_response):
+        with patch("dsp_tools.clients.ontology_get_client_live.requests.get", return_value=mock_response):
             with pytest.raises(InternalError):
                 ontology_client._get_ontology_iris()
 
     def test_get_ontology_iris_no_ontology_key(self, ontology_client: OntologyGetClientLive) -> None:
         mock_response = Mock(status_code=200, ok=True, headers={})
         mock_response.json.return_value = {"foo": "bar"}
-        with patch("dsp_tools.commands.validate_data.api_clients.requests.get", return_value=mock_response):
+        with patch("dsp_tools.clients.ontology_get_client_live.requests.get", return_value=mock_response):
             with pytest.raises(InternalError):
                 ontology_client._get_ontology_iris()
 
     def test_get_one_ontology(self, ontology_client: OntologyGetClientLive) -> None:
         mock_response = Mock(status_code=200, ok=True, headers={}, text="Turtle Text")
         mock_response.json.return_value = {}
-        with patch("dsp_tools.commands.validate_data.api_clients.requests.get", return_value=mock_response) as mock_get:
+        with patch("dsp_tools.clients.ontology_get_client_live.requests.get", return_value=mock_response) as mock_get:
             result = ontology_client._get_one_ontology("iri")
         assert result == "Turtle Text"
         assert mock_get.call_args_list[0][1]["url"] == "iri"

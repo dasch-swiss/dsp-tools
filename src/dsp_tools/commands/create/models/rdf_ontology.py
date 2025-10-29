2475d956f5cf04d7b568e60da121c9aa95963003
Nora-Olivia-Ammann <103038637+Nora-Olivia-Ammann@users.noreply.github.com>
2025-10-29 18:48:56 +0100

    refactor(create): check new `create` with `vulture` (#2023)

--- src/dsp_tools/commands/create/models/rdf_ontology.py
+++ src/dsp_tools/commands/create/models/rdf_ontology.py
@@ -7,13 +7,6 @@ from rdflib import URIRef
 
 
 @dataclass
-class RdfResourceCardinality:
-    resource_iri: URIRef
-    on_property: URIRef
-    cardinality: RdfCardinalityRestriction
-
-
-@dataclass
 class RdfCardinalityRestriction:
     owl_property: URIRef
     cardinality_value: Literal

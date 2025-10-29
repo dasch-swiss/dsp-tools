d81e0282b0d09aee5106ad908ef6556bbeb8d104
Nora-Olivia-Ammann <103038637+Nora-Olivia-Ammann@users.noreply.github.com>
2025-10-29 09:24:58 +0100

    add

---
+++ src/dsp_tools/commands/create/parsing/parse_lists.py
@@ -0,0 +1,21 @@
+from typing import Any
+
+from dsp_tools.commands.create.models.parsed_project import ParsedList
+from dsp_tools.commands.create.models.parsed_project import ParsedListNode
+from dsp_tools.commands.create.models.parsed_project import ParsedNodeInfo
+
+
+def parse_list_section(lists: dict[str, Any]) -> list[ParsedList]:
+    pass
+
+
+def _parse_one_list(one_list: dict[str, Any]) -> ParsedList:
+    pass
+
+
+def _parse_node_info(one_node: dict[str, Any]) -> ParsedNodeInfo:
+    pass
+
+
+def _parse_nodes_and_children(list_nodes: list[dict[str, Any]]) -> list[ParsedListNode]:
+    pass

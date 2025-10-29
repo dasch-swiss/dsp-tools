f62689c6430f3109a4345a2a03028298622c6adf
Nora-Olivia-Ammann <103038637+Nora-Olivia-Ammann@users.noreply.github.com>
2025-10-29 14:15:36 +0100

    s

---
+++ src/dsp_tools/commands/create/lists_only.py
@@ -0,0 +1,8 @@
+from pathlib import Path
+from typing import Any
+
+from dsp_tools.cli.args import ServerCredentials
+
+
+def create_lists_only(project_file_as_path_or_parsed: str | Path | dict[str, Any], creds: ServerCredentials) -> bool:
+    pass

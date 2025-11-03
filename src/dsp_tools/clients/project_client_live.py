562a619f296b331ef299dab3372a019023ba87cc
Nora-Olivia-Ammann <103038637+Nora-Olivia-Ammann@users.noreply.github.com>
2025-11-03 11:10:12 +0100

    Update project_client_live.py

--- src/dsp_tools/clients/project_client_live.py
+++ src/dsp_tools/clients/project_client_live.py
@@ -1,12 +1,10 @@
 from dataclasses import dataclass
-from http import HTTPStatus
 from typing import cast
 
 import requests
 from requests import RequestException
 
 from dsp_tools.clients.project_client import ProjectInfoClient
-from dsp_tools.error.exceptions import UnexpectedApiResponseError
 from dsp_tools.utils.request_utils import RequestParameters
 from dsp_tools.utils.request_utils import log_and_raise_request_exception
 from dsp_tools.utils.request_utils import log_request
@@ -31,6 +29,4 @@ class ProjectInfoClientLive(ProjectInfoClient):
         if response.ok:
             result = response.json()
             return cast(str, result["project"]["id"])
-        if response.status_code == HTTPStatus.NOT_FOUND:
-            return None
-        raise UnexpectedApiResponseError(f"Encountered unexpected API response with the code {response.status_code}")
+        return None

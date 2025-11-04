8e99d0c8db0efb35e3509a58c1da29573626b844
Nora-Olivia-Ammann <103038637+Nora-Olivia-Ammann@users.noreply.github.com>
2025-11-04 19:33:50 +0100

    Update test_authentication_client_live.py

--- test/unittests/clients/test_authentication_client_live.py
+++ test/unittests/clients/test_authentication_client_live.py
@@ -1,6 +1,7 @@
 # mypy: disable-error-code="method-assign,no-untyped-def"
 
 from http import HTTPStatus
+from json import JSONDecodeError
 from unittest.mock import Mock
 from unittest.mock import patch
 
@@ -9,6 +10,7 @@ from requests import RequestException
 
 from dsp_tools.clients.authentication_client_live import AuthenticationClientLive
 from dsp_tools.error.exceptions import BadCredentialsError
+from dsp_tools.error.exceptions import DspToolsRequestException
 from dsp_tools.error.exceptions import FatalNonOkApiResponseCode
 
 
@@ -45,6 +47,7 @@ def test_get_token_uses_cached_token(auth_client: AuthenticationClientLive):
 
 def test_get_token_error_responses(auth_client: AuthenticationClientLive):
     mock_response = Mock(status_code=HTTPStatus.BAD_REQUEST.value, ok=False, text="Error", headers={})
+    mock_response.json.side_effect = JSONDecodeError("Expecting value", "", 0)
     with patch("dsp_tools.clients.authentication_client_live.requests.post", return_value=mock_response):
         with pytest.raises(FatalNonOkApiResponseCode):
             auth_client.get_token()
@@ -52,6 +55,7 @@ def test_get_token_error_responses(auth_client: AuthenticationClientLive):
 
 def test_get_token_unauthorised_exception(auth_client: AuthenticationClientLive):
     mock_response = Mock(status_code=HTTPStatus.UNAUTHORIZED.value, ok=False, text="Error", headers={})
+    mock_response.json.side_effect = JSONDecodeError("Expecting value", "", 0)
     with patch("dsp_tools.clients.authentication_client_live.requests.post", return_value=mock_response):
         with pytest.raises(BadCredentialsError):
             auth_client.get_token()
@@ -60,5 +64,5 @@ def test_get_token_unauthorised_exception(auth_client: AuthenticationClientLive)
 def test_get_token_request_exception(auth_client: AuthenticationClientLive):
     request_error = RequestException("Connection timeout")
     with patch("dsp_tools.clients.authentication_client_live.requests.post", side_effect=request_error):
-        with pytest.raises(RequestException):
+        with pytest.raises(DspToolsRequestException):
             auth_client.get_token()

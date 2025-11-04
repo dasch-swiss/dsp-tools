2af026e7b7af4c4c1f903c60c4be2e81ebd3dab7
Nora-Olivia-Ammann <103038637+Nora-Olivia-Ammann@users.noreply.github.com>
2025-11-04 16:39:13 +0100

    Update res.py

--- src/dsp_tools/xmllib/models/res.py
+++ src/dsp_tools/xmllib/models/res.py
@@ -94,10 +94,12 @@ class Resource:
             emit_xmllib_input_type_mismatch_warning(
                 expected_type="xsd:ID", value=res_id, res_id=res_id, value_field="resource ID"
             )
+            res_id = str(res_id)
         if not is_nonempty_value(restype):
             emit_xmllib_input_type_mismatch_warning(
                 expected_type="resource type", value=restype, res_id=res_id, value_field="restype"
             )
+            restype = str(restype)
         lbl = check_and_fix_is_non_empty_string(value=label, res_id=res_id, value_field="label")
 
         return Resource(

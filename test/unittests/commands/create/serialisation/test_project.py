c6db6b2af6410479c51887c0b43edd096e08d342
Nora-Olivia-Ammann <103038637+Nora-Olivia-Ammann@users.noreply.github.com>
2025-11-06 11:30:37 +0100

    Update test_project.py

--- test/unittests/commands/create/serialisation/test_project.py
+++ test/unittests/commands/create/serialisation/test_project.py
@@ -1,3 +1,5 @@
+# mypy: disable-error-code="no-untyped-def"
+
 from dsp_tools.commands.create.models.parsed_project import ParsedGroup
 from dsp_tools.commands.create.models.parsed_project import ParsedGroupDescription
 from dsp_tools.commands.create.models.parsed_project import ParsedUser

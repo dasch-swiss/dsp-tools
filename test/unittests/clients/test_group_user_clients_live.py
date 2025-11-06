720957a5a09b65bb3c2e2f04df509157c722fb4a
Nora-Olivia-Ammann <103038637+Nora-Olivia-Ammann@users.noreply.github.com>
2025-11-06 15:36:23 +0100

    Update test_group_user_clients_live.py

--- test/unittests/clients/test_group_user_clients_live.py
+++ test/unittests/clients/test_group_user_clients_live.py
@@ -1,5 +1,6 @@
 # mypy: disable-error-code="no-untyped-def"
 
+from typing import Any
 from unittest.mock import Mock
 from unittest.mock import patch
 
@@ -10,6 +11,7 @@ from dsp_tools.clients.group_user_clients_live import GroupClientLive
 from dsp_tools.error.custom_warnings import DspToolsUnexpectedStatusCodeWarning
 from dsp_tools.error.exceptions import DspToolsRequestException
 from test.unittests.clients.constants import API_URL
+from test.unittests.clients.constants import GROUP_IRI
 from test.unittests.clients.constants import PROJECT_IRI
 
 
@@ -18,14 +20,26 @@ def group_client() -> GroupClientLive:
     return GroupClientLive(api_url=API_URL)
 
 
+@pytest.fixture
+def new_group() -> dict[str, Any]:
+    return {
+        "name": "NewGroup",
+        "descriptions": [
+            {"value": "NewGroupDescription", "language": "en"},
+            {"value": "NeueGruppenBeschreibung", "language": "de"},
+        ],
+        "project": PROJECT_IRI,
+    }
+
+
 class TestGroupClientLiveGetGroup:
     def test_get_all_groups_success(self, group_client: GroupClientLive) -> None:
         mock_response = Mock(status_code=200, ok=True, headers={})
         mock_response.json.return_value = {
             "groups": [
                 {
-                    "id": "http://rdfh.ch/groups/4123/iri-testgroupEditors",
-                    "name": "testgroupEditors",
+                    "id": GROUP_IRI,
+                    "name": "testgroup",
                     "descriptions": [],
                     "project": {
                         "id": PROJECT_IRI,
@@ -46,8 +60,8 @@ class TestGroupClientLiveGetGroup:
         with patch("dsp_tools.clients.group_user_clients_live.requests.get", return_value=mock_response) as mock_get:
             result = group_client.get_all_groups()
         assert len(result) == 2
-        assert result[0]["name"] == "testgroupEditors"
-        assert result[0]["id"] == "http://rdfh.ch/groups/4123/iri-testgroupEditors"
+        assert result[0]["name"] == "testgroup"
+        assert result[0]["id"] == GROUP_IRI
         assert result[1]["name"] == "testgroupReaders"
         assert mock_get.call_args[0][0] == f"{group_client.api_url}/admin/groups"
 
@@ -97,14 +111,38 @@ class TestGroupClientCreateNewGroup:
     }
     """
 
-    def test_success(self):
-        pass
+    def test_success(self, group_client: GroupClientLive, new_group) -> None:
+        mock_response = Mock(status_code=200, ok=True, headers={})
+        mock_response.json.return_value = {
+            "group": {
+                "id": GROUP_IRI,
+                "name": "NewGroup",
+                "descriptions": [
+                    {"value": "NewGroupDescription", "language": "en"},
+                    {"value": "NeueGruppenBeschreibung", "language": "de"},
+                ],
+                "project": {"id": PROJECT_IRI},
+            }
+        }
+        with patch("dsp_tools.clients.group_user_clients_live.requests.post", return_value=mock_response) as mock_post:
+            result = group_client.create_new_group(new_group)
+        assert result == GROUP_IRI
+        assert mock_post.call_args[0][0] == f"{group_client.api_url}/admin/groups"
 
-    def test_request_exception(self):
-        pass
+    def test_request_exception(self, group_client: GroupClientLive, new_group) -> None:
+        with patch(
+            "dsp_tools.clients.group_user_clients_live.requests.post", side_effect=requests.ReadTimeout("Timeout")
+        ):
+            with pytest.raises(DspToolsRequestException):
+                group_client.create_new_group(new_group)
 
-    def test_non_ok_response(self):
-        pass
+    def test_non_ok_response(self, group_client: GroupClientLive, new_group) -> None:
+        mock_response = Mock(status_code=500, ok=False, headers={}, text="Internal Server Error")
+        mock_response.json.return_value = {}
+        with patch("dsp_tools.clients.group_user_clients_live.requests.post", return_value=mock_response):
+            with pytest.warns(DspToolsUnexpectedStatusCodeWarning):
+                result = group_client.create_new_group(new_group)
+        assert result is None
 
 
 if __name__ == "__main__":

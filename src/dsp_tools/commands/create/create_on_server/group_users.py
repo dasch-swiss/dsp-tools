474450eade81cd2a06326d7c74c456ec1f6e8e5c
Nora-Olivia-Ammann <103038637+Nora-Olivia-Ammann@users.noreply.github.com>
2025-11-06 15:00:58 +0100

    Update group_users.py

--- src/dsp_tools/commands/create/create_on_server/group_users.py
+++ src/dsp_tools/commands/create/create_on_server/group_users.py
@@ -1,6 +1,8 @@
 from typing import Any
+
 from dsp_tools.clients.group_user_clients import GroupClient
 from dsp_tools.commands.create.models.server_project_info import GroupNameToIriLookup
+
 
 def get_group_to_iri_lookup(group_client: GroupClient, project_iri: str) -> GroupNameToIriLookup:
     all_groups = group_client.get_all_groups()
@@ -8,4 +10,9 @@ def get_group_to_iri_lookup(group_client: GroupClient, project_iri: str) -> Grou
 
 
 def _construct_group_lookup(all_groups: list[dict[str, Any]], project_iri: str) -> GroupNameToIriLookup:
-    pass
+    name2iri = {}
+    for group in all_groups:
+        group_project_iri = group["project"]["id"]
+        if group_project_iri == project_iri:
+            name2iri[group["name"]] = group["id"]
+    return GroupNameToIriLookup(name2iri=name2iri)

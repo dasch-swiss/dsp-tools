b01c8b3270f0a1515cf2b0449fa68b7e4663c024
Nora-Olivia-Ammann <103038637+Nora-Olivia-Ammann@users.noreply.github.com>
2025-11-06 14:59:31 +0100

    client

--- src/dsp_tools/clients/group_user_clients_live.py
+++ src/dsp_tools/clients/group_user_clients_live.py
@@ -1,26 +1,35 @@
+from dataclasses import dataclass
 from typing import Any
+from typing import cast
+
+import requests
+from requests import RequestException
 
 from dsp_tools.clients.group_user_clients import GroupClient
+from dsp_tools.utils.request_utils import RequestParameters
+from dsp_tools.utils.request_utils import log_and_raise_request_exception
+from dsp_tools.utils.request_utils import log_and_warn_unexpected_non_ok_response
+from dsp_tools.utils.request_utils import log_request
+from dsp_tools.utils.request_utils import log_response
+
+TIMEOUT = 30
 
 
+@dataclass
 class GroupClientLive(GroupClient):
     api_url: str
 
     def get_all_groups(self) -> list[dict[str, Any]]:
-        """Route: /admin/groups"""
-
-        """
-        response.json:
-        {
-        "groups": [
-            {
-                "id": "http://rdfh.ch/groups/4123/K6tppNTITqGY1nLvxdDL9A",
-                "name": "testgroupEditors",
-                "descriptions": [
-                ],
-                "project": {
-                    "id": "http://rdfh.ch/projects/DPCEKkIjTYe9ApkgwBTWeg",
-                    "shortname": "systematic-tp",
-            }]}
-        """
-
+        url = f"{self.api_url}/admin/groups"
+        params = RequestParameters("GET", url, TIMEOUT)
+        log_request(params)
+        try:
+            response = requests.get(url, timeout=TIMEOUT)
+        except RequestException as err:
+            log_and_raise_request_exception(err)
+        log_response(response)
+        if response.ok:
+            result = response.json()
+            return cast(list[dict[str, Any]], result["groups"])
+        log_and_warn_unexpected_non_ok_response(response.status_code, response.text)
+        return []

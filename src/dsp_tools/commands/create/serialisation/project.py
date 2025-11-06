524452f84509261370d2e88f56079a2fdc2c9cae
Nora-Olivia-Ammann <103038637+Nora-Olivia-Ammann@users.noreply.github.com>
2025-11-06 11:29:56 +0100

    Update project.py

--- src/dsp_tools/commands/create/serialisation/project.py
+++ src/dsp_tools/commands/create/serialisation/project.py
@@ -5,8 +5,23 @@ from dsp_tools.commands.create.models.parsed_project import ParsedUser
 
 
 def serialise_one_group(group: ParsedGroup, project_iri: str) -> dict[str, Any]:
-    pass
+    return {
+        "name": group.name,
+        "descriptions": [{"value": desc.text, "language": desc.lang} for desc in group.descriptions],
+        "project": project_iri,
+        "status": True,
+        "selfjoin": False,
+    }
 
 
 def serialise_one_user_for_creation(user: ParsedUser) -> dict[str, Any]:
-    pass
+    return {
+        "username": user.username,
+        "email": user.email,
+        "givenName": user.given_name,
+        "familyName": user.family_name,
+        "password": user.password,
+        "lang": user.lang,
+        "status": True,
+        "systemAdmin": False,
+    }

78b4e044a0c7efa5fc52cdbef88aba00864251fd
Nora-Olivia-Ammann <103038637+Nora-Olivia-Ammann@users.noreply.github.com>
2025-11-14 17:18:21 +0100

    refactor(create): integrate new code for user creation (DEV-5512) (#2058)

--- src/dsp_tools/commands/create/create_on_server/group_users.py
+++ src/dsp_tools/commands/create/create_on_server/group_users.py
@@ -1,6 +1,7 @@
 from typing import Any
 
 from loguru import logger
+from tqdm import tqdm
 
 from dsp_tools.clients.authentication_client import AuthenticationClient
 from dsp_tools.clients.group_user_clients import GroupClient
@@ -17,6 +18,8 @@ from dsp_tools.commands.create.models.server_project_info import GroupNameToIriL
 from dsp_tools.commands.create.models.server_project_info import UserNameToIriLookup
 from dsp_tools.commands.create.serialisation.project import serialise_one_group
 from dsp_tools.commands.create.serialisation.project import serialise_one_user_for_creation
+from dsp_tools.utils.ansi_colors import BOLD
+from dsp_tools.utils.ansi_colors import RESET_TO_DEFAULT
 
 
 def create_users(
@@ -26,6 +29,7 @@ def create_users(
     auth: AuthenticationClient,
     project_iri: str,
 ) -> CollectedProblems | None:
+    print(BOLD + "Processing user section:" + RESET_TO_DEFAULT)
     client = UserClientLive(auth.server, auth)
     user_to_iri, problems = _create_all_users(users, client)
     membership_problems = _add_all_memberships(user_memberships, user_to_iri, group_lookup, client, project_iri)
@@ -38,7 +42,9 @@ def create_users(
 def _create_all_users(users: list[ParsedUser], client: UserClient) -> tuple[UserNameToIriLookup, list[CreateProblem]]:
     problems: list[CreateProblem] = []
     user_to_iri = UserNameToIriLookup()
-    for usr in users:
+    progress_bar = tqdm(users, desc="    Creating users", dynamic_ncols=True)
+    logger.debug("Creating users")
+    for usr in progress_bar:
         result = _create_one_user(usr, client)
         if result is None:
             problems.append(UploadProblem(usr.username, ProblemType.USER_COULD_NOT_BE_CREATED))
@@ -62,7 +68,9 @@ def _add_all_memberships(
     project_iri: str,
 ) -> list[CreateProblem]:
     problems: list[CreateProblem] = []
-    for memb in memberships:
+    progress_bar = tqdm(memberships, desc="    Updating user information", dynamic_ncols=True)
+    logger.debug("Updating user information")
+    for memb in progress_bar:
         if usr_iri := user_to_iri.get_iri(memb.username):
             result = _add_user_to_project_memberships(memb, usr_iri, project_iri, client)
             problems.extend(result)
@@ -115,7 +123,10 @@ def create_groups(
     group_lookup: GroupNameToIriLookup,
 ) -> tuple[GroupNameToIriLookup, CollectedProblems | None]:
     problems: list[CreateProblem] = []
-    for gr in groups:
+    print(BOLD + "Processing group section:" + RESET_TO_DEFAULT)
+    progress_bar = tqdm(groups, desc="    Creating groups", dynamic_ncols=True)
+    logger.debug("Creating groups")
+    for gr in progress_bar:
         if group_lookup.check_exists(gr.name):
             logger.debug(f"Group with the name '{gr.name}' already exists, skipping.")
             continue

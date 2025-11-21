e2006c0795cfd3e270532f043bb98adb2497ad14
Nora-Olivia-Ammann <103038637+Nora-Olivia-Ammann@users.noreply.github.com>
2025-11-21 08:53:46 +0100

    move

--- src/dsp_tools/commands/create/serialisation/ontology.py
+++ src/dsp_tools/commands/create/serialisation/ontology.py
@@ -1,3 +1,5 @@
+from typing import Any
+
 from rdflib import OWL
 from rdflib import RDF
 from rdflib import RDFS
@@ -11,16 +13,28 @@ from dsp_tools.commands.create.create_on_server.mappers import PARSED_CARDINALIT
 from dsp_tools.commands.create.models.parsed_ontology import ParsedProperty
 from dsp_tools.commands.create.models.parsed_ontology import ParsedPropertyCardinality
 from dsp_tools.utils.rdflib_constants import KNORA_API
+from dsp_tools.utils.rdflib_utils import serialise_json
 
 
-def make_ontology_base_graph(onto_iri: URIRef, last_modification_date: Literal) -> Graph:
+def _make_ontology_base_graph(onto_iri: URIRef, last_modification_date: Literal) -> Graph:
     g = Graph()
     g.add((onto_iri, RDF.type, OWL.Ontology))
     g.add((onto_iri, KNORA_API.lastModificationDate, last_modification_date))
     return g
 
 
-def make_one_cardinality_graph(card: ParsedPropertyCardinality, res_iri: URIRef) -> Graph:
+def serialise_cardinality_graph_for_request(
+    card: ParsedPropertyCardinality, res_iri: URIRef, onto_iri: URIRef, last_modification_date: Literal
+) -> dict[str, Any]:
+    onto_g = _make_ontology_base_graph(onto_iri, last_modification_date)
+    onto_serialised = next(iter(serialise_json(onto_g)))
+    card_g = _make_one_cardinality_graph(card, res_iri)
+    card_serialised = serialise_json(card_g)
+    onto_serialised["@graph"] = card_serialised
+    return onto_serialised
+
+
+def _make_one_cardinality_graph(card: ParsedPropertyCardinality, res_iri: URIRef) -> Graph:
     card_info = PARSED_CARDINALITY_TO_RDF[card.cardinality]
     g = Graph()
     bn_card = BNode()
@@ -34,7 +48,18 @@ def make_one_cardinality_graph(card: ParsedPropertyCardinality, res_iri: URIRef)
     return g
 
 
-def make_one_property_graph(prop: ParsedProperty, list_iri: URIRef | None) -> Graph:
+def serialise_property_graph_for_request(
+    prop: ParsedProperty, list_iri: URIRef | None, onto_iri: URIRef, last_modification_date: Literal
+) -> dict[str, Any]:
+    onto_g = _make_ontology_base_graph(onto_iri, last_modification_date)
+    onto_serialised = next(iter(serialise_json(onto_g)))
+    prop_g = _make_one_property_graph(prop, list_iri)
+    prop_serialised = serialise_json(prop_g)
+    onto_serialised["@graph"] = prop_serialised
+    return onto_serialised
+
+
+def _make_one_property_graph(prop: ParsedProperty, list_iri: URIRef | None) -> Graph:
     trips = [
         (RDF.type, OWL.ObjectProperty),
         (KNORA_API.objectType, URIRef(str(prop.object))),
--- test/unittests/commands/create/serialisation/test_ontology.py
+++ test/unittests/commands/create/serialisation/test_ontology.py
@@ -5,8 +5,9 @@ from rdflib import Literal
 
 from dsp_tools.commands.create.models.parsed_ontology import Cardinality
 from dsp_tools.commands.create.models.parsed_ontology import ParsedPropertyCardinality
-from dsp_tools.commands.create.serialisation.ontology import make_one_cardinality_graph
-from dsp_tools.commands.create.serialisation.ontology import make_ontology_base_graph
+from dsp_tools.commands.create.serialisation.ontology import _make_one_cardinality_graph
+from dsp_tools.commands.create.serialisation.ontology import _make_ontology_base_graph
+from dsp_tools.commands.create.serialisation.ontology import serialise_cardinality_graph_for_request
 from test.unittests.commands.create.constants import LAST_MODIFICATION_DATE
 from test.unittests.commands.create.constants import ONTO
 from test.unittests.commands.create.constants import ONTO_IRI
@@ -16,7 +17,7 @@ PROP_IRI = ONTO.hasText
 
 
 def test_creates_graph_with_correct_structure() -> None:
-    result = make_ontology_base_graph(ONTO_IRI, LAST_MODIFICATION_DATE)
+    result = _make_ontology_base_graph(ONTO_IRI, LAST_MODIFICATION_DATE)
     assert len(result) == 2
 
 
@@ -27,7 +28,7 @@ class TestMakeOneCardinalityGraph:
             cardinality=Cardinality.C_1,
             gui_order=None,
         )
-        result_graph = make_one_cardinality_graph(property_card, RESOURCE_IRI)
+        result_graph = _make_one_cardinality_graph(property_card, RESOURCE_IRI)
         assert len(result_graph) == 5
         blank_nodes = list(result_graph.objects(RESOURCE_IRI, RDFS.subClassOf))
         assert len(blank_nodes) == 1
@@ -42,7 +43,7 @@ class TestMakeOneCardinalityGraph:
             cardinality=Cardinality.C_0_1,
             gui_order=None,
         )
-        result_graph = make_one_cardinality_graph(property_card, RESOURCE_IRI)
+        result_graph = _make_one_cardinality_graph(property_card, RESOURCE_IRI)
         bn = next(result_graph.objects(RESOURCE_IRI, RDFS.subClassOf))
         assert (bn, OWL.maxCardinality, Literal(1)) in result_graph
 
@@ -52,6 +53,49 @@ class TestMakeOneCardinalityGraph:
             cardinality=Cardinality.C_0_N,
             gui_order=None,
         )
-        result_graph = make_one_cardinality_graph(property_card, RESOURCE_IRI)
+        result_graph = _make_one_cardinality_graph(property_card, RESOURCE_IRI)
         bn = next(result_graph.objects(RESOURCE_IRI, RDFS.subClassOf))
         assert (bn, OWL.minCardinality, Literal(0)) in result_graph
+
+
+def test_serialise_card():
+    property_card = ParsedPropertyCardinality(
+        propname=str(PROP_IRI),
+        cardinality=Cardinality.C_1,
+        gui_order=None,
+    )
+    serialised = serialise_cardinality_graph_for_request(property_card, RESOURCE_IRI, ONTO_IRI, LAST_MODIFICATION_DATE)
+
+    # Check ontology-level properties
+    assert serialised["@id"] == "http://0.0.0.0:3333/ontology/9999/onto/v2"
+    assert serialised["@type"] == ["http://www.w3.org/2002/07/owl#Ontology"]
+    assert serialised["http://api.knora.org/ontology/knora-api/v2#lastModificationDate"] == [
+        {"@type": "http://www.w3.org/2001/XMLSchema#dateTimeStamp", "@value": "2025-10-14T13:00:00.000000Z"}
+    ]
+
+    # Check graph contains exactly 2 nodes
+    assert len(serialised["@graph"]) == 2
+
+    # Find the resource and restriction nodes
+    resource_node = next(
+        n for n in serialised["@graph"] if n["@id"] == "http://0.0.0.0:3333/ontology/9999/onto/v2#Resource"
+    )
+    restriction_nodes = [n for n in serialised["@graph"] if n["@id"].startswith("_:")]
+
+    # Verify resource node structure
+    assert resource_node["@type"] == ["http://www.w3.org/2002/07/owl#Class"]
+    assert len(resource_node["http://www.w3.org/2000/01/rdf-schema#subClassOf"]) == 1
+    blank_node_id = resource_node["http://www.w3.org/2000/01/rdf-schema#subClassOf"][0]["@id"]
+    assert blank_node_id.startswith("_:")
+
+    # Verify restriction node structure
+    assert len(restriction_nodes) == 1
+    restriction_node = restriction_nodes[0]
+    assert restriction_node["@id"] == blank_node_id
+    assert restriction_node["@type"] == ["http://www.w3.org/2002/07/owl#Restriction"]
+    assert restriction_node["http://www.w3.org/2002/07/owl#cardinality"] == [
+        {"@type": "http://www.w3.org/2001/XMLSchema#integer", "@value": 1}
+    ]
+    assert restriction_node["http://www.w3.org/2002/07/owl#onProperty"] == [
+        {"@id": "http://0.0.0.0:3333/ontology/9999/onto/v2#hasText"}
+    ]

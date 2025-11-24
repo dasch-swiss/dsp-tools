8099703dce560afd4035b74a459a21cf37fdc678
Nora-Olivia-Ammann <103038637+Nora-Olivia-Ammann@users.noreply.github.com>
2025-11-24 15:10:46 +0100

    Update properties.py

--- src/dsp_tools/commands/create/create_on_server/properties.py
+++ src/dsp_tools/commands/create/create_on_server/properties.py
@@ -9,6 +9,11 @@ def create_all_properties():
     pass
 
 
+def _get_property_create_order(properties: list[ParsedProperty]) -> list[str]:
+    graph, node_to_iri = _make_graph_to_sort(properties)
+    return _sort_properties(graph, node_to_iri)
+
+
 def _make_graph_to_sort(properties: list[ParsedProperty]) -> tuple[rx.PyDiGraph, dict[int, str]]:
     graph: rx.PyDiGraph[Any, Any] = rx.PyDiGraph()
     prop_iris = [x.name for x in properties]
@@ -20,3 +25,8 @@ def _make_graph_to_sort(properties: list[ParsedProperty]) -> tuple[rx.PyDiGraph,
             if super_prop in prop_iris:
                 graph.add_edge(iri_to_node[p.name], iri_to_node[super_prop], i)
     return graph, node_to_iri
+
+
+def _sort_properties(graph: rx.PyDiGraph, node_to_iri: dict[int, str]) -> list[str]:
+    node_sorting_order = rx.topological_sort(graph)
+    return [node_to_iri[x] for x in node_sorting_order]

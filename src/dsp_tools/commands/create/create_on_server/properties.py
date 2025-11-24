944221ee8432f29e08817566dd11bfefd2c70d4d
Nora-Olivia-Ammann <103038637+Nora-Olivia-Ammann@users.noreply.github.com>
2025-11-24 15:01:54 +0100

    Update properties.py

--- src/dsp_tools/commands/create/create_on_server/properties.py
+++ src/dsp_tools/commands/create/create_on_server/properties.py
@@ -1,5 +1,22 @@
+from typing import Any
 
+import rustworkx as rx
+
+from dsp_tools.commands.create.models.parsed_ontology import ParsedProperty
 
 
 def create_all_properties():
     pass
+
+
+def _make_graph_to_sort(properties: list[ParsedProperty]) -> tuple[rx.PyDiGraph, dict[int, str]]:
+    graph: rx.PyDiGraph[Any, Any] = rx.PyDiGraph()
+    prop_iris = [x.name for x in properties]
+    node_indices = list(graph.add_nodes_from(prop_iris))
+    iri_to_node = dict(zip(prop_iris, node_indices))
+    node_to_iri = dict(zip(node_indices, prop_iris))
+    for i, p in enumerate(properties):
+        for super_prop in p.supers:
+            if super_prop in prop_iris:
+                graph.add_edge(iri_to_node[p.name], iri_to_node[super_prop], i)
+    return graph, node_to_iri

584261258d45bb54f2f361f7552c627b18108951
Nora-Olivia-Ammann <103038637+Nora-Olivia-Ammann@users.noreply.github.com>
2025-11-25 10:43:55 +0100

    Update properties.py

--- src/dsp_tools/commands/create/create_on_server/properties.py
+++ src/dsp_tools/commands/create/create_on_server/properties.py
@@ -10,11 +10,13 @@ from dsp_tools.clients.ontology_clients import OntologyCreateClient
 from dsp_tools.commands.create.create_on_server.onto_utils import get_onto_lookup
 from dsp_tools.commands.create.create_on_server.onto_utils import should_retry_request
 from dsp_tools.commands.create.create_on_server.onto_utils import sort_for_upload
+from dsp_tools.commands.create.models.create_problems import CollectedProblems
 from dsp_tools.commands.create.models.create_problems import CreateProblem
 from dsp_tools.commands.create.models.create_problems import UploadProblem
 from dsp_tools.commands.create.models.create_problems import UploadProblemType
 from dsp_tools.commands.create.models.parsed_ontology import ParsedProperty
 from dsp_tools.commands.create.models.server_project_info import CreatedIriCollection
+from dsp_tools.commands.create.models.server_project_info import ListNameToIriLookup
 from dsp_tools.commands.create.models.server_project_info import OntoCreateLookup
 from dsp_tools.commands.create.models.server_project_info import ProjectIriLookup
 from dsp_tools.commands.create.serialisation.ontology import serialise_property_graph_for_request
@@ -24,18 +26,37 @@ def create_all_properties(
     properties: list[ParsedProperty],
     project_iri_lookup: ProjectIriLookup,
     created_iris: CreatedIriCollection,
-    onto_client: OntologyCreateClient,
-):
+    list_lookup: ListNameToIriLookup,
+    client: OntologyCreateClient,
+) -> tuple[CreatedIriCollection, CollectedProblems | None]:
     upload_order = _get_property_create_order(properties)
     prop_lookup = {p.name: p for p in properties}
-    problems: CreateProblem = []
-    onto_lookup = get_onto_lookup(project_iri_lookup, onto_client)
+    all_problems: list[CreateProblem] = []
+    onto_lookup = get_onto_lookup(project_iri_lookup, client)
     progress_bar = tqdm(upload_order, desc="    Creating properties", dynamic_ncols=True)
     for p in progress_bar:
-        pass
+        prop = prop_lookup[p]
+        if previous_blocker := _is_property_blocked(prop, created_iris):
+            all_problems.append(previous_blocker)
+            created_iris.failed_properties.add(prop.name)
+        else:
+            list_iri = list_lookup.get_iri(prop.node_name)
+            create_result = _create_one_property(prop, list_iri, onto_lookup, client)
+            if isinstance(create_result, Literal):
+                onto_lookup.add_last_mod_date(prop.onto_name, create_result)
+                created_iris.created_properties.add(prop.name)
+            else:
+                all_problems.append(create_result)
+                created_iris.failed_properties.add(prop.name)
+    upload_problems = None
+    if all_problems:
+        upload_problems = CollectedProblems(
+            "    While creating properties the following problems occurred:", all_problems
+        )
+    return created_iris, upload_problems
 
 
-def _check_if_property_can_be_created(prop: ParsedProperty, created_iri: CreatedIriCollection) -> CreateProblem | None:
+def _is_property_blocked(prop: ParsedProperty, created_iri: CreatedIriCollection) -> CreateProblem | None:
     if created_iri.property_failed(set(prop.supers)):
         return UploadProblem(prop.name, UploadProblemType.PROPERTY_SUPER_FAILED)
     if created_iri.class_failed(set(str(prop.object))) or created_iri.class_failed(set(prop.subject)):
@@ -65,20 +86,20 @@ def _make_graph_to_sort(properties: list[ParsedProperty]) -> tuple[rx.PyDiGraph,
 def _create_one_property(
     prop: ParsedProperty,
     list_iri: Literal | None,
-    lookup: OntoCreateLookup,
-    onto_client: OntologyCreateClient,
+    onto_lookup: OntoCreateLookup,
+    client: OntologyCreateClient,
 ) -> Literal | CreateProblem:
-    onto_iri = lookup.get_onto_iri(prop.onto_name)
+    onto_iri = onto_lookup.get_onto_iri(prop.onto_name)
     prop_serialised = serialise_property_graph_for_request(
-        prop, list_iri, onto_iri, lookup.get_last_mod_date(prop.onto_name)
+        prop, list_iri, onto_iri, onto_lookup.get_last_mod_date(prop.onto_name)
     )
-    request_result = onto_client.post_new_property(prop_serialised)
+    request_result = client.post_new_property(prop_serialised)
     if isinstance(request_result, Literal):
         return request_result
     if should_retry_request(request_result):
-        new_mod_date = onto_client.get_last_modification_date(lookup.project_iri, onto_iri)
+        new_mod_date = client.get_last_modification_date(onto_lookup.project_iri, onto_iri)
         prop_serialised = serialise_property_graph_for_request(prop, list_iri, onto_iri, new_mod_date)
-        request_result = onto_client.post_new_property(prop_serialised)
+        request_result = client.post_new_property(prop_serialised)
         if isinstance(request_result, Literal):
             return request_result
     if request_result.status_code == HTTPStatus.BAD_REQUEST:

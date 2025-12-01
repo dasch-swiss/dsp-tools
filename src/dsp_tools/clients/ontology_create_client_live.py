72084213e09d092a2ae9e21bbb00d419c1953602
Nora-Olivia-Ammann <103038637+Nora-Olivia-Ammann@users.noreply.github.com>
2025-12-01 12:57:22 +0100

    Update ontology_create_client_live.py

--- src/dsp_tools/clients/ontology_create_client_live.py
+++ src/dsp_tools/clients/ontology_create_client_live.py
@@ -96,7 +96,20 @@ class OntologyCreateClientLive(OntologyCreateClient):
         return ResponseCodeAndText(response.status_code, response.text)
 
     def post_new_ontology(self, onto_graph: dict[str, Any]) -> Literal | ResponseCodeAndText:
-        pass
+        url = f"{self.server}/v2/ontologies"
+        try:
+            response = self._post_and_log_request(url, onto_graph)
+        except RequestException as err:
+            log_and_raise_request_exception(err)
+
+        if response.ok:
+            return _parse_last_modification_date(response.text)
+        if response.status_code == HTTPStatus.FORBIDDEN:
+            raise BadCredentialsError(
+                "Only a SystemAdmin or ProjectAdmin can create new ontologies. "
+                "Your permissions are insufficient for this action."
+            )
+        return ResponseCodeAndText(response.status_code, response.text)
 
     def _post_and_log_request(
         self,

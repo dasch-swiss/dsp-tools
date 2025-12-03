e4003d5ef1360e505f14b30029bf9c3c80c83b1c
Nora-Olivia-Ammann <103038637+Nora-Olivia-Ammann@users.noreply.github.com>
2025-12-03 10:42:39 +0100

    move validate

--- test/unittests/commands/project/test_project_validate/test_default_permissions_overrule.py
+++ test/unittests/commands/project/test_project_validate/test_default_permissions_overrule.py
@@ -2,7 +2,7 @@ from typing import Any
 
 import pytest
 
-from dsp_tools.commands.project.create.project_validate import _check_for_invalid_default_permissions_overrule
+from dsp_tools.commands.create.project_validate import _check_for_invalid_default_permissions_overrule
 from dsp_tools.error.exceptions import BaseError
 
 

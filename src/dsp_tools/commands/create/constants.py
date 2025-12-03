7df8ca7e21f17eb3981569baa85da9ca6c48ad7e
Nora-Olivia-Ammann <103038637+Nora-Olivia-Ammann@users.noreply.github.com>
2025-12-03 14:41:24 +0100

    Update constants.py

--- src/dsp_tools/commands/create/constants.py
+++ src/dsp_tools/commands/create/constants.py
@@ -5,3 +5,4 @@ SALSAH_GUI_STR = "http://api.knora.org/ontology/salsah-gui/v2#"
 SALSAH_GUI = Namespace(SALSAH_GUI_STR)
 
 UNIVERSAL_PREFIXES = {"knora-api": KNORA_API_STR, "salsah-gui": SALSAH_GUI_STR}
+KNORA_ADMIN_PREFIX_STR = "http://www.knora.org/ontology/knora-admin#"

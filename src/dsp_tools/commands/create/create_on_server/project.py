bf0d93a078d474e426ef26c8ef15950d5b26c297
Nora-Olivia-Ammann <103038637+Nora-Olivia-Ammann@users.noreply.github.com>
2025-12-09 11:24:08 +0100

    Update project.py

--- src/dsp_tools/commands/create/create_on_server/project.py
+++ src/dsp_tools/commands/create/create_on_server/project.py
@@ -9,7 +9,7 @@ from dsp_tools.commands.create.exceptions import UnableToCreateProjectError
 from dsp_tools.commands.create.models.parsed_project import ParsedProjectMetadata
 from dsp_tools.commands.create.serialisation.project import serialise_project
 from dsp_tools.error.exceptions import ProjectNotFoundError
-from dsp_tools.utils.ansi_colors import BACKGROUND_BOLD_CYAN
+from dsp_tools.utils.ansi_colors import BACKGROUND_BOLD_YELLOW
 from dsp_tools.utils.ansi_colors import BOLD
 from dsp_tools.utils.ansi_colors import BOLD_RED
 from dsp_tools.utils.ansi_colors import RESET_TO_DEFAULT
@@ -35,7 +35,7 @@ def _exit_if_create_should_not_continue(shortcode: str, exit_if_exists: bool) ->
             "The process is aborted without further uploads."
         )
         logger.info(msg)
-        print(BACKGROUND_BOLD_CYAN + msg + RESET_TO_DEFAULT)
+        print(BACKGROUND_BOLD_YELLOW + msg + RESET_TO_DEFAULT)
         sys.exit(0)
 
     msg = (

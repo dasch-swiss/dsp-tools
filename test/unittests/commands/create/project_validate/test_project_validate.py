c2da113f79497928ea449f5e335042f53d3b80b1
Nora-Olivia-Ammann <103038637+Nora-Olivia-Ammann@users.noreply.github.com>
2025-12-11 13:39:10 +0100

    test flatten list

--- test/unittests/commands/create/project_validate/test_project_validate.py
+++ test/unittests/commands/create/project_validate/test_project_validate.py
@@ -4,6 +4,7 @@ from dsp_tools.commands.create.models.parsed_project import ParsedList
 from dsp_tools.commands.create.models.parsed_project import ParsedListNode
 from dsp_tools.commands.create.models.parsed_project import ParsedNodeInfo
 from dsp_tools.commands.create.project_validate import _check_for_duplicates_in_list_section
+from dsp_tools.commands.create.project_validate import _flatten_all_list
 
 
 @pytest.fixture
@@ -13,14 +14,25 @@ def two_nested_lists_ok() -> list[ParsedList]:
             list_info=ParsedNodeInfo(name="list1", labels={"en": "List 1"}, comments=None),
             children=[
                 ParsedListNode(
-                    node_info=ParsedNodeInfo(name="node1-1", labels={"en": "Node 1-1"}, comments=None), children=[]
-                ),
-                ParsedListNode(
-                    node_info=ParsedNodeInfo(name="node1-2", labels={"en": "Node 1-2"}, comments=None),
+                    node_info=ParsedNodeInfo(name="node1", labels={"en": "Node 1"}, comments=None),
                     children=[
                         ParsedListNode(
-                            node_info=ParsedNodeInfo(name="node1-2-1", labels={"en": "Node 1-2-1"}, comments=None),
-                            children=[],
+                            node_info=ParsedNodeInfo(name="node1-1", labels={"en": "node1-1"}, comments=None),
+                            children=[
+                                ParsedListNode(
+                                    node_info=ParsedNodeInfo(
+                                        name="node1-1-1", labels={"en": "node1-1-1"}, comments=None
+                                    ),
+                                    children=[
+                                        ParsedListNode(
+                                            node_info=ParsedNodeInfo(
+                                                name="node1-1-1-1", labels={"en": "node1-1-1-1"}, comments=None
+                                            ),
+                                            children=[],
+                                        )
+                                    ],
+                                ),
+                            ],
                         )
                     ],
                 ),
@@ -160,19 +172,26 @@ def list_with_duplicate_list_name_and_node_name() -> list[ParsedList]:
     ]
 
 
-def test_check_for_duplicates_ok_nested(two_nested_lists_ok: list[ParsedList]) -> None:
+def test_(two_nested_lists_ok):
+    result = _flatten_all_list(two_nested_lists_ok)
+    expected = {"node1", "node1-1", "node1-1-1", "node1-1-1-1", "node2-1", "node2-2", "node2-2-1"}
+    assert len(result) == len(expected)
+    assert set(result) == expected
+
+
+def test_check_for_duplicates_ok_nested(two_nested_lists_ok):
     result = _check_for_duplicates_in_list_section(two_nested_lists_ok)
     assert result is None
 
 
-def test_check_for_duplicates_flat_one_duplicate(two_flat_lists_one_duplicate: list[ParsedList]) -> None:
+def test_check_for_duplicates_flat_one_duplicate(two_flat_lists_one_duplicate):
     result = _check_for_duplicates_in_list_section(two_flat_lists_one_duplicate)
     assert result is not None
     assert len(result.problems) == 1
     assert "node1-1" in result.problems[0].problematic_object
 
 
-def test_check_for_duplicates_nested_two_duplicates(two_nested_lists_two_duplicates: list[ParsedList]) -> None:
+def test_check_for_duplicates_nested_two_duplicates(two_nested_lists_two_duplicates):
     result = _check_for_duplicates_in_list_section(two_nested_lists_two_duplicates)
     assert result is not None
     assert len(result.problems) == 2
@@ -181,7 +200,7 @@ def test_check_for_duplicates_nested_two_duplicates(two_nested_lists_two_duplica
     assert any("node1-2-1" in obj for obj in problematic_objects)
 
 
-def test_check_for_duplicates_duplicate_list_names(two_lists_duplicate_list_names: list[ParsedList]) -> None:
+def test_check_for_duplicates_duplicate_list_names(two_lists_duplicate_list_names):
     result = _check_for_duplicates_in_list_section(two_lists_duplicate_list_names)
     assert result is not None
     assert len(result.problems) == 1
@@ -189,8 +208,8 @@ def test_check_for_duplicates_duplicate_list_names(two_lists_duplicate_list_name
 
 
 def test_check_for_duplicates_both_list_and_node_duplicates(
-    list_with_duplicate_list_name_and_node_name: list[ParsedList],
-) -> None:
+    list_with_duplicate_list_name_and_node_name,
+):
     result = _check_for_duplicates_in_list_section(list_with_duplicate_list_name_and_node_name)
     assert result is not None
     assert len(result.problems) == 2

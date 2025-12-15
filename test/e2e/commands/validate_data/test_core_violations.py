71f6eafcb41cf0e1fe8f4de52b76c71f604c02d3
Nora-Olivia-Ammann <103038637+Nora-Olivia-Ammann@users.noreply.github.com>
2025-12-15 15:33:18 +0100

    rename

--- test/e2e/commands/validate_data/test_core_violations.py
+++ test/e2e/commands/validate_data/test_core_violations.py
@@ -29,8 +29,8 @@ from dsp_tools.commands.validate_data.process_validation_report.query_validation
 from dsp_tools.commands.validate_data.shacl_cli_validator import ShaclCliValidator
 from dsp_tools.commands.validate_data.validate_data import _get_validation_status
 from dsp_tools.commands.validate_data.validate_data import _validate_data
-from dsp_tools.commands.validate_data.validation.do_python_checks import check_for_duplicate_files
 from dsp_tools.commands.validate_data.validation.get_validation_report import get_validation_report
+from dsp_tools.commands.validate_data.validation.python_checks import check_for_duplicate_files
 from dsp_tools.utils.rdf_constants import DASH
 from dsp_tools.utils.xml_parsing.models.parsed_resource import ParsedResource
 from test.e2e.commands.validate_data.util import prepare_data_for_validation_from_file

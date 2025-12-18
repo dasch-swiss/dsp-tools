1bde0da1518296550dbd447874cf1867d28fa70b
Nora-Olivia-Ammann <103038637+Nora-Olivia-Ammann@users.noreply.github.com>
2025-12-18 08:14:40 +0100

    fix tests

--- src/dsp_tools/commands/create/lists_only.py
+++ src/dsp_tools/commands/create/lists_only.py
@@ -5,12 +5,11 @@ from loguru import logger
 from dsp_tools.cli.args import ServerCredentials
 from dsp_tools.clients.authentication_client_live import AuthenticationClientLive
 from dsp_tools.clients.project_client_live import ProjectClientLive
-from dsp_tools.commands.create.communicate_problems import print_all_problem_collections
 from dsp_tools.commands.create.communicate_problems import print_problem_collection
 from dsp_tools.commands.create.create_on_server.lists import create_lists
 from dsp_tools.commands.create.models.parsed_project import ParsedList
 from dsp_tools.commands.create.models.parsed_project import ParsedProjectMetadata
-from dsp_tools.commands.create.project_validate import parse_and_validate_project
+from dsp_tools.commands.create.project_validate import parse_and_validate_lists
 from dsp_tools.error.exceptions import ProjectNotFoundError
 from dsp_tools.error.exceptions import UnreachableCodeError
 from dsp_tools.setup.ansi_colors import BACKGROUND_BOLD_YELLOW
@@ -18,9 +17,9 @@ from dsp_tools.setup.ansi_colors import RESET_TO_DEFAULT
 
 
 def create_lists_only(project_file: Path, creds: ServerCredentials) -> bool:
-    problems, metadata, parsed_lists = parse_and_validate_project(project_file, creds.server)
+    problems, metadata, parsed_lists = parse_and_validate_lists(project_file)
     if problems:
-        print_all_problem_collections(problems)
+        print_problem_collection(problems)
         return False
     return _handle_parsed_project_result(metadata, parsed_lists, creds)
 
--- test/unittests/commands/create/parsing/test_parse_project.py
+++ test/unittests/commands/create/parsing/test_parse_project.py
@@ -253,15 +253,9 @@ class TestParseUsers:
 
 
 class TestParseLists:
-    def test_parse_lists_empty(self, minimal_project_json):
-        result, problems = parse_lists(minimal_project_json)
-        assert len(result) == 0
-        assert not problems
-
     def test_parse_lists_with_lists(self, project_json_create):
-        result, problems = parse_lists(project_json_create["project"])
+        result = parse_lists(project_json_create["project"])
         assert len(result) == 2
-        assert not problems
 
 
 class TestParseAllOntologies:

6e769954b2d1be3507f52d91818e4ed720dcfc72
Nora-Olivia-Ammann <103038637+Nora-Olivia-Ammann@users.noreply.github.com>
2026-01-12 13:38:50 +0100

    Update exceptions.py

--- src/dsp_tools/commands/xmlupload/exceptions.py
+++ src/dsp_tools/commands/xmlupload/exceptions.py
@@ -1,6 +1,7 @@
 from dsp_tools.clients.exceptions import InvalidInputError
 from dsp_tools.commands.xmlupload.models.input_problems import MultimediaFileNotFoundProblem
 from dsp_tools.error.exceptions import BaseError
+from dsp_tools.error.exceptions import InternalError
 from dsp_tools.error.exceptions import UserError
 
 LIST_SEPARATOR = "\n   - "
@@ -24,25 +25,25 @@ class XmlUploadError(BaseError):
     """Represents an error raised in the context of the xmlupload."""
 
 
-class XmlInputConversionError(BaseError):
-    """Represents an error raised in the context of the xmlupload."""
-
-
-class Id2IriReplacementError(BaseError):
-    """Represents an error raised if an internal ID could not be found in the Id2Iri mapping."""
-
-
 class XmlUploadInterruptedError(XmlUploadError):
     """Represents an error raised when the xmlupload was interrupted."""
 
 
-class XmlUploadPermissionsNotFoundError(BaseError):
+class XmlInputConversionError(InternalError):
+    """Represents an error raised in the context of the xmlupload."""
+
+
+class Id2IriReplacementError(UserError):
+    """Represents an error raised if an internal ID could not be found in the Id2Iri mapping."""
+
+
+class XmlUploadPermissionsNotFoundError(UserError):
     """Class for errors that are raised when a permission does not exist."""
 
 
-class XmlUploadAuthorshipsNotFoundError(BaseError):
+class XmlUploadAuthorshipsNotFoundError(UserError):
     """Class for errors that are raised when an authorship id does not exist."""
 
 
-class XmlUploadListNodeNotFoundError(BaseError):
+class XmlUploadListNodeNotFoundError(UserError):
     """Class for errors that are raised when a list node does not exist."""

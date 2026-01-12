80fc4008999cbab50c96d3a3950c0f485f977c95
Nora-Olivia-Ammann <103038637+Nora-Olivia-Ammann@users.noreply.github.com>
2026-01-12 13:36:08 +0100

    Update exceptions.py

--- src/dsp_tools/commands/validate_data/exceptions.py
+++ src/dsp_tools/commands/validate_data/exceptions.py
@@ -1,9 +1,9 @@
-from dsp_tools.error.exceptions import BaseError
+from dsp_tools.error.exceptions import InternalError
 
 
-class ShaclValidationCliError(BaseError):
+class ShaclValidationCliError(InternalError):
     """This error is raised when the validate data docker command has problems"""
 
 
-class ShaclValidationError(BaseError):
+class ShaclValidationError(InternalError):
     """This error is raised when an unexpected error occurs during the validation"""

0e16e6df210fedc3133ad087eb3579065887707d
Nora-Olivia-Ammann <103038637+Nora-Olivia-Ammann@users.noreply.github.com>
2026-01-12 13:32:14 +0100

    -

--- src/dsp_tools/error/exceptions.py
+++ src/dsp_tools/error/exceptions.py
@@ -98,17 +98,3 @@ class PermanentTimeOutError(BaseError):
 
 class BadCredentialsError(PermanentConnectionError):
     """This error is raised when DSP-API doesn't accept the provided credentials."""
-
-
-class InvalidLicenseError(InputError):
-    """This error is raised when a license string cannot be parsed."""
-
-    license_str: str
-
-    def __init__(self, license_str: str) -> None:
-        msg = (
-            f"The provided license string is invalid and cannot be parsed: '{license_str}'"
-            "You must provide a license that can be parsed by xmllib.find_license_in_string(). "
-            "See https://docs.dasch.swiss/latest/DSP-TOOLS/xmllib-docs/general-functions/#xmllib.general_functions.find_license_in_string"
-        )
-        super().__init__(msg)

417be2b51e47de1b598d86a675756bca424ec991
Nora-Olivia-Ammann <103038637+Nora-Olivia-Ammann@users.noreply.github.com>
2026-01-14 11:42:52 +0100

    -

--- src/dsp_tools/cli/call_action_with_network.py
+++ src/dsp_tools/cli/call_action_with_network.py
@@ -16,8 +16,8 @@ from dsp_tools.commands.ingest_xmlupload.create_resources.upload_xml import inge
 from dsp_tools.commands.ingest_xmlupload.ingest_files.ingest_files import ingest_files
 from dsp_tools.commands.ingest_xmlupload.upload_files.upload_files import upload_files
 from dsp_tools.commands.resume_xmlupload.resume_xmlupload import resume_xmlupload
-from dsp_tools.commands.start_stack import StackConfiguration
-from dsp_tools.commands.start_stack import StackHandler
+from dsp_tools.commands.start_stack.start_stack import StackConfiguration
+from dsp_tools.commands.start_stack.start_stack import StackHandler
 from dsp_tools.commands.validate_data.validate_data import validate_data
 from dsp_tools.commands.xmlupload.upload_config import UploadConfig
 from dsp_tools.commands.xmlupload.xmlupload import xmlupload

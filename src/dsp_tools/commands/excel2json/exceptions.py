078e7164ee39fb53c01eaea863f6b6a20a553a7b
Nora-Olivia-Ammann <103038637+Nora-Olivia-Ammann@users.noreply.github.com>
2026-01-14 09:22:16 +0100

    excel2json

--- src/dsp_tools/commands/excel2json/exceptions.py
+++ src/dsp_tools/commands/excel2json/exceptions.py
@@ -3,3 +3,7 @@ from dsp_tools.error.exceptions import UserError
 
 class InvalidGuiAttributeError(UserError):
     """This error is raised when a invalid gui-attribute is used."""
+
+
+class InvalidFileFormatError(UserError):
+    """When the user input file does not conform to the expected format."""

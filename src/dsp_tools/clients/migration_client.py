3a5cf1701ffc979b067360173fcdb9343a51512c
Nora-Olivia-Ammann <103038637+Nora-Olivia-Ammann@users.noreply.github.com>
2026-02-23 09:47:42 +0100

    Create migration_client.py

---
+++ src/dsp_tools/clients/migration_client.py
@@ -0,0 +1,45 @@
+from dataclasses import dataclass
+from enum import Enum
+from enum import auto
+from typing import Protocol
+
+from dsp_tools.clients.authentication_client import AuthenticationClient
+
+
+class ExportImportStatus(Enum):
+    IN_PROGRESS = auto()
+    COMPLETED = auto()
+    FAILED = auto()
+
+
+@dataclass
+class MigrationExportClient(Protocol):
+    project_iri: str
+    auth: AuthenticationClient
+
+    def get_status(self, export_id: str) -> ExportImportStatus:
+        """Get the export status."""
+
+    def post_export(self) -> str:
+        """Start an export."""
+
+    def get_download(self, export_id: str) -> None:
+        """Get the zip download."""
+
+    def delete_export(self) -> None:
+        """Delete an export after completion or failure."""
+
+
+@dataclass
+class MigrationImportClient(Protocol):
+    project_iri: str
+    auth: AuthenticationClient
+
+    def get_status(self, import_id: str) -> ExportImportStatus:
+        """Get an import status."""
+
+    def post_import(self) -> str:
+        """Post the import zip file."""
+
+    def delete_import(self, import_id: str) -> None:
+        """Delete an import after completion or failure."""

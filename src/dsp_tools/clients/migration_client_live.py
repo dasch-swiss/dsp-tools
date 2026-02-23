f51ff430db295a31d4f1839d6e8495355190aa5d
Nora-Olivia-Ammann <103038637+Nora-Olivia-Ammann@users.noreply.github.com>
2026-02-23 09:55:02 +0100

    Update migration_client_live.py

--- src/dsp_tools/clients/migration_client_live.py
+++ src/dsp_tools/clients/migration_client_live.py
@@ -3,6 +3,15 @@ from dsp_tools.clients.migration_client import ExportImportStatus
 from dsp_tools.clients.migration_client import MigrationExportClient
 from dsp_tools.clients.migration_client import MigrationImportClient
 
+STATUS_MAPPER = {
+    "in_progress": ExportImportStatus.IN_PROGRESS,
+    "completed": ExportImportStatus.COMPLETED,
+    "failed": ExportImportStatus.FAILED,
+}
+
+
+# docs: https://api.stage.dasch.swiss/api/docs/#/API%20v3/
+
 
 class MigrationExportClientLive(MigrationExportClient):
     server: str
@@ -10,16 +19,23 @@ class MigrationExportClientLive(MigrationExportClient):
     auth: AuthenticationClient
 
     def post_export(self) -> str:
-        """{self.server}/v3/projects/{projectIri}/exports"""
+        """
+        {self.server}/v3/projects/{projectIri}/exports
+        Status good: 202
+
+        """
 
     def get_status(self, export_id: str) -> ExportImportStatus:
-        """{self.server}/v3/projects/{projectIri}/exports/{exportId}"""
+        """{self.server}/v3/projects/{projectIri}/exports/{exportId}
+        Status good: 200"""
 
     def get_download(self, export_id: str) -> None:
-        """{self.server}/v3/projects/{projectIri}/exports/{exportId}/download"""
+        """{self.server}/v3/projects/{projectIri}/exports/{exportId}/download
+        Status good: 200"""
 
     def delete_export(self) -> None:
-        """{self.server}/v3/projects/{projectIri}/exports/{exportId}"""
+        """{self.server}/v3/projects/{projectIri}/exports/{exportId}
+        Status good: 204"""
 
 
 class MigrationImportClientLive(MigrationImportClient):
@@ -28,10 +44,13 @@ class MigrationImportClientLive(MigrationImportClient):
     auth: AuthenticationClient
 
     def post_import(self) -> str:
-        """{self.server}/v3/projects/{projectIri}/imports"""
+        """{self.server}/v3/projects/{projectIri}/imports
+        Status good: 202"""
 
     def get_status(self, import_id: str) -> ExportImportStatus:
-        """{self.server}/v3/projects/{projectIri}/imports/{importId}"""
+        """{self.server}/v3/projects/{projectIri}/imports/{importId}
+        Status good: 200"""
 
     def delete_import(self, import_id: str) -> None:
-        """{self.server}/v3/projects/{projectIri}/imports/{importId}"""
+        """{self.server}/v3/projects/{projectIri}/imports/{importId}
+        Status good: 204"""

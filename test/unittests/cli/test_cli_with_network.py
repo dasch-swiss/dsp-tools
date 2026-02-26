82ca728b73a4400d13868fb53cd0735b6b7cf7e4
Nora-Olivia-Ammann <103038637+Nora-Olivia-Ammann@users.noreply.github.com>
2026-02-26 08:37:00 +0100

    Update test_cli_with_network.py

--- test/unittests/cli/test_cli_with_network.py
+++ test/unittests/cli/test_cli_with_network.py
@@ -7,9 +7,7 @@ import pytest
 from dsp_tools.cli import entry_point
 from dsp_tools.cli.args import ServerCredentials
 from dsp_tools.cli.args import ValidationSeverity
-from dsp_tools.clients.migration_clients import ExportId
 from dsp_tools.commands.migration.models import MigrationConfig
-from dsp_tools.commands.migration.models import MigrationInfo
 from dsp_tools.commands.migration.models import ServerInfo
 from dsp_tools.commands.start_stack.start_stack import StackConfiguration
 from dsp_tools.commands.xmlupload.upload_config import UploadConfig
@@ -852,7 +850,8 @@ class TestMigrationExport:
     @patch("dsp_tools.cli.utils._check_network_health")
     @patch("dsp_tools.cli.call_action_with_network.export")
     def test_migration_export(self, mock_export: Mock, check_network: Mock) -> None:
-        source = ServerInfo(server="https://api.some-project.dasch.swiss", user="root1@example.com", password="test1")
+        mock_export.return_value = True, None
+        source = ServerInfo(server="http://0.0.0.0:3333", user="root1@example.com", password="test1")
         config = MigrationConfig(shortcode="0000", export_savepath=Path("testdata/migration/"), keep_local_export=False)
         args = f"migration export {MIGRATION_YAML_COMPLETE_PATH}".split()
         entry_point.run(args)
@@ -861,14 +860,15 @@ class TestMigrationExport:
     @patch("dsp_tools.cli.utils._check_network_health")
     @patch("dsp_tools.cli.call_action_with_network.export")
     def test_migration_export_keep_export(self, mock_export: Mock, check_network: Mock) -> None:
-        source = ServerInfo(server="https://api.some-project.dasch.swiss", user="root1@example.com", password="test1")
+        mock_export.return_value = True, None
+        source = ServerInfo(server="http://0.0.0.0:3333", user="root1@example.com", password="test1")
         config = MigrationConfig(shortcode="0000", export_savepath=Path("testdata/migration/"), keep_local_export=True)
-        args = f"migration export testdata/migration/migration-0000_complete_keep_export.yaml".split()
+        args = "migration export testdata/migration/migration-0000_complete_keep_export.yaml".split()
         entry_point.run(args)
         mock_export.assert_called_once_with(source, config)
 
     def test_migration_export_source_server_missing(self) -> None:
-        args = f"migration export testdata/migration/migration-0000_target_only.yaml".split()
+        args = "migration export testdata/migration/migration-0000_target_only.yaml".split()
         with pytest.raises(SystemExit):
             entry_point.run(args)
 

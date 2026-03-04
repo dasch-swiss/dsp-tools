94cc681aeb768b877c81f4cbb4fe3ab5f236949a
Nora-Olivia-Ammann <103038637+Nora-Olivia-Ammann@users.noreply.github.com>
2026-03-04 09:23:49 +0100

    add clean up

--- src/dsp_tools/commands/migration/migration.py
+++ src/dsp_tools/commands/migration/migration.py
@@ -4,6 +4,7 @@ from dsp_tools.clients.migration_clients import MigrationImportClient
 from dsp_tools.clients.migration_clients_live import MigrationExportClientLive
 from dsp_tools.clients.migration_clients_live import MigrationImportClientLive
 from dsp_tools.clients.project_client_live import ProjectClientLive
+from dsp_tools.commands.migration.clean_up import handle_local_clean_up
 from dsp_tools.commands.migration.download import execute_download
 from dsp_tools.commands.migration.exceptions import MigrationDownloadFailureError
 from dsp_tools.commands.migration.exceptions import MigrationExportFailureError
@@ -31,6 +32,12 @@ def migration(migration_info: MigrationInfo) -> bool:
     import_success, import_id = execute_import(import_client, migration_info.config)
     if not import_success:
         raise MigrationImportFailureError()
+
+    # clean up
+    export_client.delete_export(export_id)
+    import_client.delete_import(import_id)
+    handle_local_clean_up(migration_info.config)
+    return True
 
 
 def _get_export_client(source_info: ServerInfo, config: MigrationConfig) -> MigrationExportClient:

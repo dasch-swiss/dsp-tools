088d08af3bbc1bc56be9f73115c0c08de88db3a7
Nora-Olivia-Ammann <103038637+Nora-Olivia-Ammann@users.noreply.github.com>
2026-03-04 09:30:01 +0100

    Update migration.py

--- src/dsp_tools/commands/migration/migration.py
+++ src/dsp_tools/commands/migration/migration.py
@@ -1,6 +1,6 @@
+from typing import cast
+
 from dsp_tools.clients.authentication_client_live import AuthenticationClientLive
-from dsp_tools.clients.migration_clients import MigrationExportClient
-from dsp_tools.clients.migration_clients import MigrationImportClient
 from dsp_tools.clients.migration_clients_live import MigrationExportClientLive
 from dsp_tools.clients.migration_clients_live import MigrationImportClientLive
 from dsp_tools.clients.project_client_live import ProjectClientLive
@@ -11,13 +11,24 @@ from dsp_tools.commands.migration.exceptions import MigrationExportFailureError
 from dsp_tools.commands.migration.exceptions import MigrationImportFailureError
 from dsp_tools.commands.migration.export import execute_export
 from dsp_tools.commands.migration.import_zip import execute_import
-from dsp_tools.commands.migration.models import MigrationConfig
 from dsp_tools.commands.migration.models import MigrationInfo
 from dsp_tools.commands.migration.models import ServerInfo
 
 
 def migration(migration_info: MigrationInfo) -> bool:
-    export_client = _get_export_client(migration_info.source, migration_info.config)
+    project_iri = _export_download(migration_info)
+
+    _import(migration_info, project_iri)
+
+    handle_local_clean_up(migration_info.config)
+    return True
+
+
+def _export_download(migration_info: MigrationInfo) -> str:
+    source_server = cast(ServerInfo, migration_info.source)
+    auth = AuthenticationClientLive(source_server.server, source_server.user, source_server.password)
+    project_iri = ProjectClientLive(source_server.server, auth).get_project_iri(migration_info.config.shortcode)
+    export_client = MigrationExportClientLive(source_server.server, project_iri, auth)
 
     export_success, export_id = execute_export(export_client, migration_info.config.reference_savepath)
     if not export_success:
@@ -27,25 +38,18 @@ def migration(migration_info: MigrationInfo) -> bool:
     if not download_success:
         raise MigrationDownloadFailureError()
 
-    import_client = _get_import_client(migration_info.target, export_client.project_iri)
+    export_client.delete_export(export_id)
+
+    return project_iri
+
+
+def _import(migration_info: MigrationInfo, project_iri: str):
+    target_server = cast(ServerInfo, migration_info.target)
+    auth = AuthenticationClientLive(target_server.server, target_server.user, target_server.password)
+    import_client = MigrationImportClientLive(target_server.server, project_iri, auth)
 
     import_success, import_id = execute_import(import_client, migration_info.config)
     if not import_success:
         raise MigrationImportFailureError()
 
-    # clean up
-    export_client.delete_export(export_id)
     import_client.delete_import(import_id)
-    handle_local_clean_up(migration_info.config)
-    return True
-
-
-def _get_export_client(source_info: ServerInfo, config: MigrationConfig) -> MigrationExportClient:
-    auth = AuthenticationClientLive(source_info.server, source_info.user, source_info.password)
-    project_iri = ProjectClientLive(source_info.server, auth).get_project_iri(config.shortcode)
-    return MigrationExportClientLive(source_info.server, project_iri, auth)
-
-
-def _get_import_client(target_info: ServerInfo, project_iri: str) -> MigrationImportClient:
-    auth = AuthenticationClientLive(target_info.server, target_info.user, target_info.password)
-    return MigrationImportClientLive(target_info.server, project_iri, auth)

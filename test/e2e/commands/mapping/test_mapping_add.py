daec1e947c6f73f5a0d40238399c455247e9d513
Nora-Olivia-Ammann <103038637+Nora-Olivia-Ammann@users.noreply.github.com>
2026-04-09 14:36:50 +0200

    Update test_mapping_add.py

--- test/e2e/commands/mapping/test_mapping_add.py
+++ test/e2e/commands/mapping/test_mapping_add.py
@@ -1,12 +1,15 @@
 from pathlib import Path
 
 import pytest
+import requests
+from rdflib import Graph
 
 from dsp_tools.cli.args import ServerCredentials
 from dsp_tools.commands.create.create import create
 from dsp_tools.commands.mapping.config_file import parse_mapping_config
 from dsp_tools.commands.mapping.mapping_add import mapping_add
 from dsp_tools.utils.data_formats.iri_util import make_dsp_ontology_prefix
+from dsp_tools.utils.exceptions import DspToolsRequestException
 
 SHORTCODE = "4124"
 ONTO_NAME = "minimal-tp"
@@ -17,8 +20,8 @@ TESTDATA = Path("testdata/mapping")
 
 
 @pytest.fixture(scope="module")
-def ontology_iri(creds: ServerCredentials) -> str:
-    return make_dsp_ontology_prefix(creds.server, SHORTCODE, ONTO_NAME).rstrip("#")
+def ontology_namespace(creds: ServerCredentials) -> str:
+    return make_dsp_ontology_prefix(creds.server, SHORTCODE, ONTO_NAME)
 
 
 @pytest.fixture(scope="module")
@@ -32,6 +35,16 @@ def test_add_mapping_good():
     config_info = parse_mapping_config(config_file)
     success = mapping_add(config_info)
     assert success
+
+
+@pytest.mark.usefixtures("test_add_mapping_good")
+def test_check_successful_mapping_result(ontology_namespace):
+    url = f"{ontology_namespace.rstrip('#')}{ONTO_NAME}"
+    response = requests.get(url, headers={"Accept": "text/turtle"}, timeout=5)
+    if not response.ok:
+        raise DspToolsRequestException("Non-ok response when requesting the ontology from the server.")
+    onto_g = Graph()
+    onto_g.parse(data=response.text, format="ttl")
 
 
 @pytest.mark.usefixtures("create_minimal_project")

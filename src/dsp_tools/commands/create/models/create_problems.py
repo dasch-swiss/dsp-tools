eb6b110a6c8483cb1dd09e027f455f10852c3ca7
Johannes Nussbaum <johannes.nussbaum@dasch.swiss>
2026-04-28 11:39:57 +0200

    fix problem message

--- src/dsp_tools/commands/create/models/create_problems.py
+++ src/dsp_tools/commands/create/models/create_problems.py
@@ -55,7 +55,8 @@ class InputProblemType(ProblemType):
         "Defining 'default_permissions' is mandatory and must either be 'public' or 'private'."
     )
     LIMITED_VIEW_PERMISSIONS_NOT_CORRECT = (
-        "Your input for the 'limited_view' permissions is invalid. It must be either 'all' or a list of image classes."
+        "Your input for the 'limited_view' permissions is invalid. "
+        "It must be either 'all' or a list of representation classes."
     )
     UNKNOWN_IRI_IN_PERMISSIONS_OVERRULE = (
         "The referenced class or property in the private overrule permissions is not defined in the ontology."

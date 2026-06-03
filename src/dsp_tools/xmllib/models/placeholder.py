b7c4be8c46b960deb9694f86d8303c2e9a7f0cd3
Nora-Olivia-Ammann <103038637+Nora-Olivia-Ammann@users.noreply.github.com>
2026-06-03 16:35:16 +0200

    Create placeholder.py

---
+++ src/dsp_tools/xmllib/models/placeholder.py
@@ -0,0 +1,11 @@
+from enum import Enum
+from enum import auto
+
+
+class PlaceholderFiles(Enum):
+    ARCHIVE_REPRESENTATION = auto()
+    AUDIO_REPRESENTATION = auto()
+    DOCUMENT_REPRESENTATION = auto()
+    MOVING_IMAGE_REPRESENTATION = auto()
+    STILL_IMAGE_REPRESENTATION = auto()
+    TEXT_REPRESENTATION = auto()

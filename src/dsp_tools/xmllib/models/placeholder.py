3b9987b6e7e9153cfb0f8dfc4e0936bc769da59a
Nora-Olivia-Ammann <103038637+Nora-Olivia-Ammann@users.noreply.github.com>
2026-06-04 11:23:56 +0200

    Update placeholder.py

--- src/dsp_tools/xmllib/models/placeholder.py
+++ src/dsp_tools/xmllib/models/placeholder.py
@@ -2,6 +2,26 @@ from enum import StrEnum
 
 
 class PlaceholderFile(StrEnum):
+    """
+    Temporary stand-in to use instead of a real file when the actual file is not yet available.
+    This is only allowed on localhost and test servers.
+    Production environments do not allow a resource with a placeholder.
+
+    The chosen value must match the representation type defined in the ontology.
+    For example, if the resource type in the ontology uses `StillImageRepresentation`,
+    use `PlaceholderFile.STILL_IMAGE_REPRESENTATION`.
+
+    Example:
+        ```python
+        resource = resource.add_file(
+            filename=PlaceholderFile.STILL_IMAGE_REPRESENTATION,
+            license=xmllib.LicenseRecommended.CC.BY_NC_ND,
+            copyright_holder="Example University",
+            authorship=["Jane Doe"],
+        )
+        ```
+    """
+
     ARCHIVE_REPRESENTATION = "ArchiveRepresentation"
     AUDIO_REPRESENTATION = "AudioRepresentation"
     DOCUMENT_REPRESENTATION = "DocumentRepresentation"
